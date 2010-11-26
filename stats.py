c459d661ce635f8054b1c9365912da2f838fcb86
rhcarvalho <rhcarvalho@4f571728-e339-11dd-ab07-b19b4dad1b3d>
2010-11-26 18:06:21 +0000

    Add small utility to print list of dojos by language.
    git-svn-id: http://dojo-rio.googlecode.com/svn/trunk@101 4f571728-e339-11dd-ab07-b19b4dad1b3d

---
+++ stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import sys
+from collections import defaultdict
+#from pprint import pprint
+
+def merge_by_language(languages_dict, dojo):
+    languages_dict[dojo.get('language', '?')].append(dojo)
+    return languages_dict
+
+def to_dojo(string):
+    parts = string.split(' - ')
+    if len(parts) == 3:
+        date, problem, language = parts
+        return dict(date=date, problem=problem, language=language)
+    return dict(date=parts[0])
+
+def dojos_by_language(dir):
+    return dict(reduce(merge_by_language,
+                       (to_dojo(x) for x in sorted(os.listdir(dir)) if x[0].isdigit()), defaultdict(list)))
+
+if __name__ == '__main__':
+    #pprint(dojos_by_language('2010'))
+    if len(sys.argv) == 2:
+        result = dojos_by_language(sys.argv[1])
+        for language in sorted(result.keys()):
+            print '-- %s %s' % (language, '-'*(70 - len(language)))
+            for dojo in result[language]:
+                print '%s: %s' % (dojo['date'], dojo.get('problem', '?'))
+            print
+    else:
+        print "USAGE: %s [directory]\nPrints information about dojos in that directory." % sys.argv[0]
