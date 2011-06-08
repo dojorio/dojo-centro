710cf129e7e97117be747757f894cf8abdd21d77
rhcarvalho@gmail.com <rhcarvalho@gmail.com@4f571728-e339-11dd-ab07-b19b4dad1b3d>
2011-06-08 22:48:57 +0000

    Dojo 2011-06-08 with Guilherme Araujo Juan Lopes
    git-svn-id: http://dojo-rio.googlecode.com/svn/trunk@130 4f571728-e339-11dd-ab07-b19b4dad1b3d

--- 2011/20110608 - wordwrap - csharp/wordwrap.cs
+++ 2011/20110608 - wordwrap - csharp/wordwrap.cs
@@ -3,15 +3,31 @@ using NUnit.Framework;
 
 class QuebraDeLinha {
     public static string Quebrar(string input, int columns) {
-        return "";
+        
+        
+        return input;
     }
 }
 
 class QuebraDeLinhaTestes {
     [Test]
     public void ao_quebrar_string_vazia_retorna_uma_string_vazia() {
-        var esperado = "";
         var encontrado = QuebraDeLinha.Quebrar("", 80);
+        var esperado = "";
+        Assert.AreEqual(esperado, encontrado);
+    }
+    
+    [Test]
+    public void ao_quebrar_uma_letra_retorna_uma_letra(){
+        var encontrado = QuebraDeLinha.Quebrar("A", 80);
+        var esperado = "A";
+        Assert.AreEqual(esperado, encontrado);
+    }
+    
+    [Test]
+    public void ao_quebrar_duas_letras_com_uma_coluna_retorna_em_duas_linhas(){
+        var encontrado = QuebraDeLinha.Quebrar("AB", 1);
+        var esperado = "A\nB";
         Assert.AreEqual(esperado, encontrado);
     }
 }
