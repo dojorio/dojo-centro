9934b34483f2328ee3129d4c0f6d7e4713c9c16f
rhcarvalho <rhcarvalho@4f571728-e339-11dd-ab07-b19b4dad1b3d>
2011-07-29 01:35:45 +0000

    Update stats.py script
    git-svn-id: http://dojo-rio.googlecode.com/svn/trunk@160 4f571728-e339-11dd-ab07-b19b4dad1b3d

--- stats.py
+++ stats.py
@@ -1,32 +1,89 @@
 #!/usr/bin/env python
+# Author: Rodolfo Henrique Carvalho
+# This is in public domain.
+
 import os
 import sys
 from collections import defaultdict
-#from pprint import pprint
 
-def merge_by_language(languages_dict, dojo):
-    languages_dict[dojo.get('language', '?')].append(dojo)
-    return languages_dict
 
-def to_dojo(string):
-    parts = string.split(' - ')
+def dirname_to_dojo(dirname):
+    """Return a dojo from a string representation.
+    
+    `dirname': "YYYYMMDD[ - problem name - language name]"
+    `dojo': dict(date) or dict(date, problem, language)
+    """
+    parts = dirname.split(' - ', 2)
     if len(parts) == 3:
         date, problem, language = parts
         return dict(date=date, problem=problem, language=language)
-    return dict(date=parts[0])
+    else:
+        return dict(date=parts[0])
 
-def dojos_by_language(dir):
-    return dict(reduce(merge_by_language,
-                       (to_dojo(x) for x in sorted(os.listdir(dir)) if x[0].isdigit()), defaultdict(list)))
+
+def dojos_from_dir(dirpath):
+    """Return a list of dojos in dirpath.
+    
+    A `dojo' is a directory contained in dirpath that starts
+    with a digit (for the date).
+    """
+    pth = dirpath
+    dtd = dirname_to_dojo
+    ls, isdir, jn = os.listdir, os.path.isdir, os.path.join
+    isdig = str.isdigit
+    cond = lambda x: isdir(jn(pth, x)) and isdig(x[0])
+    return [dtd(x) for x in ls(pth) if cond(x)]
+
+
+def group_by_language(dojos):
+    """Return a dictionary of dojos grouped by languages."""
+    def append_to_language_group(dikt, dojo):
+        dikt[dojo.get('language', '?')].append(dojo)
+        return dikt
+    return dict(reduce(append_to_language_group, dojos, defaultdict(list)))
+
+
+def print_dojos_by_language(dojos):
+    groups = group_by_language(dojos)
+    for language in sorted(groups.keys(), key=lambda lang: (-len(groups[lang]), lang)):
+        print '-- %s (%d) %s' % (language, len(groups[language]), '-'*(70 - len(language)))
+        for dojo in sorted(groups[language]):
+            print '%s: %s' % (dojo['date'], dojo.get('problem', '?'))
+        print
+
+def print_dojos_by_language_summary(dojos):
+    groups = group_by_language(dojos)
+    print "Linguagem\t# dojos"
+    for language in sorted(groups.keys(), key=lambda lang: (-len(groups[lang]), lang)):
+        print '%s\t%d' % (language, len(groups[language]))
 
 if __name__ == '__main__':
-    #pprint(dojos_by_language('2010'))
-    if len(sys.argv) == 2:
-        result = dojos_by_language(sys.argv[1])
-        for language in sorted(result.keys()):
-            print '-- %s (%d) %s' % (language, len(result[language]), '-'*(70 - len(language)))
-            for dojo in result[language]:
-                print '%s: %s' % (dojo['date'], dojo.get('problem', '?'))
-            print
+    dirs = set(sys.argv[1:])
+    help_ops = ["-h", "--help"]
+    short_ops = ["-s", "--short"]
+    
+    if not dirs or dirs.intersection(help_ops):
+        usage = ("Prints information about dojos in directories.\n"
+                 "USAGE: %s [options] DIRECTORY [DIR2] [DIR3] ...\n"
+                 "Options:\n"
+                 " %-16s                show this help\n"
+                 " %-16s                show only a summary"
+                 % (sys.argv[0],
+                    ' , '.join(help_ops),
+                    ' , '.join(short_ops)))
+        print >>sys.stderr, usage
+        sys.exit(1)
     else:
-        print "USAGE: %s [directory]\nPrints information about dojos in that directory." % sys.argv[0]
+        dojos = []
+        
+        if dirs.intersection(short_ops):
+            print_ = print_dojos_by_language_summary
+            dirs.difference_update(short_ops)
+        else:
+            print_ = print_dojos_by_language
+        
+        while dirs:
+            dojos.extend(dojos_from_dir(dirs.pop()))
+        
+        print_(dojos)
+
