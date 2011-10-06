bc705f2543317ca7968182895448eb68c5cb6737
rhcarvalho@gmail.com <rhcarvalho@gmail.com@4f571728-e339-11dd-ab07-b19b4dad1b3d>
2011-10-06 00:44:36 +0000

    dojo antigo
    git-svn-id: http://dojo-rio.googlecode.com/svn/trunk@173 4f571728-e339-11dd-ab07-b19b4dad1b3d

---
+++ 2011/20110914 - intervalos - python/intervalos.py
@@ -0,0 +1,12 @@
+﻿def eh_consecutivo(primeiro, segundo):
+    return (primeiro + 1) == segundo
+
+def intervalos(numeros):
+    intervalos = [[numeros[0]]]
+    
+    for i in range(len(numeros)-1):
+        if not eh_consecutivo(numeros[i], numeros[(i+1)]):
+            intervalos[-1].append(numeros[i])
+            intervalos.append([numeros[i+1]])
+
+    return map(tuple, intervalos)
---
+++ 2011/20110914 - intervalos - python/teste_intervalos.py
@@ -0,0 +1,23 @@
+﻿import unittest
+from intervalos import intervalos
+
+class TestIntervalos(unittest.TestCase):
+    def test_um_numero(self):
+        self.assertEqual(intervalos([100]), [(100,)])
+
+    def test_dois_numeros_avulsos(self):
+        self.assertEqual(intervalos([100, 110]), 
+                                   [(100,), (110,)])
+                                   
+    def test_dois_numeros_consecutivos(self):
+        self.assertEqual(intervalos([100, 101]), [(100, 101)])
+        
+    def test_tres_numeros_consecutivos(self):
+        self.assertEqual(intervalos([100, 101, 102]), 
+                                   [(100, 102)])
+                                   
+    # def test_tres_numeros_consecutivos_e_um_numero_avulso(self):
+        # self.assertEqual(intervalos([100, 101, 102, 110]), 
+                                   # [(100, 102), (110,)])
+
+unittest.main()
