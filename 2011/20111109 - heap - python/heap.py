ec5e5f3df83f0779cb1643188fe8fbbfb3b95fc6
israelst <israelst@4f571728-e339-11dd-ab07-b19b4dad1b3d>
2011-11-10 16:23:32 +0000

    Dojo do dia 09/11/2011 - Heap - python
    git-svn-id: http://dojo-rio.googlecode.com/svn/trunk@179 4f571728-e339-11dd-ab07-b19b4dad1b3d

---
+++ 2011/20111109 - heap - python/heap.py
@@ -0,0 +1,20 @@
+﻿class Heap:
+    def __init__(self):
+        self.state = [None]
+    
+    def get_state(self):
+        return self.state[1:]
+    
+    def swap(self, i1, i2):
+        self.state[i1], self.state[i2] = self.state[i2], self.state[i1]
+    
+    def push(self,element):
+        self.state.append(element)
+        ultimo = len(self.state)-1
+        while ultimo != 1 and self.state[ultimo] < self.state[ultimo/2]:
+            pai = ultimo/2
+            self.swap(ultimo, pai)
+            ultimo = pai
+
+                
+                
---
+++ 2011/20111109 - heap - python/heap_test.py
@@ -0,0 +1,54 @@
+﻿import unittest
+from heap import *
+
+class TestHeap(unittest.TestCase):
+    def test_insercao_heap_vazia(self):
+        heap = Heap()
+        heap.push(1)
+        self.assertEqual(heap.get_state(), [1])
+        
+    def test_insercao_numero_maior_que_raiz(self):
+        heap = Heap()
+        heap.push(5)
+        heap.push(6)
+        self.assertEqual(heap.get_state(), [5, 6])
+       
+    def test_insercao_numero_menor_que_raiz(self):
+        heap = Heap()
+        heap.push(5)
+        heap.push(4)
+        self.assertEqual(heap.get_state(), [4, 5])
+        
+    def test_insercao_3_numeros_em_ordem_decrescente(self):
+        heap = Heap()
+        heap.push(5)
+        heap.push(4)
+        heap.push(3)
+        self.assertEqual(heap.get_state(), [3, 5, 4])
+
+    def test_insercao_4_numeros_em_ordem_decrescente(self):
+        heap = Heap()
+        heap.push(6)
+        heap.push(5)
+        heap.push(4)
+        heap.push(3)
+        self.assertEqual(heap.get_state(), [3, 4, 5, 6])
+        
+    def test_insercao_4_numeros_em_ordem_crescente(self):
+        heap = Heap()
+        heap.push(3)
+        heap.push(4)
+        heap.push(5)
+        heap.push(6)
+        self.assertEqual(heap.get_state(), [3, 4, 5, 6])
+        
+    def test_insercao_6_numeros_desordenadamente(self):
+        heap = Heap([0, 4, 7, 9, 8])
+        heap.push(0)
+        heap.push(4)
+        heap.push(7)
+        heap.push(9)
+        heap.push(8)
+        heap.push (5)
+        self.assertEqual(heap.get_state(), [0, 4, 5, 9, 8, 7])
+unittest.main()
