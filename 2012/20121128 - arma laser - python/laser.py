b928bd4b73996ad178061a1027c60cfdc0df350e
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-11-28 20:53:13 -0200

    Juan Lopes e Eduardo Vinicius

--- 2012/20121128 - arma laser - python/laser.py
+++ 2012/20121128 - arma laser - python/laser.py
@@ -5,21 +5,27 @@ coluna = lambda soldado: soldado[1]
 def laser(soldados):
 	columns = set(map(coluna, soldados))
 	lines = set(map(linha, soldados))
-	mapa = {}
+	
+	mapa = {}	
 	for col in columns:
-		mapa[col] = qtd_soldados_em_colunas(soldados, col)
-		{1: 5, }
-	qtd_soldados_em_colunas = qtd_soldados_na_coluna(coluna, soldados)
-	qtd_soldados_em_linas qtd_soldados_na_linha(linha, soldados)
+		mapa[col] = qtd_soldados_na_coluna(soldados, col)
+
+	mapa = dict(col, len(solds) for col, solds in groupby(soldados, coluna))
+
+	print('de colunas:', mapa)
+	print()
+
+	#qtd_soldados_em_colunas = qtd_soldados_na_coluna(coluna, soldados)
+	#qtd_soldados_em_linas = qtd_soldados_na_linha(linha, soldados)
 
 	
 
 
-	return min(len(same_column), len(same_line))
+	return min(len(columns), len(lines))
 
 def qtd_soldados_na_coluna(soldados, col):
 	n = 0
-	for soldado in soldado:
+	for soldado in soldados:
 		if coluna(soldado) == col:
 			n += 1
 	return n
