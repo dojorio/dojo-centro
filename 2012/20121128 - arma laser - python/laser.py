ad8176290f1cebc3c87c394017e5a279079ebf6a
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-11-28 20:17:25 -0200

    Eduardo Stalinho e Eduardo Vinicius

--- 2012/20121128 - arma laser - python/laser.py
+++ 2012/20121128 - arma laser - python/laser.py
@@ -5,15 +5,14 @@ coluna = lambda soldado: soldado[1]
 
 def laser(soldados):
 	tiros = len(soldados)
+	listMesmaColuna = []
+	listMesmaLinha = []
 
-	for soldado1, soldado2 in zip(soldados, soldados[1:]):
+	for soldado in soldados:
+		listMesmaColuna.append(coluna(soldado))
+		listMesmaLinha.append(linha(soldado))
 
-		if linha(soldado1) == linha(soldado2) or coluna(soldado1) == coluna(soldado2):
-			tiros -= 1
+	conjuntoLinhas = set(listMesmaLinha)
+	conjuntoColunas = set(listMesmaColuna)
 
-		soldados.remove(soldado1)
-
-	if len(soldados) > 1:
-		return laser(soldados) + tiros
-
-	return tiros
+	return min(len(conjuntoColunas), len(conjuntoLinhas))
