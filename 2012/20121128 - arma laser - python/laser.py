7e356347135d4839132ed97f353f13b11345e16e
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-11-28 21:24:31 -0200

    Eduardo Stalinho e Juan Lopes

--- 2012/20121128 - arma laser - python/laser.py
+++ 2012/20121128 - arma laser - python/laser.py
@@ -9,27 +9,15 @@ def laser(soldados):
 	columns = set(map(coluna, soldados))
 	lines = set(map(linha, soldados))
 	
-	qtd_col = [qtd_soldados_na_coluna(soldados, col), col for col in columns]
-	qtd_lin = [qtd_soldados_na_linha(soldados, lin), lin for lin in lines]
+	qtd_col = [(qtd_na(coluna, soldados, col), col, coluna) for col in columns]
+	qtd_lin = [(qtd_na(linha, soldados, lin), lin, linha) for lin in lines]
 
-	if max(qtd_col) > max(qtd_lin):
-		soldados = filter(soldados, lambda soldado: coluna(soldado) != max(qtd_col)[1])
-	else:
-		soldados = filter(soldados, lambda soldado: linha(soldado) != max(qtd_lin)[1])
+	qtd, y, func = max(max(qtd_col), max(qtd_lin))
+	soldados = filter(lambda soldado: func(soldado) != y, soldados)
 
 	return laser(soldados) + 1
 
 
-def qtd_soldados_na_coluna(soldados, col):
-	n = 0
-	for soldado in soldados:
-		if coluna(soldado) == col:
-			n += 1
-	return n
-
-def qtd_soldados_na_linha(soldados, lin):
-	n = 0
-	for soldado in soldados:
-		if linha(soldado) == lin:
-			n += 1
-	return n
+def qtd_na(func, soldados, y):
+	return len(filter(lambda x: func(x)==y, soldados))
+	
