b594d67ab640b37e9402af955d4fc859295cb04b
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-11-28 19:58:52 -0200

    Israel Teixeira e Thiago Belem

--- 2012/20121128 - arma laser - python/laser.py
+++ 2012/20121128 - arma laser - python/laser.py
@@ -3,20 +3,17 @@
 linha = lambda soldado: soldado[0]
 coluna = lambda soldado: soldado[1]
 
-def laser(soldados):
-	tiros = len(soldados)
-
-	if tiros < 2:
-		return tiros
+def laser(soldados, tiros = 0):
+	tiros += len(soldados)
 
 	for soldado1, soldado2 in zip(soldados, soldados[1:]):
-		if linha(soldado1) == linha(soldado2):
+
+		if linha(soldado1) == linha(soldado2) or coluna(soldado1) == coluna(soldado2):
 			tiros -= 1
 
-		if coluna(soldado1) == coluna(soldado2):
-			tiros -= 1
+		soldados.remove(soldado1)
 
+	if len(soldados) > 1:
+		return laser(soldados, tiros)
 
 	return tiros
-
-
