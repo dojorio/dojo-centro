dfb9e406db1b166219057b8812725559a4e80881
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-11-28 20:35:26 -0200

    Thiago Belem e Juan Lopes

--- 2012/20121128 - arma laser - python/laser.py
+++ 2012/20121128 - arma laser - python/laser.py
@@ -1,9 +1,22 @@
 #coding: utf-8
+from itertools import groupby, chain
 
 linha = lambda soldado: soldado[0]
 coluna = lambda soldado: soldado[1]
 
 def laser(soldados):
+	colunas = groupby(soldados, coluna)
+	linhas = groupby(soldados, linha)
+
+	tudao = colunas + linha
+
+	for col, soldados_coluna in colunas:
+
+
+
+	qtd_soldados_em_colunas = qtd_soldados_na_coluna(coluna, soldados)
+	qtd_soldados_em_linas qtd_soldados_na_linha(linha, soldados)
+
 	same_column = set(map(coluna, soldados))
 	same_line = set(map(linha, soldados))
 
