dca1b4d410c9b69caf3957640ead7aaba3892544
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-12-05 19:53:50 -0200

    Juan Lopes e Otávio Cardoso

--- 2012/20121204 - bridezilla - python/bridezilla.py
+++ 2012/20121204 - bridezilla - python/bridezilla.py
@@ -14,3 +14,7 @@ def bridezilla(casais, adulterios):
 		return ['M0', 'H1', 'M2']
 
 	resultado = ['M' + str(n) for n in range(casais)]
+
+	for adulterio in adulterios:
+		if adulterio[0].temH and adulterio[1].temH:
+			troca_mulher_do_resultado adulterio[1].numero
