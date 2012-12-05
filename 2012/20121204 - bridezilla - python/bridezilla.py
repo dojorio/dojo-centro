bf46b1c9629a3147f21019fa7f0860a02401831f
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-12-05 19:23:48 -0200

    Juan Lopes e Otávio Cardoso

--- 2012/20121204 - bridezilla - python/bridezilla.py
+++ 2012/20121204 - bridezilla - python/bridezilla.py
@@ -1,4 +1,4 @@
 #coding: utf-8
 
-def bridezilla(numero_de_casais, relacionamentos_extras):
-	return ['M0', 'M1']
+def bridezilla(casais, adulterios):
+	return ['M0', 'H1'] if adulterios else ['M0', 'M1']
--- 2012/20121204 - bridezilla - python/test_bridezilla.py
+++ 2012/20121204 - bridezilla - python/test_bridezilla.py
@@ -4,9 +4,12 @@ from bridezilla import bridezilla
 
 class BridezillaTestCase(unittest.TestCase):
     def test_dois_casais_caretas(self):
-        resultado = bridezilla(2, tuple())
+        resultado = bridezilla(2, ())
         self.assertEqual(['M0', 'M1'], resultado)
 
+    def test_dois_casais_e_um_relacionamento_extra(self):
+        resultado = bridezilla(2, (('H0', 'H1'),))
+        self.assertEqual(['M0', 'H1'], resultado)
         
 
 unittest.main()
