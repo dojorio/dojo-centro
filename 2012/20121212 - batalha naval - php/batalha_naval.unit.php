6628a94ad856b69671d83e7281958a5dd817fd1e
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-12-12 19:32:12 -0200

    Susana Santos e André Pfeiffer

--- 2012/20121212 - batalha naval - php/batalha_naval.unit.php
+++ 2012/20121212 - batalha naval - php/batalha_naval.unit.php
@@ -4,10 +4,10 @@ include "batalha_naval.php";
 
 class TestBatalhaNaval extends PHPUnit_Framework_TestCase {
 
-    public test_tiro_acerta(){
+    public function test_tiro_acerta(){
         $tabuleiro = array(
             array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 1, 1, 1, 0, 0, 0, 0, 0, 0),
             array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
             array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
             array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
@@ -17,5 +17,10 @@ class TestBatalhaNaval extends PHPUnit_Framework_TestCase {
             array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
             array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
             );
+
+        $jogo = new BatalhaNaval();
+        $jogo->insereNavio(array(1,1), array(1,3));
+
+        $this->assertSame($tabuleiro, $jogo->$tabuleiro);
     }
 }
