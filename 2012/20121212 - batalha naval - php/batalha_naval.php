5d728ed00b0e393cd6c7621fd915616d2d951e4b
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-12-12 19:43:53 -0200

    Otávio Cardoso e Israel Teixeira

--- 2012/20121212 - batalha naval - php/batalha_naval.php
+++ 2012/20121212 - batalha naval - php/batalha_naval.php
@@ -1,9 +1,24 @@
 <?php
 
 class BatalhaNaval{
+	public function __construct(){
+		$this->$tabuleiro = array(
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+            );
+	}
+
 	public function insereNavio($inicio,$fim){
 		for($i=1;$i<=3;$i++){
-			
+			$this->$tabuleiro[1][$i] = 1;
 		}
 	}
 }
