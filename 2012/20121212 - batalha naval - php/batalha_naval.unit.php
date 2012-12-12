64e5923aab16c0055b54102ed1d1de33feda73cd
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-12-12 19:23:56 -0200

    André Pfeiffer e Otávio Cardoso

---
+++ 2012/20121212 - batalha naval - php/batalha_naval.unit.php
@@ -0,0 +1,21 @@
+<?php
+
+include "batalha_naval.php";
+
+class TestBatalhaNaval extends PHPUnit_Framework_TestCase {
+
+    public test_tiro_acerta(){
+        $tabuleiro = array(
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+            );
+    }
+}
