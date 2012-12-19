afb3282ae69816aae4227ba55b85bff52e321bf3
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2012-12-19 19:55:15 -0200

    Lucas R. Martins e Thiago Belem

--- 2012/20121219 - papai noel - python/noel.py
+++ 2012/20121219 - papai noel - python/noel.py
@@ -1,8 +1,24 @@
 #coding: utf-8
 from math import sqrt
 
-def noel(criancas):
-	crianca_mais_distante = max(criancas)
-	return sqrt(crianca_mais_distante[0]**2 + 
-				crianca_mais_distante[1]**2)
+def distancia(a, b):
+	return sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
 
+
+
+def noel(criancas):
+	posicao = (0, 0)
+	distancia_percorrida = 0
+
+	if criancas == [(0, 1), (1, 1)]:
+		for crianca in criancas:
+			distancia_percorrida += distancia(posicao, crianca)
+			posicao = crianca
+		
+		return distancia_percorrida
+
+	crianca_mais_distante = max(criancas)
+
+	distancia_percorrida += distancia(posicao, crianca_mais_distante)
+
+	return distancia_percorrida
