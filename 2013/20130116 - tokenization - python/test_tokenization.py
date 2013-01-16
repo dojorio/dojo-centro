9ef6426efd705863a9d725e51058946930c80f8c
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-01-16 19:50:44 -0200

    Jacqueline Abreu e Eduardo Vinicius

--- 2013/20130116 - tokenization - python/test_tokenization.py
+++ 2013/20130116 - tokenization - python/test_tokenization.py
@@ -39,6 +39,21 @@ class TokenizeTestCase(unittest.TestCase):
 			('N', '1')
 		], tokenize('1 1'))
 
+	def test_11_mais_11(self):
+		self.assertEqual([
+			('N', '11'),
+			('+', '+'),
+			('N', '11')
+		], tokenize('11+11'))
+
+	def test_1_mais_2_menos_3(self):
+		self.assertEqual([
+			('N', '1'),
+			('+', '+'),
+			('N', '2'),
+			('-', '-'),
+			('N', '3')
+		], tokenize('1+2-3'))
 
 
 unittest.main()
