ce8a2969fa2588ea74fd3076038a4a185263da7f
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-01-30 19:13:39 -0200

    Renan Cardoso e carlos Cunha

--- 2013/20130130 - formiga no tabuleiro - python/formiga.py
+++ 2013/20130130 - formiga no tabuleiro - python/formiga.py
@@ -1,13 +1,16 @@
 #-*- coding: utf-8 -*-
 
 def formiga(tempo):
+	'''
 	posicoes = {
-		0: (0, 0),
+		0: (0, 0), # tempo /2 , 
 		1: (0, 1),
 		2: (1, 1),
 		3: (1, 0),
 		4: (2, 0),
 		5: (2, 1)
 	}
+	'''
+	return (tempo/2, tempo%2)
 
-	return posicoes[tempo]
+	#return posicoes[tempo]
