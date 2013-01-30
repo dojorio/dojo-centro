62433ce229b354ef4c3432e01de641ecd36cd537
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-01-30 19:36:33 -0200

    Thiago Belem e Otávio Cardoso

--- 2013/20130130 - formiga no tabuleiro - python/test_formiga.py
+++ 2013/20130130 - formiga no tabuleiro - python/test_formiga.py
@@ -4,34 +4,34 @@ from formiga import formiga
 
 class FormigaTestCase(unittest.TestCase):
 
-	#linha, coluna = y, x
-	
+	# coluna, linha = (x, y)
+
 	def test_0_segundos(self):
 		self.assertEqual((0, 0), formiga(0))
 
 	def test_1_segundo(self):
-		self.assertEqual((0, 1), formiga(1))
+		self.assertEqual((1, 0), formiga(1))
 
 	def test_2_segundos(self):
 		self.assertEqual((1, 1), formiga(2))
 
 	def test_3_segundos(self):
-		self.assertEqual((1, 0), formiga(3))
+		self.assertEqual((0, 1), formiga(3))
 
 	def test_4_segundos(self):
-		self.assertEqual((2, 0), formiga(4))
+		self.assertEqual((0, 2), formiga(4))
 
 	def test_5_segundos(self):
-		self.assertEqual((2, 1), formiga(5))
+		self.assertEqual((1, 2), formiga(5))
 
 	def test_6_segundos(self):
 		self.assertEqual((2, 2), formiga(6))
 
 	def test_7_segundos(self):
-		self.assertEqual((2, 1), formiga(6))
+		self.assertEqual((2, 1), formiga(7))
 
-	def test_7_segundos(self):
-		self.assertEqual((2, 1), formiga(6))
+	def test_8_segundos(self):
+		self.assertEqual((2, 0), formiga(8))
 
 
 unittest.main()
