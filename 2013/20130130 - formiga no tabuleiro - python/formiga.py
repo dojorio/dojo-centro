780934d19f7565a741a2e74b21ff6fd5f4afba8a
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-01-30 21:13:47 -0200

    Thiago Belem e Jacqueline Abreu

--- 2013/20130130 - formiga no tabuleiro - python/formiga.py
+++ 2013/20130130 - formiga no tabuleiro - python/formiga.py
@@ -3,13 +3,13 @@
 from math import *
 
 def formiga(tempo):
-	tempo_raiz = sqrt(tempo)
+	
+	raiz_tempo = int(sqrt(tempo))
+	quina = (raiz_tempo + 1)*raiz_tempo
 
-	tempo_raiz_int = int(tempo_raiz)
-	medio = (tempo_raiz_int + 1)*tempo_raiz_int
-	max(tempo, medio)
-
-	if tempo_raiz_int % 2 == 0:
-		return (tempo_raiz_int-max(medio-tempo, 0), tempo_raiz_int-max(tempo-medio, 0))
-	else:
-		return (tempo_raiz_int-max(tempo-medio, 0), tempo_raiz_int-max(medio-tempo, 0))
+	x, y = raiz_tempo-max(quina-tempo, 0), raiz_tempo-max(tempo-quina, 0)
+	
+	if raiz_tempo % 2 != 0:
+		y, x = x, y
+	
+	return (x, y)
