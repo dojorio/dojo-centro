a62c79a53b247cdd5e6ef2e1c03e9ee32fbbcd9d
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-02-06 20:05:42 -0200

    carlos Cunha e Flávio Amieiro

--- 2013/20130206 - parser - python/parser.py
+++ 2013/20130206 - parser - python/parser.py
@@ -1,15 +1,20 @@
 #-*- coding: utf-8 -*-
 
-#2+3+4
 
-#2
-#('+', 2, 3)
-#('+', ('+', 2, 3), 4)
 
+
+#[('N', '2'),('+', '+'),('N', '3'),('-', '-'),('N', '4')]
+# 2 + 3 - 4
+# 2
+# (+, 2, 3)
+# (-, (+, 2, 3), 4)
 def parse(tokens):
-	resultado = int(tokens[0][1])
+	if len(tokens) == 1:
+		return int(tokens[0][1])
+	return (tokens[-2][1], parse(tokens[:-2]), int(tokens[-1][1]))
 
-	for i in range(1, len(tokens), 2):
-		resultado = (tokens[i][0], resultado, int(tokens[i+1][1]))
 
-	return resultado
+	#for i in range(1, len(tokens), 2):
+	#	resultado = (tokens[i][0], resultado, int(tokens[i+1][1]))
+
+	#return resultado
