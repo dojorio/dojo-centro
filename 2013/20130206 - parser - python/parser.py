e2ead6a209e0b0b12ec9a6660ec5b01c02a30e46
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-02-06 20:33:00 -0200

    Israel Teixeira e Orlando Seixas

--- 2013/20130206 - parser - python/parser.py
+++ 2013/20130206 - parser - python/parser.py
@@ -9,18 +9,12 @@
 # (+, 2, 3)
 # (-, (+, 2, 3), 4)
 def parse(tokens):
-	#if len(tokens) == 1:
-	#	return int(tokens[0][1])
-	#return (tokens[-2][1], parse(tokens[:-2]), int(tokens[-1][1]))
-
-	resultado = int(tokens[0][1])
-
-	for i in range(1, len(tokens), 2):
-
-		if (tokens[i][0] !='+'):
-			resultado = (tokens[i][0], resultado, int(tokens[i+1][1]))
-		if (tokens[i][0] =='+'):
-			resultado = (tokens[i][0], int(tokens[i+1][1]), resultado)
+	if len(tokens) == 1:
+		return int(tokens[0][1])
+	return (tokens[-2][1], parse(tokens[:-2]), int(tokens[-1][1]))
 
 
-	return resultado
+	#for i in range(1, len(tokens), 2):
+	#	resultado = (tokens[i][0], resultado, int(tokens[i+1][1]))
+
+	#return resultado
