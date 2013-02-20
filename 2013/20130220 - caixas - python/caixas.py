0ff4a10249c870673cc898467677b38fdecadc5b
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-02-20 19:38:33 -0300

    Edson Pinages e Flávio Amieiro

--- 2013/20130220 - caixas - python/caixas.py
+++ 2013/20130220 - caixas - python/caixas.py
@@ -1,11 +1,16 @@
 #-*- coding: utf-8 -*-
 
+class Caixa(object):
+	def __init__(self, peso, capacidade):
+		self.peso = peso
+		self.capacidade = capacidade
+
 def empilhar(caixas):
 	"""
 	caixas: [(peso, capacidade)...]
 	"""
 
-	if len(caixas) >= 1 and caixas[0] == (2, 1):
+	if len(caixas) > 1 and caixas[1].peso > caixas[0].capacidade:
 		return 1
 
 	return len(caixas)
--- 2013/20130220 - caixas - python/test_caixas.py
+++ 2013/20130220 - caixas - python/test_caixas.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 import unittest
-from caixas import empilhar
+from caixas import empilhar, Caixa
 
 class CaixasTestCase(unittest.TestCase):
 
@@ -10,16 +10,16 @@ class CaixasTestCase(unittest.TestCase):
 		self.assertEquals(0, resultado)
 
 	def test_uma_caixa(self):
-		caixas = [(1, 1)]
+		caixas = [Caixa(1, 1)]
 		resultado = empilhar(caixas)
 		self.assertEquals(1, resultado)
 
 	def test_duas_caixas(self):
-		caixas = [(2, 1), (2, 1)]
+		caixas = [Caixa(2, 1), Caixa(2, 1)]
 		self.assertEquals(1, empilhar(caixas))
 
 	def test_duas_caixas_que_empilham(self):
-		caixas = [(2, 1), (1, 1)]
+		caixas = [Caixa(2, 1), Caixa(1, 1)]
 		self.assertEquals(2, empilhar(caixas))
 
 unittest.main()
