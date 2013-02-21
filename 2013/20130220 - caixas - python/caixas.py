c976a6e6536ca3ab86b5579522952bc91aa8be2e
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-02-20 21:40:03 -0300

    Jacqueline Abreu e Edson Pinages

--- 2013/20130220 - caixas - python/caixas.py
+++ 2013/20130220 - caixas - python/caixas.py
@@ -8,12 +8,12 @@ class Caixa(object):
 INFINITO = 1e300
 
 def empilhar(caixas):
-	caixas_sort_capacidade = caixas.sort(key=lambda caixa: (-caixa.capacidade, -caixa.peso))
+	caixas.sort(key=lambda caixa: (-caixa.capacidade, -caixa.peso))
 
 	capacidade_da_pilha = INFINITO
 	tamanho_da_pilha = 0
 
-	for caixa in caixas_sort_capacidade:
+	for caixa in caixas:
 		if caixa.peso <= capacidade_da_pilha:
 			capacidade_da_pilha = min(
 				capacidade_da_pilha - caixa.peso, caixa.capacidade)
