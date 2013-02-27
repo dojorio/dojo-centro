9c0be0ee43ea453bba36481c9dd16d2796f90861
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-02-27 19:31:43 -0300

    Israel Teixeira e Juan Lopes

--- 2013/20130227 - laser - php/laser.php
+++ 2013/20130227 - laser - php/laser.php
@@ -1,5 +1,9 @@
 <?php
 
 function laser($inimigos) {
+	if (isset($inimigos[1]) and (
+			$inimigos[1][1] == $inimigos[0][1] or 
+			$inimigos[1][0] == $inimigos[0][0] ))
+		return 1;
 	return count($inimigos);
 }
--- 2013/20130227 - laser - php/laser.unit.php
+++ 2013/20130227 - laser - php/laser.unit.php
@@ -3,11 +3,12 @@
 include "laser.php";
 
 /*
-|       x
+     |  | 
+|    |  x
 ---x----x--
-|    
+|    |  |     
 -    x  x  |
-- x  x  x  |
+--x--x--x--|
 |----|--|--|
 */
 
@@ -33,10 +34,18 @@ class LaserTest extends PHPUnit_Framework_TestCase {
 		$this->assertSame(2, laser($tabuleiro));
 	}
 
-	function test_tabuleiro_com_dois_inimigos_em_linha() {
+	function test_tabuleiro_com_dois_inimigos_em_coluna() {
 		$tabuleiro = array(
 			array(0, 0),
 			array(1, 0)
+		);
+		$this->assertSame(1, laser($tabuleiro));
+	}
+
+	function test_tabuleiro_com_dois_inimigos_em_linha() {
+		$tabuleiro = array(
+			array(0, 0),
+			array(0, 1)
 		);
 		$this->assertSame(1, laser($tabuleiro));
 	}
