6f1817d11570ffbfcc0d91db899e02cd0188de7d
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-02-27 19:37:46 -0300

    Flávio Amieiro e Israel Teixeira

--- 2013/20130227 - laser - php/laser.php
+++ 2013/20130227 - laser - php/laser.php
@@ -1,9 +1,19 @@
 <?php
 
+class Inimigo {
+	public $x;
+	public $y;
+	
+	function __constructor($x, $y){
+		$this->x = $x;
+		$this->y = $y;
+	}
+}
+
 function laser($inimigos) {
 	if (isset($inimigos[1]) and (
-			$inimigos[1][1] == $inimigos[0][1] or 
-			$inimigos[1][0] == $inimigos[0][0] ))
+			$inimigos[1]->x == $inimigos[0]->x or 
+			$inimigos[1]->y == $inimigos[0]->y ))
 		return 1;
 	return count($inimigos);
 }
--- 2013/20130227 - laser - php/laser.unit.php
+++ 2013/20130227 - laser - php/laser.unit.php
@@ -21,31 +21,31 @@ class LaserTest extends PHPUnit_Framework_TestCase {
 
 	function test_tabuleiro_com_um_inimigo() {
 		$tabuleiro = array(
-			array(0, 0)
+			new Inimigo(0, 0)
 		);
 		$this->assertSame(1, laser($tabuleiro));
 	}
 
 	function test_tabuleiro_com_dois_inimigos() {
 		$tabuleiro = array(
-			array(0, 0),
-			array(1, 1)
+			new Inimigo(0, 0),
+			new Inimigo(1, 1)
 		);
 		$this->assertSame(2, laser($tabuleiro));
 	}
 
 	function test_tabuleiro_com_dois_inimigos_em_coluna() {
 		$tabuleiro = array(
-			array(0, 0),
-			array(1, 0)
+			new Inimigo(0, 0),
+			new Inimigo(1, 0)
 		);
 		$this->assertSame(1, laser($tabuleiro));
 	}
 
 	function test_tabuleiro_com_dois_inimigos_em_linha() {
 		$tabuleiro = array(
-			array(0, 0),
-			array(0, 1)
+			new Inimigo(0, 0),
+			new Inimigo(0, 1)
 		);
 		$this->assertSame(1, laser($tabuleiro));
 	}
