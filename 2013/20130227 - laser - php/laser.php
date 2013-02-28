8f07fc50cd436a063e1a4a022fd4fce49b83ebb8
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-02-27 21:20:52 -0300

    Thiago Belem e Thiago Belem

--- 2013/20130227 - laser - php/laser.php
+++ 2013/20130227 - laser - php/laser.php
@@ -11,38 +11,29 @@ function laser($inimigos) {
 	if (empty($inimigos))
 		return 0;
 
-	$linhas = array();
-	$colunas = array();
+	$linhas = $colunas = array();
 
 	foreach ($inimigos as $inimigo) {
-		if(isset($linhas[$inimigo->x])){
-			$linhas[$inimigo->x]++;
-		}else{
-			$linhas[$inimigo->x] = 1;
-		}
+		if (!isset($linhas[$inimigo->x]))
+			$linhas[$inimigo->x] = 0;
 
-		if(isset($colunas[$inimigo->y])){
-			$colunas[$inimigo->y]++;
-		}else{
-			$colunas[$inimigo->y] = 1;
-		}
+		if (!isset($colunas[$inimigo->y]))
+			$colunas[$inimigo->y] = 0;
+
+		$linhas[$inimigo->x]++;
+		$colunas[$inimigo->y]++;
 	}
-
-	$maiorLinha = max($linhas);
-	$maiorColuna = max($colunas);
-
 	
-	if (($maiorLinha > $maiorColuna) && count($linhas) < count($colunas)) {
-		$maiorLinha = array_search($maiorLinha, $linhas);
+	// Escolhe qual linha ou coluna atirar
+	if (count($linhas) < count($colunas)) {
+		$maiorLinha = array_search(max($linhas), $linhas);
 		$maiorColuna = false;
-	} else if (count($linhas) > count($colunas)){
-		$maiorColuna = array_search($maiorColuna, $colunas);
+	} else {
+		$maiorColuna = array_search(max($colunas), $colunas);
 		$maiorLinha = false;	
-	}else{
-		$maiorLinha = array_search($maiorLinha, $linhas);
-		$maiorColuna = false;
 	}
 
+	// Remove os inimigos da linha/coluna escolhida
 	foreach ($inimigos as $key => $inimigo) {
 		if ($inimigo->x === $maiorLinha or $inimigo->y === $maiorColuna) {
 			unset($inimigos[$key]);
