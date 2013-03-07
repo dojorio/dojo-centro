83b36148c11326e786cdda2fa5809e3d6eee9203
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-03-06 21:38:01 -0300

    Trilha em Ruby

--- 2013/20130306 - trilha - ruby/trilha.rb
+++ 2013/20130306 - trilha - ruby/trilha.rb
@@ -14,16 +14,18 @@ end
 
 
 def trilha(distancias, dias)
+    # Somatório das distancias em um único dia
     return distancias.sum if dias == 1
+
+    # Maior "pedaço" (de 2) em dois dias
+    return 1.upto(distancias.count).map{|i| split(distancias, i)}.min if dias == 2
+
+    # Maior distância se tenho mais dias do que paradas
     return distancias.max if dias >= distancias.count
-    if dias == 2
-        return 1.upto(distancias.count).map{|i| split(distancias, i)}.min
-    end
 
     if distancias.count == 5
         return 3
     end
 
     return distancias.max
-
 end
--- 2013/20130306 - trilha - ruby/trilha_spec.rb
+++ 2013/20130306 - trilha - ruby/trilha_spec.rb
@@ -2,104 +2,68 @@ require "trilha"
 
 describe "Trilha" do
 
-  it "vazia" do
-    distancias = []
-    trilha(distancias, 1).should eq(0)
-  end
-
   context "em um dia" do
+    let(:dias) { 1 }
+
+    it "vazia" do
+      trilha([], dias).should eq(0)
+    end
+
     it "e com dois acampamentos" do
-      distancias = [2]
-      trilha(distancias, 1).should eq(2)
+      trilha([2], dias).should eq(2)
     end
 
     it "e com três acampamentos" do
-      distancias = [2, 3]
-      trilha(distancias, 1).should eq(5)
+      trilha([2, 3], dias).should eq(5)
     end
 
     it "e com quatro acampamentos" do
-      distancias = [2, 3, 4]
-      trilha(distancias, 1).should eq(9)
+      trilha([2, 3, 4], dias).should eq(9)
     end
   end
 
 
   context "em dois dias" do
+    let(:dias) { 2 }
+
     it "e com dois acampamentos" do
-      trilha([2], 2).should eq(2)
+      trilha([2], dias).should eq(2)
     end
 
-    it "e com três acampamentos" do
-      distancias = [2, 3]
-      trilha(distancias, 2).should eq(3)
+    context "com três acampamentos" do
+      it { trilha([2, 3], dias).should eq(3) }
+      it { trilha([2, 4], dias).should eq(4) }
+      it { trilha([2, 4, 1], dias).should eq(5) }
     end
 
-    it "e com três acampamentos II" do
-      distancias = [2, 4]
-      trilha(distancias, 2).should eq(4)
+    context "com três acampamentos" do
+      it { trilha([3, 4, 1], dias).should eq(5) }
+      it { trilha([3, 4, 2], dias).should eq(6) }
+      it { trilha([2, 4, 3], dias).should eq(6) }
     end
 
-    it "e com quatro acampamentos" do
-      trilha([2, 4, 1], 2).should eq(5)
-    end
-
-    it "e com quatro acampamentos II" do
-      trilha([3, 4, 1], 2).should eq(5)
-    end
-
-    it "e com quatro acampamentos III" do
-      trilha([3, 4, 2], 2).should eq(6)
-    end
-
-    it "e com quatro acampamentos III invertido" do
-      trilha([2, 4, 3], 2).should eq(6)
-    end
-
-    it "e com cinco acampamentos" do
-      trilha([3, 2, 2, 3], 2).should eq(5)
-    end
-
-    it "e com cinco acampamentos II" do
-      trilha([4, 2, 2, 4], 2).should eq(6)
-    end
-
-    it "e com cinco acampamentos III" do
-      trilha([1, 1, 1, 4], 2).should eq(4)
-    end
-
-    it "e com cinco acampamentos IV" do
-      trilha([1, 1, 1, 9], 2).should eq(9)
-    end
-
-    it "e com cinco acampamentos V" do
-      trilha([1, 1, 1, 5], 2).should eq(5)
-    end
-
-    it "e com cinco acampamentos V invertido" do
-      trilha([5, 1, 1, 1], 2).should eq(5)
-    end
-
-    it "e com cinco acampamentos VI" do
-      trilha([1, 5, 1, 1], 2).should eq(6)
+    context "com cinco acampamentos" do
+      it { trilha([3, 2, 2, 3], dias).should eq(5) }
+      it { trilha([4, 2, 2, 4], dias).should eq(6) }
+      it { trilha([1, 1, 1, 4], dias).should eq(4) }
+      it { trilha([1, 1, 1, 9], dias).should eq(9) }
+      it { trilha([1, 1, 1, 5], dias).should eq(5) }
+      it { trilha([5, 1, 1, 1], dias).should eq(5) }
+      it { trilha([1, 5, 1, 1], dias).should eq(6) }
     end
   end
 
   context "em três dias" do
-    it "e com cinco acampamentos" do
-      trilha([1, 5, 1, 1], 3).should eq(5)
-    end
+    let(:dias) { 3 }
 
-    it "e com cinco acampamentos II" do
-      trilha([1, 6, 1, 1], 3).should eq(6)
-    end
-
-    it "e com cinco acampamentos III" do
-      trilha([1, 1, 6, 1], 3).should eq(6)
+    context "e com cinco acampamentos" do
+      it { trilha([1, 5, 1, 1], dias).should eq(5) }
+      it { trilha([1, 6, 1, 1], dias).should eq(6) }
+      it { trilha([1, 1, 6, 1], dias).should eq(6) }
     end
 
     it "e com seis acampamentos" do
-      trilha([1, 1, 2, 2, 1], 3).should eq(3)
+      trilha([1, 1, 2, 2, 1], dias).should eq(3)
     end
   end
 end
