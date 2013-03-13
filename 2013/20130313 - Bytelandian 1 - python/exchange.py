f0e601bd21f55cb7ef410d02af3a3d0a7b29e446
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-03-13 20:23:48 -0300

    Carlos Cunha e Flávio Amieiro

--- 2013/20130313 - Bytelandian 1 - python/exchange.py
+++ 2013/20130313 - Bytelandian 1 - python/exchange.py
@@ -1,10 +1,9 @@
 cache = {0: 1}
 
 def exchange(coin):
-    if coin not in cache:
-        cache[coin] = exchange(coin/2) + \
-                      exchange(coin/3) + \
-                      exchange(coin/4)
-
+    cache[coin] = cache.get(coin) or \
+                        exchange(coin/2) + \
+                        exchange(coin/3) + \
+                        exchange(coin/4)
     return cache[coin]
 
