2e01cae34c92d7bc451127d637223126c5f5a9d0
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-03-20 20:35:30 -0300

    carlos Cunha e Juan Lopes

--- 2013/20130320 - bagagem - python/bagagem.py
+++ 2013/20130320 - bagagem - python/bagagem.py
@@ -1,5 +1,19 @@
 #-*- coding: utf-8 -*-
+
+def tentar(malas, soma):
+	if len(malas) == 0:
+		return True
+
+
+	return tentar(malas[:-1], soma-malas[-1]) or tentar(malas[:-1], soma)
+
 def divisivel(malas):
+	if sum(malas) % 2:
+		return False
+
+	if len(malas) == 1:
+		return False
+
 	if len(malas) == 2 and malas[0] == malas[1]:
 		return True
 
@@ -10,7 +24,8 @@ def divisivel(malas):
 		return True
 
 	if len(malas) == 4 and (
-		malas[0] + malas[1] + malas[2] == malas[3]):
+		malas[0] + malas[1] + malas[2] == malas[3] or
+		malas[0] + malas[1] == malas[2] + malas[3]):
 		return sum(malas) % 2 == 0
 
 	return not malas
