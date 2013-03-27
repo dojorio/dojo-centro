d14b3c879608141ba760f4531641ca1d29adf5c9
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-03-27 19:46:35 -0300

    Otávio Cardoso e Israel Teixeira

--- 2013/20130327 - esquibunda - python/esquibunda.py
+++ 2013/20130327 - esquibunda - python/esquibunda.py
@@ -2,6 +2,7 @@
 
 def esquibunda(montanha):
 	if montanha:
-		return len(montanha[0])
+		max_index = montanha[0].index(max(montanha[0]))
+		return len(montanha[0][:max_index + 1])
 
 	return len(montanha)
