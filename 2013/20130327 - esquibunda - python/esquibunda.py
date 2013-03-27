b57eab745dc5b838e0518a1b9b03a981f34d8631
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-03-27 20:20:48 -0300

    Flávio Amieiro e Jacqueline Abreu

--- 2013/20130327 - esquibunda - python/esquibunda.py
+++ 2013/20130327 - esquibunda - python/esquibunda.py
@@ -6,10 +6,11 @@ def esquibunda(montanha):
 
     pista = montanha[0]
 
+    if len(set(pista)) == 1:
+        return 1
+
     if len(pista) == 3 and pista[1] == 1:
         return 2
 
-    if pista[0] == pista[1] and pista[1] == pista[2]:
-        return 1
     max_index = pista.index(max(pista))
     return max(max_index + 1, len(pista) - max_index)
