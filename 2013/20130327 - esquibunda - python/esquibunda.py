669e98ef93d4570267f48e8d9c6f4f60c9383cf9
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-03-27 19:58:32 -0300

    Cláudio Berrondo e Flávio Amieiro

--- 2013/20130327 - esquibunda - python/esquibunda.py
+++ 2013/20130327 - esquibunda - python/esquibunda.py
@@ -4,5 +4,7 @@ def esquibunda(montanha):
     if not montanha:
         return 0
 
-    index = montanha[0].index(max(montanha[0]))
-    return max(index + 1, len(montanha[0]) - index)
+    pista = montanha[0]
+
+    max_index = pista.index(max(pista))
+    return max(max_index + 1, len(pista) - max_index)
--- 2013/20130327 - esquibunda - python/test_esquibunda.py
+++ 2013/20130327 - esquibunda - python/test_esquibunda.py
@@ -3,40 +3,45 @@ import unittest
 from esquibunda import esquibunda
 
 class EsquibundaTestCase(unittest.TestCase):
-	def test_planicie(self):
-		montanha = []
-		self.assertEquals(0, esquibunda(montanha))
+    def test_planicie(self):
+        montanha = []
+        self.assertEquals(0, esquibunda(montanha))
 
-	def test_lombada(self):
-		montanha = [
-			[1]
-		]
-		self.assertEquals(1, esquibunda(montanha))
+    def test_lombada(self):
+        montanha = [
+            [1]
+        ]
+        self.assertEquals(1, esquibunda(montanha))
 
-	def test_lombada_dupla(self):
-		montanha = [
-			[1, 2]
-		]
-		self.assertEquals(2, esquibunda(montanha))
+    def test_lombada_dupla(self):
+        montanha = [
+            [1, 2]
+        ]
+        self.assertEquals(2, esquibunda(montanha))
 
-	def test_lombada_tripla(self):
-		montanha = [
-			[1, 2, 3]
-		]
-		self.assertEquals(3, esquibunda(montanha))
+    def test_lombada_tripla(self):
+        montanha = [
+            [1, 2, 3]
+        ]
+        self.assertEquals(3, esquibunda(montanha))
 
 
-	def test_lombada_tripla_com_pico_central(self):
-		montanha = [
-			[1, 3, 2]
-		]
-		self.assertEquals(2, esquibunda(montanha))
+    def test_lombada_tripla_com_pico_central(self):
+        montanha = [
+            [1, 3, 2]
+        ]
+        self.assertEquals(2, esquibunda(montanha))
 
-	def test_lombada_dupla_invertida(self):
-		montanha = [
-			[2, 1]
-		]
-		self.assertEquals(2, esquibunda(montanha))
+    def test_lombada_dupla_invertida(self):
+        montanha = [
+            [2, 1]
+        ]
+        self.assertEquals(2, esquibunda(montanha))
 
+    def test_lombada_tripla_com_vale_central(self):
+        montanha = [
+            [2, 1, 3]
+        ]
+        self.assertEquals(2, esquibunda(montanha))
 
 unittest.main()
