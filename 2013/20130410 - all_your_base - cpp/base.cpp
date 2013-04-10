d014598c0dcda5fafc1fafabcb5f6a7cf85572fc
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-04-10 20:01:42 -0300

    Breno Ferreira e Higor Cesar

--- 2013/20130410 - all_your_base - cpp/base.cpp
+++ 2013/20130410 - all_your_base - cpp/base.cpp
@@ -3,6 +3,10 @@
 #include <cmath>
 using namespace std;
 
+int CalcularNaBase2(int n){
+	return (pow(2,n));
+}
+
 int main() {
 	int caso = 0, casos;
 	string simbolos;
@@ -10,9 +14,11 @@ int main() {
 
 	while(caso++ < casos) {
 		int total = 0;
+		int qtdSimbolos = 0;
+		char simbolos[];
 		cin >> simbolos;
 		for(int i = 0; i < simbolos.size(); i++){
-			total += 1*(pow(2,i));
+			total += CalcularNaBase2(i);
 		}
 
 		cout << "Case #" << caso << ": " << total << endl;
--- 2013/20130410 - all_your_base - cpp/base.sol
+++ 2013/20130410 - all_your_base - cpp/base.sol
@@ -1,3 +1,4 @@
 Case #1: 1
 Case #2: 3
 Case #3: 7
+Case #4: 2
