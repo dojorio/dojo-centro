22a69805bf81ecdda87fed4e3d3f787d81fc382d
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-04-17 20:23:03 -0300

    Juan Lopes e Israel Teixeira

--- 2013/20130417 - aeroporto - python/aeroporto.py
+++ 2013/20130417 - aeroporto - python/aeroporto.py
@@ -3,11 +3,23 @@
 def aeroporto(patio):
     espacos = 0
 
+    for x, linha in enumerate(patio):
+        y = linha.find('*')
+        if y >= 0:
+            entrada = (x, y)
+
+    return ver_a_partir_de(patio, *entrada)
+
     for linha in patio:
         espacos += andar_linha(linha)
         espacos += andar_linha(''.join(reversed(linha)))
 
     return espacos
+
+def ver_a_partir_de(patio, x, y):
+
+
+    pass
 
 def andar_linha(linha):
     espacos = 0
