56a208a14b285e110eded811ee708c92c1eea9e1
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-04-24 20:55:03 -0300

    Israel Teixeira e Cláudio Berrondo

--- 2013/20130424 - ritmo - php/ritmo.php
+++ 2013/20130424 - ritmo - php/ritmo.php
@@ -1,22 +1,13 @@
 <?php
 
+function inverso($numero){
+	return 1 / $numero;
+}
+
 function ritmo($durações) {
-	if (count($durações) == 1) {
-		$tempos = count($durações);
-		$unidade = $durações[0];
-		return "{$tempos}/{$unidade}";
-	}
-
-	//if ($durações == array(1, 2, 1))
-	//	return '5/2';
-
 	$unidade = max($durações);
-	$tempos = array_sum($durações);
 
-	foreach($durações AS &$duração){
-		$duração = 1/$duração;
-	}
-	$soma = array_sum($durações)*2;
+	$tempos = array_sum(array_map('inverso', $durações)) * $unidade;
 
 	return "{$tempos}/{$unidade}";
 }
