bf52881769c73ebe1933f9390fbfa2596dc10bdd
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-04-24 20:31:20 -0300

    Israel Teixeira e Rodrigo Vidal

--- 2013/20130424 - ritmo - php/ritmo.php
+++ 2013/20130424 - ritmo - php/ritmo.php
@@ -1,12 +1,12 @@
 <?php
 
 function ritmo($durações) {
-	if (sizeof(array_unique($durações)) == 2){
-		return "3/2";
+	if (count(array_unique($durações)) == 2) {
+		$tempos = array_sum($durações);
+		$unidade = max($durações);
+	} else {
+		$tempos = count($durações);
+		$unidade = $durações[0];
 	}
-
-	$tempos = sizeof($durações);
-	$unidade = $durações[0];
-
 	return "{$tempos}/{$unidade}";
 }
