ab7a50401acd52bf319dc2fb8904665fcbbe1793
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-15 19:30:58 -0300

    Israel Teixeira e Juan Lopes

--- 2013/20130515 - vigilantes - python/vigilantes.py
+++ 2013/20130515 - vigilantes - python/vigilantes.py
@@ -1,12 +1,5 @@
 #-*- coding: utf-8 -*-
 
 def vigilantes(tabela, maximo):
+    return max([sabor for pontos, sabor in tabela if pontos <= maximo] or [0])
 
-
-    if len(tabela)==2:
-        if tabela[1][1] < tabela[0][1] and tabela[0][0] <= maximo:
-            return tabela[0][1]
-
-        if tabela[1][1] > tabela[0][1] and tabela[1][0] <= maximo:
-            return tabela[1][1]
-    return tabela[0][1]
