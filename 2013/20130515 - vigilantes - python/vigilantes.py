4218bb31b53abc8a4e4263100df24f5abf882e2c
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-15 19:25:10 -0300

    Juan Lopes e angelica

--- 2013/20130515 - vigilantes - python/vigilantes.py
+++ 2013/20130515 - vigilantes - python/vigilantes.py
@@ -1,7 +1,12 @@
 #-*- coding: utf-8 -*-
 
 def vigilantes(tabela, maximo):
+
+
     if len(tabela)==2:
+        if tabela[1][1] < tabela[0][1] and tabela[0][0] <= maximo:
+            return tabela[0][1]
+
         if tabela[1][1] > tabela[0][1] and tabela[1][0] <= maximo:
             return tabela[1][1]
     return tabela[0][1]
