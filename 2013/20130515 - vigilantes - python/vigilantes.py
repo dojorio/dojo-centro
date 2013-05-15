c7031208d99e9d8f4f7cc8b24c8fad4dddb9eff0
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-15 20:25:51 -0300

    carlos Cunha e Jonatas Emidio

--- 2013/20130515 - vigilantes - python/vigilantes.py
+++ 2013/20130515 - vigilantes - python/vigilantes.py
@@ -2,16 +2,11 @@
 
 def vigilantes(tabela, maximo):
     soma_pontos_abaixo_do_max = 0
-    soma_sabor_abaixo_do_max = 0
-
-    if sum(pontos for pontos, sabor in tabela) <= maximo:
-        return sum(sabor for pontos, sabor in tabela)
+    soma_sabor = 0
 
     for pontos, sabor in sorted(tabela, key = lambda x: -x[1]):
         if pontos + soma_pontos_abaixo_do_max <= maximo:
             soma_pontos_abaixo_do_max+=pontos
-            soma_sabor_abaixo_do_max+=sabor
+            soma_sabor+=sabor
 
-
-
-    return soma_sabor_abaixo_do_max
+    return soma_sabor
