0fdd20d57fddb9e7d74393249917da8ff1d51a8e
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-15 21:30:52 -0300

    Jacqueline Abreu e Jonatas Emidio

--- 2013/20130515 - vigilantes - python/vigilantes.py
+++ 2013/20130515 - vigilantes - python/vigilantes.py
@@ -5,21 +5,11 @@ def iterar(tabela, maximo, i):
         return 0
 
     pontos, sabor = tabela[i]
-    if maximo - pontos >= 0:
-        return sabor + iterar(tabela, maximo-pontos, i+1)
-    else:
-        return iterar(tabela, maximo, i+1)
+    if (maximo - pontos < 0 ):
+        return iterar (tabela, maximo, i+1)
+    return max(sabor + iterar(tabela, maximo-pontos, i+1),
+        iterar(tabela, maximo, i+1))
 
 def vigilantes(tabela, maximo):
-    soma_pontos_abaixo_do_max = 0
-    soma_sabor = 0
 
-    ordenador_sabor = lambda (pontos, sabor): sabor
-    ordenador_pontos = lambda (pontos, sabor): pontos
-
-    tabela = sorted(tabela, key = ordenador_sabor)
-    tabela_invertida = tabela[::-1]
-
-    return max(
-        iterar(tabela, maximo, 0),
-        iterar(tabela_invertida, maximo, 0))
+    return iterar(tabela, maximo, 0)
