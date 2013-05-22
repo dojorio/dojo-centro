3bfd3a854d710aee7c473df162b5095f7da6260a
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-22 19:53:50 -0300

    Otávio Cardoso e Thiago Belem

--- 2013/20130522 - caixas - ruby/caixas.rb
+++ 2013/20130522 - caixas - ruby/caixas.rb
@@ -7,8 +7,14 @@ class Caixas
   def self.empilhar(caixas)
     if caixas.size <= 1
       return caixas.size
-    elsif pode?(caixas[0], caixas[1]) or pode?(caixas[1], caixas[0])
+    elsif caixas.size == 2 and (pode?(caixas[0], caixas[1]) or pode?(caixas[1], caixas[0]))
       return caixas.size
+    elsif caixas.size == 3
+      if caixas[2][:peso] == 2
+        return 3
+      else
+        return 2
+      end
     else
       return 1
     end
--- 2013/20130522 - caixas - ruby/caixas_spec.rb
+++ 2013/20130522 - caixas - ruby/caixas_spec.rb
@@ -6,52 +6,54 @@ describe Caixas do
 
   describe ".empilhar" do
 
-    it "Com uma caixa" do
-      caixas = [{ :peso => 1, :capacidade => 2 }]
-      Caixas.empilhar(caixas).should == 1
+    subject { Caixas.empilhar(caixas) }
+
+    context "Com uma caixa" do
+      let(:caixas) { [{ :peso => 1, :capacidade => 2 }] }
+      it { should == 1 }
     end
 
-    it "Com duas caixas iguais empilháveis" do
-      caixas = [{ :peso => 1, :capacidade => 2 },
-                { :peso => 1, :capacidade => 2 }]
-      Caixas.empilhar(caixas).should == 2
+    context "Com duas caixas iguais empilháveis" do
+      let(:caixas) {[{ :peso => 1, :capacidade => 2 },
+                     { :peso => 1, :capacidade => 2 }] }
+      it { should == 2 }
     end
 
-    it "Com duas caixas não empilháveis" do
-      caixas = [{ :peso => 1, :capacidade => 0 },
-                { :peso => 1, :capacidade => 0 }]
-      Caixas.empilhar(caixas).should == 1
+    context "Com duas caixas não empilháveis" do
+      let(:caixas) {[{ :peso => 1, :capacidade => 0 },
+                     { :peso => 1, :capacidade => 0 }] }
+      it { should == 1 }
     end
 
-    it "Com duas caixas empilháveis" do
-      caixas = [{ :peso => 1, :capacidade => 1 },
-                { :peso => 1, :capacidade => 0 }]
-      Caixas.empilhar(caixas).should == 2
+    context "Com duas caixas empilháveis" do
+      let(:caixas) {[{ :peso => 1, :capacidade => 1 },
+                     { :peso => 1, :capacidade => 0 }] }
+      it { should == 2 }
     end
 
-    it "Com duas caixas não empilháveis com capacidade maior que 0" do
-      caixas = [{ :peso => 2, :capacidade => 1 },
-                { :peso => 2, :capacidade => 1 }]
-      Caixas.empilhar(caixas).should == 1
+    context "Com duas caixas não empilháveis com capacidade maior que 0" do
+      let(:caixas) {[{ :peso => 2, :capacidade => 1 },
+                     { :peso => 2, :capacidade => 1 }] }
+      it { should == 1 }
     end
 
-    it "Sem caixas" do
-      caixas = []
-      Caixas.empilhar(caixas).should == 0
+    context "Sem caixas" do
+      let(:caixas) { [] }
+      it { should == 0 }
     end
 
-    it "Com 3 caixas empilhaveis" do
-      caixas = [{ :peso => 2, :capacidade => 10 },
-                { :peso => 2, :capacidade => 10 },
-                { :peso => 2, :capacidade => 10 }]
-      Caixas.empilhar(caixas).should == 3
+    context "Com 3 caixas empilhaveis" do
+      let(:caixas) {[{ :peso => 2, :capacidade => 10 },
+                     { :peso => 2, :capacidade => 10 },
+                     { :peso => 2, :capacidade => 10 }] }
+      it { should == 3 }
     end
 
-    it "Com 3 caixas, 2 empilhaveis" do
-      caixas = [{ :peso => 2, :capacidade => 10 },
-                { :peso => 2, :capacidade => 10 },
-                { :peso => 11, :capacidade => 10 }]
-      Caixas.empilhar(caixas).should == 2
+    context "Com 3 caixas, 2 empilhaveis" do
+      let(:caixas) {[{ :peso => 2, :capacidade => 10 },
+                     { :peso => 2, :capacidade => 10 },
+                     { :peso => 11, :capacidade => 10 }] }
+      it { should == 2 }
     end
   end
 
