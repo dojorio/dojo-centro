64c67ea684edfe8870fec9ee529bedf11fc2cb4e
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-22 19:21:38 -0300

    Juan Lopes e Eduardo Stalinho

--- 2013/20130522 - caixas - ruby/caixas.rb
+++ 2013/20130522 - caixas - ruby/caixas.rb
@@ -1,13 +1,19 @@
 class Caixas
 
   def self.empilhar(caixas)
+    nao_empilhaveis = 0
+
     caixas.each do |caixa|
       if caixa[:capacidade] == 0
-        return 1
+        nao_empilhaveis += 1
       end
     end
 
-    caixas.size
+    if nao_empilhaveis == caixas.size
+      1
+    else
+      caixas.size
+    end
   end
 
 end
--- 2013/20130522 - caixas - ruby/caixas_spec.rb
+++ 2013/20130522 - caixas - ruby/caixas_spec.rb
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 require "caixas"
 
 describe Caixas do
@@ -9,22 +11,28 @@ describe Caixas do
       Caixas.empilhar(caixas).should == 1
     end
 
-    it "Com duas caixas iguais empilhaveis" do
+    it "Com duas caixas iguais empilháveis" do
       caixas = [{ :peso => 1, :capacidade => 2 },
                 { :peso => 1, :capacidade => 2 }]
       Caixas.empilhar(caixas).should == 2
     end
 
-    it "Com duas caixas nao empilhaveis" do
+    it "Com duas caixas não empilháveis" do
       caixas = [{ :peso => 1, :capacidade => 0 },
                 { :peso => 1, :capacidade => 0 }]
       Caixas.empilhar(caixas).should == 1
     end
 
-    it "Com duas caixas empilhaveis" do
+    it "Com duas caixas empilháveis" do
       caixas = [{ :peso => 1, :capacidade => 1 },
                 { :peso => 1, :capacidade => 0 }]
       Caixas.empilhar(caixas).should == 2
+    end
+
+    it "Com duas caixas não empilháveis com capacidade maior que 0" do
+      caixas = [{ :peso => 2, :capacidade => 1 },
+                { :peso => 2, :capacidade => 1 }]
+      Caixas.empilhar(caixas).should == 1
     end
   end
 
