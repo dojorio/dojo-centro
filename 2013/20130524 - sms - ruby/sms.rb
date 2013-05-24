d14cafcfe8d3c48a1683576fe8c8f1598886f103
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-24 20:54:32 -0300

    WIllian Oizumi e Thiago Belem

--- 2013/20130524 - sms - ruby/sms.rb
+++ 2013/20130524 - sms - ruby/sms.rb
@@ -1,28 +1,11 @@
 class SMS
 
   def numbers(text)
-    dic = {
-      ' '=> '0',
-      'a'=> '2',
-      'b'=> '22',
-      'c'=> '222',
-      'd' => '3',
-      'e' => '33',
-      'g' => '4',
-      'h'=> '44',
-      'j'=> '5',
-      'l'=> '555',
-      'm'=> '6',
-      'n'=> '66',
-      'o'=> '666',
-      'u'=> '88'
-    }
-
     letters = text.split('')
     old_digits = ""
 
     letters.map { |letter|
-      digits = dic[letter]
+      digits = letter_to_digits(letter)
 
       digits = '_' + digits if old_digits[-1] == digits[0]
 
@@ -45,7 +28,10 @@ class SMS
 
     for key in dic.keys
        if key.include? letter
-          (key.index(letter) + 1) * dic[key]
+          digit = dic[key]
+          repeat = key.index(letter) + 1
+
+          return digit * repeat
        end
     end
   end
