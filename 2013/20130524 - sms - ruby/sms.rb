6a9a3ad9c26047a8ca4a3e9c5a3e11ceda5f6b62
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-24 20:49:18 -0300

    Thiago Belem e Flavia Fortes

--- 2013/20130524 - sms - ruby/sms.rb
+++ 2013/20130524 - sms - ruby/sms.rb
@@ -29,5 +29,25 @@ class SMS
       old_digits = digits
     }.join('')
   end
+
+  def letter_to_digits(letter)
+    dic = {
+      'abc' => '2',
+      'def' => '3',
+      'ghi' => '4',
+      'jkl' => '5',
+      'mno' => '6',
+      'pqrs' => '7',
+      'tuv' => '8',
+      'wxyz' => '9',
+      ' ' => '0',
+    }
+
+    for key in dic.keys
+       if key.include? letter
+          (key.index(letter) + 1) * dic[key]
+       end
+    end
+  end
 end
 
