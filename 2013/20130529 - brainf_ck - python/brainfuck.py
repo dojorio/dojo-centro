c05eafbdf92221544e8a067d8f3946a02062b3df
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-29 20:56:02 -0300

    Juan Lopes e Eduardo Stalinho

--- 2013/20130529 - brainf_ck - python/brainfuck.py
+++ 2013/20130529 - brainf_ck - python/brainfuck.py
@@ -1,18 +1,17 @@
 #-*- coding: utf-8 -*-
+from collections import defaultdict
 
 def brainfuck(programa, entrada):
 	saida = ''
-	fita = []
+	fita = defaultdict(lambda: 0)
 	celula = 0
 	entrada = iter(entrada)
 
 	for char in programa:
 		if char == '>':
 			celula += 1
-			fita[celula] = 0 if not fita[celula]
-		elif char == '>':
+		elif char == '<':
 			celula -= 1
-			fita[celula] = 0 if not fita[celula]
 		elif char == '.':
 			saida += chr(fita[celula] % 256)
 		elif char == '+':
