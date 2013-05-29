a2c0c7d1baa03469d77a54cdc2e9d79fa278cd86
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-29 20:50:08 -0300

    Eduardo Stalinho e Thiago Belem

--- 2013/20130529 - brainf_ck - python/brainfuck.py
+++ 2013/20130529 - brainf_ck - python/brainfuck.py
@@ -2,20 +2,27 @@
 
 def brainfuck(programa, entrada):
 	saida = ''
+	fita = []
 	celula = 0
 	entrada = iter(entrada)
+
 	for char in programa:
-		if char == '.':
-			saida += chr(celula % 256)
-		elif char == '+':
+		if char == '>':
 			celula += 1
-		elif char == '-':
+			fita[celula] = 0 if not fita[celula]
+		elif char == '>':
 			celula -= 1
+			fita[celula] = 0 if not fita[celula]
+		elif char == '.':
+			saida += chr(fita[celula] % 256)
+		elif char == '+':
+			fita[celula] += 1
+		elif char == '-':
+			fita[celula] -= 1
 		elif char == ',':
 			try:
-				celula = ord(next(entrada))
+				fita[celula] = ord(next(entrada))
 			except StopIteration:
-				celula = '\x00'
-
+				fita[celula] = 0
 
 	return saida
--- 2013/20130529 - brainf_ck - python/test_brainfuck.py
+++ 2013/20130529 - brainf_ck - python/test_brainfuck.py
@@ -1,79 +1,97 @@
-#-*- coding: utf-8 -*-
+# coding: utf-8
+
 import unittest
 from brainfuck import brainfuck
 
 class BrainfuckTestCase(unittest.TestCase):
-    def test_exibe_0(self):
-        programa = '.'
-        entrada = ''
-        saida = '\x00'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_exibe_0(self):
+		programa = '.'
+		entrada = ''
+		saida = '\x00'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_exibe_0_0(self):
-        programa = '..'
-        entrada = ''
-        saida = '\x00\x00'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_exibe_0_0(self):
+		programa = '..'
+		entrada = ''
+		saida = '\x00\x00'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_exibe_1(self):
-        programa = '+.'
-        entrada = ''
-        saida = '\x01'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_exibe_1(self):
+		programa = '+.'
+		entrada = ''
+		saida = '\x01'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_exibe_2(self):
-        programa = '++.'
-        entrada = ''
-        saida = '\x02'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_exibe_2(self):
+		programa = '++.'
+		entrada = ''
+		saida = '\x02'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_exibe_255(self):
-        programa = '-.'
-        entrada = ''
-        saida = '\xFF'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_exibe_255(self):
+		programa = '-.'
+		entrada = ''
+		saida = '\xFF'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
+	def test_incrementa_256_vezes_e_exibe_0(self):
+		programa = '+' * 256 + '.'
+		entrada = ''
+		saida = '\x00'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_incrementa_256_vezes_e_exibe_0(self):
-        programa = '+' * 256 + '.'
-        entrada = ''
-        saida = '\x00'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_exibe_0_1(self):
+		programa = '.+.'
+		entrada = ''
+		saida = '\x00\x01'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_exibe_0_1(self):
-        programa = '.+.'
-        entrada = ''
-        saida = '\x00\x01'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_exibe_0_1_2(self):
+		programa = '.+.+.'
+		entrada = ''
+		saida = '\x00\x01\x02'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_exibe_0_1_2(self):
-        programa = '.+.+.'
-        entrada = ''
-        saida = '\x00\x01\x02'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_le_A_e_exibe_A(self):
+		programa = ',.'
+		entrada = 'A'
+		saida = '\x41'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_le_A_e_exibe_A(self):
-        programa = ',.'
-        entrada = 'A'
-        saida = '\x41'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_le_AB_e_exibe_B(self):
+		programa = ',,.'
+		entrada = 'AB'
+		saida = '\x42'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_le_AB_e_exibe_B(self):
-        programa = ',,.'
-        entrada = 'AB'
-        saida = '\x42'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_le_AB_e_exibe_AB(self):
+		programa = ',.,.'
+		entrada = 'AB'
+		saida = '\x41\x42'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_le_AB_e_exibe_AB(self):
-        programa = ',.,.'
-        entrada = 'AB'
-        saida = '\x41\x42'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_le_entrada_vazia_e_exibe_0(self):
+		programa = ',.'
+		entrada = ''
+		saida = '\x00'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
-    def test_le_AB_e_exibe_AB(self):
-        programa = ',.'
-        entrada = ''
-        saida = '\x00'
-        self.assertEquals(saida, brainfuck(programa, entrada))
+	def test_incrementa_le_entrada_vazia_e_exibe_0(self):
+		programa = '+,.'
+		entrada = ''
+		saida = '\x00'
+		self.assertEquals(saida, brainfuck(programa, entrada))
+
+	def test_incrementa_anda_na_fita(self):
+		programa = '+>.'
+		entrada = ''
+		saida = '\x00'
+		self.assertEquals(saida, brainfuck(programa, entrada))
+
+	def test_incrementa_anda_volta_na_fita(self):
+		programa = '+><.'
+		entrada = ''
+		saida = '\x01'
+		self.assertEquals(saida, brainfuck(programa, entrada))
 
 unittest.main()
