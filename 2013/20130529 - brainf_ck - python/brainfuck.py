b3bdd1bedb42c345881d012ba2836bd710f89e37
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-05-29 21:14:20 -0300

    Carlos Cunha e Patrick Porto

--- 2013/20130529 - brainf_ck - python/brainfuck.py
+++ 2013/20130529 - brainf_ck - python/brainfuck.py
@@ -2,30 +2,35 @@
 from collections import defaultdict
 
 def brainfuck(programa, entrada):
-	saida = ''
-	fita = defaultdict(lambda: 0)
-	celula = 0
-	entrada = iter(entrada)
+  saida = ''
+  fita = defaultdict(lambda: 0)
+  celula = 0
+  entrada = iter(entrada)
 
-	pc = 0
+  pc = 0
+  posicao_abre = 0
 
-	while pc < len(programa):
-		char = programa[pc]
-		if char == '>':
-			celula += 1
-		elif char == '<':
-			celula -= 1
-		elif char == '.':
-			saida += chr(fita[celula] % 256)
-		elif char == '+':
-			fita[celula] += 1
-		elif char == '-':
-			fita[celula] -= 1
-		elif char == ',':
-			try:
-				fita[celula] = ord(next(entrada))
-			except StopIteration:
-				fita[celula] = 0
-		pc += 1
+  while pc < len(programa):
+    char = programa[pc]
+    if char == '>':
+      celula += 1
+    elif char == '<':
+      celula -= 1
+    elif char == '.':
+      saida += chr(fita[celula] % 256)
+    elif char == '+':
+      fita[celula] += 1
+    elif char == '-':
+      fita[celula] -= 1
+    elif char == ',':
+      try:
+        fita[celula] = ord(next(entrada))
+      except StopIteration:
+        fita[celula] = 0
+    elif char == '[':
+        posicao_abre = pc
+    elif char == ']':
+        pc = posicao_abre
+    pc += 1
 
-	return saida
+  return saida
