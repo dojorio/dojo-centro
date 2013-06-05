a49e7cc82ca7b3294f60d1cd3afbcf63fa4ebe4a
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-06-05 20:13:16 -0300

    Andre Oliveira e Otávio Cardoso

--- 2013/20130605 - pintar - ruby/pintar.rb
+++ 2013/20130605 - pintar - ruby/pintar.rb
@@ -1,5 +1,5 @@
 def pintar(arvore)
   return 0 if arvore.nil?
 
-  arvore[0] + 2 * pintar(arvore[1]) + 2 * pintar(arvore[2])
+  arvore[0] + 2 * [arvore[1][0],arvore[2][0]].max + 3 * [arvore[1][0],arvore[2][0]].min
 end
--- 2013/20130605 - pintar - ruby/pintar_spec.rb
+++ 2013/20130605 - pintar - ruby/pintar_spec.rb
@@ -3,34 +3,38 @@ require './pintar.rb'
 describe 'pintar arvore' do
 
   it 'vazia' do
-    arvore = nil
+    arvore = []
     expect(pintar arvore).to eq 0
   end
 
   it 'somente com raiz' do
-    arvore = [10, nil, nil]
+    arvore = [10, [], []]
     expect(pintar arvore).to eq 10
   end
 
   it 'somente com o filho esquerdo' do
     arvore = [10,
-                [2,
-                  nil,
-                  nil
-                ],
-                nil
+                [2, [], []],
+                []
              ]
     expect(pintar arvore).to eq 14
   end
 
   it 'somente com o filho direito' do
     arvore = [10,
-                nil,
-                [2,
-                  nil,
-                  nil
-                ]
+                [],
+                [2, [], []]
              ]
     expect(pintar arvore).to eq 14
   end
+
+
+  it 'com dois filhos iguais' do
+    arvore = [10,
+                [2, [], []],
+                [2, [], []]
+             ]
+    expect(pintar arvore).to eq 20
+  end
+
 end
