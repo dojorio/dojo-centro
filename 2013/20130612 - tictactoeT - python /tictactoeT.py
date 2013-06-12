4af13b34a67f3c9c7ea838738eea28f79ddb8998
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-06-12 19:40:18 -0300

    Andre Oliveira e Carlos Cunha

--- 2013/20130612 - tictactoeT - python /tictactoeT.py
+++ 2013/20130612 - tictactoeT - python /tictactoeT.py
@@ -1,9 +1,5 @@
 def verifica(entrada):
 
-	if entrada[0].count('T') == 1:
-	 	if entrada[0].count('x') <3 and entrada[0].count('o') <3:
-			return 'Rolando'
-	elif entrada[0].count('x') <4 and entrada[0].count('o') <4:
-		return 'Rolando'
-
-	return entrada[0][1] + ' Ganhou'
+	if entrada[0].count('T') + entrada[0].count('x') == 4: return 'x Ganhou'
+	if entrada[0].count('T') + entrada[0].count('o') == 4: return 'o Ganhou'
+	return 'Rolando'
