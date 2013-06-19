5af57d52b37d7bd8345b6fb339503626314f825c
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-06-19 20:27:20 -0300

    Israel Teixeira e Juan Lopes

--- 2013/20130619 - mpl - cpp/mpl.cpp
+++ 2013/20130619 - mpl - cpp/mpl.cpp
@@ -12,32 +12,29 @@ int custo_trajeto(int estacoes) {
 }
 
 int main() {
-	int caso = 0, casos;
+    int valor_pas, qtd_trajetos;
+ 	int caso = 0, casos;
 	cin >> casos;
-
     
-	while(caso++ < casos) {
-	
-	    int valorPago = 0;
-        int valorDevido = 0;
-        int totalEstacoes = 0;
-	
-	    int valorPas, qtd_trajetos;
-	    cin >> valorPas >> qtd_trajetos;
+	while(caso++, cin >> valor_pas >> qtd_trajetos) {
+	    int valor_pago = 0;
+        int valor_devido = 0;
+        int total_estacoes = 0;
+	    int max_pessoas=0;
 	    
 	    for(int i=0; i<qtd_trajetos; i++) {
 	        int origem, destino, qtd_pessoas;
 	        cin >> origem >> destino >> qtd_pessoas;
 	        
 	        int tamanho_trajeto = destino - origem;
-	        valorDevido += custo_trajeto(tamanho_trajeto) *qtd_pessoas;
-            totalEstacoes += tamanho_trajeto;
-            
+	        valor_devido += custo_trajeto(tamanho_trajeto) * qtd_pessoas;
+            total_estacoes += tamanho_trajeto;
+            max_pessoas = max(max_pessoas, qtd_pessoas);
 	    }
         
-        valorPago = custo_trajeto(totalEstacoes);
+        valor_pago = custo_trajeto(total_estacoes)*max_pessoas;
 	    
-	    int resultado = valorDevido - valorPago;
+	    int resultado = valor_devido - valor_pago;
 	
 		cout << "Case #" << caso << ": " << resultado << endl;
 	}
--- 2013/20130619 - mpl - cpp/mpl.sol
+++ 2013/20130619 - mpl - cpp/mpl.sol
@@ -2,3 +2,4 @@ Case #1: 0
 Case #2: 1
 Case #3: 4
 Case #4: 8
+Case #5: 8
