de297883d4111d2907b2272df843d6217f68aabf
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-06-19 19:31:36 -0300

    Juan Lopes e Israel Teixeira

---
+++ 2013/20130619 - mpl - cpp/mpl.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main() {
+	int caso = 0, casos;
+	cin >> casos;
+
+	while(caso++ < casos) {
+		cout << "Case #" << caso << ": " << "0" << endl;
+	}
+}
