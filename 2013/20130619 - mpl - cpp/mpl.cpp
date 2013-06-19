778fceb4ebce33d5a6840871f54d0fc0c9540c00
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-06-19 19:44:01 -0300

    Israel Teixeira e Jacqueline Abreu

--- 2013/20130619 - mpl - cpp/mpl.cpp
+++ 2013/20130619 - mpl - cpp/mpl.cpp
@@ -6,14 +6,19 @@ using namespace std;
 int main() {
 	int caso = 0, casos;
 	cin >> casos;
-
+    double valorPago
+    double valorDevido
+    
 	while(caso++ < casos) {
-	    int n, m;
-	    cin >> n >> m;
+	    int valorPas, nEst;
+	    cin >> valorPas >> estacoes;
 	    
-	    for(int i=0; i<m; i++) {
-	        int e, s, p;
-	        cin >> e >> s >> p;
+	    for(int i=0; i<estacoes; i++) {
+	        int origem, destino, qtd;
+	        cin >> origem >> destino >> qtd;
+	        
+	        
+	        
 	    }
 	    
 	
