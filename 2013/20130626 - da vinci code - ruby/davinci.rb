d058c77b9c9101023fb64fcbe0cac3a07d1979c6
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-06-26 20:44:26 -0300

    Juan Lopes e Israel Teixeira

--- 2013/20130626 - da vinci code - ruby/davinci.rb
+++ 2013/20130626 - da vinci code - ruby/davinci.rb
@@ -1,22 +1,16 @@
 def davinci(chave, codigo)
 
-  limFib = chave.max
-  fib(limFib).map do |n|
-    idx = chave.index(n)
+  lim_fib = (chave.max or 0)
+  a = b = 1
+  mensagem = ''
+  while a <= lim_fib
+    idx = chave.index(a)
     if idx
-      codigo[idx].chr
+      mensagem << codigo[idx].chr
     else
-      ' '
+      mensagem << ' '
     end
-  end.join
-end
-
-#saida[fib.chave[i].search] = codigo[i]
-
-def fib(limite)
-  fib = [1,2]
-
-  limite.times do
-    fib[2] = fib[0]+fib[1]
+    a, b = a + b, a
   end
+  mensagem
 end
