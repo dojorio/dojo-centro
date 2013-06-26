809674e2af4099c39fe4a3936d7b46a67fef9515
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-06-26 20:04:47 -0300

    Juan Lopes e Jonatas Emidio

--- 2013/20130626 - da vinci code - ruby/davinci.rb
+++ 2013/20130626 - da vinci code - ruby/davinci.rb
@@ -3,10 +3,14 @@ def davinci(chave, codigo)
   fib = [1, 2, 3]
   frase = ""
 
-  fib.each{|n|
-    chave.each{|x|
-      if( n==x)
-  }
+  fib.each do |n|
+    chave.each_with_index do |x, i|
+      if n==x
+          frase << codigo[i]
+      end
+    end
+  end
+  return frase
 
   return chave.map {|x| codigo[x-1].chr }.join
 end
