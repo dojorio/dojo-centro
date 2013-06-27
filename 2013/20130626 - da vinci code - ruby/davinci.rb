945cc7a8494df86a7bcf52d6f2d4580af6e70984
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-06-26 21:14:41 -0300

    Israel Teixeira e Juan Lopes

--- 2013/20130626 - da vinci code - ruby/davinci.rb
+++ 2013/20130626 - da vinci code - ruby/davinci.rb
@@ -1,10 +1,9 @@
 require 'generator'
 
 def davinci(chave, codigo)
-  codigo = codigo.chars.select{|x|('A'..'Z').include?(x)}.join
+  codigo = codigo.gsub(/[^A-Z]/, '')
 
-  lim_fib = (chave.max or 0)
-  fib(lim_fib).map do |n|
+  fib(chave.max || 0).map do |n|
     idx = chave.index(n)
     idx ? codigo[idx].chr : ' '
   end.join
@@ -12,11 +11,11 @@ def davinci(chave, codigo)
 end
 
 def fib(limite)
-  Generator.new { |g|
+  Generator.new do |g|
     a = b = 1
     while a <= limite
       g.yield a
       a, b = a + b, a
     end
-  }
+  end
 end
