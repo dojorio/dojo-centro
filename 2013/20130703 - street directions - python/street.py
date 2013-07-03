568bb0d0d0855f05ee440cc730dcf6eaf87706f4
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-07-03 20:13:58 -0300

    Renan Cardoso e Juan Lopes

--- 2013/20130703 - street directions - python/street.py
+++ 2013/20130703 - street directions - python/street.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
+def dfs(graph, intersection, visited):
+	visited.append(intersection)
+
+	for other in graph[intersection]:
+		if other not in visited:
+			dfs(graph, other, visited)
+		else:
+			visited.index(other)
+
+
 def directions(streets):
 	graph = defaultdict(lambda:[])
 
@@ -9,7 +19,6 @@ def directions(streets):
 	for a, b in streets:
 		graph[a].append(b)
 		graph[b].append(a)
-
 
 
 	return len([value for value in graph.values() if len(value) >= 2])
