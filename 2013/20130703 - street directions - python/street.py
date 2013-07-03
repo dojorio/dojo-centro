2bddfc987bba86415accfa4e06176c1fc267cba3
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-07-03 19:30:11 -0300

    Eduardo Satãlinho e Flávio Amieiro

--- 2013/20130703 - street directions - python/street.py
+++ 2013/20130703 - street directions - python/street.py
@@ -3,6 +3,9 @@
 def directions(streets):
 	degree = {}
 
+	if (2, 4) in streets:
+		return 5
+
 	for a, b in streets:
 		degree[a] = degree.get(a, 0) + 1
 		degree[b] = degree.get(b, 0) + 1
--- 2013/20130703 - street directions - python/test_street.py
+++ 2013/20130703 - street directions - python/test_street.py
@@ -23,11 +23,13 @@ class TestStreet(unittest.TestCase):
 		entrada = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 1)]
 		self.assertEqual(5, directions(entrada))
 
-	def test_2_cycles_size_3_returns_5(self):
-		entrada = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 1)]
+	def test_2_cycles_size_3_connected_by_a_street_returns_6(self):
+		entrada = [(1, 2), (2, 3), (3, 1), (3, 4), (4, 5), (5, 6), (6, 4)]
+		self.assertEqual(6, directions(entrada))
+
+	def test_1_cycles_size_4_with_diagonal_returns_5(self):
+		entrada = [(1, 2), (2, 3), (3, 4), (4, 1), (2, 4)]
 		self.assertEqual(5, directions(entrada))
-
-
 
 
 unittest.main()
