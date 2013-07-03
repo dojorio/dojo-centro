8eca654642f0ba6d990ff192a228c4a824a9bc9b
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-07-03 20:59:43 -0300

    Carlos Cunha e Juan Lopes

--- 2013/20130703 - street directions - python/street.py
+++ 2013/20130703 - street directions - python/street.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
-def dfs(graph, intersection, visited):
-	visited.append(intersection)
+def dfs(graph, begin, end, visited, level=0):
+	visited[end] = level
 
-	for other in graph[intersection]:
+	result = 0
+	for other in graph[end]:
 		if other not in visited:
-			dfs(graph, other, visited)
-		else:
-			visited.index(other)
+			result = max(result, dfs(graph, end, other, visited, level+1)-1)
+		elif other != begin:
+			result = max(result, level - visited[other] + 1)
+
+	print (begin, end)
+
+	return result
+
 
 
 def directions(streets):
 	graph = defaultdict(lambda:[])
 
-	if (2, 4) in streets or (4, 2) in streets:
-		return 5
-
 	for a, b in streets:
 		graph[a].append(b)
 		graph[b].append(a)
+
+	dfs(graph, None, streets[0][0], {})
 
 
 	return len([value for value in graph.values() if len(value) >= 2])
