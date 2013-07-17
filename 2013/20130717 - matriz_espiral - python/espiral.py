be7354d6c867cd748fbfff622521139376d566cd
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-07-17 20:07:12 -0300

    Eduardo Almeida e Otávio Cardoso

--- 2013/20130717 - matriz_espiral - python/espiral.py
+++ 2013/20130717 - matriz_espiral - python/espiral.py
@@ -1,32 +1,33 @@
 # -*- coding: utf-8 -*-
 
-def andar_pra_frente(largura):
+def andar_pra_frente(largura, inicial = 1):
 	linha = []
 
-	for i in range(largura):
-		linha.append(i + 1)
+	for i in range(inicial, inicial + largura):
+		linha.append(i)
 	return linha
 
-def andar_pra_baixo(altura):
-	coluna = []
-	for i in range(altura):
-		linha = [i + 1]
-		coluna.append(linha)
+def andar_pra_tras(largura, inicial = 1):
+	linha = []
 
-	return coluna
+	for i in range(inicial, inicial + largura):
+		linha.insert(i, 0)
+	return linha
 
 def espiral(largura, altura):
 	matriz = []
+
 	if largura == 2 and altura == 2:
 		return [[1,2],[4,3]]
 
+	inicial = 1
 
 	for i in range(altura):
-		linha = andar_pra_frente(largura)
+		linha = andar_pra_frente(largura, inicial)
 		matriz.append(linha)
+		inicial = linha[-1] + 1
+
+		if i % 2 == 0:
+			linha = andar_pra_tras(largura, inicial)
+
 	return matriz
-
-
-	if altura > 1:
-		return andar_pra_baixo(altura)
-	return [andar_pra_frente(largura)]
