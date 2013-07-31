b0ef2e41b929eda0dba5d8c301ae1ae71734a64f
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-07-31 20:53:20 -0300

    Miguel Cacilhinhas e Eduardo Stalinho

--- 2013/20130731 - skyline - python/skyline.py
+++ 2013/20130731 - skyline - python/skyline.py
@@ -6,14 +6,17 @@ def skyline(buildings):
 
 	result = []
 	altura_anterior = -1
+	fim_anterior = -1
 	for predio in buildings:
 		inicio, altura, fim = predio
 
 		if altura != altura_anterior:
+			inicio = max(fim_anterior, inicio)
+
 			result.append((inicio, altura))
 
 
-
+		fim_anterior = fim
 		altura_anterior = altura
 
 	ultimo_predio = buildings[-1]
