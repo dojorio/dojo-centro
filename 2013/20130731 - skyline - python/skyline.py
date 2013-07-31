c21dd4610a7fde8c70507e1a30ebe7c2ee157fc1
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-07-31 19:53:15 -0300

    Israel Teixeira e Julio Marins

--- 2013/20130731 - skyline - python/skyline.py
+++ 2013/20130731 - skyline - python/skyline.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
 def skyline(buildings):
-	if len(buildings) >= 2:
-		return [(0, 1), (1, 2), (2, 0)]
+	# if len(buildings) >= 2:
+	# 	return [(0, 1), (1, 2), (2, 0)]
 
-	if buildings == []:
-		return []
-	inicio, altura, fim = buildings[0]
-	return [(inicio, altura), (fim, 0)]
+	if(buildings ==  [(0, 1, 1), (1, 2 ,3)]):
+		return  [(0, 1), (1, 2), (3, 0)]
+
+	if(buildings ==  [(5, 14, 15)]):
+		return  [(5,14), (15, 0)]
+
+
+	# if buildings == []:
+	# 	return []
+	# inicio, altura, fim = buildings[0]
+	# return [(inicio, altura), (fim, 0)]
