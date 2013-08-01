82684f7fb44e14e22f8b8d25fa491f56608a3c34
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-07-31 21:21:37 -0300

    miguel Cacilhinhas e Renan Cardoso

--- 2013/20130731 - skyline - python/skyline.py
+++ 2013/20130731 - skyline - python/skyline.py
@@ -5,19 +5,6 @@ def skyline(buildings):
 		return []
 
 	result = []
-	for predio in buildings:
-		inicio, altura, fim = predio
-		result += [(inicio, altura), (fim, 0)]
-	result.sort()
-
-	result2 = []
-	altura_anterior = 0
-	for cota in result:
-		inicio, altura = cota
-		if altura > altura_anterior:
-			result2.append(cota)
-		altura_anterior = altura
-	return result2
 
 	altura_anterior = -1
 	fim_anterior = -1
