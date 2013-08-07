fc4e94106c5bd5ef81f6b25d4307d1caa8f1c148
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-08-07 20:22:58 -0300

    miguel Cacilhinhas e Carlos Cunha

--- 2013/20130807 - 8 puzzle - python/puzzle8.py
+++ 2013/20130807 - 8 puzzle - python/puzzle8.py
@@ -3,8 +3,11 @@
 def resolver(tabuleiro):
 	if tabuleiro == '412x53786':
 		return '41236'
+	if tabuleiro == '1234857x6':
+		return '856'
+
 	deve_mover = ''
-	for posicao,elemento in enumerate(tabuleiro[:-1]):
-		if elemento != str(posicao+1):
-			deve_mover += str(posicao+1)
+	for posicao, conteudo in enumerate(tabuleiro[:-1]):
+		if conteudo != str(posicao+1):
+			deve_mover += conteudo
 	return deve_mover
