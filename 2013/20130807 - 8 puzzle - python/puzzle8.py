e389267a2932d4f34b5c4f1eb5c400662602c257
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-08-07 20:43:52 -0300

    Juan Lopes e Jacqueline Abreu

--- 2013/20130807 - 8 puzzle - python/puzzle8.py
+++ 2013/20130807 - 8 puzzle - python/puzzle8.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
+def mover(tabuleiro, a, b):
+
+	a, b = min(a, b), max(a, b)
+	return tabuleiro[0:a]+tabuleiro[b]+tabuleiro[a+1:b]+tabuleiro[a]+tabuleiro[b:]
+
 def resolver(tabuleiro):
 	deve_mover = ''
-	while tabuleiro != "x":
+	while tabuleiro[-1] != "x":
 		posicao_do_x = tabuleiro.find('x')
-		aonde_ta_quem_deveria_estar_na_posicao_do_x = tabuleiro.find(posicao_do_x)
+		aonde_ta_quem_deveria_estar_na_posicao_do_x = tabuleiro.find(str(posicao_do_x + 1))
+		deve_mover += str(posicao_do_x +1)
+		tabuleiro = mover(tabuleiro, posicao_do_x, aonde_ta_quem_deveria_estar_na_posicao_do_x)
 
+	return deve_mover
 
 	for posicao, conteudo in enumerate(tabuleiro):
 		if conteudo != str(posicao+1) and conteudo != 'x':
