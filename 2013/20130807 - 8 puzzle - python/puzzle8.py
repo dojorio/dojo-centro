ba2ab01f3fe201b090c4cb74e3af9897f3f58567
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-08-07 20:38:37 -0300

    Jacqueline Abreu e Flávio Amieiro

--- 2013/20130807 - 8 puzzle - python/puzzle8.py
+++ 2013/20130807 - 8 puzzle - python/puzzle8.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 def resolver(tabuleiro):
-	if tabuleiro = '412753x86'
-		return '741236'
-
 	deve_mover = ''
+	while tabuleiro != "x":
+		posicao_do_x = tabuleiro.find('x')
+		aonde_ta_quem_deveria_estar_na_posicao_do_x = tabuleiro.find(posicao_do_x)
+
+
 	for posicao, conteudo in enumerate(tabuleiro):
 		if conteudo != str(posicao+1) and conteudo != 'x':
 			deve_mover += conteudo
