f33fca9faf9fe4a3e6e60cbbd3f44fbeb89ac456
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-08-21 20:14:28 -0300

    miguel Cacilhinhas e Otávio Cardoso

--- 2013/20130821 - catdog - python/catdog.py
+++ 2013/20130821 - catdog - python/catdog.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def votacao(gatos, caes, votos):
-	if len(votos) == 2 and votos[0] == (votos[1][1], votos[1][0]):
-		return 1
-	return  len(votos)
+	if len(votos) == 2:
+		if votos[0][1] == votos[1][0] or votos[0][0] == votos[1][1]:
+			return 1
+	return len(votos)
