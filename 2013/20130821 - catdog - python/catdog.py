278ee08c6411531a4101fc05f0c322d819ca69d5
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-08-21 20:20:23 -0300

    VitorHP e miguel Cacilhinhas

--- 2013/20130821 - catdog - python/catdog.py
+++ 2013/20130821 - catdog - python/catdog.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
+votos_conflitam = votos[0][1] == votos[1][0] or votos[0][0] == votos[1][1]
 def votacao(gatos, caes, votos):
 	if len(votos) == 2:
-		if votos[0][1] == votos[1][0] or votos[0][0] == votos[1][1]:
+		if votos_conflitam
 			return 1
 	return len(votos)
