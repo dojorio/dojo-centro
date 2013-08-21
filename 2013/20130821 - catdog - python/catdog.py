ee09bc8d581205dd056056dd6023d2bcb5db4f7d
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-08-21 20:02:43 -0300

    Israel Teixeira e Carlos Figueiredo

--- 2013/20130821 - catdog - python/catdog.py
+++ 2013/20130821 - catdog - python/catdog.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 def votacao(gatos, caes, votos):
-	if len(gatos) > 1:
+	if gatos > 1:
 		return caes
 	return gatos
--- 2013/20130821 - catdog - python/test_catdog.py
+++ 2013/20130821 - catdog - python/test_catdog.py
@@ -15,4 +15,8 @@ class TestCatDog(unittest.TestCase):
 		votos = [('G1', 'C1')]
 		self.assertEqual(1, votacao(2, 1, votos))
 
+	def test_1_voto_2_caes_2_gatos(self):
+		votos = [('G1','C1')]
+		self.assertEqual(1, votacao(2, 2, votos))
+
 unittest.main()
