6db11b53c4626f06c935d1bbed08470b40ff83d0
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-08-21 21:01:36 -0300

    Carlos Cunha e miguel Cacilhinhas

--- 2013/20130821 - catdog - python/catdog.py
+++ 2013/20130821 - catdog - python/catdog.py
@@ -3,10 +3,12 @@ def votos_conflitam(voto1, voto2):
 	return voto1[1] == voto2[0] or voto1[0] == voto2[1]
 
 def votacao(gatos, caes, votos):
-	votos_x
-	if len(votos) >= 2:
-		for outro in votos[1:]:
-			votos_x = votos - outro
-			if votos_conflitam(votos[0], outro):
-				return len(votos)-1
+	for voto in votos:
+		return len(votos) - qtd_conflitos(voto, votos)
+#	votos_x
+#	if len(votos) >= 2:
+#		for outro in votos[1:]:
+#			votos_x = votos - outro
+#			if votos_conflitam(votos[0], outro):
+#				return len(votos)-1
 	return len(votos)
