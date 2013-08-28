25f9c2e7e90b6b508185d28bd44f5f9802d13678
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-08-28 19:47:13 -0300

    Carlos Figueiredo e Otávio Cardoso

--- 2013/20130828 - telefone - ruby/telefone.rb
+++ 2013/20130828 - telefone - ruby/telefone.rb
@@ -1,3 +1,4 @@
 def busca(lista, busca)
+	return [] if busca == '1' && !lista.values[0].include?('1')
 	lista.keys
 end
--- 2013/20130828 - telefone - ruby/telefone_spec.rb
+++ 2013/20130828 - telefone - ruby/telefone_spec.rb
@@ -13,5 +13,19 @@ describe "Telefone" do
       }
       busca(telefones, "").should == [:joao]
     end
+
+    it "ninguem se busca nao inclui joao" do
+      telefones = {
+        :joao => '22334455'
+      }
+      busca(telefones, "1").should == []
+    end
+
+    it "joao se busca inclui telefone do joao" do
+      telefones = {
+        :joao => '21334455'
+      }
+      busca(telefones, "1").should == [:joao]
+    end
   end
 end
