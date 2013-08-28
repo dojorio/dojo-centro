00ae57206e19d10ee7e59ca0b25ccbb26f33c7f8
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-08-28 20:28:25 -0300

    Otávio Cardoso e Carlos Figueiredo

--- 2013/20130828 - telefone - ruby/telefone.rb
+++ 2013/20130828 - telefone - ruby/telefone.rb
@@ -1,5 +1,8 @@
+def matches(lista, busca)
+	lista.select{ |nome, telefone| telefone.include?(busca) }
+end
 def busca(lista, busca)
 	return [] if lista.values.empty?
 
-	return lista.select{ |nome, telefone| telefone.include?(busca) }.sort_by{|t| t[1].index(busca)}.map{|k,v| k}
+	return matches(lista,busca).sort_by{|t| t[1].index(busca)}.map{|k,v| k}
 end
