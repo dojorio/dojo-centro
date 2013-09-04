1ffbe772afc0df6f5bee02b40f3e958d37316569
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-09-04 19:40:22 -0300

    Jacqueline Abreu e Juan Lopes

--- 2013/20130904 - inimigo meu - python/inimigo.py
+++ 2013/20130904 - inimigo meu - python/inimigo.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 
 class Guerra:
-	def __init__(self, *paises):
-		self.numero_de_paises = len(paises)
-		self.paises1 = []
-		self.paises2 = []
+	def __init__(self):
+		self.ladoA = set()
+		self.ladoB = set()
 
 	def sao_inimigos(self, pais1, pais2):
-		self.paises1.append(pais1)
-		self.paises2.append(pais2)
+		self.ladoA.add(pais1)
+		self.ladoB.add(pais2)
 
 	def sao_amigos(self, pais1, pais2):
-		self.paises1.append(pais1)
-		self.paises1.append(pais2)
+		self.ladoA.add(pais1)
+		self.ladoA.add(pais2)
 
 	def lados(self):
-		if self.numero_de_paises == 3:
-			return (['A', 'C'], ['B'])
-
-		return (self.paises1, self.paises2)
+		return (self.ladoA, self.ladoB)
--- 2013/20130904 - inimigo meu - python/test_inimigo.py
+++ 2013/20130904 - inimigo meu - python/test_inimigo.py
@@ -3,31 +3,36 @@ import unittest
 from inimigo import Guerra
 
 class TestInimigo(unittest.TestCase):
+	def assertLados(self, guerra, ladoA, ladoB):
+		lados = guerra.lados()
+		self.assertIn(ladoA, lados)
+		self.assertIn(ladoB, lados)
+
 	def test_dois_paises(self):
-		guerra = Guerra('A', 'B')
+		guerra = Guerra()
 		guerra.sao_inimigos('A', 'B')
-		self.assertEqual((['A'],['B']), guerra.lados())
+		self.assertLados(guerra, {'A'}, {'B'})
 
 	def test_dois_paises2(self):
-		guerra = Guerra('A', 'C')
+		guerra = Guerra()
 		guerra.sao_inimigos('A', 'C')
-		self.assertEqual((['A'],['C']), guerra.lados())
+		self.assertLados(guerra, {'A'}, {'C'})
 
 	def test_dois_paises_amigos(self):
-		guerra = Guerra('A', 'C')
+		guerra = Guerra()
 		guerra.sao_amigos('A', 'C')
-		self.assertEqual((['A', 'C'],[]), guerra.lados())
+		self.assertLados(guerra, {'A', 'C'}, set())
 
 	def test_tres_paises(self):
-		guerra = Guerra('A', 'B', 'C')
+		guerra = Guerra()
 		guerra.sao_inimigos('A','B')
 		guerra.sao_amigos('A', 'C')
-		self.assertEqual((['A', 'C'], ['B']), guerra.lados())
+		self.assertLados(guerra, {'A', 'C'}, {'B'})
 
 	def test_tres_paises2(self):
-		guerra = Guerra('A', 'B', 'C')
+		guerra = Guerra()
 		guerra.sao_inimigos('A','C')
 		guerra.sao_amigos('A', 'B')
-		self.assertEqual((['A', 'B'], ['C']), guerra.lados())
+		self.assertLados(guerra, {'A', 'B'}, {'C'})
 
 unittest.main()
