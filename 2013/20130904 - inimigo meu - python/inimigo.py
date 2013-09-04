f49e164bc668d8b93818ce8ac1abe99c4d8467f9
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-09-04 20:05:22 -0300

    Juan Lopes e Lucas Sampaio

--- 2013/20130904 - inimigo meu - python/inimigo.py
+++ 2013/20130904 - inimigo meu - python/inimigo.py
@@ -6,30 +6,20 @@ class Guerra:
 		self.ladoB = set()
 
 	def sao_inimigos(self, pais1, pais2):
-		if   pais1 in self.ladoA:
-			self.ladoB.add(pais2)
-		elif pais1 in self.ladoB:
-			self.ladoA.add(pais2)
-		elif pais2 in self.ladoA:
-			self.ladoB.add(pais1)
-		elif pais2 in self.ladoB:
+		if pais1 in self.ladoA or pais2 in self.ladoB:
 			self.ladoA.add(pais1)
+			self.ladoB.add(pais2)
 		else:
-			self.ladoA.add(pais1)
-			self.ladoB.add(pais2)
+			self.ladoB.add(pais1)
+			self.ladoA.add(pais2)
 
 	def sao_amigos(self, pais1, pais2):
-		if   pais1 in self.ladoA:
-			self.ladoA.add(pais2)
-		elif pais1 in self.ladoB:
-			self.ladoB.add(pais2)
-		elif pais2 in self.ladoA:
+		if pais1 in self.ladoA or pais2 in self.ladoA:
 			self.ladoA.add(pais1)
-		elif pais2 in self.ladoB:
-			self.ladoB.add(pais1)
+			self.ladoA.add(pais2)
 		else:
-			self.ladoA.add(pais1)
-			self.ladoA.add(pais2)
+			self.ladoB.add(pais1)
+			self.ladoB.add(pais2)
 
 	def lados(self):
 		return (self.ladoA, self.ladoB)
