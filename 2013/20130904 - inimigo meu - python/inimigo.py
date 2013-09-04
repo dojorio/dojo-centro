5afd318b2b057b6c2fe0cce6882f7652128dbb67
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-09-04 19:34:20 -0300

    Juan Lopes e Carlos Cunha

--- 2013/20130904 - inimigo meu - python/inimigo.py
+++ 2013/20130904 - inimigo meu - python/inimigo.py
@@ -3,17 +3,19 @@
 class Guerra:
 	def __init__(self, *paises):
 		self.numero_de_paises = len(paises)
+		self.paises1 = []
+		self.paises2 = []
 
 	def sao_inimigos(self, pais1, pais2):
-		self.pais1 = [pais1]
-		self.pais2 = [pais2]
+		self.paises1.append(pais1)
+		self.paises2.append(pais2)
 
 	def sao_amigos(self, pais1, pais2):
-		self.pais1 = [pais1, pais2]
-		self.pais2 = []
+		self.paises1.append(pais1)
+		self.paises1.append(pais2)
 
 	def lados(self):
 		if self.numero_de_paises == 3:
 			return (['A', 'C'], ['B'])
 
-		return (self.pais1, self.pais2)
+		return (self.paises1, self.paises2)
