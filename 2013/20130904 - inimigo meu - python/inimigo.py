41f97a1e98de85835064807555ad2f3d013f0591
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-09-04 20:12:00 -0300

    Otávio Cardoso e Juan Lopes

--- 2013/20130904 - inimigo meu - python/inimigo.py
+++ 2013/20130904 - inimigo meu - python/inimigo.py
@@ -2,10 +2,12 @@
 
 class Guerra:
 	def __init__(self):
-		self.ladoA = set()
-		self.ladoB = set()
+		self.lados = []
 
 	def sao_inimigos(self, pais1, pais2):
+		self.lados.append({pais1})
+		self.lados.append({pais2})
+
 		if pais1 in self.ladoA or pais2 in self.ladoB:
 			self.ladoA.add(pais1)
 			self.ladoB.add(pais2)
@@ -14,6 +16,8 @@ class Guerra:
 			self.ladoA.add(pais2)
 
 	def sao_amigos(self, pais1, pais2):
+		self.lados.append({pais1, pais2})
+
 		if pais1 in self.ladoA or pais2 in self.ladoA:
 			self.ladoA.add(pais1)
 			self.ladoA.add(pais2)
