088611b9ad193ffd9a00dab39f775f496a2093bd
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-09-04 20:33:39 -0300

    miguel Cacilhinhas e Lucas Sampaio

--- 2013/20130904 - inimigo meu - python/inimigo.py
+++ 2013/20130904 - inimigo meu - python/inimigo.py
@@ -15,4 +15,15 @@ class Guerra:
 		self.relacoes.append(('amigo', pais1, pais2))
 
 	def lados(self):
+		ladoA = set()
+		ladoB = set()
 
+		for relacao, pais1, pais2 in self.relacoes:
+			if relacao == 'amigo':
+				ladoA.add(pais1)
+				ladoA.add(pais2)
+			else:
+				ladoA.add(pais1)
+				ladoB.add(pais2)
+
+		return ladoA, ladoB
