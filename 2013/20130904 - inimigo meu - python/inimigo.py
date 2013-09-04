f51bcb308838c4c5eb8ab10a336edb06f83adc6b
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-09-04 20:50:33 -0300

    Juan Lopes e Otávio Cardoso

--- 2013/20130904 - inimigo meu - python/inimigo.py
+++ 2013/20130904 - inimigo meu - python/inimigo.py
@@ -5,14 +5,25 @@ class Guerra:
 		self.ladoA = set()
 		self.ladoB = set()
 		self.relacoes = []
-
+		self.panelinhas = {}
 
 	def sao_inimigos(self, pais1, pais2):
 		self.relacoes.append(('inimigo', pais1, pais2))
 
-
 	def sao_amigos(self, pais1, pais2):
 		self.relacoes.append(('amigo', pais1, pais2))
+		self.unir(pais1, pais2)
+
+	def unir(self, pais1, pais2):
+		if pais1 not in self.panelinhas:
+			self.panelinhas[pais1] = {pais1}
+		if pais2 not in self.panelinhas:
+			self.panelinhas[pais2] = {pais2}
+
+		unidos = self.panelinhas[pais1].union(self.panelinhas[pais2])
+
+		for pais in unidos:
+			self.panelinhas[pais] = unidos
 
 	def lados(self):
 		ladoA = set()
