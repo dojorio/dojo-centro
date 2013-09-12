79f4d2cc4e81fd241f6ef549f86fead1fff722ed
Dojo Iparos <iparos@iparos-OptiPlex-360.(none)>
2013-09-11 21:03:52 -0300

    Israel Teixeira e Alexandre Vaz

--- 2013/20130911 - antivirus - python/antivirus.py
+++ 2013/20130911 - antivirus - python/antivirus.py
@@ -1,8 +1,7 @@
 def norton(input_file, viruses):
-
-    founded_virus = ['aba']
-    for char_index in range(len(input_file)):
-
+    if input_file == 'aba':
+        for char_index in range(len(input_file)):
+            if input_file[char_index:].startswith(viruses[0])     
     result = [char for char in input_file if char in viruses ]
     
     return 
