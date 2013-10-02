7fe6699f1c93cc760f16f3b5ba336553f5940208
DojoRio <contato@dojorio.org>
2013-10-02 20:20:50 -0300

    Israel Teixeira and Flávio Amieiro

--- 2013/20131002 - containeres - python/containeres.py
+++ 2013/20131002 - containeres - python/containeres.py
@@ -1,10 +1,17 @@
 def quantos_movimentos(patio):
     gabarito = sorted(patio)
-    movimentos = 0    
-    while patio != gabarito:
-        for i in range(len(patio)-1):
-            if patio[i] > patio[i+1]:
-                movimentos += 1
-                patio[i], patio[i+1] = patio[i+1], patio[i]
+    movimentos = 0
+    for i, container in enumerate(patio):
+        certo = gabarito[i]
+        if container != certo:
+            movimentos += 1
+            i_do_certo = patio.index(certo)
+            patio[i], patio[i_do_certo] = patio[i_do_certo], patio[i]
+
+
+    if patio == sorted(patio):
+        return 0
+    return patio[:-1] + 1        
+
 
     return movimentos
