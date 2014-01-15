a198d99b7d2561895e1ff90949a3b10f45e82d0b
DojoRio <issae@issae.com>
2014-01-15 19:46:00 -0200

    Juan Lopes and tomás Lima

--- 2014/20140115 - queue - python/queue.py
+++ 2014/20140115 - queue - python/queue.py
@@ -1,4 +1,5 @@
-def salim(estacionamento):
-    if len(estacionamento) == 2:
-        return 2
-    return len(estacionamento[0])
+def salim(grid):
+    if grid[0][-1] == '#' or grid[-1][0] == '#':
+        return 1
+    else:
+        return len(grid[0]) * len(grid)
--- 2014/20140115 - queue - python/test_queue.py
+++ 2014/20140115 - queue - python/test_queue.py
@@ -15,4 +15,17 @@ class TestQueue(unittest.TestCase):
                           '.']
         self.assertEquals(salim(estacionamento), 2)
 
+    def test_1x2_com_obstaculo(self):
+        estacionamento = ['.#']
+        self.assertEquals(salim(estacionamento), 1)
+
+    def test_2x1_com_obstaculo(self):
+        estacionamento = ['.',
+                          '#']
+        self.assertEquals(salim(estacionamento), 1)
+
+    def test_1x3_com_obstaculo(self):
+        estacionamento = ['.#.']
+        self.assertEquals(salim(estacionamento), 1)
+
 unittest.main()
