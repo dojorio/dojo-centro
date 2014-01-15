88fd09dc2b5b3ec75eba43b1dbd4eb0c6fe2fc8a
DojoRio <issae@issae.com>
2014-01-15 20:59:59 -0200

    Carlos Figueiredo and Otávio Cardoso

--- 2014/20140115 - queue - python/queue.py
+++ 2014/20140115 - queue - python/queue.py
@@ -9,7 +9,7 @@ def salim(grid):
     andou = encontra(grid[0], str.find)
 
     if len(grid) > 1:
-        andou += encontra(grid[1], str.rfind)
+        andou += encontra(grid[1][::-1], str.find)
 
     if len(grid) > 2:
         andou += encontra(grid[2], str.find)
