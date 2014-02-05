6422dedf7be4b2b52ddbc78a105c5ed4ad353c5b
DojoRio <issae@issae.com>
2014-02-05 20:43:12 -0200

    Jacqueline Abreu e Mariana Bedran

--- 2014/20140205 - formiga - python/formiga.py
+++ 2014/20140205 - formiga - python/formiga.py
@@ -1,8 +1,20 @@
+import math
+
 def onde_está(tempo):
-    if tempo >= 9:
-        return (tempo-9, 3)
-    elif tempo >= 6:
-        return (8-tempo, 2)
-    x = tempo // 2     
-    y = (tempo + 1) //2 % 2 
-    return (x  , y)
+	quadrado = math.sqrt(tempo)
+	if str(quadrado).isdigit():
+		if tempo % 2 == 0:
+			y = 0
+			x = quadrado
+		else:
+			x = 0
+			y = quadrado
+	else:
+
+	    if tempo >= 9:
+	        return (tempo-9, 3)
+	    elif tempo >= 6:
+	        return (8-tempo, 2)
+	    x = tempo // 2     
+	    y = (tempo + 1) //2 % 2 
+	    return (x  , y)
