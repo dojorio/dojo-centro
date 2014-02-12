42cd43060675d649c837fcb0114966957301226f
DojoRio <issae@issae.com>
2014-02-12 20:28:07 -0200

    Israel Teixeira e Flávio Amieiro

--- 2014/20140212 - roleta - javascript/roleta.js
+++ 2014/20140212 - roleta - javascript/roleta.js
@@ -1,8 +1,15 @@
 roleta = function(casas, bolinhas){
     dupla1  = casas[0] + casas[1]
     dupla2 = casas[1] + casas[2]
+    dupla3 = casas[0] + casas[2]
     bola = bolinhas[0]
-    return Math.abs(Math.min(dupla1*bola, dupla2*bola))
+
+    possibilidades = [
+        dupla1*bola, 
+        dupla2*bola, 
+        dupla3*bola
+    ]
+    return Math.abs(possibilidades.reduce(Math.min, -9999))
 }
 
 module.exports = roleta;
--- 2014/20140212 - roleta - javascript/test/test_roleta.js
+++ 2014/20140212 - roleta - javascript/test/test_roleta.js
@@ -4,13 +4,13 @@ var roleta = require('../roleta');
 describe('Roleta', function(){
     context('3 casas 1 bolinha', function(){
         it('tudo 0', function(){
-            casas = [0,0,0]
+            casas = [0, 0, 0]
             bolinhas = [0]
             assert.equal(roleta(casas, bolinhas), 0);
         });
 
         it('uma casa 1 bolinha -1', function(){
-            casas = [1,0,0]
+            casas = [1, 0, 0]
             bolinhas = [-1]
             assert.equal(roleta(casas, bolinhas), 1);
         });
@@ -19,6 +19,12 @@ describe('Roleta', function(){
             casas = [-1, 0, 0]
             bolinhas = [-1]
             assert.equal(roleta(casas, bolinhas), 0)
+        })
+
+        it('ultima casa fechando com a primeira', function() {
+            casas = [1, 0, 1]
+            bolinhas = [-1]
+            assert.equal(roleta(casas, bolinhas), 2)
         })
     })
     
