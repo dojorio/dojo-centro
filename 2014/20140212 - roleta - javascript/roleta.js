ccd2aaea635b2fd37f13a67d51c0178551c93a6a
DojoRio <issae@issae.com>
2014-02-12 21:04:31 -0200

    Carlos Figueiredo e Otávio Cardoso

--- 2014/20140212 - roleta - javascript/roleta.js
+++ 2014/20140212 - roleta - javascript/roleta.js
@@ -1,13 +1,15 @@
 roleta = function(casas, bolinhas){
-    var duplas = [];
+    var duplas = [[casas[casas.length - 1], casas[0]]];
+
     for(var i = 0; i < casas.length - 1; i++){
         duplas.push([casas[i], casas[i + 1]])
     }
-    duplas.push([casas[i], casas[0]])
 
     bola = bolinhas[0]
 
-    possibilidades = duplas.map(function(a, b){ return (a + b) * bola;};
+    possibilidades = duplas.map(function(dupla){
+        return (dupla[0] + dupla[1]) * bola;
+    });
 
     return Math.abs(Math.min.apply(this, possibilidades))
 }
