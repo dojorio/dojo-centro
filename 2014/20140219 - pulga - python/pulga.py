0ae6e3618e35bc7b9f9ea6b51c1a7cb1e6d94c22
DojoRio <issae@issae.com>
2014-02-19 19:43:36 -0300

    Carlos Cunha e Otávio Cardoso

--- 2014/20140219 - pulga - python/pulga.py
+++ 2014/20140219 - pulga - python/pulga.py
@@ -1,4 +1,4 @@
 def pulga(s, x, y, dx, dy):
-    if x+y==10: 
+    if 10 in (x, y):
         return 1
     return 0
--- 2014/20140219 - pulga - python/pulga_test.py
+++ 2014/20140219 - pulga - python/pulga_test.py
@@ -5,7 +5,7 @@ class TestPulga(unittest.TestCase):
     def test_ja_esta_na_casa_branca(self):
         s = 10
         x, y = 11, 1
-        dx, dy = 123, 456
+        dx, dy = 1, 1
         self.assertEqual(0, pulga(s, x, y, dx, dy))
 
     def test_falta_1mm_para_chegar_na_casa_branca(self):
@@ -17,6 +17,18 @@ class TestPulga(unittest.TestCase):
     def test_falta_1mm_no_x_para_chegar_na_casa_branca(self):
         s = 10
         x, y = 10, 5
+        dx, dy = 1, 1
+        self.assertEqual(1, pulga(s, x, y, dx, dy))
+
+    def test_falta_1mm_no_y_para_chegar_na_casa_branca(self):
+        s = 10
+        x, y = 5, 10
+        dx, dy = 1, 1
+        self.assertEqual(1, pulga(s, x, y, dx, dy))
+
+    def test_falta_1mm_em_y_com_x_30_para_chegar_na_casa_branca(self):
+        s = 10
+        x, y = 5, 10
         dx, dy = 1, 1
         self.assertEqual(1, pulga(s, x, y, dx, dy))
 
