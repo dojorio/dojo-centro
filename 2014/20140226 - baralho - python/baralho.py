62fa40e67010c1d86a2a21e1c0925ff87933ea6d
DojoRio <issae@issae.com>
2014-02-26 20:01:25 -0300

    Tino Gomes e Julio Marins

--- 2014/20140226 - baralho - python/baralho.py
+++ 2014/20140226 - baralho - python/baralho.py
@@ -1,11 +1,8 @@
 def baralho(n):
     descartadas = list(range(1, n + 1, 2))
-    if n == 4:
-        descartadas.append(2)
-    if n == 3:
-        return ([1, 3], 2)
-    if n == 2:
-        descartadas.append(2)
-        return (descartadas, 2)
-    return (descartadas, 1)
+
+    if n == 1:
+        return ([],1)
+    
+    return (descartadas,n)
 
