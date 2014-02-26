c94292d53168c1c16da09a9518342ac663f3651c
DojoRio <issae@issae.com>
2014-02-26 20:39:04 -0300

    Carlos Cunha e Tomás Lima

--- 2014/20140226 - baralho - python/baralho.py
+++ 2014/20140226 - baralho - python/baralho.py
@@ -1,36 +1,14 @@
 def baralho(tamanho_baralho):
-
     deck = list(range(1, tamanho_baralho + 1))
     cemiterio = []
+    impar = True
 
+    while len(deck) > 1:
+        primeiro = deck.pop(0)
+        if impar:
+            cemiterio.append(primeiro)
+        else:
+            deck.append(primeiro)
+        impar = not impar
 
-    for i in range(n):
-
-        if len(deck) == 1:
-            resto = 
-
-        if deck[i] % 2 != 0:
-
-            cemiterio.append(deck[i])
-        else
-            deck.append(deck[i])
-
-    return cemiterio, 
-
-
-
-
-
-
-    # if tamanho_baralho == 1:
-    #     return ([],1)
-
-    # descartadas = list(range(1, tamanho_baralho + 1, 2))
-
-    # if tamanho_baralho == 4:
-    #     descartadas.append(2)
-    
-    # n = max(range(0, tamanho_baralho + 1, 2))
-
-    # return (descartadas, n)
-
+    return (cemiterio, deck[0])
