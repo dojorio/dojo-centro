d27ea045080308fe68f5aa7a326bdfb0df0a673b
DojoRio <issae@issae.com>
2014-03-26 21:17:39 -0300

    Jacqueline Abreu e Carlos Cunha

--- 2014/20140326 - tartaruga - ruby/tartaruga.rb
+++ 2014/20140326 - tartaruga - ruby/tartaruga.rb
@@ -14,12 +14,20 @@ def pega_tartaruga(tratador, tartaruga)
   tempo_x = x(tartaruga) - x(tratador)
   tempo_y = y(tartaruga) - y(tratador)
 
-  if tempo_x < 0 && direcao(tartaruga) == 'D'
-    tempo_x = (tempo_x.abs + 2) / 3
+  if tempo_x < 0 
+    if direcao(tartaruga) == 'D'
+      tempo_x = (tempo_x.abs + 2) / 3
+    else 
+      tempo_x = (tempo_x.abs + 1) / 2
+    end
   end
 
-  if tempo_y < 0 && direcao(tartaruga) == 'C'
-    tempo_y = (tempo_y.abs + 2) / 3
+  if tempo_y < 0 
+    if direcao(tartaruga) == 'C'
+      tempo_y = (tempo_y.abs + 2) / 3
+    else 
+      tempo_y = (tempo_y.abs + 1) / 2
+    end
   end
 
   return tempo_x.abs + tempo_y.abs
