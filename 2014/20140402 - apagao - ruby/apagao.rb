7117036f5c358b83cf4e5582988a4c83732a5659
DojoRio <issae@issae.com>
2014-04-02 20:04:24 -0300

    Carlos Figueiredo e Otavio Cardoso

--- 2014/20140402 - apagao - ruby/apagao.rb
+++ 2014/20140402 - apagao - ruby/apagao.rb
@@ -1,5 +1,14 @@
 def apagao(ruas)
-  if ruas.count < 3 
+  vertices = []
+
+  for rua in ruas
+    vertices.push(rua[0])
+    vertices.push(rua[1])
+  end
+
+  vertices = vertices.uniq
+
+  if ruas.count <= vertices.count - 1
     return 0
   end
 
--- 2014/20140402 - apagao - ruby/apagao_spec.rb
+++ 2014/20140402 - apagao - ruby/apagao_spec.rb
@@ -1,9 +1,16 @@
 require './apagao'
 
 describe "Apagão" do
-  it 'dois pontos com uma rua' do
-    ruas = [[0, 1, 1]]
-    expect(apagao(ruas)).to eq(0)
+  context 'dois pontos' do
+    it 'com uma rua' do
+      ruas = [[0, 1, 1]]
+      expect(apagao(ruas)).to eq(0)
+    end
+
+    it 'com duas ruas iguais' do
+      ruas = [[0, 1, 1], [0, 1, 1]]
+      expect(apagao(ruas)).to eq(1)
+    end
   end
 
   context 'três pontos' do
@@ -19,11 +26,6 @@ describe "Apagão" do
 
     it 'tres ruas uma maior' do
       ruas = [[0, 1, 1], [0, 2, 1], [1, 2, 2]]
-      expect(apagao(ruas)).to eq(2)
-    end
-
-    it 'quatro ruas mesmo tamanho' do
-      ruas = [[0, 1, 1], [0, 2, 1], [1, 2, 1],[0, 2, 1]]
       expect(apagao(ruas)).to eq(2)
     end
   end
