55c4e2ce0a5b0648c888175edc9fcba13c37262b
DojoRio <issae@issae.com>
2014-04-09 20:49:33 -0300

    Rodolfo Carvalho e Juan Lopes

--- 2014/20140409 - hurry up - python/hurry.py
+++ 2014/20140409 - hurry up - python/hurry.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 def tempo(jogador, chegada):
     jx, jy, jv = jogador
     cx, cy = chegada
@@ -7,6 +9,20 @@ def distancia(x1, y1, x2, y2):
     return ((x1 - x2)**2 + (y1 - y2)**2)**.5
 
 def hurry(jogadores, chegadas):
-    
-    return max(min(tempo(jogador, chegada) for chegada in chegadas)
-                                           for jogador in jogadores)
+    arranjos = permutations(chegadas,len(jogadores))
+
+    min_global = 10000
+    for arranjo in arranjos:
+        max_local = 0
+        for jogador, chegada in zip(jogadores, arranjo):
+            max_local = max(max_local, tempo(jogador, chegada))
+
+        min_global = min(min_global, max_local)
+    return min_global
+
+
+    return min(max(tempo(jogador,chegada) 
+        for jogador,chegada in zip(jogadores,arranjo)
+
+        ) for arranjo in arranjos
+        )
