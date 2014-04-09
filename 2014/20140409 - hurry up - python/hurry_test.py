8bddc754559e86955ac6bf312d6acbbf2b4884cd
DojoRio <issae@issae.com>
2014-04-09 20:11:38 -0300

    Rodolfo Carvalho e Julio Marins

--- 2014/20140409 - hurry up - python/hurry_test.py
+++ 2014/20140409 - hurry up - python/hurry_test.py
@@ -36,9 +36,21 @@ class TestHurry(unittest.TestCase):
         chegadas = [(2, 2), (1, 1)]
         self.assertEqual(2**.5, hurry.hurry(jogadores, chegadas))
 
-    def test_2_jogadores_2_saídas(self):
+    def test_2_jogadores_2_saídas_uma_dimensao(self):
+        jogadores = [(0, 0, 1), (3, 0, 1)] #x, y, velocidade
+        chegadas = [(2, 0), (4, 0)]
+        self.assertEqual(2, hurry.hurry(jogadores, chegadas))
+
+    def test_2_jogadores_2_saídas_diagonal(self):
         jogadores = [(0, 0, 1), (3, 3, 1)] #x, y, velocidade
         chegadas = [(2, 2), (1.5, 1.5)]
-        self.assertEqual((3*2**.5)/2, hurry.hurry(jogadores, chegadas))
+        self.assertAlmostEqual((3*2**.5)/2, hurry.hurry(jogadores, chegadas))
+
+    def test_2_jogadores_2_saídas_rápido_perto_lento_longe(self):
+        jogadores = [(0, 0, 3), (0, 2, 1)] #x, y, velocidade
+        chegadas = [(0, 1), (0, 4)]
+        self.assertAlmostEqual((3*2**.5)/2, hurry.hurry(jogadores, chegadas))
+
+
 
 unittest.main()
