581d0f8afc7cec5be2d8af704df41d5f96b0600a
DojoRio <issae@issae.com>
2014-04-09 20:36:56 -0300

    Julio Marins e Jacqueline Abreu

--- 2014/20140409 - hurry up - python/hurry.py
+++ 2014/20140409 - hurry up - python/hurry.py
@@ -7,5 +7,6 @@ def distancia(x1, y1, x2, y2):
     return ((x1 - x2)**2 + (y1 - y2)**2)**.5
 
 def hurry(jogadores, chegadas):
+    
     return max(min(tempo(jogador, chegada) for chegada in chegadas)
                                            for jogador in jogadores)
