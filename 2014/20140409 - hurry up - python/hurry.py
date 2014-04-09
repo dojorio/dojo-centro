5d3bc5522c0b86eee8f43d534a8458cffd51dc5c
DojoRio <issae@issae.com>
2014-04-09 19:49:51 -0300

    Carlos Figueiredo e Juan Lopes

--- 2014/20140409 - hurry up - python/hurry.py
+++ 2014/20140409 - hurry up - python/hurry.py
@@ -1,8 +1,7 @@
 def hurry(jogadores, chegadas):
     jx, jy, jv = jogadores[0]
-    menor = 1000
-    for cx, cy in chegadas:
-        distancia = ((jx - cx)**2 + (jy - cy)**2)**.5
-        tempo = distancia/jv
-        menor = min (menor, tempo)
-    return menor
+    def distancia(x, y):
+        return ((jx - x)**2 + (jy - y)**2)**.5
+    
+    return min(distancia(x, y)/jv for x, y in chegadas)
+    
