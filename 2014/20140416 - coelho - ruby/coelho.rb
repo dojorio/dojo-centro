1ab338ec6323b1c0d12e695151c26b14507ff76f
DojoRio <issae@issae.com>
2014-04-16 20:24:52 -0300

    Israel Teixeira e Otavio Cardoso

--- 2014/20140416 - coelho - ruby/coelho.rb
+++ 2014/20140416 - coelho - ruby/coelho.rb
@@ -1,19 +1,17 @@
 def coelho(ovos, velocidade)
   velocidade_atual = velocidade / 2
+  tempo_restante = 720
   qnt_de_ovos = 0
 
-  if ovos.size == 2
-    ovos.each do |ovo|
-      velocidade_atual = (ovo[1] / velocidade_atual)
-      return 1 if velocidade_atual < 720
-      qnt_de_ovos = qnt_de_ovos + 1 
+  ovos.each do |ovo|
+    distancia = ovo[1]
+    tempo_para_pegar_o_ovo = (distancia / velocidade_atual)
+
+    if tempo_para_pegar_o_ovo < tempo_restante
+      qnt_de_ovos += 1
+      tempo_restante -= tempo_para_pegar_o_ovo
     end
-    return qnt_de_ovos
   end
-  y = ovos[0][1]
-  if y / velocidade_atual < 720
-    1
-  else
-    0
-  end
+
+  qnt_de_ovos
 end
