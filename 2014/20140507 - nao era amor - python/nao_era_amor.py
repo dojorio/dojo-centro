0a6c9422aba797bde6ca22d0c0c274a0424d854d
DojoRio <issae@issae.com>
2014-05-07 21:01:47 -0300

    Julio Marins e Tino Gomes

--- 2014/20140507 - nao era amor - python/nao_era_amor.py
+++ 2014/20140507 - nao era amor - python/nao_era_amor.py
@@ -5,21 +5,22 @@ MULTIPLICADORES = {
 }
 
 def aplica_padrao(linha, padrão):
-  resultado = 0
-  for numero, operador in zip(linha, padrão):
-    resultado += numero * MULTIPLICADORES[operador]
-  return resultado
+    resultado = 0
+    for numero, operador in zip(linha, padrão):
+        resultado += numero * MULTIPLICADORES[operador]
+    return resultado
 
 def cilada(tabuleiro, padrões):
-  resultados_possiveis = []
-  último_padrão = None
+    resultados_possiveis = []
+    último_padrão = None
 
-  for linha in tabuleiro:
-    for padrão in padrões:
-      if padrão != último_padrão:
-        resultado = aplica_padrao(linha, padrão)
-        resultados_possiveis.append(resultado)
-      último_padrão = padrão
+    for linha in tabuleiro:
+        for padrão in padrões:
+            if padrão != último_padrão:
+                resultado = aplica_padrao(linha, padrão)
+                resultados_possiveis.append(resultado)
+
+            último_padrão = padrão
 
 
-  return max(resultados_possiveis)
+    return max(resultados_possiveis)
