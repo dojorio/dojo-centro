97136575bb50b56f670aef95770487a89c53947f
DojoRio <issae@issae.com>
2014-05-14 20:06:56 -0300

    Otavio Cardoso e Julio Marins

--- 2014/20140514 - game of life - python/game_of_life.py
+++ 2014/20140514 - game of life - python/game_of_life.py
@@ -1,6 +1,7 @@
 def game_of_life(board):
     if len(board[0]) == 3:
-        resultado = [1, 1, 1]
+        resultado = [[1, 1, 1]]
+
         for idx, cell in enumerate(board[0]):
             if idx == 0:
                 previous = 0
@@ -14,8 +15,8 @@ def game_of_life(board):
 
             if cell == 1:
                 if previous + next < 2:
-                    board[0][idx] = 0
-
-        return board
+                    resultado[0][idx] = 0
+           
+        return resultado
 
     return [[0] * len(board[0])]
