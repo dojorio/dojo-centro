26f59362e457b2f2ce57547be45063f76574e271
DojoRio <issae@issae.com>
2014-05-14 21:34:05 -0300

    Israel Teixeira e Cláudio Berrondo

--- 2014/20140514 - game of life - python/game_of_life.py
+++ 2014/20140514 - game of life - python/game_of_life.py
@@ -1,8 +1,9 @@
 import copy
 
 
-POSITIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0),
-             (1, 1)]
+POSITIONS = [(-1, -1), (-1, 0), (-1, 1),
+             (0, -1),           (0, 1),
+             (1, -1),  (1, 0),  (1, 1)]
 
 def neighbors(board, x, y):
     neighbors = 0
@@ -13,27 +14,6 @@ def neighbors(board, x, y):
         except IndexError:
             pass
     return neighbors
-
-    if x > 0:
-        neighbors.append(board[x-1][y])
-        neighbors.append(board[x-1][y-1])
-        neighbors.append(board[x-1][y+1])
-        neighbors.append(board[x][y-1])
-
-    if y > 0 and x > 0:
-        neighbors.append(board[x-1][y])
-        neighbors.append(board[x-1][y-1])
-        neighbors.append(board[x-1][y+1])
-        neighbors.append(board[x][y-1])
-    if y < len(board[x]) - 1:
-        neighbors.append(board[x][y+1])
-        neighbors.append(board[x-1][y-1])
-
-
-
-        neighbors.append()
-
-    return sum(neighbors)
 
 ALIVE = 1
 DEAD = 0
