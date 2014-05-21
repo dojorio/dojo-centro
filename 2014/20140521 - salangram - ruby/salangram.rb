1f99144f20cdd8b7b7b1a11c50a707e15f912ce7
DojoRio <issae@issae.com>
2014-05-21 19:56:17 -0300

    Carlos Cunha e Julio Marins

--- 2014/20140521 - salangram - ruby/salangram.rb
+++ 2014/20140521 - salangram - ruby/salangram.rb
@@ -1,10 +1,19 @@
 def salangram(salao, largura, comprimentos)
-  area_tabua = largura / 100 * comprimentos[0]
+  
   area_salao = salao[0] * salao[1]
 
-  if(area_salao == area_tabua)
-    1
-  else
-   'impossivel'
+  areas_tabuas = []
+  largura /= 100
+  for tabua in comprimentos
+    areas_tabuas.push(largura * tabua)
   end
+
+  for tabua in areas_tabuas 
+    if(area_salao == tabua)
+      1
+    else
+     'impossivel'
+    end
+  end
+
 end
