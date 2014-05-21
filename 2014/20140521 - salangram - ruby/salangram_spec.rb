b829616f20bceb18cee2c84a90585285b7e494cb
DojoRio <issae@issae.com>
2014-05-21 19:26:45 -0300

    Carlos Figueiredo e Julio Marins

--- 2014/20140521 - salangram - ruby/salangram_spec.rb
+++ 2014/20140521 - salangram - ruby/salangram_spec.rb
@@ -8,4 +8,12 @@ describe 'Salangram' do
 
     expect(salangram(salao, largura, comprimentos)).to eq(1)
   end
+
+  it 'salao 1mx2m 1 tauba 100cmx1m' do
+    salao = [1, 2]
+    largura = 100
+    comprimentos = [1]
+
+    expect(salangram(salao, largura, comprimentos)).to eq('impossivel')
+  end
 end
