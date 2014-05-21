2876550b6b078e35483dcd1839246a5135f887ce
DojoRio <issae@issae.com>
2014-05-21 19:50:31 -0300

    Julio Marins e Otavio Cardoso

--- 2014/20140521 - salangram - ruby/salangram.rb
+++ 2014/20140521 - salangram - ruby/salangram.rb
@@ -1,10 +1,10 @@
 def salangram(salao, largura, comprimentos)
-  
-  if(salao == [1,1] || comprimentos == [2])
-    1 
+  area_tabua = largura / 100 * comprimentos[0]
+  area_salao = salao[0] * salao[1]
+
+  if(area_salao == area_tabua)
+    1
   else
-   'impossivel' 
-  end 
-
-
+   'impossivel'
+  end
 end
--- 2014/20140521 - salangram - ruby/salangram_spec.rb
+++ 2014/20140521 - salangram - ruby/salangram_spec.rb
@@ -32,4 +32,21 @@ describe 'Salangram' do
 
     expect(salangram(salao, largura, comprimentos)).to eq(1)
   end
+
+  it 'salao 1mx1m 1 tabua 100cmx2m' do
+    salao = [1, 1]
+    largura = 100
+    comprimentos = [2]
+
+    expect(salangram(salao, largura, comprimentos)).to eq('impossivel')
+  end
+
+  it 'salao 1mx1m 1 tabua 100cmx2m 100cmx1m' do
+    salao = [1, 1]
+    largura = 100
+    comprimentos = [2, 1]
+
+    expect(salangram(salao, largura, comprimentos)).to eq(1)
+  end
+
 end
