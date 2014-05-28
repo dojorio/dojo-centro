456cddc6b7bc3e1896a3d27a35de6469290b2afb
DojoRio <issae@issae.com>
2014-05-28 20:12:07 -0300

    Jonatas Emidio e Valeria Parajara

--- 2014/20140528 - estrela da val - ruby/estrela_da_val.rb
+++ 2014/20140528 - estrela da val - ruby/estrela_da_val.rb
@@ -1,16 +1,26 @@
 require 'prime'
 
 def estrela_da_val(pontos)
-  if Prime.prime?(pontos)
-    return pontos / 2
-  end
+  # if Prime.prime?(pontos)
+  #   return pontos / 2
+  # end
 
-  case pontos
-  when 8, 10, 12
-    2
-  when 9
-    3
+  # case pontos
+  # when 8, 10, 12
+  #   2
+  # when 9
+  #   3
+  # else
+  #   1
+  # end
+
+  if pontos < 5
+    return 1
+  elsif pontos.even?
+    return 2
+  elsif pontos == 3
+    return 6
   else
-    1
+    (pontos / (pontos-1) / 2) + 1
   end
 end
