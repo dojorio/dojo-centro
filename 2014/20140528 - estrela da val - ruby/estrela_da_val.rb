d96ba9a069d3268a1277c510b29c7ceae3d90829
DojoRio <issae@issae.com>
2014-05-28 19:43:42 -0300

    Julio Marins e Tomás Lima

--- 2014/20140528 - estrela da val - ruby/estrela_da_val.rb
+++ 2014/20140528 - estrela da val - ruby/estrela_da_val.rb
@@ -1,17 +1,14 @@
 def estrela_da_val(pontos)
   
   case pontos
-    when 5
-      return 2
-    when 7
-      return 3
-    when 8
-      return 2
-    when 9
-      return 3
-    when 10
-      return 2
-  end
+
+    # when 10
+    #   return 2
+    # else
+      for i in 1..(pontos/2)
+        i if pontos / i == 0
+      end.count
+  # end
 
   return 1 
 
