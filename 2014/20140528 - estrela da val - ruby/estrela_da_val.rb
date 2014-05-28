25b930a324a8a7d2a2d53260ae4d45206e3364ba
DojoRio <issae@issae.com>
2014-05-28 19:52:40 -0300

    Thiago Belem e Julio Marins

--- 2014/20140528 - estrela da val - ruby/estrela_da_val.rb
+++ 2014/20140528 - estrela da val - ruby/estrela_da_val.rb
@@ -1,14 +1,21 @@
 def estrela_da_val(pontos)
   
   case pontos
-
-    # when 10
-    #   return 2
-    # else
-      for i in 1..(pontos/2)
-        i if pontos / i == 0
-      end.count
-  # end
+    when 5
+      return 2
+    when 7
+      return 3
+    when 8
+      return 2
+    when 9
+      return 3
+  when 10
+    return 2
+  else
+    for i in 1..(pontos/2)
+      return i if pontos / i == 0
+    end.count
+  end
 
   return 1 
 
