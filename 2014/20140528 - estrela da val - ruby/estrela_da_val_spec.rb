46846b4ad4b010951c10696947696c18a6a47ffc
DojoRio <issae@issae.com>
2014-05-28 19:19:23 -0300

    Julio Marins e Jonatas Emidio

--- 2014/20140528 - estrela da val - ruby/estrela_da_val_spec.rb
+++ 2014/20140528 - estrela da val - ruby/estrela_da_val_spec.rb
@@ -8,4 +8,15 @@ describe 'estrela_da_val' do
   it '5 pontos' do
     expect(estrela_da_val(5)).to eq(2)
   end
+
+  it '6 pontos' do
+    expect(estrela_da_val(6)).to eq(1)
+  end
+
+  it '7 pontos' do
+    expect(estrela_da_val(7)).to eq(3)
+  end
+
+
+
 end
