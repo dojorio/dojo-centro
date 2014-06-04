e8ae363c7f704a1164ff323348ac03fab2510c24
DojoRio <issae@issae.com>
2014-06-04 19:58:25 -0300

    Julio Marins e Elias Tandel

--- 2014/20140604 - naves e tetraedros - javascript/tetraedros.js
+++ 2014/20140604 - naves e tetraedros - javascript/tetraedros.js
@@ -1,4 +1,7 @@
 module.exports.pontoAPonto = function (ponto1, ponto2) {
+    if (ponto1[1] != 0){
+        return ponto1[1]
+    }
     return ponto2[0]
 }
 
--- 2014/20140604 - naves e tetraedros - javascript/tetraedros_spec.js
+++ 2014/20140604 - naves e tetraedros - javascript/tetraedros_spec.js
@@ -16,5 +16,19 @@ describe('Tetraedros', function () {
 
             assert.equal(pontoAPonto(ponto1, ponto2), 10)
         })
+
+        it('no mesmo eixo, só no y', function () {
+            var ponto1 = [0, 1, 0],
+            ponto2 = [0, 0, 0]
+
+            assert.equal(pontoAPonto(ponto1, ponto2), 1)
+        })
+
+        it('no mesmo eixo, só no y', function () {
+            var ponto1 = [0, 0, 0],
+            ponto2 = [0, 2, 0]
+
+            assert.equal(pontoAPonto(ponto1, ponto2), 2)
+        })
     })
 })
