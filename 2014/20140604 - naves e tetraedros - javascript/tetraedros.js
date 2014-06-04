0064c9e56a5c24583ed542df250d5b1ea02f29a8
DojoRio <issae@issae.com>
2014-06-04 20:25:26 -0300

    Julio Marins e Israel Teixeira

--- 2014/20140604 - naves e tetraedros - javascript/tetraedros.js
+++ 2014/20140604 - naves e tetraedros - javascript/tetraedros.js
@@ -1,6 +1,4 @@
 module.exports.pontoAPonto = function (ponto1, ponto2) {
-
-
     return Math.abs(ponto2[0] - ponto1[0]) ||
            Math.abs(ponto2[1] - ponto1[1]) ||
            Math.abs(ponto2[2] - ponto1[2])
--- 2014/20140604 - naves e tetraedros - javascript/tetraedros_spec.js
+++ 2014/20140604 - naves e tetraedros - javascript/tetraedros_spec.js
@@ -4,67 +4,71 @@ var tetraedros = require('./tetraedros');
 describe('Tetraedros', function () {
     describe('distancias ponto a ponto', function () {
         var pontoAPonto = tetraedros.pontoAPonto
-        it('no mesmo eixo', function () {
-            var ponto1 = [0, 0, 0],
-            ponto2 = [1, 0, 0]
+        describe('no mesmo eixo', function(){
+            context('no eixo x', function(){
+                it('0 e 1', function () {
+                    var ponto1 = [0, 0, 0],
+                    ponto2 = [1, 0, 0]
 
-            assert.equal(pontoAPonto(ponto1, ponto2), 1)
+                    assert.equal(pontoAPonto(ponto1, ponto2), 1)
+                })
+                it('só que mais longe', function () {
+                    var ponto1 = [0, 0, 0],
+                    ponto2 = [10, 0, 0]
+
+                    assert.equal(pontoAPonto(ponto1, ponto2), 10)
+                })
+            })
+
+            it('no mesmo eixo, só no y', function () {
+                var ponto1 = [0, 1, 0],
+                ponto2 = [0, 0, 0]
+
+                assert.equal(pontoAPonto(ponto1, ponto2), 1)
+            })
+
+            it('no mesmo eixo, só no y no ponto2', function () {
+                var ponto1 = [0, 0, 0],
+                ponto2 = [0, 2, 0]
+
+                assert.equal(pontoAPonto(ponto1, ponto2), 2)
+            })
+
+            it('no mesmo eixo, só no z', function () {
+                var ponto1 = [0, 0, 10],
+                ponto2 = [0, 0, 0]
+
+                assert.equal(pontoAPonto(ponto1, ponto2), 10)
+            })
+
+            it('no mesmo eixo, só no z no ponto2', function () {
+                var ponto1 = [0, 0, 0],
+                ponto2 = [0, 0, 10]
+
+                assert.equal(pontoAPonto(ponto1, ponto2), 10)
+            })
+
+            it('no mesmo eixo, fora da origem', function () {
+                var ponto1 = [1, 0, 0],
+                ponto2 = [2, 0, 0]
+
+                assert.equal(pontoAPonto(ponto1, ponto2), 1)
+            })
+
+            it('no eixo y, fora da origem', function () {
+                var ponto1 = [0, 2, 0],
+                ponto2 = [0, 3, 0]
+
+                assert.equal(pontoAPonto(ponto1, ponto2), 1)
+            })
+
+            it('no eixo z, fora da origem', function () {
+                var ponto1 = [0, 0, 1],
+                ponto2 = [0, 0, 2]
+
+                assert.equal(pontoAPonto(ponto1, ponto2), 1)
+            })
         })
-        it('no mesmo eixo, só que mais longe', function () {
-            var ponto1 = [0, 0, 0],
-            ponto2 = [10, 0, 0]
-
-            assert.equal(pontoAPonto(ponto1, ponto2), 10)
-        })
-
-        it('no mesmo eixo, só no y', function () {
-            var ponto1 = [0, 1, 0],
-            ponto2 = [0, 0, 0]
-
-            assert.equal(pontoAPonto(ponto1, ponto2), 1)
-        })
-
-        it('no mesmo eixo, só no y no ponto2', function () {
-            var ponto1 = [0, 0, 0],
-            ponto2 = [0, 2, 0]
-
-            assert.equal(pontoAPonto(ponto1, ponto2), 2)
-        })
-
-        it('no mesmo eixo, só no z', function () {
-            var ponto1 = [0, 0, 10],
-            ponto2 = [0, 0, 0]
-
-            assert.equal(pontoAPonto(ponto1, ponto2), 10)
-        })
-
-        it('no mesmo eixo, só no z no ponto2', function () {
-            var ponto1 = [0, 0, 0],
-            ponto2 = [0, 0, 10]
-
-            assert.equal(pontoAPonto(ponto1, ponto2), 10)
-        })
-
-        it('no mesmo eixo, fora da origem', function () {
-            var ponto1 = [1, 0, 0],
-            ponto2 = [2, 0, 0]
-
-            assert.equal(pontoAPonto(ponto1, ponto2), 1)
-        })
-
-        it('no eixo y, fora da origem', function () {
-            var ponto1 = [0, 2, 0],
-            ponto2 = [0, 3, 0]
-
-            assert.equal(pontoAPonto(ponto1, ponto2), 1)
-        })
-
-        it('no eixo z, fora da origem', function () {
-            var ponto1 = [0, 0, 1],
-            ponto2 = [0, 0, 2]
-
-            assert.equal(pontoAPonto(ponto1, ponto2), 1)
-        })
-
+        
     })
 })
