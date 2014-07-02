cfeb1631554647c497358d8155f794e3e5860020
DojoRio <issae@issae.com>
2014-07-02 20:15:18 -0300

    Julio Marins e Camila Nunes

--- 2014/20140702 - 8damas - python/oitorainhas.py
+++ 2014/20140702 - 8damas - python/oitorainhas.py
@@ -1,8 +1,10 @@
 def pode_atacar(tabuleiro):
-    if (tabuleiro[0].count(1) >= 2)
+    if (tabuleiro[0].count(1) >= 2):
         return True
-    if (tabuleiro[1].count(1) >= 2)
+    if (tabuleiro[1].count(1) >= 2):
         return True
-    if (tabuleiro[2].count(1) >= 2)
+    if (tabuleiro[2].count(1) >= 2):
+        return True
+    if (tabuleiro[3].count(1) >= 2):
         return True
     return False
--- 2014/20140702 - 8damas - python/oitorainhas_test.py
+++ 2014/20140702 - 8damas - python/oitorainhas_test.py
@@ -85,4 +85,30 @@ class OitoRainhas(unittest.TestCase):
         ]
         self.assertEquals(pode_atacar(tabuleiro), True)
 
+    def test_tabuleiro_com_tres_na_terceira_linha(self):
+        tabuleiro = [ 
+            [0,0,0,0],
+            [0,0,0,0],
+            [1,1,1,0],
+            [0,0,0,0],
+        ]
+        self.assertEquals(pode_atacar(tabuleiro), True)
+
+    def test_tabuleiro_com_um_na_quarta_linha(self):
+        tabuleiro = [ 
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,1,0],
+        ]
+        self.assertEquals(pode_atacar(tabuleiro), False)
+
+    def test_tabuleiro_com_dois_na_quarta_linha(self):
+        tabuleiro = [ 
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,1,1,0],
+        ]
+        self.assertEquals(pode_atacar(tabuleiro), True)
 unittest.main() 
