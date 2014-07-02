6a1e1fc1d8bc2dd648b67778a493a03816f12aa5
DojoRio <issae@issae.com>
2014-07-02 19:27:29 -0300

    Flávio Amieiro e Carlos Figueiredo

--- 2014/20140611 - flor do mario - python/flor_mario.py
+++
@@ -1,3 +0,0 @@
-def demarcar(flor, demarcacao):
-#	distancia + raio_da_flor <= raio_demarcado
-	return ((flor[1][0] - demarcacao[1][0])**2 - (flor[1][1] - demarcacao[1][1])**2)**0.5 <= raio_demarcacao[0]
---
+++ 2014/20140702 - 8damas - python/oitodamas_test.py
@@ -0,0 +1,4 @@
+import unittest
+from oitodamas import oitodamas
+
+unittest.main() 
