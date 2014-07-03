19658e2ef5abe6ce753e5bd0b8982825ed6b2cb9
DojoRio <issae@issae.com>
2014-07-02 21:01:14 -0300

    Thiago Belem e Paulo Gallotti

--- 2014/20140702 - 8damas - python/oitorainhas.py
+++ 2014/20140702 - 8damas - python/oitorainhas.py
@@ -7,8 +7,9 @@ def pode_atacar(tabuleiro):
         coluna = [linha[j] for linha in tabuleiro]
         if (coluna.count(1) >= 2):
             return True
-    acumulador_de_damas = 0
-    for j in xrange(0, 4):
-        if(tabuleiro[j][j] == 1):
-            acumulador_de_damas += 1
-    return acumulador_de_damas > 0
+    
+    diagonal = [tabuleiro[j][j] for j in xrange(0, 4)]
+    if (diagonal.count(1) >= 2):
+        return True
+
+    return False
--- 2014/20140702 - 8damas - python/oitorainhas_test.py
+++ 2014/20140702 - 8damas - python/oitorainhas_test.py
@@ -176,5 +176,13 @@ class OitoRainhas(unittest.TestCase):
         ]
         self.assertEquals(pode_atacar(tabuleiro), True)
 
+    def test_2_na_diagonal_linha1_linha2_linha3_oposta(self):
+        tabuleiro = [ 
+            [0,0,0,1],
+            [0,0,1,0],
+            [0,0,0,0],
+            [0,0,0,0],
+        ]
+        self.assertEquals(pode_atacar(tabuleiro), True)
 
 unittest.main() 
