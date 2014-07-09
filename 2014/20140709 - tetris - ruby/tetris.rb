42451b288c8b071af81d86862d629995e4c37f8b
DojoRio <issae@issae.com>
2014-07-09 19:38:02 -0300

    Israel Teixeira e Carlos Figueiredo

--- 2014/20140709 - tetris - ruby/tetris.rb
+++ 2014/20140709 - tetris - ruby/tetris.rb
@@ -1,6 +1,7 @@
 def turn_piece(piece)
-  ['#',
-   '#',
-   '#',
-   '#']
+  if piece.length == 1
+    piece.split('')
+  else
+    [piece.join('')]
+  end
 end
--- 2014/20140709 - tetris - ruby/tetris_spec.rb
+++ 2014/20140709 - tetris - ruby/tetris_spec.rb
@@ -2,13 +2,22 @@ require './tetris'
 
 describe 'Tetris' do
   describe 'turn_piece' do
-    it 'turn piece 90 degrees' do
+    it 'puts I piece vertical' do
       piece = ['####']
 
       expect(turn_piece(piece)).to eq(['#',
                                        '#',
                                        '#',
                                        '#'])
+    end
+
+    it 'turn I piece horizontal' do
+      piece = ['#',
+               '#',
+               '#',
+               '#']
+
+      expect(turn_piece(piece)).to eq(['####'])
     end
   end
 end
