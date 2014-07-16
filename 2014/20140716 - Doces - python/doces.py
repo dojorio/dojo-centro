f4678eb24ace430fe532a1bb04433f7cfe288983
DojoRio <issae@issae.com>
2014-07-16 19:37:24 -0300

    Otavio Cardoso e Juan Lopes

--- 2014/20140716 - Doces - python/doces.py
+++ 2014/20140716 - Doces - python/doces.py
@@ -1,7 +1,4 @@
 def menor_diferenca(doces):
-    if len(doces) == 1:
-        return doces[0]
-    elif len(doces) == 2:
-        return max(doces) - min(doces)
-    else:
-        return 2*max(doces) - sum(doces)
+    if len(doces) <= 3:
+        return abs(sum(doces) - 2*max(doces))
+    return max(doces)-min(doces)
--- 2014/20140716 - Doces - python/doces_test.py
+++ 2014/20140716 - Doces - python/doces_test.py
@@ -22,14 +22,30 @@ class DocesTest(unittest.TestCase):
         doces = [2, 2, 2]
         self.assertEqual(menor_diferenca(doces), 2)
 
-
     def test_2kg_3kg_2kg(self):
         doces = [2, 3, 2]
         self.assertEqual(menor_diferenca(doces), 1)
 
-
     def test_3kg_2kg_2kg(self):
         doces = [3, 2, 2]
         self.assertEqual(menor_diferenca(doces), 1)
+
+    def test_5kg_2kg_2kg(self):
+        doces = [5, 2, 2]
+        self.assertEqual(menor_diferenca(doces), 1)
+
+    def test_2kg_2kg_2kg_2kg(self):
+        doces = [2, 2, 2, 2]
+        self.assertEqual(menor_diferenca(doces), 0)
+
+    def test_2kg_2kg_2kg_3kg(self):
+        doces = [2, 2, 2, 3]
+        self.assertEqual(menor_diferenca(doces), 1)
+
+    def test_1kg_2kg_3kg_4kg(self):
+        doces = [1, 2, 3, 4]
+        self.assertEqual(menor_diferenca(doces), 0)
+
+
 
 unittest.main()
