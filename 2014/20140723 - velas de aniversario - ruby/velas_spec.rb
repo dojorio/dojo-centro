8e8a15192294c475eafb5f58abb8920b2a804a74
DojoRio <issae@issae.com>
2014-07-23 20:04:03 -0300

    Carlos Figueiredo e Rodrigo Couto

--- 2014/20140723 - velas de aniversario - ruby/velas_spec.rb
+++ 2014/20140723 - velas de aniversario - ruby/velas_spec.rb
@@ -16,18 +16,24 @@ describe "Velas" do
   end
 
 
-  it 'duas velas nao exatas com velas' do
+  it 'duas idades exatas com velas distintas' do
     idades = [5,5]
     velas  = [2,4]
 
     expect(particao_de_velas(idades, velas)).to eq(4)
   end
 
-  it 'duas velas nao exatas com velas 2' do
-    idades = [5,5]
+  it 'duas idades distintas com 2 velas distintas' do
+    idades = [5,3]
     velas  = [1,4]
 
-    expect(particao_de_velas(idades, velas)).to eq(4)
+    expect(particao_de_velas(idades, velas)).to eq(3)
   end
 
+  it 'duas idades distintas com 2 velas distintas invertidas' do
+    idades = [5,3]
+    velas  = [4,1]
+
+    expect(particao_de_velas(idades, velas)).to eq(3)
+  end
 end
