0b3d587bb301fe9cca39f44e01c7e9c5110f77f6
DojoRio <issae@issae.com>
2014-07-23 20:41:45 -0300

    Julio Marins e Rodrigo Couto

--- 2014/20140723 - velas de aniversario - ruby/velas.rb
+++ 2014/20140723 - velas de aniversario - ruby/velas.rb
@@ -5,7 +5,8 @@ def particao_de_velas(idades, velas)
     dif1 = (velas[0] * 10 + velas[1]) - idades[0]
     dif2 = (velas[1] * 10 + velas[0]) - idades[0]
 
-    return [dif1.abs, dif2.abs].min
+    return velas[0] + velas[1] == idades[0]? 
+    idades[0]: [dif1.abs, dif2.abs].min
   end
 
   dif1 = velas.max - idades.max
