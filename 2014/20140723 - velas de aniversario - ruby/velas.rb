5b3712826a95d60bb801fecba2f8ed7aaf47e014
DojoRio <issae@issae.com>
2014-07-23 20:52:57 -0300

    Otavio Cardoso e Tomás Lima

--- 2014/20140723 - velas de aniversario - ruby/velas.rb
+++ 2014/20140723 - velas de aniversario - ruby/velas.rb
@@ -9,11 +9,21 @@ def particao_de_velas(idades, velas)
       return velas.min
     end
 
+    dif3 = idades[0] - velas[0]
+    dif4 = idades[0] - velas[1]
+
     return [dif1.abs, dif2.abs].min
   end
 
   dif1 = velas.max - idades.max
   dif2 = velas.min - idades.min
+  
+  # dif3 = (velas[0] * 10 + velas[1]) - idades[0]
+  # dif4 = (velas[1] * 10 + velas[0]) - idades[0]
+  
+  # dif5 = (velas[0] * 10 + velas[1]) - idades[-1]
+  # dif6 = (velas[1] * 10 + velas[0]) - idades[-1]
 
-  dif1.abs + dif2.abs
+  (dif1.abs + dif2.abs)
+
 end
