be8b36b6c5bf3cc702ed5a2a1fad9a6e5a5ad5c0
DojoRio <issae@issae.com>
2014-07-23 21:01:43 -0300

    Thiago Belem e Otavio Cardoso

--- 2014/20140723 - velas de aniversario - ruby/velas.rb
+++ 2014/20140723 - velas de aniversario - ruby/velas.rb
@@ -1,18 +1,11 @@
 def particao_de_velas(idades, velas)
   if idades.count == 1
-    return 0 if velas.include?(idades[0])
-
     dif1 = (velas[0] * 10 + velas[1]) - idades[0]
     dif2 = (velas[1] * 10 + velas[0]) - idades[0]
-
-    if((velas[0] + velas[1]) == idades[0])
-      return velas.min
-    end
-
     dif3 = idades[0] - velas[0]
     dif4 = idades[0] - velas[1]
 
-    return [dif1.abs, dif2.abs].min
+    return [dif1, dif2, dif3, dif4].map(&:abs).min
   end
 
   dif1 = velas.max - idades.max
