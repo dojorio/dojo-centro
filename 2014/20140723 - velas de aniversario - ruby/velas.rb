16b73879e1d54392472acfd16ab1dbcea8200fec
DojoRio <issae@issae.com>
2014-07-23 21:26:34 -0300

    Leandro Thimoteo e Tomás Lima

--- 2014/20140723 - velas de aniversario - ruby/velas.rb
+++ 2014/20140723 - velas de aniversario - ruby/velas.rb
@@ -1,25 +1,27 @@
+def base10(a,b)
+  a * 10 + b
+
+end
+
 def particao_de_velas(idades, velas)
   diffs = []
 
-  # if idades.count == 1
-    diffs << (velas[0] * 10 + velas[1]) - idades[0]
-    diffs << (velas[1] * 10 + velas[0]) - idades[0]
+  if idades.count == 1
+    diffs << base10(velas[0], velas[1]) - idades[0]
+    diffs << base10(velas[1], velas[0]) - idades[0]
     diffs << idades[0] - velas[0]
     diffs << idades[0] - velas[1]
-  # end
+  end
 
-  # if idades.count == 2
+  if idades.count == 2
     diffs << (velas.max - idades.max).abs + (velas.min - idades.min).abs
-  # end
+    diffs << base10(velas[0], velas[1]) - idades[0]
+    diffs << base10(velas[1], velas[0]) - idades[0]
+    diffs << base10(velas[0], velas[1]) - idades[1]
+    diffs << base10(velas[1], velas[0]) - idades[1]
+  end
 
   return diffs.map(&:abs).min
   
-  # dif3 = (velas[0] * 10 + velas[1]) - idades[0]
-  # dif4 = (velas[1] * 10 + velas[0]) - idades[0]
-  
-  # dif5 = (velas[0] * 10 + velas[1]) - idades[-1]
-  # dif6 = (velas[1] * 10 + velas[0]) - idades[-1]
-
-  #(dif1.abs + dif2.abs)
-
+ 
 end
