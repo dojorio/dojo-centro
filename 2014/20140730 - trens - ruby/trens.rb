3b87399aaf28207d5ac282d3160a5b77da4a3b5d
DojoRio <issae@issae.com>
2014-07-30 20:33:05 -0300

    Otavio Cardoso e Jonatas Emidio

--- 2014/20140730 - trens - ruby/trens.rb
+++ 2014/20140730 - trens - ruby/trens.rb
@@ -1,12 +1,13 @@
 def routes_number(edges, origin, destination, max_distance)
-  distance = edges.first[2].to_i
-  
-  if edges.first[0] != origin 
+  first_distance = edges.first[2].to_i
+  last_distance = edges.last[2].to_i
+
+  if edges.first[0] != origin && edges.last[0] != origin 
     return 0
   end
 
 
-  if distance <= max_distance
+  if first_distance <= max_distance || last_distance <= max_distance
     return 1
   end
 
--- 2014/20140730 - trens - ruby/trens_spec.rb
+++ 2014/20140730 - trens - ruby/trens_spec.rb
@@ -1,6 +1,7 @@
 require './trens'
 
 describe "Trens" do
+
   it "origin A destination B max_distance 5 return 1" do
     expect(routes_number(['AB5'], 'A', 'B', 5)).to eq(1)
   end
@@ -20,4 +21,15 @@ describe "Trens" do
   it "origin B destination A max_distance 6 return 1" do
     expect(routes_number(['BA6'], 'B', 'A', 6)).to eq(1)
   end
+
+  it "origin A destination B max_distance 6 return 1" do
+    expect(routes_number(['BA6', 'AB6'], 'A', 'B', 6)).to eq(1)
+  end
+
+  it "origin A destination B max_distance 6 return 1" do
+    expect(routes_number(['BA7', 'AB6'], 'A', 'B', 6)).to eq(1)
+  end
+
+
+
 end
