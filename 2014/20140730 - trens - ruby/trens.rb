e8cbda0e8a705c8e18cc4e429ae579d7a8f6506f
DojoRio <issae@issae.com>
2014-07-30 21:06:01 -0300

    Otavio Cardoso e Carlos Figueiredo

--- 2014/20140730 - trens - ruby/trens.rb
+++ 2014/20140730 - trens - ruby/trens.rb
@@ -2,6 +2,7 @@ def routes_number(edges, origin, destination, max_distance)
   first_distance = edges.first[2].to_i
   last_distance = edges.last[2].to_i
 
+
   if edges.first[0] != origin && edges.last[0] != origin 
     return 0
   end
--- 2014/20140730 - trens - ruby/trens_spec.rb
+++ 2014/20140730 - trens - ruby/trens_spec.rb
@@ -38,8 +38,12 @@ describe "Trens" do
     expect(routes_number(['BA7', 'AC5'], 'A', 'B', 5)).to eq(0)
   end
 
-  it "origin A destination C max_distance 5 return 0" do
-    expect(routes_number(['AB5', 'DC5'], 'A', 'C', 5)).to eq(0)
+  it "origin A destination B max_distance 5 return 0" do
+    expect(routes_number(['AB10'], 'A', 'B', 5)).to eq(0)
   end
+
+  # it "origin A destination C max_distance 5 return 0" do
+  #   expect(routes_number(['AB5', 'DC5'], 'A', 'C', 5)).to eq(0)
+  # end
 
 end
