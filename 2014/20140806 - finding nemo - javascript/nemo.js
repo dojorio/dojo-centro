20eb8602f9d4df03a4b71a9365f3939eacf46baf
DojoRio <issae@issae.com>
2014-08-06 20:56:58 -0300

    Tomás Lima e Carlos Figueiredo

--- 2014/20140806 - finding nemo - javascript/nemo.js
+++ 2014/20140806 - finding nemo - javascript/nemo.js
@@ -3,11 +3,17 @@ exports.findNemo = function(walls, doors, nemo){
     return 0;
   }
 
+  var sum_directions = walls.reduce(function(soFar,wall){
+    return soFar += wall[2];
+  });
+
+  if (sum_directions != 2) {
+    return 0;
+  }
+
   if(walls[0][1] == walls[3][1]) {
     return 0;
   }
 
   return doors.length;
 }
-
-
