50492140db0e3175789ba249e275c272322427aa
DojoRio <issae@issae.com>
2014-08-06 21:03:13 -0300

    Thiago Belem e Tomás Lima

--- 2014/20140806 - finding nemo - javascript/nemo.js
+++ 2014/20140806 - finding nemo - javascript/nemo.js
@@ -4,16 +4,18 @@ exports.findNemo = function(walls, doors, nemo){
   }
 
   var sum_directions = walls.reduce(function(soFar,wall){
+    console.log(soFar, 123)
     return soFar += wall[2];
   });
 
-  if (sum_directions != 2) {
+
+  if (sum_directions != 2 ) {
     return 0;
   }
 
-  if(walls[0][1] == walls[3][1]) {
-    return 0;
-  }
+  // if(walls[0][1] == walls[3][1]) {
+  //   return 0;
+  // }
 
   return doors.length;
 }
