fd252cb7adfcb71c214d503af448f60dca4781c0
DojoRio <issae@issae.com>
2014-08-13 21:02:03 -0300

    Otavio Cardoso e Julio Marins

--- 2014/20140813 - crazy frog - ruby/crazy_frog.rb
+++ 2014/20140813 - crazy frog - ruby/crazy_frog.rb
@@ -3,15 +3,18 @@ def bigger_jump(distance, stones)
 
   return distance if stones.empty?
 
+  betweens = stones.map do | stone |
+    stone[1..-1].to_i
+  end
+
+  stones
+    
   if stones.count == 2
     to_end = distance - stones[1][1..-1].to_i
+    between = stones[1][1..-1].to_i - stones[0][1..-1].to_i
     from_start = stones[0][1..-1].to_i
 
-    if to_end > from_start
-      return to_end
-    else
-      return stones[0][1..-1].to_i
-    end
+    return [from_start,between,to_end].max
   end
 
   if stones[0][0] == 'B'
