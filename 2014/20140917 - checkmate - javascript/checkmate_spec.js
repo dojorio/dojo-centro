a75afe4be3c2e37120047ad4b7a92db775a3abc6
DojoRio <issae@issae.com>
2014-09-17 21:26:45 -0300

    Jonatas Emidio e Leonardo Alberto Souza

--- 2014/20140917 - checkmate - javascript/checkmate_spec.js
+++ 2014/20140917 - checkmate - javascript/checkmate_spec.js
@@ -1,68 +1,68 @@
 var assert = require('assert'),
     checkmate = require('./checkmate').checkmate,
-    alinhados = require('./checkmate').alinhados
+    alinhados = require('./checkmate').alinhados;
 
 describe('alinhados', function () {
   it("'Wa1', 'Wa3'", function () {
-    assert.equal(alinhados('Wa3', 'Wa1'), false)
+    assert.equal(alinhados('Wa3', 'Wa1'), false);
   });
 
   it("'Wa1', 'Tc1'", function () {
-    assert.equal(alinhados('Wa1', 'Tc1'), true)
+    assert.equal(alinhados('Wa1', 'Tc1'), true);
   });
 
   it("'Wa1'", function() {
-    assert.equal(alinhados('Wa1'), false)
+    assert.equal(alinhados('Wa1'), false);
   });
 });
 
 describe('checkmate', function () {
   it("['Wa3'], 'Wa1'", function () {
-    assert.equal(checkmate(['Wa3'], 'Wa1'), false)
+    assert.equal(checkmate(['Wa3'], 'Wa1'), false);
   });
 
   it("['Wa3', 'Tc1'], 'Wa1'", function () {
-    assert.equal(checkmate(['Wa3', 'Tc1'], 'Wa1'), true)
+    assert.equal(checkmate(['Wa3', 'Tc1'], 'Wa1'), true);
   });
 
   it("['Wa4', 'Tc1'], 'Wa1'", function () {
-    assert.equal(checkmate(['Wa4', 'Tc1'], 'Wa1'), false)
+    assert.equal(checkmate(['Wa4', 'Tc1'], 'Wa1'), false);
   });
 
   it("['Tc1', 'Wa3'], 'Wa1'", function () {
-    assert.equal(checkmate(['Tc1', 'Wa3'], 'Wa1'), true)
+    assert.equal(checkmate(['Tc1', 'Wa3'], 'Wa1'), true);
   });
 
   it("['Tc2', 'Wa3'], 'Wa1'", function () {
-    assert.equal(checkmate(['Tc2', 'Wa3'], 'Wa1'), false)
+    assert.equal(checkmate(['Tc2', 'Wa3'], 'Wa1'), false);
   });
 
   it("['Rc1', 'Wa3'], 'Wa1'", function () {
-    assert.equal(checkmate(['Rc1', 'Wa3'], 'Wa1'), true)
+    assert.equal(checkmate(['Rc1', 'Wa3'], 'Wa1'), true);
   });
 
   it("['Rd1', 'Wa3'], 'Wa1'", function () {
-    assert.equal(checkmate(['Rd1', 'Wa3'], 'Wa1'), true)
+    assert.equal(checkmate(['Rd1', 'Wa3'], 'Wa1'), true);
   });
 
   it("['Rd1', 'Wa3'], 'Wh8'", function () {
-    assert.equal(checkmate(['Rd1', 'Wa3'], 'Wh8'), false)
+    assert.equal(checkmate(['Rd1', 'Wa3'], 'Wh8'), false);
   });
 
   it("['Rd1', 'Wa3'], 'Wb1'", function () {
-    assert.equal(checkmate(['Rd1', 'Wa3'], 'Wb1'), true)
+    assert.equal(checkmate(['Rd1', 'Wa3'], 'Wb1'), true);
   });
 
   it("['Td1', 'Wa3'], 'Wb1'", function () {
-    assert.equal(checkmate(['Td1', 'Wa3'], 'Wb1'), false)
+    assert.equal(checkmate(['Td1', 'Wa3'], 'Wb1'), false);
   });
 
   it("['Bd1', 'Wa3'], 'Wb1'", function () {
-    assert.equal(checkmate(['Bd1', 'Wa3'], 'Wb1'), false)
+    assert.equal(checkmate(['Bd1', 'Wa3'], 'Wb1'), false);
   });
 
   it("['Rd1', 'Wb3','Ta3'], 'Wb1'", function () {
-    assert.equal(checkmate(['Rd1', 'Wb3','Ta3'], 'Wb1'), true)
+    assert.equal(checkmate(['Rd1', 'Wb3','Ta3'], 'Wb1'), true);
   });
 
 });
