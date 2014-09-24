3507786416aba0444c2b970fde4845883e2428e4
DojoRio <issae@issae.com>
2014-09-24 19:36:01 -0300

    Ricardo Augusto e Carlos Cunha

--- 2014/20140924 - moeda - javascript/moeda_spec.js
+++ 2014/20140924 - moeda - javascript/moeda_spec.js
@@ -9,4 +9,8 @@ describe("Moeda", function() {
     it("uma moeda para valor maior", function() {
         assert.equal(troco([1], 3), 3)
     })
+
+    it("uma moeda para o mesmo valor", function() {
+        assert.equal(troco([2], 2), 1)
+    })
 })
