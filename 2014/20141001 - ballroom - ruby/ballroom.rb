bf11bb6f8a9946f4f3d27fe4eaa723de77175fd7
DojoRio <issae@issae.com>
2014-10-01 20:17:06 -0300

    André Calazans e Mariana Bedran

--- 2014/20141001 - ballroom - ruby/ballroom.rb
+++ 2014/20141001 - ballroom - ruby/ballroom.rb
@@ -1,8 +1,16 @@
 def ballroom(height, width, planks)
   area = width * height
 
+  sum = 0
+  planks.each do |plank|
+    sum += plank
+    if sum == area
+      return planks.index(plank) + 1
+    end
+  end
+
   if planks.include? area
-    1
+    1  
   elsif planks.reduce(:+) != area
     "impossivel"
   else
