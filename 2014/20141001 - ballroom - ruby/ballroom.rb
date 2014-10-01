483a15ce47fa7afa64ac540548cb3bd7f93d7d2c
DojoRio <issae@issae.com>
2014-10-01 20:53:36 -0300

    Israel Teixeira e Otavio Cardoso

--- 2014/20141001 - ballroom - ruby/ballroom.rb
+++ 2014/20141001 - ballroom - ruby/ballroom.rb
@@ -1,17 +1,23 @@
 def ballroom(height, width, planks)
   area = width * height
-  sum = 0
+  planks.sort!.reverse!
 
-  count = 0
+  while planks.size > 0
+    sum = 0
+    count = 0
 
-  planks.sort.reverse.each do |plank|
-    return 1 if plank == area
+    planks.each do |plank|
+      return 1 if plank == area
 
-    if sum + plank <= area
-      sum += plank
-      count += 1
-      return count if sum == area
+      if sum + plank <= area
+        sum += plank
+        count += 1
+
+        return count if sum == area
+      end
     end
+
+    planks.shift
   end
 
   "impossivel"
