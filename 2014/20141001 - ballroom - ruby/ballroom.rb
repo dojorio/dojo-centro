8c0922a1a6583bb294042ffbf9508abf5b0afd44
DojoRio <issae@issae.com>
2014-10-01 19:26:20 -0300

    Israel Teixeira e Carlos Figueiredo

--- 2014/20141001 - ballroom - ruby/ballroom.rb
+++ 2014/20141001 - ballroom - ruby/ballroom.rb
@@ -1,3 +1,6 @@
 def ballroom(height, width, planks)
+  if planks.last == width
+    return 1
+  end
   planks.size
 end
--- 2014/20141001 - ballroom - ruby/ballroom_spec.rb
+++ 2014/20141001 - ballroom - ruby/ballroom_spec.rb
@@ -12,4 +12,8 @@ describe 'Ballroom' do
   it '2x1 with 2 planks 1m' do
     expect(ballroom(2,1,[1,1])).to eq(2)
   end
+
+  it '1x2 with 2 planks 1m 2m' do
+    expect(ballroom(1, 2, [1, 2])).to eq(1)
+  end
 end
