c4e6f4d9dbade3a905bfbab5a30dd37ee9df8668
DojoRio <issae@issae.com>
2014-10-15 20:47:53 -0300

    Carlos Figueiredo e Rafael Couto

--- 2014/20141015 - tictactoe1d - javascript/tictactoe.js
+++ 2014/20141015 - tictactoe1d - javascript/tictactoe.js
@@ -2,7 +2,8 @@ exports.maryWins = maryWins
 
 
 function maryWins(table) {
-    var qtt_x    = table.split('x').length - 1
+    var qtt_x    = table.split('x').length - 1,
+        qtt_dot  = table.split('.').length - 1
  
-    return qtt_x % 2 == 0 || table == 'x...' || table == '...x'
+    return qtt_x % 2 == 0 && qtt_dot % 2 == 1
 }
