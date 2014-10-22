64e189f89c103c947ce29eeade6cf21e66772d8b
DojoRio <issae@issae.com>
2014-10-22 20:56:10 -0200

    Israel Teixeira e Otavio Cardoso

--- 2014/20141022 - logic operations - python/logic.py
+++ 2014/20141022 - logic operations - python/logic.py
@@ -1,8 +1,9 @@
 #proibido usar + - * /
 
-def add(a, b):
+def add(a, b, carry = 0):
     carry = (a & b) << 1
-    return carry | xor(a, b)
+
+    return carry | xor(a, b) | (b << 1)
 
 def xor(a, b):
     return (a | b) & (not(a & b))
