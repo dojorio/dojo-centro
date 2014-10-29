e6ee3df999833666b674492c7e0117b4b8fba858
DojoRio <issae@issae.com>
2014-10-29 19:59:05 -0200

    Rafael Couto e Ricardo Augusto

--- 2014/20141029 - different digits - ruby/digits.rb
+++ 2014/20141029 - different digits - ruby/digits.rb
@@ -1,6 +1,13 @@
 def houses(first, last)
-  return 9  if last == 11 and first == 2
-  return 10 if last == 11 and first == 1
-  return 8 if last == 11 and first == 3
-  last - first + 1
+  # return 9  if last == 11 and first == 2
+  # return 10 if last == 11 and first == 1
+  # return 8 if last == 11 and first == 3
+  # casas = first + 1
+  if last == 11
+    return last - first
+  end
+
+  return 2 if last == 3 and first == 2
+
+  return last
 end
