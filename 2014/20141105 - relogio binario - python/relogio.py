a35ebfc487d2ba4c155a8b29cc49d8983c583100
DojoRio <issae@issae.com>
2014-11-05 20:29:52 -0200

    Mariana Bedran e Jonatas Emidio

--- 2014/20141105 - relogio binario - python/relogio.py
+++ 2014/20141105 - relogio binario - python/relogio.py
@@ -2,9 +2,12 @@ def hora_binaria(hora, minuto):
     sufix = str(minuto % 2)
     quociente = minuto // 2
     sufix += str(quociente % 2)
-    quociente = quociente // 2
+    quociente = minuto // 4
+    sufix += str(quociente % 2)
+
+    quociente = minuto // 8
     sufix += str(quociente % 2)
 
 
-    return ('0001','000' + sufix[::-1])
+    return ('0001','00' + sufix[::-1])
 
