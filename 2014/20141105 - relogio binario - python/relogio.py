8a69a2526258fc7f4b06d5533a28f167aab75444
DojoRio <issae@issae.com>
2014-11-05 21:15:19 -0200

    Lucas R. Martins e Israel Teixeira

--- 2014/20141105 - relogio binario - python/relogio.py
+++ 2014/20141105 - relogio binario - python/relogio.py
@@ -1,14 +1,14 @@
+def _binarizator(numero, qtd_de_bits):
+    bin = ''
+    for i in range(0, qtd_de_bits):
+        quociente = numero // 2 ** i
+        bin = str(quociente % 2) + bin
+    return bin
+
+
 def hora_binaria(hora, minuto):
-    bin_minuto = ''
-    bin_hora = ''
-
-    for i in range(0, 2):
-        quociente = hora // 2 ** i
-        bin_hora = str(quociente % 2) + bin_minuto
-
-    for i in range(0, 6):
-        quociente = minuto // 2 ** i
-        bin_minuto = str(quociente % 2) + bin_minuto
+    bin_minuto = _binarizator(minuto, 6)
+    bin_hora = _binarizator(hora, 2)
 
     return ('00' + bin_hora, bin_minuto)
 
