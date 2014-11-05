9c2d325898ee05faf6f845818a37ad1a098b3d62
DojoRio <issae@issae.com>
2014-11-05 20:04:23 -0200

    Ricardo Augusto e Lucas R. Martins

--- 2014/20141105 - relogio binario - python/relogio.py
+++ 2014/20141105 - relogio binario - python/relogio.py
@@ -1,9 +1,8 @@
 def hora_binaria(hora, minuto):
-    sufix = minuto % 2
+    sufix = str(minuto % 2)
 
     if minuto < 2:
-        return ('0001','00000'+str(sufix))
-    elif minuto == 2:
-        return ('0001','000010')
-    elif minuto == 3:
-        return ('0001','000011')
+        return ('0001','00000' + sufix)
+    else:
+        return ('0001','00001' + sufix)
+
