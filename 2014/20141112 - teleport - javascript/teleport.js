f1547e77d4b4d4f270aad68f1b278d9c9981d8ac
DojoRio <issae@issae.com>
2014-11-12 20:53:16 -0200

    Ricardo Augusto e Thiago Garcia

--- 2014/20141112 - teleport - javascript/teleport.js
+++ 2014/20141112 - teleport - javascript/teleport.js
@@ -1,14 +1,16 @@
 exports.teleport = function teleport($, origem, destino, paineis) {
-    if (paineis[0][3] === 2) {
-        var count = 0
+    
+    var count1 = 0
+    var count2 = 0
 
-        for(var i = 0; i < 4; i ++) {
-            if (paineis[0][i] === 1)
-                count ++
-        }
 
-        return count
+    for(var i = 0; i < 4; i ++) {
+        if (paineis[0][i] === 1)
+            count1 ++
     }
+
+    return count
+
 
 
     return Math.pow(4, $)
--- 2014/20141112 - teleport - javascript/teleport_spec.js
+++ 2014/20141112 - teleport - javascript/teleport_spec.js
@@ -29,7 +29,7 @@ describe('Teleporte', function () {
         assert.equal(result, 16)
     })
 
-     it('1 nave viagem de 1 para 1, com 3$', function() {
+    it('1 nave viagem de 1 para 1, com 3$', function() {
         var $ = 3,
             origem = destino = 1,
             paineis = [[1, 1, 1, 1]],
@@ -38,7 +38,7 @@ describe('Teleporte', function () {
         assert.equal(result, 64)
     })
 
-     it('2 nave viagem de 1 para 1, com 1$', function() {
+    it('2 nave viagem de 1 para 1, com 1$', function() {
         var $ = 1,
             origem = destino = 1,
             paineis = [[2, 2, 2, 2], [2, 2, 2, 2]],
@@ -47,7 +47,7 @@ describe('Teleporte', function () {
         assert.equal(result, 0)
     })
 
-     it('2 nave viagem de 1 para 1, com 1$ sendo o primeiro diferente', function() {
+    it('2 nave viagem de 1 para 1, com 1$ sendo o primeiro diferente', function() {
         var $ = 1,
             origem = destino = 1,
             paineis = [[1, 2, 2, 2], [2, 2, 2, 2]],
@@ -56,12 +56,21 @@ describe('Teleporte', function () {
         assert.equal(result, 1)
     })
 
-     it('2 nave viagem de 1 para 1, com 1$ sendo o primeiro diferente', function() {
+    it('2 nave viagem de 1 para 1, com 1$ sendo o primeiro e segundo diferentes', function() {
         var $ = 1,
             origem = destino = 1,
             paineis = [[1, 1, 2, 2], [2, 2, 2, 2]],
             result = teleport($, origem, destino, paineis)
 
         assert.equal(result, 2)
+    })
+
+    it('2 nave viagem de 1 para 1, com 1$ sendo o primeiro,segundo e ultimo diferentes', function() {
+        var $ = 1,
+            origem = destino = 1,
+            paineis = [[1, 1, 2, 1], [2, 2, 2, 2]],
+            result = teleport($, origem, destino, paineis)
+
+        assert.equal(result, 3)
     })
 })
