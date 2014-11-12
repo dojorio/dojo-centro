8d709837058dac6bdd023304d01afaf50bc9185e
DojoRio <issae@issae.com>
2014-11-12 21:18:33 -0200

    Mariana Bedran e Israel Teixeira

--- 2014/20141112 - teleport - javascript/teleport.js
+++ 2014/20141112 - teleport - javascript/teleport.js
@@ -1,18 +1,7 @@
 exports.teleport = function teleport($, origem, destino, paineis) {
-    var count1 = 0
-    
-    if(paineis.length == 2) {
-
-        for(var i = 0; i < 4; i ++) {
-            if (paineis[0][i] === 1){
-                count1++
-            }
-        }
-
-        //return count1
-
-    }
-
+    var count1 = paineis[0].filter(function (botao){
+        return botao == 1;
+    }).length
 
     return Math.pow(count1, $)
 }
