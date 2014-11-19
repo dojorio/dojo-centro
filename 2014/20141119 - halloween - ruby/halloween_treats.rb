dca1f8861c4aa7fc9c22db60856cec6b0c2b257f
DojoRio <issae@issae.com>
2014-11-19 21:51:12 -0200

    Add problem url

--- 2014/20141119 - halloween - ruby/halloween_treats.rb
+++ 2014/20141119 - halloween - ruby/halloween_treats.rb
@@ -1,15 +1,14 @@
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1656
 def halloween(criancas, doces)
   casas = (1..doces.length).to_a
   total_doces = doces.reduce(:+)
 
   return casas if total_doces % criancas == 0
 
-  pior = nil
-
-  casas.reverse_each do |casa|
-    pior ||= casa - 1 if doces[casa - 1] < criancas
+  pior = casas.rindex do |casa|
+    doces[casa -1] < criancas
   end
-  
+
   casas.delete_at(pior) if pior
 
   casas
