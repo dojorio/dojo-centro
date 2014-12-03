3923ef152b33d6f304cfa8dc4f68c5cf5356f955
DojoRio <issae@issae.com>
2014-12-03 21:25:13 -0200

    Ricardo Augusto e Otavio Cardoso

--- 2014/20141203 - casamentos - ruby/casamentos.rb
+++ 2014/20141203 - casamentos - ruby/casamentos.rb
@@ -6,24 +6,27 @@ end
 def casais(solteiros, solteiras)
   return [] if solteiras.empty?
 
-  if solteiros.count == 5
-    lista = [[solteiros.max, solteiras.max]]
-    lista += [[25, 21]]
-  end
+  lista_casais = []
 
-  solteiros = solteiros.sort.reverse
+  solteiros.sort.reverse.each do |solteiro|
+    diff = 1000
+    index_solteira = 0
 
-  diff = 1000
-  index_solteira = 0
+    solteiras.each_with_index do |solteira, index|
+      local_diff = (solteiro - solteira).abs
+      if local_diff < diff
+        diff = local_diff
+        index_solteira = index
 
-  solteiras.each_with_index do |solteira, index|
-    local_diff = (solteiro - solteira).abs
-    if local_diff < diff
-      diff = local_diff
-      index_solteira = index
+        break if local_diff == 0
+      end
     end
+
+    lista_casais << [solteiro, solteiras[index_solteira]]
+    solteiras.delete_at(index_solteira)
+
+    break if solteiras.empty?
   end
 
-  return [[solteiro, solteiras[index_solteira]]]
-
+  lista_casais
 end
