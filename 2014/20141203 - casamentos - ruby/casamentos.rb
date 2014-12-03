8ae9e57daa6243f966644d7a68d7570b8cb2bbdf
DojoRio <issae@issae.com>
2014-12-03 21:17:50 -0200

    Otavio Cardoso e Carlos Figueiredo

--- 2014/20141203 - casamentos - ruby/casamentos.rb
+++ 2014/20141203 - casamentos - ruby/casamentos.rb
@@ -11,14 +11,14 @@ def casais(solteiros, solteiras)
     lista += [[25, 21]]
   end
 
-  solteiro = solteiros[0]
+  solteiros = solteiros.sort.reverse
 
   diff = 1000
   index_solteira = 0
 
   solteiras.each_with_index do |solteira, index|
     local_diff = (solteiro - solteira).abs
-    if local_diff < diff do
+    if local_diff < diff
       diff = local_diff
       index_solteira = index
     end
