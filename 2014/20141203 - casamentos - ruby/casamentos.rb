cdd2c9ab233d68222476d33e32e84b826147cc59
DojoRio <issae@issae.com>
2014-12-03 20:16:12 -0200

    Otavio Cardoso e Rafael Couto

--- 2014/20141203 - casamentos - ruby/casamentos.rb
+++ 2014/20141203 - casamentos - ruby/casamentos.rb
@@ -1,12 +1,11 @@
 def casamentos(solteiros, solteiras)
+  solteiro_moco = solteiros.min
+ 
   if solteiras.empty?
-    solteiro_moco = solteiros.min
     return [solteiros.count, solteiro_moco]
   elsif solteiros.count >  solteiras.count
     quantidade_solteiros = solteiros.count - solteiras.count
-    menor = solteiros.min
-    
-    return [quantidade_solteiros,menor]
+    return [quantidade_solteiros,solteiro_moco]
   end
 
   [0]
--- 2014/20141203 - casamentos - ruby/casamentos_spec.rb
+++ 2014/20141203 - casamentos - ruby/casamentos_spec.rb
@@ -43,9 +43,21 @@ describe "Casamentos" do
     expect(casamentos(solteiros, solteiras)).to eq([1, 21])
   end
 
-  it "dois solteiro 21 22 30 anos, uma solteira" do
+  it "tres solteiro 21 22 30 anos, uma solteira" do
     solteiros = [21, 22,30]
     solteiras = [21]
     expect(casamentos(solteiros, solteiras)).to eq([2, 21])
+  end
+
+  it "um solteiro 21 anos, dois solteira" do
+    solteiros = [21]
+    solteiras = [21, 15]
+    expect(casamentos(solteiros, solteiras)).to eq([0])
+  end
+
+  it "cinco solteiro 21, 22, 23, 24, 25 anos, dois solteira" do
+    solteiros = [21, 22, 23, 25, 25]
+    solteiras = [21, 22]
+    expect(casamentos(solteiros, solteiras)).to eq([3, 21])
   end
 end
