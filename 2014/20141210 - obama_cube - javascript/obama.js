60d46bfd5f43eecf5ad39400a2eead7d11384ff8
DojoRio <issae@issae.com>
2014-12-10 21:36:05 -0200

    Carlos Figueiredo e Ricardo Augusto

--- 2014/20141210 - obama_cube - javascript/obama.js
+++ 2014/20141210 - obama_cube - javascript/obama.js
@@ -7,7 +7,13 @@ exports.distinct_cubes = function(cubes) {
     var c2 = cubes[1].split('')
     var tb1 = [c1[0],c1[1]].sort().toString()
     var tb2 = [c2[0],c2[1]].sort().toString()
-    var lat2 = cubes[1].slice(2).indexOf(c1[0])
+
+    var lat1 = cubes[0].slice(2)
+    var lat2 = cubes[1].slice(2)
+
+    var posicao_inicial = lat2.indexOf(lat1[0])
+
+
     if (tb1 == tb2 && c1.slice(2).toString() == c2.slice(2).toString())
         return 1
     return 2
