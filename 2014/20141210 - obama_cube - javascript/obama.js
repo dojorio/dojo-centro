02592969ec4e45cbad849baee740a31bea7422ec
DojoRio <issae@issae.com>
2014-12-10 20:18:34 -0200

    Tomás Lima e Ricardo Augusto

--- 2014/20141210 - obama_cube - javascript/obama.js
+++ 2014/20141210 - obama_cube - javascript/obama.js
@@ -1,3 +1,7 @@
 exports.distinct_cubes = function(cubes) {
+    if(cubes[0] != cubes[1]) {
+        return 2
+    }
+
     return 1
 }
--- 2014/20141210 - obama_cube - javascript/obama_spec.js
+++ 2014/20141210 - obama_cube - javascript/obama_spec.js
@@ -1,10 +1,19 @@
 var assert = require('assert'),
-    obama  = require('obama')
+    distinct_cubes  = require('./obama').distinct_cubes
+
 
 describe('Obama', function() {
     context('compara 2 cubos', function() {
         it('111111 e 111111', function() {
             assert.equal(distinct_cubes(['111111', '111111']), 1)
+        })
+
+        it('111111 e 011111', function() {
+            assert.equal(distinct_cubes(['111111', '011111']), 2)
+        })
+
+        it('011111 e 101111', function() {
+            assert.equal(distinct_cubes(['011111', '101111']), 1)
         })
     })
 })
