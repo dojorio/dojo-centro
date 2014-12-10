4d76c88b8bb645e069025bd3cac890cfcafc03a0
DojoRio <issae@issae.com>
2014-12-10 20:29:06 -0200

    Carlos Figueiredo e Tomás Lima

--- 2014/20141210 - obama_cube - javascript/obama.js
+++ 2014/20141210 - obama_cube - javascript/obama.js
@@ -1,7 +1,14 @@
 exports.distinct_cubes = function(cubes) {
-    if(cubes[0] != cubes[1]) {
+
+    var c1 = cubes[0].split('')
+    var c2 = cubes[1].split('')
+    var tb1 = [c1[0],c1[1]].sort().toString()
+    var tb2 = [c2[0],c2[1]].sort().toString()
+
+    if (tb1 == tb2) {
+        return 1
+    } else {
         return 2
     }
 
-    return 1
 }
