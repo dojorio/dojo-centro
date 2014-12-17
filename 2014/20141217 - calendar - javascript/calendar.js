dc74fdaac816a7fc42abb95ba484e61f001c77d8
DojoRio <issae@issae.com>
2014-12-17 20:28:54 -0200

    Carlos Figueiredo e Julio Marins

--- 2014/20141217 - calendar - javascript/calendar.js
+++ 2014/20141217 - calendar - javascript/calendar.js
@@ -1,6 +1,6 @@
 exports.isValidDate = function(dateStr) {
   var months = [
-    'Aligator',
+    'Alligator',
     'Bog',
     'Crayfish', 
     'Damp', 
@@ -15,19 +15,14 @@ exports.isValidDate = function(dateStr) {
     'Marsh'
   ];
 
-  var Alligator = ["0","13", "26"];
-  var Bog = ["1", "14"];
-  
   var date = dateStr.split('-');
   var day = date[0];
   var month = date[1];
-  var year = date[2];
 
-  if (month == 'Alligator' && Alligator.indexOf(day) != -1) {
+  /*if (day % 13 == months.indexOf(month)) {
     return true;
-  }
-
-  if (month == 'Bog' && day % 13 == months.indexOf("Bog")) {
+  }*/
+  if (months[day % 13] == month) {
     return true;
   }
 
