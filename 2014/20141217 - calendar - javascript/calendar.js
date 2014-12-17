0924d7c416e1faa448d0736305125ef510c72a97
DojoRio <issae@issae.com>
2014-12-17 21:24:02 -0200

    Julio Marins e Israel Teixeira

--- 2014/20141217 - calendar - javascript/calendar.js
+++ 2014/20141217 - calendar - javascript/calendar.js
@@ -1,11 +1,16 @@
 exports.isValidDate = function(dateStr) {
-  var date = dateStr.split('-');
-  var day = date[0];
-  var month = date[1];
+  var date = dateStr.split('-'),
+      day = date[0],
+      month = date[1],
+      year = date[2],
+      qc;
 
   if (month == 'Newt') return day == 0;
 
-  if (month == 'Overflow') return day != 1;
+  if (month == 'Overflow') {
+    qc = ((683 * year) % 2820) < 683;
+    return qc;
+  }
 
   return day < 28;
 }
