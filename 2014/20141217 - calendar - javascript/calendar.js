46a054d1f3fb60db0f64c14aad079b8ef6e93f16
DojoRio <issae@issae.com>
2014-12-17 20:14:05 -0200

    Ricardo Augusto e Otavio Cardoso

--- 2014/20141217 - calendar - javascript/calendar.js
+++ 2014/20141217 - calendar - javascript/calendar.js
@@ -1,15 +1,30 @@
 exports.isValidDate = function(dateStr) {
+  var months = [
+    'Aligator',
+    'Bog',
+    'Crayfish', 
+    'Damp', 
+    'Eel', 
+    'Fen',
+    'Gumbo', 
+    'Hurricane', 
+    'Inundation', 
+    'Jaguar', 
+    'Kudzu', 
+    'Lake', 
+    'Marsh'
+  ];
+
   var Alligator = ["0","13", "26"];
   var Bog = ["1", "14"];
   
   var date = dateStr.split('-');
-  if (date[1] == 'Alligator' && Alligator.indexOf(date[0]) != -1) {
+  var month = months.indexOf(date[1]);
+  
+  if (parseInt(date[0]) % 13 === month) {
     return true;
   }
 
-  if (date[1] == 'Bog' && Bog.indexOf(date[0]) != -1) {
-    return true;
-  }
 
   return false;
 }
