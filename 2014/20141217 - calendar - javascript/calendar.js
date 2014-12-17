1a1c0266713b79a423c18544a251158479793bda
DojoRio <issae@issae.com>
2014-12-17 20:03:16 -0200

    Mario Mariani e Carlos Figueiredo

--- 2014/20141217 - calendar - javascript/calendar.js
+++ 2014/20141217 - calendar - javascript/calendar.js
@@ -1,7 +1,15 @@
 exports.isValidDate = function(dateStr) {
   Alligator = ["0","13", "26"];
-  if (Alligator.indexOf(dateStr.split('-')[0]) != -1) {
+  Bog = ["1"];
+  
+  date = dateStr.split('-');
+  if (Alligator.indexOf(date[0]) != -1) {
     return true;
   }
+
+  if (date[1] == 'Bog' && Bog.indexOf(date[0]) != -1) {
+    return true;
+  }
+
   return false;
 }
--- 2014/20141217 - calendar - javascript/calendar_spec.js
+++ 2014/20141217 - calendar - javascript/calendar_spec.js
@@ -8,17 +8,25 @@ describe('Calendar', function () {
     it('0-Alligator-0', function () {
       assert.equal(isValidDate('0-Alligator-0'), true);
     })
+
     it('1-Alligator-0', function () {
       assert.equal(isValidDate('1-Alligator-0'), false);
     })
+
     it('13-Alligator-0', function () {
       assert.equal(isValidDate('13-Alligator-0'), true);
     })
+
     it('26-Alligator-0', function () {
       assert.equal(isValidDate('26-Alligator-0'), true);
     })
+
     it('1-Bog-0', function () {
       assert.equal(isValidDate('1-Bog-0'), true);
+    })
+
+    it('14-Bog-0', function () {
+      assert.equal(isValidDate('14-Bog-0'), true);
     })
 
   })
