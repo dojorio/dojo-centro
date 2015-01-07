0c8ae0d80b0d23659cd8eb1bf9004573f92636b3
DojoRio <issae@issae.com>
2015-01-07 19:41:14 -0200

    Rodolfo Carvalho e Mario Mariani

--- 2015/20150107 - zeckendorf - go/zeckendorf_test.go
+++ 2015/20150107 - zeckendorf - go/zeckendorf_test.go
@@ -4,6 +4,10 @@ import "testing"
 
 func Test_1(t *testing.T) {
 	if(zeckendorf(1) != 1) {
-		t.Error("Errado")
+		t.Error("Errado zeckendorf(1)")
+	}
+
+	if(zeckendorf(2) != 10) {
+		t.Error("Errado zeckendorf(2)")
 	}
 }
