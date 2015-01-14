11c1f286d400fb3027bbb20ad99ccef1418ca4de
DojoRio <issae@issae.com>
2015-01-14 20:18:04 -0200

    Otavio Cardoso e Israel Teixeira

--- 2015/20150114 - poker - ruby/poker.rb
+++ 2015/20150114 - poker - ruby/poker.rb
@@ -1,9 +1,9 @@
 def maior_carta(mao)
-  baralho = [2,3,4,5,6,7,8,9,'T','J','Q','K','A']
+  baralho = '  23456789TJQKA'
 
   mao.map do |carta|
     baralho.index(carta[0])
-  end.max 
+  end.max
 
 end
 
