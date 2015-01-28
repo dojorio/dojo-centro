107711025053992299ae7bff39b9ec49ff51b05b
DojoRio <issae@issae.com>
2015-01-28 20:19:44 -0200

    Carlos Figueiredo e Jackson Garcia

--- 2015/20150128 - roman numbers - ruby/roman.rb
+++ 2015/20150128 - roman numbers - ruby/roman.rb
@@ -1,17 +1,14 @@
 def to_roman(integer)
-  if (integer == 1)
-    'I'
-  elsif (integer == 5)
-    'V'
-  elsif (integer == 10)  
-    'X'
-  elsif (integer == 50)  
-    'L'
-  elsif (integer == 100)
-    'C'
-  elsif (integer == 500)
-    'D'
-  else
-    'M'
-  end
+roman_number = { 
+  1     => "I", 
+  5     => "V", 
+  10    => "X", 
+  50    => "L", 
+  100   => "C", 
+  500   => "D",
+  1000  => "M"
+}
+
+  roman_number[integer]
+
 end
