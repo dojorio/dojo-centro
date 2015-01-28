8b3c581be0fc80019b85b4e1113259aa428e934c
DojoRio <issae@issae.com>
2015-01-28 19:57:34 -0200

    Michel Jean e Cristina Silva

--- 2015/20150128 - roman numbers - ruby/roman.rb
+++ 2015/20150128 - roman numbers - ruby/roman.rb
@@ -1,7 +1,11 @@
 def to_roman(integer)
   if (integer == 1)
     'I'
-  else (integer == 5)
+  elsif (integer == 5)
     'V'
+  elsif (integer == 10)  
+    'X'
+  elsif (integer == 50)  
+    'L'
   end
 end
--- 2015/20150128 - roman numbers - ruby/roman_spec.rb
+++ 2015/20150128 - roman numbers - ruby/roman_spec.rb
@@ -8,4 +8,12 @@ describe "A roman number" do
   it "represents 5 as V" do
     expect(to_roman(5)).to eq("V")
   end
+
+  it "represents 10 as X" do
+    expect(to_roman(10)).to eq("X")
+  end
+
+  it "represents 50 as L" do
+    expect(to_roman(50)).to eq("L")
+  end
 end
