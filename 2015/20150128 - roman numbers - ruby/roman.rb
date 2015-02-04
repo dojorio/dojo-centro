4c5fbb6a8a98ed26f0ff3ec3012b34edc1b63561
DojoRio <issae@issae.com>
2015-02-04 18:51:18 -0200

    Carlos Figueiredo e Cristina Silva

--- 2015/20150128 - roman numbers - ruby/roman.rb
+++ 2015/20150128 - roman numbers - ruby/roman.rb
@@ -1,22 +1,33 @@
 def to_roman(integer)
   roman_numbers = { 
-    1     => "I", #
-    5     => "V", 
-    10    => "X", 
-    50    => "L", 
-    100   => "C", 
+    1     => "I",
+    5     => "V",
+    10    => "X",
+    50    => "L",
+    100   => "C",
     500   => "D",
     1000  => "M"
   }
 
-  if integer == 2 || integer == 3
-    roman_number = roman_numbers[1] * integer
-  elsif integer == 20 || integer == 30
-    roman_number = roman_numbers[10] * (integer / 10)
-  elsif integer == 200 || integer == 300
-    roman_number = roman_numbers[100] * (integer / 100)
-  else
-    roman_numbers[integer]
-  end
+  latin_numbers = roman_numbers.keys.sort{|a,b| b <=> a}
+
+  latin_numbers.each do ||
+
+  # while integer > 0
+  #   iterator = 7
+  #   if roman_number.at[]
+      
+  #   end
+  # end
+
+  # if integer == 2 || integer == 3
+  #   roman_number = roman_numbers[1] * integer
+  # elsif integer == 20 || integer == 30
+  #   roman_number = roman_numbers[10] * (integer / 10)
+  # elsif integer == 200 || integer == 300
+  #   roman_number = roman_numbers[100] * (integer / 100)
+  # else
+  #   roman_numbers[integer]
+  # end
 
 end
