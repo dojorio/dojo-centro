4a278e00cab2491a0d31c8979e8df6bc8bfe86c3
DojoRio <issae@issae.com>
2015-03-18 20:02:06 -0300

    Vivian Macedo e Leandro Thimoteo

--- 2015/20150318 - Jokenpo - python/jokenpo.py
+++ 2015/20150318 - Jokenpo - python/jokenpo.py
@@ -1,2 +1,6 @@
 def jokenpo(play1, play2):
-    return 'draw'
+    if play1 == play2:
+        return 'draw'
+    if play2 == "spock"
+        return play1
+    return 'play2'
--- 2015/20150318 - Jokenpo - python/test_jokenpo.py
+++ 2015/20150318 - Jokenpo - python/test_jokenpo.py
@@ -9,4 +9,7 @@ class TestJokenpo(unittest.TestCase):
     def test_spock_and_lizard(self):
         self.assertEqual(jokenpo('spock', 'lizard'), 'play2')
 
+    def test_lizard_and_spock(self):
+        self.assertEqual(jokenpo('lizard', 'spock'), 'play1')
+
 unittest.main()
