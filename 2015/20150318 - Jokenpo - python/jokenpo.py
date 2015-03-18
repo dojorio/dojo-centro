1eb73ff3c7aaa4ec6993284a9008627972e09aa0
DojoRio <issae@issae.com>
2015-03-18 20:21:02 -0300

    Thiago Garcia e Jonatas Emidio

--- 2015/20150318 - Jokenpo - python/jokenpo.py
+++ 2015/20150318 - Jokenpo - python/jokenpo.py
@@ -1,8 +1,13 @@
 def jokenpo(play1, play2):
+    try:
+        rock = [play1, play2].index('rock') + 1
+        return 'play' + str(rock)
+    except:
+        pass
+
+
     if play1 == play2:
         return 'draw'
-    if play2 == 'rock' :
-        return 'play2'
     if play1 == 'lizard':
         return 'play1'
     return 'play2' 
