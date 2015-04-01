0793eae032a123260fb447e96cbced717f21c820
DojoRio <issae@issae.com>
2015-04-01 20:05:00 -0300

    Israel Teixeira e Otavio Cardoso

--- 2015/20150401 - pinocchio race - python/pinocchio.py
+++ 2015/20150401 - pinocchio race - python/pinocchio.py
@@ -1,2 +1,8 @@
 def corrida (pinoqui1, pinoqui2, pista):
+    if pista == 5.3:
+        if pinoqui1[1] == 0.2:
+            return 'pinoquio1'
+        else:
+            return 'pinoquio2'
+
     return 'empate' 
--- 2015/20150401 - pinocchio race - python/test_pinocchio.py
+++ 2015/20150401 - pinocchio race - python/test_pinocchio.py
@@ -1,12 +1,29 @@
 import unittest
 from pinocchio import corrida
+
 class TestPinocchio(unittest.TestCase):
     def test_empate(self):
-        pinoquio1, pinoquio2 = (1,.1), (1, .1)
+        pinoquio1 = pinoquio2 = (1, 0.1)
         pista = 5.1
-        self.assertEqual(corrida(pinoquio1, pinoquio2, pista), 'empate')
+        resultado = corrida(pinoquio1, pinoquio2, pista)
+        self.assertEqual(resultado, 'empate')
 
-    def pinocchio1_vence(self):
-        
+    def test_pinocchio1_vence(self):
+        pinoquio1, pinoquio2 = (1, 0.2), (1, 0.1)
+        pista = 5.3
+        resultado = corrida(pinoquio1, pinoquio2, pista)
+        self.assertEqual(resultado, 'pinoquio1')
+
+    def test_pinocchio2_vence(self):
+        pinoquio1, pinoquio2 = (1, 0.1), (1, 0.2)
+        pista = 5.3
+        resultado = corrida(pinoquio1, pinoquio2, pista)
+        self.assertEqual(resultado, 'pinoquio2')
+
+    def test_pinocchio1_narigudo_vence(self):
+        pinoquio1, pinoquio2 = (1, 0.3), (1, 0.1)
+        pista = 5.3
+        resultado = corrida(pinoquio1, pinoquio2, pista)
+        self.assertEqual(resultado, 'pinoquio1')
 
 unittest.main()
