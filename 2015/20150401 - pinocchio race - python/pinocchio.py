cc6010a1d5379abfa7cc5d9442e8d7438dc16f67
DojoRio <issae@issae.com>
2015-04-01 20:15:12 -0300

    Fabio Oliveira e Israel Teixeira

--- 2015/20150401 - pinocchio race - python/pinocchio.py
+++ 2015/20150401 - pinocchio race - python/pinocchio.py
@@ -1,6 +1,9 @@
 def corrida (pinoqui1, pinoqui2, pista):
+    nariz = 1
     if pista == 5.3:
-        if pinoqui1[1] == 0.2:
+        if pinoqui1[nariz] == pinoqui2[nariz]:
+            return 'empate'
+        elif pinoqui1[nariz] > pinoqui2[nariz]:
             return 'pinoquio1'
         else:
             return 'pinoquio2'
--- 2015/20150401 - pinocchio race - python/test_pinocchio.py
+++ 2015/20150401 - pinocchio race - python/test_pinocchio.py
@@ -26,4 +26,10 @@ class TestPinocchio(unittest.TestCase):
         resultado = corrida(pinoquio1, pinoquio2, pista)
         self.assertEqual(resultado, 'pinoquio1')
 
+    def test_empate_com_nariz_iguais(self):
+        pinoquio1, pinoquio2 = (1, 0.3), (1, 0.3)
+        pista = 5.3
+        resultado = corrida(pinoquio1, pinoquio2, pista)
+        self.assertEqual(resultado, 'empate')
+
 unittest.main()
