0a4eca8478c421261986e59a5ab39a5d3eec334a
DojoRio <issae@issae.com>
2015-04-01 19:44:33 -0300

    Otavio Cardoso e Eldio Santos

--- 2015/20150401 - pinocchio race - python/test_pinocchio.py
+++ 2015/20150401 - pinocchio race - python/test_pinocchio.py
@@ -1,10 +1,12 @@
 import unittest
-
+from pinocchio import corrida
 class TestPinocchio(unittest.TestCase):
-    def test_pass(self):
+    def test_empate(self):
         pinoquio1, pinoquio2 = (1,.1), (1, .1)
-        pista = 1.1
+        pista = 5.1
         self.assertEqual(corrida(pinoquio1, pinoquio2, pista), 'empate')
+
+    def pinocchio1_vence(self):
         
 
 unittest.main()
