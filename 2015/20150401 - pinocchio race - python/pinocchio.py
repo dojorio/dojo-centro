f67dce8cf7180c75637a20d95a97b97f0d0a270b
DojoRio <issae@issae.com>
2015-04-01 21:17:37 -0300

    Jonatas Emidio e Israel Teixeira

--- 2015/20150401 - pinocchio race - python/pinocchio.py
+++ 2015/20150401 - pinocchio race - python/pinocchio.py
@@ -1,9 +1,12 @@
+
 def corrida (pinoqui1, pinoqui2, pista):
     nariz = 1
     tamanho = 0
 
-    dist1 = 5.0/pinoqui1[tamanho] + pinoqui1[nariz]
-    dist2 = 5.0/pinoqui2[tamanho] + pinoqui2[nariz]
+    v = lambda pinoquio: 5.0/pinoquio[tamanho]
+
+    dist1 = v(pinoqui1) + pinoqui1[nariz]
+    dist2 = v(pinoqui2) + pinoqui2[nariz]
 
     if dist1 > dist2:
         return 'pinoquio1'
