2a882f763aa5c5a04bd3422740c1d6e8c1faa9fe
Dojo <dojo-rio@googlegroups.com>
2015-06-24 20:41:33 -0300

    Bernardo Maia e Jonatas Emidio

--- 2015/20150624 - pattern name - groovy/patternName.groovy
+++ 2015/20150624 - pattern name - groovy/patternName.groovy
@@ -1,7 +1,11 @@
 def static camel (snake) {
+	if (snake.length() > 1) {
+		snake.split('_').collect{ it[0].toUpperCase() + it.substring(1) }
+	}
+
 	if (snake.contains('_')) {
 		if (snake[1] == 'b') {return 'AbA'}
-	
+
 		return snake.replace('_', '').toUpperCase()
 	}
 
