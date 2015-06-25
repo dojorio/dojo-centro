7f0559d86a7d872d4571529f4882178f4e04c8fa
Dojo <dojo-rio@googlegroups.com>
2015-06-24 21:00:11 -0300

    Jonatas Emidio e Otavio Cardoso

--- 2015/20150624 - pattern name - groovy/patternName.groovy
+++ 2015/20150624 - pattern name - groovy/patternName.groovy
@@ -1,5 +1,5 @@
 def static camel (snake) {
-			snake.split('_').collect{ 
-			it[0].toUpperCase() + it.substring(1) 
-		}.join()
-	}
+	snake.split('_').collect {
+		it[0].toUpperCase() + it.substring(1)
+	}.join()
+}
