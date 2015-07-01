13654e8403343eb201249022dc74f312bd40c84a
Dojo <dojo-rio@googlegroups.com>
2015-07-01 20:19:30 -0300

    Israel Teixeira e charles

--- 2015/20150701 - caixa eletronico - ruby/caixa_eletronico.rb
+++ 2015/20150701 - caixa eletronico - ruby/caixa_eletronico.rb
@@ -1,18 +1,21 @@
 def sacar(valor)
+
+  notas = [10, 20, 50, 100]
+
   if valor == 100
-    return [100]
+    return notas[3]
   end
   if valor == 50
-    return [50]
+    return notas[2]
   end
 
-  if valor == 20
-    return [20]
+  if valor == 20 
+    return notas[1]
   end
 
   if valor == 30
-    return [10, 20]
+    return notas[0,1]
   end
 
-  [10]
+  return notas[0]
 end
