1db524e6b881670ea205c6c57f49f5920ef2c1ba
Dojo <dojo-rio@googlegroups.com>
2015-07-15 19:48:42 -0300

    toti Cavalcanti e Fabio Oliveira

--- 2015/20150715 - indice de equilibrio - ruby/indice.rb
+++ 2015/20150715 - indice de equilibrio - ruby/indice.rb
@@ -1,3 +1,7 @@
 def equilibrio(lista)
-  0
+  if lista.size == 1
+    return 0
+  else
+    return -1
+  end
 end
--- 2015/20150715 - indice de equilibrio - ruby/indice_spec.rb
+++ 2015/20150715 - indice de equilibrio - ruby/indice_spec.rb
@@ -4,4 +4,8 @@ describe "Indice de equilibrio" do
   it 'um elemento' do
     expect(equilibrio([1])).to eq(0)
   end
+
+  it 'dois elemento sem equilibrio' do
+    expect(equilibrio([1,2])).to eq(-1)
+  end
 end
