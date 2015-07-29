7a4acf4aca18a77af4ee763c0821aff2e7ed06ec
Dojo <dojo-rio@googlegroups.com>
2015-07-29 20:25:40 -0300

    Bernardo Maia e Otavio Cardoso

--- 2015/20150729 - uma inversao do barulho - python/inversao.py
+++ 2015/20150729 - uma inversao do barulho - python/inversao.py
@@ -1,10 +1,4 @@
 def inverte_um(numeros):
-	if numeros == [0,1] or \
-	numeros == [1,0]:
-		return [1, 1]
-	if numeros == [0, 0, 0]:
-		return [1,0,0]
-	if numeros == [0,1,0]:
-		return [1,1,0]
+	numeros[numeros.index(0)] = 1
 
-	return [1, 0]
+	return numeros
--- 2015/20150729 - uma inversao do barulho - python/test_inversao.py
+++ 2015/20150729 - uma inversao do barulho - python/test_inversao.py
@@ -25,4 +25,10 @@ class TestInversao(unittest.TestCase):
 	def test_1_0_0(self):
 		self.assertEquals(inverte_um([1, 0, 0]), [1, 1, 0])
 
+	def test_0_0_1(self):
+		self.assertEquals(inverte_um([0, 0, 1]), [1, 0, 1])
+
+	def test_1_0_0_1_1(self):
+		self.assertEquals(inverte_um([1, 0, 0, 1, 1]), [1, 1, 0, 1, 1])
+
 unittest.main()
