e00bb518fc5ef3e360809f74b2a2d662a3bb4e82
Dojo <dojo-rio@googlegroups.com>
2015-08-05 19:56:50 -0300

    Toti Cavalcanti e Otavio Cardoso

---
+++ 2015/20150805 - arrumando tarefas - ruby/tarefas.rb
@@ -0,0 +1,5 @@
+def perda_minima(horas, tarefas)
+  return 1 if tarefas.size == 2
+
+  0
+end
---
+++ 2015/20150805 - arrumando tarefas - ruby/tarefas_spec.rb
@@ -0,0 +1,15 @@
+require './tarefas'
+
+describe 'Tarefas' do
+  it 'uma hora uma tarefa até uma hora' do
+    horas = 1
+    tarefas = [[1, 1]]
+    expect(perda_minima(horas, tarefas)).to eq(0)
+  end
+
+  it 'uma hora duas tarefas' do
+    horas = 1
+    tarefas = [[1, 1], [1, 1]]
+    expect(perda_minima(horas, tarefas)).to eq(1)
+  end
+end
