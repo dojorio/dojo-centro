b92f2ef347fa16c238625045219ee92c6b431df9
Dojo <dojo-rio@googlegroups.com>
2015-08-05 21:23:26 -0300

    Jonatas Emidio e Otavio Cardoso

--- 2015/20150805 - arrumando tarefas - ruby/tarefas.rb
+++ 2015/20150805 - arrumando tarefas - ruby/tarefas.rb
@@ -1,7 +1,7 @@
 def perda_minima(horas, tarefas)
   if tarefas.size == 2
     if horas == 1 || (tarefas[0][1] == 1 && tarefas[1][1] != 2)
-      return [tarefas[0][0], tarefas[1][0]].min
+      return tarefas.map(&:first).min
     end
   end
 
@@ -9,7 +9,7 @@ def perda_minima(horas, tarefas)
     if horas == 2 || tarefas[0][1] == 1 || 
       tarefas[1][1] == 1 || tarefas[1][1] == 2 && 
       tarefas[0][1] < 3
-      return 1 
+      return tarefas.map(&:first).min
     end
   end
 
