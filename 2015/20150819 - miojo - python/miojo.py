ecd8ce7895a29880f31d791c2399c7d1601599a8
Dojo <dojo-rio@googlegroups.com>
2015-08-19 20:54:12 -0300

    Otavio Cardoso e Elias Tandel

--- 2015/20150819 - miojo - python/miojo.py
+++ 2015/20150819 - miojo - python/miojo.py
@@ -1,15 +1,18 @@
 def tempo_minimo(ampulheta_1, ampulheta_2):
-	if {1, 3} & {ampulheta_1, ampulheta_2}:
+	if (ampulheta_1, ampulheta_2) == (1, 1):
+		return 3
+	elif 3 in (ampulheta_1, ampulheta_2):
 		return 3
 	elif ampulheta_1 % 2 == 0 and ampulheta_2 % 2 == 0:
 		return None
-	elif ampulheta_2 == ampulheta_1:
+	elif ampulheta_2 == ampulheta_1 and ampulheta_1 > 3:
 		return None
 
 	maior = max(ampulheta_1, ampulheta_2)
 	menor = min(ampulheta_1, ampulheta_2)
 
-	if maior - menor == 3:
+	diff = maior - menor
+	if diff == 3:
 		return maior
 	else:
-		return menor + tempo_minimo(menor, maior - menor)
+		return menor + tempo_minimo(menor, diff)
