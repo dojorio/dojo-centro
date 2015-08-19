d397e103005b5447b715e815845592e741d23d51
Dojo <dojo-rio@googlegroups.com>
2015-08-19 20:06:32 -0300

    Toti Cavalcanti e Jonathan Alves

--- 2015/20150819 - miojo - python/miojo.py
+++ 2015/20150819 - miojo - python/miojo.py
@@ -1,7 +1,7 @@
 def tempo_minimo(ampulheta_1, ampulheta_2):
 	if ampulheta_1 == 1:
 		return 3
-	elif ampulheta_1 != 3 and ampulheta_2 == 4 and ampulheta_1 == ampulheta_2:
+	elif ampulheta_1 != 3 or (ampulheta_1 == 2 and ampulheta_2 == 4) or ampulheta_1 == ampulheta_2:
 		return None
 	else:
 		return max(ampulheta_1, ampulheta_2)
