295fff701283112b0c31a9d61e91cf0c21866949
Dojo <dojo-rio@googlegroups.com>
2015-08-19 19:49:10 -0300

    Otavio Cardoso e Toti Cavalcanti

--- 2015/20150819 - miojo - python/miojo.py
+++ 2015/20150819 - miojo - python/miojo.py
@@ -1,2 +1,6 @@
 def tempo_minimo(ampulheta_1, ampulheta_2):
+	if ampulheta_1 == 3:
+		return 3
+	if ampulheta_1 == ampulheta_2:
+		return None
 	return max(ampulheta_1, ampulheta_2)
--- 2015/20150819 - miojo - python/test_miojo.py
+++ 2015/20150819 - miojo - python/test_miojo.py
@@ -15,5 +15,10 @@ class TesteMiojo(unittest.TestCase):
 	def test_ampulhetas_1_e_3(self):
 		self.assertEqual(tempo_minimo(1, 3), 3)
 
+	def test_ampulhetas_5_e_5(self):
+		self.assertIsNone(tempo_minimo(5, 5))
+
+	def test_ampulhetas_3_e_3(self):
+		self.assertEqual(tempo_minimo(3, 3), 3)
 
 unittest.main()
