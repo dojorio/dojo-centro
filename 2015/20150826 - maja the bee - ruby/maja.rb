63449e5ea83e073ed5594a98c8a5fb0f5752b38e
Dojo <dojo-rio@googlegroups.com>
2015-08-26 20:56:42 -0300

    Fabio Oliveira e Toti Cavalcanti

--- 2015/20150826 - maja the bee - ruby/maja.rb
+++ 2015/20150826 - maja the bee - ruby/maja.rb
@@ -7,6 +7,13 @@ def maja_coord(willi)
   
   elsif willi == 4
     return [-1, 0]
+
+  elsif willi == 5
+    return [0, -1]
+  end
+
+elsif willi == 6
+    return [1, -1]
   end
 
   [0, 0]
--- 2015/20150826 - maja the bee - ruby/maja_spec.rb
+++ 2015/20150826 - maja the bee - ruby/maja_spec.rb
@@ -6,5 +6,6 @@ describe "Maja" do
   it { expect(maja_coord(3)).to eq([-1, 1]) }
   it { expect(maja_coord(4)).to eq([-1, 0]) }
   it { expect(maja_coord(5)).to eq([0, -1]) }
+  it { expect(maja_coord(6)).to eq([1, -1]) }
 
 end
