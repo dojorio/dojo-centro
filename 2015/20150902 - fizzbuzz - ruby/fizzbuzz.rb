60d35b3ae3f9620cefd7ed4d1d110c4def5df5f5
Dojo <dojo-rio@googlegroups.com>
2015-09-02 19:38:41 -0300

    Andre Galdino e Fernanda Teich

--- 2015/20150902 - fizzbuzz - ruby/fizzbuzz.rb
+++ 2015/20150902 - fizzbuzz - ruby/fizzbuzz.rb
@@ -1,5 +1,18 @@
 def fizzbuzz(num) 
-	return "1" if (num == 1)
+	#return "1" if (num == 1)
+
+	case num
+
+	when 1
+
+	return "1"
+		
+
+	return "fizz" if (num == 3)
+
+	return "buzz" if (num == 5)
 
 	return "2"	
+
 end
+
--- 2015/20150902 - fizzbuzz - ruby/fizzbuzz_spec.rb
+++ 2015/20150902 - fizzbuzz - ruby/fizzbuzz_spec.rb
@@ -5,6 +5,6 @@ describe "inicio" do
 	it {expect(fizzbuzz(1)).to eq("1")}
 	it {expect(fizzbuzz(2)).to eq("2")}
 	it {expect(fizzbuzz(3)).to eq("fizz")}
-
+    it {expect(fizzbuzz(5)).to eq("buzz")}
 
 end
