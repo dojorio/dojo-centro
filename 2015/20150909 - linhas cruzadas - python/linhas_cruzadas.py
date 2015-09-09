3b6e98b0dbe725197d90ac54fbc0558bd21cd881
Dojo <dojo-rio@googlegroups.com>
2015-09-09 19:34:39 -0300

    Willian Alves e Fabio Oliveira

--- 2015/20150909 - linhas cruzadas - python/linhas_cruzadas.py
+++ 2015/20150909 - linhas cruzadas - python/linhas_cruzadas.py
@@ -1,2 +1,4 @@
 def linhas_cruzadas(linha1, linha2):
+	if linha1 != linha2:
+		return False
 	return True
--- 2015/20150909 - linhas cruzadas - python/test_linhas_cruzadas.py
+++ 2015/20150909 - linhas cruzadas - python/test_linhas_cruzadas.py
@@ -7,4 +7,9 @@ class TestLinhasCruzadas(unittest.TestCase):
 		linha2 = ((0,0), (1,0))
 		self.assertTrue(linhas_cruzadas(linha1, linha2))
 
+	def test_2_linhas_que_não_se_cruzam(self):
+		linha1 = ((0,0), (1,0))
+		linha2 = ((2,0), (3,0))
+		self.assertFalse(linhas_cruzadas(linha1, linha2))
+
 unittest.main()
