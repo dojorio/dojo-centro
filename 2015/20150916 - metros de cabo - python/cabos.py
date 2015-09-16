ff988fb39c235f5ec8f667223f9de62f62ded1bf
Dojo <dojo-rio@googlegroups.com>
2015-09-16 20:25:30 -0300

    Flávio Amieiro e Israel Teixeira

--- 2015/20150916 - metros de cabo - python/cabos.py
+++ 2015/20150916 - metros de cabo - python/cabos.py
@@ -1,7 +1,10 @@
 def metros_de_cabo(grafo):
-	pesos = (aresta[1] for aresta in grafo)
-	vertices_unicos = set(sum((aresta[0] for aresta in grafo), tuple()))
-	if len(vertices_unicos) - 1< len(grafo):
-		return sum(pesos) - max(pesos)
+	pesos = [aresta[1] for aresta in grafo]
+	vertices = sum((aresta[0] for aresta in grafo), tuple())
+	vertices_unicos = set(vertices)
+	minimo_de_arestas_necessarias = len(vertices_unicos) -1
+
+	if minimo_de_arestas_necessarias < len(grafo):
+		return sum(pesos) - max(list(pesos))
 	return sum(pesos)
 	
