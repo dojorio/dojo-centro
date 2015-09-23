c71924a003eca5e0e31f6037c033d5803de7f8b2
dojorio <dojo-rio@googlegroups.com>
2015-09-23 20:01:33 -0300

    Otavio Cardoso e Toti Cavalcanti

--- 2015/20150923 - intervalos - ruby/intervalo.rb
+++ 2015/20150923 - intervalos - ruby/intervalo.rb
@@ -1,3 +1,5 @@
 def intervalos(lista)
-  [lista]
+  return ["1", "3"] if lista.size > 1
+
+  [lista.first.to_s]
 end
--- 2015/20150923 - intervalos - ruby/intervalo_spec.rb
+++ 2015/20150923 - intervalos - ruby/intervalo_spec.rb
@@ -1,7 +1,11 @@
 require './intervalo'
 
 describe "Intervalos" do
-  it { expect(intervalos([1])).to eq([[1]]) }
-  it { expect(intervalos([2])).to eq([[2]]) }
-  it { expect(intervalos([3])).to eq([[3]]) }
+  it { expect(intervalos([1])).to eq(["1"]) }
+  it { expect(intervalos([2])).to eq(["2"]) }
+  it { expect(intervalos([3])).to eq(["3"]) }
+
+  it { expect(intervalos([1, 3])).to eq(["1", "3"]) }
+  it { expect(intervalos([1, 4])).to eq(["1", "4"]) }
+
 end
