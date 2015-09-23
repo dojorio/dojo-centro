89324e3320418fdac648f57e51b9bbfef5fdba12
dojorio <dojo-rio@googlegroups.com>
2015-09-23 20:32:42 -0300

    Toti Cavalcanti e Otavio Cardoso

--- 2015/20150923 - intervalos - ruby/intervalo.rb
+++ 2015/20150923 - intervalos - ruby/intervalo.rb
@@ -1,20 +1,18 @@
 def intervalos(lista)
-  if lista[2] == 5
-    return ["2-3", "5"]
-  end
-  
-  if lista == [1,2,4]
-    return ["1-2", "4"]
-  end
-
-  if lista.size == 2 && lista[1] - lista[0] == 1
-    return ["#{lista[0]}-#{lista[1]}"]
-  end
-
   resultado = []
 
-  lista.each do |elemento|
-    resultado << elemento.to_s  
+  if lista.size > 1
+    if lista[1] - lista[0] == 1
+      resultado << "#{lista[0]}-#{lista[1]}"
+    end
+
+    if lista.size == 3
+      resultado << lista[2].to_s
+    end
+  else
+    lista.each do |elemento|
+      resultado << elemento.to_s  
+    end
   end
 
   resultado
--- 2015/20150923 - intervalos - ruby/intervalo_spec.rb
+++ 2015/20150923 - intervalos - ruby/intervalo_spec.rb
@@ -15,5 +15,5 @@ describe "Intervalos" do
  
   it { expect(intervalos([1, 2, 4])).to eq(["1-2", "4"]) }
   it { expect(intervalos([2, 3, 5])).to eq(["2-3", "5"]) }
-
+  it { expect(intervalos([3, 4, 6])).to eq(["3-4", "6"]) }
 end
