11d64c827687e23a6de5749c291734408222bd24
dojorio <dojo-rio@googlegroups.com>
2015-09-23 21:08:45 -0300

    Otavio Cardoso e Toti Cavalcanti

--- 2015/20150923 - intervalos - ruby/intervalo.rb
+++ 2015/20150923 - intervalos - ruby/intervalo.rb
@@ -1,9 +1,22 @@
 def intervalos(lista)
   resultado = []
 
-  if lista.size == 3
+  if lista.size == 1
+    resultado << lista[0].to_s
+  end
+
+  if lista.size == 2 
     if lista[1] - lista[0] == 1
-      if lista[2] - lista[1] == 1
+      resultado << "#{lista[0]}-#{lista[1]}"
+    else
+      resultado << lista[0].to_s
+      resultado << lista[1].to_s
+    end
+  end
+
+  if lista.size == 3
+    if lista[2] - lista[1] == 1
+      if lista[1] - lista[0] == 1
         resultado << "#{lista[0]}-#{lista[2]}"
       else
         resultado << "#{lista[0]}-#{lista[1]}"
@@ -20,19 +33,6 @@ def intervalos(lista)
       end
     end
   end
-
-  if lista.size == 2 
-    if lista[1] - lista[0] == 1
-      resultado << "#{lista[0]}-#{lista[1]}"
-    else
-      resultado << lista[0].to_s
-      resultado << lista[1].to_s
-    end
-  end
-
-  if lista.size == 1
-    resultado << lista[0].to_s
-  end
-
+  
   resultado
 end
