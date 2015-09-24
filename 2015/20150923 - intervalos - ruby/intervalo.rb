77b77a2a9b3364df156090b67d65978adf48fab8
dojorio <dojo-rio@googlegroups.com>
2015-09-23 21:00:51 -0300

    Toti Cavalcanti e Otavio Cardoso

--- 2015/20150923 - intervalos - ruby/intervalo.rb
+++ 2015/20150923 - intervalos - ruby/intervalo.rb
@@ -1,21 +1,37 @@
 def intervalos(lista)
   resultado = []
 
-  if lista.size == 3 && lista[1] - lista[0] == 1 && lista[2] - lista[1] == 1
-    return ["1-3"]
-  end
-  if lista.size > 1 && lista[1] - lista[0] == 1
-    resultado << "#{lista[0]}-#{lista[1]}"
-    resultado << lista[2].to_s if lista.size == 3
-  else
-    resultado << lista[0].to_s
+  if lista.size == 3
+    if lista[1] - lista[0] == 1
+      if lista[2] - lista[1] == 1
+        resultado << "#{lista[0]}-#{lista[2]}"
+      else
+        resultado << "#{lista[0]}-#{lista[1]}"
+        resultado << lista[2].to_s
+      end
+    else
+      resultado << lista[0].to_s
 
-    if lista.size == 3 && lista[2] - lista[1] == 1
-      resultado << "#{lista[1]}-#{lista[2]}"
-    elsif lista.size > 1
-      resultado << lista[1].to_s
-      resultado << lista[2].to_s if lista.size == 3
+      if lista[2] - lista[1] == 1
+        resultado << "#{lista[1]}-#{lista[2]}"
+      else
+        resultado << lista[1].to_s
+        resultado << lista[2].to_s
+      end
     end
+  end
+
+  if lista.size == 2 
+    if lista[1] - lista[0] == 1
+      resultado << "#{lista[0]}-#{lista[1]}"
+    else
+      resultado << lista[0].to_s
+      resultado << lista[1].to_s
+    end
+  end
+
+  if lista.size == 1
+    resultado << lista[0].to_s
   end
 
   resultado
