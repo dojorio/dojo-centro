fb7d208c92163ea58be0d42c000123c3f87bd786
dojorio <dojo-rio@googlegroups.com>
2015-09-30 20:27:07 -0300

    Otavio Cardoso e  Rai Mesquita

--- 2015/20150930 - ratinho - groovy/ratinho.groovy
+++ 2015/20150930 - ratinho - groovy/ratinho.groovy
@@ -1,10 +1,14 @@
 def static testeDeDna(pai1, pai2, filho) {
 
-	if (pai1[1][0] == pai2[1][0] || 
-	   ( pai1[1].length() > 1 && pai1[1][1] == pai2[1][1]))
+	if (pai1[1].length() > 1)
+		if (pai1[1] == filho)
+			return pai1[0]
+		else if (pai1[1][1] == pai2[1][1])
 			return 'CADIM'
-	if (pai1[1] == filho)
-		return pai1[0]
+
+	if (pai1[1] == pai2[1])
+		return 'CADIM'
+
 
 	pai1[1]==filho?pai1[0]:(pai2[1]==filho?pai2[0]:'CADIM')
 
