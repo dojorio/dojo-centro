61e04b8453a9e064e558a334f255e6b541fe86b9
dojorio <dojo-rio@googlegroups.com>
2015-09-30 20:16:30 -0300

    Toti Cavalcanti e Jonatas Emidio

--- 2015/20150930 - ratinho - groovy/ratinho.groovy
+++ 2015/20150930 - ratinho - groovy/ratinho.groovy
@@ -1,11 +1,10 @@
 def static testeDeDna(pai1, pai2, filho) {
 
-	if (pai1[1][0] == pai2[1][0])
-		return 'CADIM'
-	else
-		if (pai1[1].length() > 1 && pai1[1][1] == pai2[1][1])
+	return 	pai1[1]==filho?pai1[0]:(pai2[1]==filho?pai2[0]:'CADIM')
+
+	if (pai1[1][0] == pai2[1][0] || 
+	   ( pai1[1].length() > 1 && pai1[1][1] == pai2[1][1]))
 			return 'CADIM'
 
-	pai1[1]==filho?pai1[0]:(pai2[1]==filho?pai2[0]:'CADIM')
 
 }
