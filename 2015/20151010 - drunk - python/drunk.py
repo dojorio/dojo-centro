e5c03eeff51ae84e8107cff99ab810316efbdfff
dojorio <dojo-rio@googlegroups.com>
2015-10-07 20:51:42 -0300

    Jonatas Emidio e Toti Cavalcanti

--- 2015/20151010 - drunk - python/drunk.py
+++ 2015/20151010 - drunk - python/drunk.py
@@ -1,2 +1,2 @@
-def fail(initial_house, drunks_house, tries):
+def fail(initial_house, drunks_house, tries, probabilities):
 	return 0
--- 2015/20151010 - drunk - python/test_drunk.py
+++ 2015/20151010 - drunk - python/test_drunk.py
@@ -4,15 +4,13 @@ from drunk import fail
 
 class TestDrunk(unittest.TestCase):
 	def test_one_house(self):
-		initial_house = 0
-		drunks_house = 0
-		tries = 1
 		probabilities = [
 			[1],
 		]
-
-		self.assertEqual(fail(initial_house,
-		 					   drunks_house, 
-		 					   tries), 0)
+		result_fail = fail(initial_house=0,
+		 				   drunks_house=0, 
+		 				   tries=1,
+		 				   probabilitie=probabilities)
+		self.assertEqual(result_fail, 0)
 
 unittest.main()
