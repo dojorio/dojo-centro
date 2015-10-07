66a9a15d4c0db4508f6191397d24b63f2331f9dd
dojorio <dojo-rio@googlegroups.com>
2015-10-07 20:56:58 -0300

    Israel Teixeira e Jonatas Emidio

--- 2015/20151010 - drunk - python/test_drunk.py
+++ 2015/20151010 - drunk - python/test_drunk.py
@@ -10,7 +10,17 @@ class TestDrunk(unittest.TestCase):
 		result_fail = fail(initial_house=0,
 		 				   drunks_house=0, 
 		 				   tries=1,
-		 				   probabilitie=probabilities)
+		 				   probabilities=probabilities)
 		self.assertEqual(result_fail, 0)
 
+	def test_two_houses(self):
+		probabilities = [
+			[.5, .5], 
+			[.5, .5]
+		]
+		result_fail = fail(initial_house=1,
+		 				   drunks_house=0, 
+		 				   tries=1,
+		 				   probabilities=probabilities)
+		self.assertEqual(result_fail, 0)
 unittest.main()
