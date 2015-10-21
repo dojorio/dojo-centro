8313d61024fd3b66457623e21a1dc1eb60abb2a6
dojorio <dojo-rio@googlegroups.com>
2015-10-21 20:40:48 -0200

    Otavio Cardoso e Toti Cavalcanti

--- 2015/20151021 - back to the future - ruby/back_to_the_future_spec.rb
+++ 2015/20151021 - back to the future - ruby/back_to_the_future_spec.rb
@@ -24,4 +24,16 @@ describe "Back to the Future" do
       expect(min_money(routes, 2, 3)).to eq(2)
     end
   end
+
+  context "other route" do
+    let(:routes) { [[1, 2, 2]] }
+
+    it 'costs 4 with 2 friends and 3 sits' do
+      expect(min_money(routes, 2, 3)).to eq(4)
+    end
+
+    it 'costs 4 with 3 friends and 3 sits' do
+      expect(min_money(routes, 3, 3)).to eq(6)
+    end
+  end
 end
