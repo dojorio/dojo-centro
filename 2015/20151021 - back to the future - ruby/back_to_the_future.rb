ee8a989703a4316adfe26ef3998b16765800939b
dojorio <dojo-rio@googlegroups.com>
2015-10-21 21:30:48 -0200

    Toti Cavalcanti e Otavio Cardoso

--- 2015/20151021 - back to the future - ruby/back_to_the_future.rb
+++ 2015/20151021 - back to the future - ruby/back_to_the_future.rb
@@ -1,10 +1,11 @@
 def min_money(routes, friends, sits)
-
   return 'impossible' if friends > sits
 
   routes = routes.sort_by { |route| route[1] }
-  
-  return friends * 2 if routes.last[0] == 2
+
+  if routes.last[0] == 2
+    return friends * routes.map(&:last).reduce(0, :+)
+  end
 
   friends * routes.last[2]
 end
