e50b66270f31a8e49d933410359ee41e4642be7b
dojorio <dojo-rio@googlegroups.com>
2015-10-28 19:54:57 -0200

    cleyton P. Santos e jonatas Emidio

--- 2015/20151028 - fizzbuzz - ruby/fizzbuzz.rb
+++ 2015/20151028 - fizzbuzz - ruby/fizzbuzz.rb
@@ -1,16 +1,7 @@
 def fizzbuzz(number)
-
-  if number % 15 == 0
-    return "FizzBuzz"
-  end
-
-  if number % 3 == 0 
-    return "Fizz"
-  end
-
-  if number % 5 == 0
-    return "Buzz"
-  end
-
+  return "FizzBuzz" if number % 15 == 0
+  return "Fizz"     if number % 3 == 0 
+  return "Buzz"     if number % 5 == 0
+  
   number.to_s
 end
