a0dfce34dd85aff854532078f774115aed88a441
dojorio <dojo-rio@googlegroups.com>
2015-11-04 19:30:57 -0200

    Flávio Amieiro e Alex Tercete

---
+++ 2015/20151104 - minesweeper - go/mines.go
@@ -0,0 +1,5 @@
+package mines
+
+func FillMines(board string) string {
+  return board
+}
--- 2015/20151104 - minesweeper - go/mines_test.go
+++ 2015/20151104 - minesweeper - go/mines_test.go
@@ -1,2 +1,12 @@
+package mines
+
 import "testing"
 
+func TestMines(t *testing.T) {
+	// "*" -> "*"
+	got := FillMines("*")
+	want := "*"
+	if got != want {
+		t.Errorf("FillMines() = %v; want %v", got, want)
+	}
+}
