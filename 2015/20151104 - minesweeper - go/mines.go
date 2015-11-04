76d8df3942603c069ede102274c9a0b43c69a35e
dojorio <dojo-rio@googlegroups.com>
2015-11-04 20:37:50 -0200

    Otavio Cardoso e Alex Tercete

--- 2015/20151104 - minesweeper - go/mines.go
+++ 2015/20151104 - minesweeper - go/mines.go
@@ -3,6 +3,20 @@ package mines
 import "strings"
 import "strconv"
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func FillMines(board string) string {
 	var out string
 	for i, c := range board {
@@ -10,6 +24,8 @@ func FillMines(board string) string {
 		case '*':
 			out += "*"
 		case '.':
+			start := i - 1
+			i = i + 1 if i == 0
 			count := strings.Count(board[i-1:i+2], "*")
 			out += strconv.Itoa(count)
 		}
