d61cf607683d781a8fbf666d2686c80e6672dcc7
dojorio <dojo-rio@googlegroups.com>
2015-11-04 20:26:54 -0200

    Israel Teixeira e Rodolfo Carvalho

--- 2015/20151104 - minesweeper - go/mines.go
+++ 2015/20151104 - minesweeper - go/mines.go
@@ -4,6 +4,15 @@ import "strings"
 import "strconv"
 
 func FillMines(board string) string {
-	count := strings.Count(board, "*")
-	return strings.Replace(board, ".", strconv.Itoa(count), -1)
+	var out string
+	for i , c:= range board{
+		switch c {
+		case "*":
+			out += "*"
+		case ".":
+			count := strings.Count(board[i-1:i] +board[i+1:i+2] "*")
+	out += strings.Replace(board, ".", strconv.Itoa(count), -1)
+				}
+	}	
+	return out
 }
