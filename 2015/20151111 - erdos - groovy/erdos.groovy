4bf3144fe141d4c2f31a88fa9e1c2883bab70b6c
dojorio <dojo-rio@googlegroups.com>
2015-11-11 20:42:25 -0200

    Toti Cavalcanti e Jonatas Emidio

--- 2015/20151111 - erdos - groovy/erdos.groovy
+++ 2015/20151111 - erdos - groovy/erdos.groovy
@@ -2,13 +2,16 @@ static def numbers(articles) {
 	
 	def result = ["Erdos": 0]
 
-	if(articles.X.size() == 1){
-		return result
-	} else if(articles.X.size() == 2){
+	if(articles.X.size() == 2){
 		def nome = articles.X[1]
 		result[nome] = 1
-		return result
-	} else {
-		return  ["Erdos": 0, "Ze": 1, "Maria": 1]
+
+	} else if(articles.X.size() == 3){
+		result[articles.X[1]] = 1
+		result[articles.X[2]] = 
+		
 	}
+
+	return  result 
+
 }
