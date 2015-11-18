2d1915cfdc00d24031b65393006094fda40373e4
dojorio <dojo-rio@googlegroups.com>
2015-11-18 20:14:15 -0200

    Lorena Miceli e Bruno Barros

--- 2015/20151118 - cat taro - ruby/cat_taro.rb
+++ 2015/20151118 - cat taro - ruby/cat_taro.rb
@@ -1,7 +1,6 @@
 def cat_taro(string)
-  if string == "C" || string == "A" || 
-    string.length == 2 || string == "B" ||
-    string == "X" 
+  if string != "C" || string != "A" || 
+    string.length != 3 || string != "T"
     return "Impossible"
   end
 
