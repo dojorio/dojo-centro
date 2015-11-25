5bdbae713d452a4c8a95baa8ef6b403586cd5435
dojorio <dojo-rio@googlegroups.com>
2015-11-25 19:52:04 -0200

    Marcio e Toti Cavalcanti

--- 2015/20151125 - folding - ruby/folding.rb
+++ 2015/20151125 - folding - ruby/folding.rb
@@ -1,3 +1,5 @@
 def fold_test(tape_in, tape_out)
+
+  return false if tape_out[0] == 2 || tape_out[0] == 3
   return true
 end
--- 2015/20151125 - folding - ruby/folding_spec.rb
+++ 2015/20151125 - folding - ruby/folding_spec.rb
@@ -8,4 +8,12 @@ describe "Folding" do
   it "is false for tapes size 1 and in != out" do
     expect(fold_test([1],[2])).to eq(false)
   end
+
+  it "is false for tapes size 1 and in != out" do
+    expect(fold_test([1],[3])).to eq(false)
+  end
+
+  it "is false for tapes size 1 and in != out" do
+    expect(fold_test([1],[4])).to eq(false)
+  end
 end
