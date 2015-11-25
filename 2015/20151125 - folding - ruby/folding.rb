bcdf585339032512232700ff51344c5f4a7c1df9
dojorio <dojo-rio@googlegroups.com>
2015-11-25 20:38:17 -0200

    jonatas Emidio e Marcio

--- 2015/20151125 - folding - ruby/folding.rb
+++ 2015/20151125 - folding - ruby/folding.rb
@@ -3,9 +3,11 @@ def fold_test(tape_in, tape_out)
 
   if tape_out.size == tape_in.size
     return true if tape_in == tape_out || tape_in.reverse == tape_out
+    return false if tape_in[0] != tape_out[0] 
   end
 
-  return true if tape_out == [3]
+
+  ##return true if tape_out == [3]
 
   false
 end
