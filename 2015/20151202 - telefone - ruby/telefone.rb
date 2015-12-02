9473b07fe2e8df3f806c1fb9e9ae18790f4fb859
dojorio <dojo-rio@googlegroups.com>
2015-12-02 19:58:34 -0200

    Otavio Cardoso e Marcio de Jesus

--- 2015/20151202 - telefone - ruby/telefone.rb
+++ 2015/20151202 - telefone - ruby/telefone.rb
@@ -1,7 +1,9 @@
 def telefone_de(nome)
-  if nome == 'D' || nome == 'E'
+  if nome == 'D' || nome == 'E' || nome == 'F'
     return '3'
-  end
+  elsif nome == 'G' || nome == 'H' || nome == 'I'
+    return '4'
+  end  
 
   '2'
 end
--- 2015/20151202 - telefone - ruby/telefone_spec.rb
+++ 2015/20151202 - telefone - ruby/telefone_spec.rb
@@ -12,4 +12,20 @@ describe "Telefone" do
   it "is 3 for E" do
     expect(telefone_de('E')).to eq('3')
   end
+
+    it "is 3 for F" do
+    expect(telefone_de('F')).to eq('3')
+  end
+
+    it "is 4 for G" do
+    expect(telefone_de('G')).to eq('4')
+  end
+
+     it "is 4 for H" do
+    expect(telefone_de('H')).to eq('4')
+  end
+
+    it "is 4 for I" do
+    expect(telefone_de('I')).to eq('4')
+  end
 end
