afa4e2648dbea80dff5a5e6fc5f5965036a43504
dojorio <dojo-rio@googlegroups.com>
2015-12-09 19:54:55 -0200

    Toti Cavalcanti e Otavio Cardoso

--- 2015/20151209 - animal game - ruby/animal_game.rb
+++ 2015/20151209 - animal game - ruby/animal_game.rb
@@ -1,4 +1,9 @@
 def prize(value, bet, drawed)
-  return value * 16 if bet <= 1299
+  verifier = bet - 1200
+  drawed = 100 if drawed == 0
+
+  return value * 16 if verifier / 4 == drawed / 4
+
+  
   0
 end
--- 2015/20151209 - animal game - ruby/animal_game_spec.rb
+++ 2015/20151209 - animal game - ruby/animal_game_spec.rb
@@ -5,16 +5,28 @@ describe "Animal game" do
     expect(prize(1, 1234, 0)).to eq(0)
   end
 
-  it "gives 16x value if is from same group" do
+  it "gives 16x value if is from same group 1" do
     expect(prize(1, 1299, 0)).to eq(16)
   end
 
-  it "gives 16x value if is from same group" do
+  it "gives 16x value if is from same group 2" do
     expect(prize(2, 1299, 0)).to eq(32)
   end
 
-  it "gives 16x value if is from same group" do
+  it "gives 16x value if is from same group 3" do
     expect(prize(2, 1297, 0)).to eq(32)
+  end
+
+  it "gives 16x value if is from same group 4" do
+    expect(prize(2, 1201, 2)).to eq(32)
+  end
+
+  it "gives 16x value if is from same group 5" do
+    expect(prize(2, 1205, 7)).to eq(32)
+  end
+
+  it "gives 16x value if is from same group 6" do
+    expect(prize(2, 1205, 3)).to eq(0)
   end
 
 end
