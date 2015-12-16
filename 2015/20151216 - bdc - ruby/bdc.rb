9e403815b89e23944951d2cd438934ddd02b27ec
dojorio <dojo-rio@googlegroups.com>
2015-12-16 20:53:22 -0200

    Toti Cavalcanti e Otavio Cardoso

--- 2015/20151216 - bdc - ruby/bdc.rb
+++ 2015/20151216 - bdc - ruby/bdc.rb
@@ -1,4 +1,15 @@
 def bdc(size, grid)
-  return 0 if size > 1
-  grid.first.first
+  return 0 if size > grid.size
+  
+  count = 0
+  
+  grid.each do |line|
+    line.each do |cell|
+      if cell == 1
+        count = count + 1
+      end
+    end
+  end
+
+  count
 end
--- 2015/20151216 - bdc - ruby/bdc_spec.rb
+++ 2015/20151216 - bdc - ruby/bdc_spec.rb
@@ -9,11 +9,16 @@ describe "BDC" do
     expect(bdc(1, [[1]])).to eq(1)
   end
 
-  it "1x1 and size >1 grid 1" do
+  it "1x1 and size > 1 grid 1" do
     expect(bdc(2, [[1]])).to eq(0)
   end
 
   it "2x1 and size 1 grid 0,1" do
     expect(bdc(1, [[0, 1]])).to eq(1)
+  end
+
+  it "1x2 and size 2 grid 1,1" do
+    expect(bdc(2, [[1],
+                   [1]])).to eq(1)
   end
 end
