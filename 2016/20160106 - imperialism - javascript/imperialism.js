f88af63eaaace3576940ad29760d0275b3a0f908
dojorio <dojo-rio@googlegroups.com>
2016-01-06 21:04:19 -0200

    Giovanni Bassi e Julio Marins

--- 2016/20160106 - imperialism - javascript/imperialism.js
+++ 2016/20160106 - imperialism - javascript/imperialism.js
@@ -1,17 +1,8 @@
 exports.conquer = function (roads) {
-
-    var test = roads.reduce(function(memo, elm){
-        if(memo.indexOf(elm) === -1){
-            memo.push(elm);
-        }
-        return memo;
-    }, []).length
-
-    if (test >= 2 && test == roads.length){
-        return test - 1
-    } else {
-        return test
-    }
+    var graph = exports.toGraph(roads);
+    graph.map(function(v){
+        return v.length
+    })
 };
 
 exports.toGraph = function(roads) {
