bacccb5e210333db95b4898aa9fe8149a7bda5d5
dojorio <dojo-rio@googlegroups.com>
2016-01-13 20:48:52 -0200

    Marcio de Jesus e Toti Cavalcanti

--- 2016/20160113 - jukebox - javascript/jukebox.js
+++ 2016/20160113 - jukebox - javascript/jukebox.js
@@ -5,13 +5,14 @@ exports.search = function (playlist) {
         return 9;
     }
 
-    if (musics.length >= 4){
+    if (musics.length >= 3){
         return musics.length + 1;
     }
 
     if (musics[0] == 'aa') {
         return 3;
     }
+
 
     return musics.length;
 };
