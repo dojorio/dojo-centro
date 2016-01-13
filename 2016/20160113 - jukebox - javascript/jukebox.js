e656cc35cc81e1fa8a6b89646911cb9186fc4b9c
dojorio <dojo-rio@googlegroups.com>
2016-01-13 20:54:17 -0200

    Julio Marins e Marcio de Jesus

--- 2016/20160113 - jukebox - javascript/jukebox.js
+++ 2016/20160113 - jukebox - javascript/jukebox.js
@@ -1,18 +1,27 @@
 exports.search = function (playlist) {
     var musics = Object.keys(playlist)
 
+    for (mainMusic in playlist){
+        for (secondMusic in playlist){
+            
+        };
+    }
     if (musics.length == 6) {
         return 9;
     }
 
-    if (musics.length >= 3){
+    if (musics.length >= 4){
         return musics.length + 1;
     }
+
 
     if (musics[0] == 'aa') {
         return 3;
     }
 
+    if (musics[0] == 'adc'){
+        return 4;
+    }
 
     return musics.length;
 };
