32ac67dc4f20357ab499974d4356d9121dc35402
dojorio <dojo-rio@googlegroups.com>
2016-01-27 19:32:33 -0200

    Julio Marins e Otavio Cardoso

--- 2016/20160127 - attacking rooks - python/attacking_rooks.py
+++ 2016/20160127 - attacking rooks - python/attacking_rooks.py
@@ -1,6 +1,8 @@
 def attacking_rooks(board):
+	if len(board[0]) == 2:
+		return 2 - board[0].count('x')
 
 	if board[0][0] == ".":
-		return 1
+		return len(board[0])
 
 	return 0
--- 2016/20160127 - attacking rooks - python/test_attacking_rooks.py
+++ 2016/20160127 - attacking rooks - python/test_attacking_rooks.py
@@ -2,6 +2,8 @@
 import unittest
 from attacking_rooks import attacking_rooks
 
+# board: integer N (1 ≤ N ≤ 100)
+
 class TestAttackingRooks(unittest.TestCase):
 	def test_1_pawn(self):
 		board = [['x']]
@@ -10,6 +12,24 @@ class TestAttackingRooks(unittest.TestCase):
 
 	def test_1_empty(self):
 		board = [['.']]
+		maximum_rooks = 1
+		self.assertEquals(attacking_rooks(board), maximum_rooks)
+
+	def test_2x2_empty(self):
+		board = [['.', '.'],
+		         ['.', '.']]
+		maximum_rooks = 2
+		self.assertEquals(attacking_rooks(board), maximum_rooks)
+
+	def test_2x2_1_pawn(self):
+		board = [['x', '.'],
+		         ['.', '.']]
+		maximum_rooks = 2
+		self.assertEquals(attacking_rooks(board), maximum_rooks)
+
+	def test_2x2_2_pawns_line(self):
+		board = [['x', 'x'],
+		         ['.', '.']]
 		maximum_rooks = 1
 		self.assertEquals(attacking_rooks(board), maximum_rooks)
 
