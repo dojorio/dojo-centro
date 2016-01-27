1cc7abd86ec36aabe8ad8c974ae1d389a030a14a
dojorio <dojo-rio@googlegroups.com>
2016-01-27 19:49:39 -0200

    Otavio Cardoso e Jonatas Emidio

--- 2016/20160127 - attacking rooks - python/attacking_rooks.py
+++ 2016/20160127 - attacking rooks - python/attacking_rooks.py
@@ -1,11 +1,12 @@
 def attacking_rooks(board):
 	count = 0
-	if len(board[0]) == 2:
-		if board[1] != ['x', 'x']:
-			count = count + 1  
-		return count
+	if len(board[0]) == 2 and board[0][1] == "x":
+		return 1
 
 	if board[0][0] == ".":
-		return len(board[0])
+		return len(board)
+
+	if len(board[0]) == 2 and board[0][0] == "x":
+		return 2
 
 	return 0
