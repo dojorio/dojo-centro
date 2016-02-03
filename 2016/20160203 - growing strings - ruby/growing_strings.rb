fdc590920abcab1a428006e742b8040479e1ad58
dojorio <dojo-rio@googlegroups.com>
2016-02-03 20:46:26 -0200

    Julio Marins e Otavio Cardoso

--- 2016/20160203 - growing strings - ruby/growing_strings.rb
+++ 2016/20160203 - growing strings - ruby/growing_strings.rb
@@ -1,6 +1,14 @@
 def growing_strings(strings)
-	if strings.count == 2 and strings[1].include?(strings[0])
-		return 2
-	end
-	1
+  if strings.length == 3
+    if strings[1].include?(strings[0])
+      return 3
+    else
+      return 2
+    end
+  end
+
+  if strings.count == 2
+    return 2 if strings[1].include?(strings[0]) || strings[0].include?(strings[1])
+  end
+  1
 end
--- 2016/20160203 - growing strings - ruby/growing_strings_spec.rb
+++ 2016/20160203 - growing strings - ruby/growing_strings_spec.rb
@@ -6,11 +6,28 @@ describe "growing_strings" do
     expect(growing_strings(['foo'])).to eq(1)
   end
 
-  it "2 string" do
+  it "2 strings" do
     expect(growing_strings(['foo', 'bar'])).to eq(1)
   end
 
-  it "2 string with successors" do
+  it "2 strings with successors" do
     expect(growing_strings(['fo', 'foo'])).to eq(2)
   end
+
+  it "2 strings with successors reloaded" do
+    expect(growing_strings(['foo', 'fo'])).to eq(2)
+  end
+
+  it "3 strings sequence" do
+    expect(growing_strings(['f', 'fo', 'foo'])).to eq(3)
+  end
+
+  it "3 strings, sequence of 2" do
+    expect(growing_strings(['b', 'fo', 'foo'])).to eq(2)
+  end
+
+  it "3 strings, other sequence of 2" do
+    expect(growing_strings(['fo', 'foo', 'b'])).to eq(2)
+  end
+
 end
