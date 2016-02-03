4a5d666e00f40667206e4d43cf11377dc5dd46ac
dojorio <dojo-rio@googlegroups.com>
2016-02-03 21:29:43 -0200

    Otavio Cardoso e Julio Marins

--- 2016/20160203 - growing strings - ruby/growing_strings.rb
+++ 2016/20160203 - growing strings - ruby/growing_strings.rb
@@ -1,13 +1,15 @@
 def growing_strings(strings)
-  map_count = Hash.new(0)
+  map_count = Hash.new([])
 
   strings.each do |string1|
     strings.reverse.each do |string2|
       if string1.include?(string2) && string1 != string2
-        map_count[string1] += 1
+        map_count[string1] += [string2]
       end
     end
   end
 
-  map_count
+  map_count.reduce([]) do |memo, (k,v)|
+    memo.count < v.count ? v : memo
+  end
 end
