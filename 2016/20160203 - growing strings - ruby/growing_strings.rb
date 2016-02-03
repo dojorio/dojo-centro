d03112dc9bc0ce080311a5e575a9b0ac36e2ddbd
dojorio <dojo-rio@googlegroups.com>
2016-02-03 21:05:16 -0200

    Julio Marins e Otavio Cardoso

--- 2016/20160203 - growing strings - ruby/growing_strings.rb
+++ 2016/20160203 - growing strings - ruby/growing_strings.rb
@@ -1,14 +1,13 @@
 def growing_strings(strings)
-  if strings.length == 3
-    if strings[1].include?(strings[0])
-      return 3
-    else
-      return 2
+  count = 0
+
+  strings.each do |string1|
+    strings.reverse.each do |string2|
+      if string1.include?(string2)
+        count += 1
+      end
     end
   end
 
-  if strings.count == 2
-    return 2 if strings[1].include?(strings[0]) || strings[0].include?(strings[1])
-  end
-  1
+  count
 end
