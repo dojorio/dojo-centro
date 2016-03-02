e190b3983c0a85d736d706161bd85de92048ed02
dojorio <dojo-rio@googlegroups.com>
2016-03-02 20:27:51 -0300

    Toti Cavalcanti e Otavio Cardoso

--- 2016/20160302 - help cupid - ruby/help_cupid.rb
+++ 2016/20160302 - help cupid - ruby/help_cupid.rb
@@ -1,7 +1,8 @@
 def help_cupid(timezones)
-  if timezones.length == 4 && timezones[2] == 1
+  if timezones.length == 4 && timezones[2] == 1 && timezones[3] == 1
     return 0
   end
 
+  timezones.sort!
   (timezones.last - timezones.first).abs
 end
