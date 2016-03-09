0f7e09d1d1a290ccd6eab5929c8c1d09f83ecbfb
dojorio <dojo-rio@googlegroups.com>
2016-03-09 19:52:32 -0300

    Heyder Santos e Otavio Cardoso

--- 2016/20160309 - fizzbuzz - ruby/fizzbuzz.rb
+++ 2016/20160309 - fizzbuzz - ruby/fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
-  
-  if number == 5
+
+  if number % 5 == 0
     return 'Buzz'
   end
 
-  if number == 3 || number == 9
+  if number % 3 == 0
     return 'Fizz'
   end
 
--- 2016/20160309 - fizzbuzz - ruby/fizzbuzz_spec.rb
+++ 2016/20160309 - fizzbuzz - ruby/fizzbuzz_spec.rb
@@ -6,12 +6,24 @@ describe 'Fizzbuzz' do
     expect(fizzbuzz(3)).to eq('Fizz')
   end
  
+  it 'is Fizz for 6' do
+    expect(fizzbuzz(6)).to eq('Fizz')
+  end
+
   it 'is Fizz for 9' do
     expect(fizzbuzz(9)).to eq('Fizz')
   end
 
   it 'is Buzz for 5' do
     expect(fizzbuzz(5)).to eq('Buzz')
+  end
+
+  it 'is Buzz for 10' do
+    expect(fizzbuzz(10)).to eq('Buzz')
+  end
+
+  it 'is Buzz for 15' do
+    expect(fizzbuzz(15)).to eq('FizzBuzz')
   end
   
   it 'is 1 for 1' do
