2f312bd169a4ecbe40577f6bcd1e14f8bd72fc5d
dojorio <dojo-rio@googlegroups.com>
2016-03-09 20:12:01 -0300

    Otavio Cardoso e Jonatas Emidio

--- 2016/20160309 - fizzbuzz - ruby/fizzbuzz.rb
+++ 2016/20160309 - fizzbuzz - ruby/fizzbuzz.rb
@@ -1,14 +1,10 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    return 'Fizzbuzz'
-  end
-  if number % 5 == 0
-    return 'Buzz'
-  end
 
-  if number % 3 == 0
-    return 'Fizz'
-  end
+  return 'FizzBuzz' if number % 15 == 0
+  
+  return 'Fizz'     if number % 3  == 0
+
+  return 'Buzz'     if number % 5  == 0
 
   number
 end
