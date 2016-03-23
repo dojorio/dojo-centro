0eacf67d34d86a8946bc469e6d02168dafaefc9e
dojorio <dojo-rio@googlegroups.com>
2016-03-23 19:40:02 -0300

    Otavio Cardoso e Julio Marins

--- 2016/20160322 - even tree - javascript/eventree.js
+++ 2016/20160322 - even tree - javascript/eventree.js
@@ -1,12 +1,16 @@
 var evenTree = function (edges) {
 	if (edges.length == 3) {
 		return 1
-	}
+	};
 
-	if(edges[3][0] == 2){
-		return 0
+	var arrFirsts = edges.map(function(elm){
+		return elm[0];
+	});
+
+	var uniqueElm = arrFirsts[0];
+	if(arrFirsts.every(function(elm){ return elm == uniqueElm })){
+		return 0;
 	}
-	return 0
 }
 
 exports.evenTree = evenTree
