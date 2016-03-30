d50059c2c47dd7957eff8051c6eb1bd9979da07e
dojorio <dojo-rio@googlegroups.com>
2016-03-30 20:47:22 -0300

    Rafael Miceli e Otavio Cardoso

--- 2016/20160330 - miojo - python/miojo.py
+++ 2016/20160330 - miojo - python/miojo.py
@@ -1,2 +1,5 @@
 def miojo(amp1, amp2):
-	return 3
+	if (amp1 == 1 or amp2 == 1 or amp1 == 3 or amp2 == 3):
+		return 3
+
+	return max(amp1, amp2)
--- 2016/20160330 - miojo - python/test_miojo.py
+++ 2016/20160330 - miojo - python/test_miojo.py
@@ -3,5 +3,21 @@ from miojo import miojo
 
 class TestMiojo(unittest.TestCase):
 	def test_1_1(self):
-		self.assertEquals(miojo(1, 1), 3) 
+		self.assertEquals(miojo(1, 1), 3)
 
+	def test_5_2(self):
+		self.assertEquals(miojo(5, 2), 5)
+
+	def test_5_1(self):
+		self.assertEquals(miojo(5, 1), 3)
+
+	def test_5_3(self):
+		self.assertEquals(miojo(5, 3), 3)
+
+	def test_3_5(self):
+		self.assertEquals(miojo(3, 5), 3)
+
+	def test_6_9(self):
+		self.assertEquals(miojo(6, 9), 9)
+
+unittest.main()
