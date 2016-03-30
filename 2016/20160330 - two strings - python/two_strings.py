52cd8495604c67e41e07d5eeb890a65d18605083
dojorio <dojo-rio@googlegroups.com>
2016-03-30 19:57:13 -0300

    Julio Marins e Euclides da Cunha

--- 2016/20160330 - two strings - python/two_strings.py
+++ 2016/20160330 - two strings - python/two_strings.py
@@ -1,4 +1,7 @@
 def two_strings(strings):
+    for char in strings[0]:
+        if(char in strings[1]):
+            return True
     if(not strings[0] in strings[1]):
         return False
     if(not strings[1] in strings[0]):
