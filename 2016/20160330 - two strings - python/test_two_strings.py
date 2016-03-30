3d427c2ac800dc9523f5963634bcd5875c5e92ed
dojorio <dojo-rio@googlegroups.com>
2016-03-30 19:08:38 -0300

    Rodrigo Garcia e Julio Marins

---
+++ 2016/20160330 - two strings - python/test_two_strings.py
@@ -0,0 +1,34 @@
+#https://www.hackerrank.com/challenges/two-strings
+import unittest
+from two_strings import two_strings
+
+class TestAttackingRooks(unittest.TestCase):
+	def test_1(self):
+		strings = [
+			'a',
+			'a'
+		]
+		self.assertEquals(two_strings(strings), True)
+
+	def test_2(self):
+		strings = [
+			'c',
+			'ab'
+		]
+		self.assertEquals(two_strings(strings), False)
+
+	def test_3(self):
+		strings = [
+			'cd',
+			'ab'
+		]
+		self.assertEquals(two_strings(strings), False)
+
+	def test_4(self):
+		strings = [
+			'cd',
+			'abd'
+		]
+		self.assertEquals(two_strings(strings), True)
+
+unittest.main()
---
+++ 2016/20160330 - two strings - python/two_strings.py
@@ -0,0 +1,6 @@
+def two_strings(strings):
+    if(strings[0] == 'c'):
+        return False
+    if(strings[0] == 'cd'):
+        return False
+    return True
