36a1c696b696a721f4fc7a2447836f6ec2630222
dojorio <dojo-rio@googlegroups.com>
2016-04-06 20:01:03 -0300

    Leandro Thimóteo e Claudio Berrondo

--- 2016/20160406 - erdos number - python/erdos.py
+++ 2016/20160406 - erdos number - python/erdos.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def wrote_with(paper, author, coauthor):
-	if author in paper and coauthor in paper:
-		return True
-	else:
-		return False
+def wrote_together(author, coauthor):
+	def g(paper):
+		return author in paper and coauthor in paper
+	return g
 
 def erdos_number(papers, author):
 	if author == 'Erdos':
 		return 0
 
-	if any(wrote_with(paper, author, 'Erdos') for paper in papers):
+	if any(wrote_together(author, 'Erdos')(paper) for paper in papers):
 		return 1
 
 	if len(papers) == 2:
