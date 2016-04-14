7f257d632e3f6b0d9c8d434cfbdeb6254be39078
dojorio <dojo-rio@googlegroups.com>
2016-04-13 21:09:47 -0300

    Otavio Cardoso e Jonatas Emidio

---
+++ 2016/20160413 - Rails Again - groovy/railsAgain.groovy
@@ -0,0 +1,3 @@
+def static sort (trainIn, trainOut) {
+	trainIn.size() == 2 ? 'IRIR' : 'IR'	
+}
---
+++ 2016/20160413 - Rails Again - groovy/railsAgainSpec.groovy
@@ -0,0 +1,16 @@
+//https://www.urionlinejudge.com.br/judge/en/problems/view/1063
+import spock.lang.*
+import static railsAgain.sort 
+
+class railsAgainSpec extends spock.lang.Specification {
+    def "test"() {
+        expect:
+        sort(trainIn, trainOut) == result
+        
+
+    	where:
+        trainIn  | trainOut | result
+        'a'      | 'a'      | 'IR'    
+        'ab'     | 'ba'     | 'IRIR'
+    }
+}  
