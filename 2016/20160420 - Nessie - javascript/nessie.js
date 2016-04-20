55bc95daaa541cda26055116336babc2979fd5e4
dojorio <dojo-rio@googlegroups.com>
2016-04-20 20:44:46 -0300

    Otavio Cardoso e Jonatas Emidio

--- 2016/20160420 - Nessie - javascript/nessie.js
+++ 2016/20160420 - Nessie - javascript/nessie.js
@@ -2,18 +2,8 @@ var sonar = function (edges) {
 	// return 1;  [1,1] == [1,1]
 	edges = edges.sort(function (a, b) { return a - b })
 	
-	if (edges[1] == 6 || edges[1] == 8	|| edges[1] == 7){
-		return 2
-	}
+	return parseInt(edges[1] / 3) 
 
-	if(edges[1] == 9 || edges[1] === 10 || edges[1] === 11) {
-		return 3
-	}	
-	if(edges[1] == 12 || edges[1] == 13) {
-		return 4
-	}
-
-	return 1
 }
 
 exports.sonar = sonar
