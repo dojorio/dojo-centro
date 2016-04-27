fb72e2e6064549fea7be6a1d10818906e0af1b7c
dojorio <dojo-rio@googlegroups.com>
2016-04-27 20:18:50 -0300

    Jonatas Emidio e Otavio Cardoso

--- 2016/20160427 - brick on the wall - groovy/brickOnTheWall.groovy
+++ 2016/20160427 - brick on the wall - groovy/brickOnTheWall.groovy
@@ -1,11 +1,12 @@
 def static completeWall(bricks) {
-    def end
-    
-    if(bricks.sum() == 4){return [3,2,1,1,1,0]}
-
-    if(bricks[0] == bricks[1] + bricks[2]){
-        end = [bricks[1], 0, bricks[2]]
+    if (bricks[0] == bricks[1] + bricks[2]) {
+        return bricks + [bricks[1], 0, bricks[2]]
     }
 
-    return bricks + end
+    if (bricks.sum() == 4) {
+        switch(bricks[1]) {
+            case 0: return [3,1,2,0,1,1];
+            case 1: return [3,2,1,1,1,0];
+        }
+    }
 }
