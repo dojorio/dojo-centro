3104214cd8745b46dee81fa4affde1a4ac0dca5d
dojorio <dojo-rio@googlegroups.com>
2016-04-27 19:48:29 -0300

    Toti e Jonatas Emidio

--- 2016/20160427 - brick on the wall - groovy/brickOnTheWall.groovy
+++ 2016/20160427 - brick on the wall - groovy/brickOnTheWall.groovy
@@ -2,7 +2,13 @@ def static completeWall(bricks) {
     def end
 
     if(bricks[0] == 3){
-        end = bricks[1] == 1 ? [1,0,2] : [2,0,1]
+        if(bricks[1]){
+            return end = bricks[1] == 1 ? [1,0,2] : [2,0,1]   
+        }
+
+        return [3,0,3,0,0,3]
+
+        
     }
 
     if(bricks[0] == 1){
--- 2016/20160427 - brick on the wall - groovy/brickOnTheWallSpec.groovy
+++ 2016/20160427 - brick on the wall - groovy/brickOnTheWallSpec.groovy
@@ -15,5 +15,6 @@ class brickOnTheWallSpec extends spock.lang.Specification {
         [1,1,0]          | [1,1,0,1,0,0]
         [1,0,1]          | [1,0,1,0,0,1]
         [3,1,2]          | [3,1,2,1,0,2]
+        [3,0,3]          | [3,0,3,0,0,3]
     }
 }
