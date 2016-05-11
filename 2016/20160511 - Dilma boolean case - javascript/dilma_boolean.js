2a39d332e55b57280a15b8b51938c5669819421d
dojorio <dojo-rio@googlegroups.com>
2016-05-11 19:55:02 -0300

    Otavio Cardoso e Fabio Oliveira

--- 2016/20160511 - Dilma boolean case - javascript/dilma_boolean.js
+++ 2016/20160511 - Dilma boolean case - javascript/dilma_boolean.js
@@ -1,5 +1,5 @@
 exports.dilma_boolean = function (intentions) {
-	if (intentions.toLowerCase().indexOf('n') > -1 ) {
+	if (intentions ! intentions.toLowerCase().indexOf('n') > -1 ) {
 		return 'não vai ter golpe!'
 	}
 
