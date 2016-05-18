1c82fa083922a7710010c22733917f28c303da98
dojorio <dojo-rio@googlegroups.com>
2016-05-18 19:48:00 -0300

    Otavio Cardoso e Raysa Dutra

--- 2016/20160518 - brainfuck - groovy/brainfuck.groovy
+++ 2016/20160518 - brainfuck - groovy/brainfuck.groovy
@@ -1,6 +1,10 @@
 def static execute (input, code) {
     if (code) {
-		return input ? input[0] : ''
+    	def letra = input[0]
+    	if(code.contains("+")){
+    		letra++
+    	} 
+		return input ? letra: ''
 	}
 	return ''
 }
