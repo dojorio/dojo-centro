2b6fbc9ebe881c7584a759e8815e50428f8fccf0
dojorio <dojo-rio@googlegroups.com>
2016-05-18 20:30:35 -0300

    Raysa Dutra e Guilherme Zagatti

--- 2016/20160518 - brainfuck - groovy/brainfuck.groovy
+++ 2016/20160518 - brainfuck - groovy/brainfuck.groovy
@@ -7,14 +7,14 @@ def static execute (input, code) {
 		return ''
 	}
 
-	def index = code.count(',') - 1
+	def index = code.count(",") - 1
 	def letter = input[index]
 
-	if(code.indexOf("+") > index){
+	if(code.indexOf("+", code.indexOf(",")) > 0){
 		letter++
 	}
 
-	if(code.indexOf("-") > index){
+	if(code.indexOf("-", index)  > 0){
 		letter--
 	}
 
