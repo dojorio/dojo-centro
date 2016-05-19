820be91c6441bbb46ddd30bb57053e22fb8b1fe9
dojorio <dojo-rio@googlegroups.com>
2016-05-18 21:31:17 -0300

    Raysa Dutra e Guilherme Zagatti

--- 2016/20160518 - brainfuck - groovy/brainfuck.groovy
+++ 2016/20160518 - brainfuck - groovy/brainfuck.groovy
@@ -1,7 +1,4 @@
 def static execute (input, code) {
-	if(code.contains("++")){
-		return 'a'	
-	}
 
 	if (!code.contains('.') || !input) {
 		return ''
@@ -10,18 +7,18 @@ def static execute (input, code) {
     def output = '',
 	    index = 0,
 		ptr = 0,
-		memory = []
+		memory = [0]
 
 	code.each {
     	switch(it) {
     		case ',' : memory[ptr] = input[index++]; break;
     		case '.' : output += memory[ptr]; break;
-    		case '+' : memory[ptr] && memory[ptr]++; break;
+    		case '+' : memory[ptr] != null && memory[ptr]++; break;
     		case '-' : memory[ptr] && memory[ptr]--; break;
     		case '>' : ptr++; break;
     		case '<' : ptr--; break;
     	}
 	}
 
-	output
+	output ?: memory[0]
 }
