8befa24f870eefd66648fe96e2e809e520be9b52
dojorio <dojo-rio@googlegroups.com>
2016-05-25 20:29:16 -0300

    Pedro Richetti e Otavio Cardoso

--- 2016/20160525 - vogons - python/test_vogon.py
+++ 2016/20160525 - vogons - python/test_vogon.py
@@ -5,81 +5,65 @@ class TestVogonReport(unittest.TestCase):
 	def test_1(self):
 		road_start = (0, 0)
 		road_end = (1, 0)
-		other_planets = [
-			(0, 1, 1)
-		]
-		report_output = {
-			"deaths": 0
-		}
+		planets = [(0, 1, 1)]
+		report_output = { "deaths": 0 }
 		self.assertEquals(vogon_report(
-			road_start, road_end, other_planets), 
-				report_output)
+			road_start, road_end, planets),
+			report_output)
 
 	def test_2(self):
 		road_start = (0, 0)
 		road_end = (1, 0)
-		other_planets = [
-			(0, 0, 1)
-		]
-		report_output = {
-			"deaths": 1
-		}
+		planets = [(0, 0, 1)]
+		report_output = { "deaths": 1 }
 		self.assertEquals(vogon_report(
-			road_start, road_end, other_planets), 
-				report_output)
+			road_start, road_end, planets),
+			report_output)
 
 	def test_3(self):
 		road_start = (1, 0)
 		road_end = (2, 0)
-		other_planets = [
-			(0, 0, 1)
-		]
-		report_output = {
-			"deaths": 0
-		}
+		planets = [(0, 0, 1)]
+		report_output = { "deaths": 0 }
 		self.assertEquals(vogon_report(
-			road_start, road_end, other_planets), 
-				report_output)
+			road_start, road_end, planets),
+			report_output)
 
 
 	def test_4(self):
 		road_start = (0, 0)
 		road_end = (1, 0)
-		other_planets = [
-			(0, 0, 2)
-		]
-		report_output = {
-			"deaths": 2
-		}
+		planets = [(0, 0, 2)]
+		report_output = { "deaths": 2 }
 		self.assertEquals(vogon_report(
-			road_start, road_end, other_planets), 
-				report_output)
+			road_start, road_end, planets),
+			report_output)
 
 	def test_5(self):
 		road_start = (0, 0)
 		road_end = (1, 0)
-		other_planets = [
-			(2, 0, 2)
-		]
-		report_output = {
-			"deaths": 0
-		}
+		planets = [(2, 0, 2)]
+		report_output = { "deaths": 0 }
 		self.assertEquals(vogon_report(
-			road_start, road_end, other_planets), 
-				report_output)
+			road_start, road_end, planets),
+		    report_output)
 
-
-	def test_3(self):
+	def test_6(self):
 		road_start = (0, 0)
 		road_end = (1, 0)
-		other_planets = [
-			(3, 0, 2)
-		]
-		report_output = {
-			"deaths": 0
-		}
+		planets = [(3, 0, 2)]
+		report_output = { "deaths": 0 }
 		self.assertEquals(vogon_report(
-			road_start, road_end, other_planets), 
-				report_output)
+			road_start, road_end, planets),
+			report_output)
+
+	def test_7(self):
+		road_start = (0, 0)
+		road_end = (2, 0)
+		planets = [(0, 0, 2)]
+		report_output = { "deaths": 0 }
+		self.assertEquals(vogon_report(
+			road_start, road_end, planets),
+			report_output)
 
 unittest.main()
--- 2016/20160525 - vogons - python/vogon.py
+++ 2016/20160525 - vogons - python/vogon.py
@@ -1,10 +1,5 @@
-def vogon_report(road_start, road_end, other_planets):
-	
-	if other_planets[0][1] == 0 and road_start[0] == 0 and road_end[0] >= other_planets[0][0] :
-		return {"deaths": other_planets[0][2]} 
+def vogon_report(road_start, road_end, planets):
+	if planets[0][1] == 0 and road_start[0] == 0 and road_end[0] >= planets[0][0]:
+		return {"deaths": planets[0][2]} 
 	else:
 		return {"deaths": 0}
-
-	
-		
-	
