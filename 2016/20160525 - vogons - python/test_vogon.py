854bce299aaf872963cd05496b5331ad210a244b
dojorio <dojo-rio@googlegroups.com>
2016-05-25 20:23:52 -0300

    Otavio Cardoso e Jonatas Emidio

--- 2016/20160525 - vogons - python/test_vogon.py
+++ 2016/20160525 - vogons - python/test_vogon.py
@@ -68,4 +68,18 @@ class TestVogonReport(unittest.TestCase):
 			road_start, road_end, other_planets), 
 				report_output)
 
+
+	def test_3(self):
+		road_start = (0, 0)
+		road_end = (1, 0)
+		other_planets = [
+			(3, 0, 2)
+		]
+		report_output = {
+			"deaths": 0
+		}
+		self.assertEquals(vogon_report(
+			road_start, road_end, other_planets), 
+				report_output)
+
 unittest.main()
--- 2016/20160525 - vogons - python/vogon.py
+++ 2016/20160525 - vogons - python/vogon.py
@@ -1,7 +1,6 @@
 def vogon_report(road_start, road_end, other_planets):
-	if other_planets[0][0] == 2:
-		return {"deaths": 0}
-	if other_planets[0][1] == 0 and road_start[0] == 0:
+	
+	if other_planets[0][1] == 0 and road_start[0] == 0 and road_end[0] >= other_planets[0][0] :
 		return {"deaths": other_planets[0][2]} 
 	else:
 		return {"deaths": 0}
