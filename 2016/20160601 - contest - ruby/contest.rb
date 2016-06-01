860449fd381d60cb7babc3b775ed1b4b0a3f1eef
dojorio <dojo-rio@googlegroups.com>
2016-06-01 19:28:02 -0300

    Toti Cavalcanti e raysa Dutra

--- 2016/20160601 - contest - ruby/contest.rb
+++ 2016/20160601 - contest - ruby/contest.rb
@@ -1,7 +1,15 @@
 def nobody_solved_all(test)
-	return 1 if test[0].count(1) < 3 &&
-				test[1].count(1) < 3 &&
-				test[2].count(1) < 3
-	0
+    result = 0
+
+    test.each do |contestant|
+      result = 1 if contestant.count(1) < 3
+
+    end
+    return 
+
+    # return 1 if test[0].count(1) < 3 &&
+    #             test[1].count(1) < 3 &&
+    #             test[2].count(1) < 3
+    0
 
 end
