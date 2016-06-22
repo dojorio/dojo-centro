9e6a042cbb939b872a6e96915d4773c3cd6ce143
dojorio <dojo-rio@googlegroups.com>
2016-06-22 20:00:48 -0300

    Euclides da Cunha e Jonatas Emidio

--- 2016/20160622 - Barbarossa - groovy/barbarossa.groovy
+++ 2016/20160622 - Barbarossa - groovy/barbarossa.groovy
@@ -1,5 +1,8 @@
 def static armedSoldiers (soldiers, arms, daysToSnow) {
-	if (daysToSnow == 9)
-		return (arms >= 3 && soldiers == 1 ? 1 : 0)
+	if (daysToSnow == 9) {
+
+		return ((arms == 5) || arms >= 3 && soldiers == 1 ? 1 : 0)
+	}
+
 	arms ? soldiers : 0
 }
