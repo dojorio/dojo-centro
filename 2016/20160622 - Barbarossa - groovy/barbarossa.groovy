58b815463f48e6a9eb115978326a5545b24cfc98
dojorio <dojo-rio@googlegroups.com>
2016-06-22 21:17:44 -0300

    jonatas Emidio e Euclides da Cunha

--- 2016/20160622 - Barbarossa - groovy/barbarossa.groovy
+++ 2016/20160622 - Barbarossa - groovy/barbarossa.groovy
@@ -5,7 +5,6 @@ def static armedSoldiers (soldiers, arms, daysToSnow) {
 	def armsPerSoldiers = arms / soldiers
 
 	if (daysToSnow == 9) {
-		
 		def mod = arms % soldiers
 
 		if (mod == 0) {
@@ -14,7 +13,13 @@ def static armedSoldiers (soldiers, arms, daysToSnow) {
 			return armsPerSoldiers as int == 2 ? mod : 0
 		}
 	} else if (daysToSnow == 11) {
-		return armsPerSoldiers > 0 ? [arms, soldiers].min() : 0
+		def mod = arms % soldiers
+
+		if (mod == 0) {
+			return armsPerSoldiers > 0 ? soldiers : 0
+		} else {
+			return (armsPerSoldiers as int) > 0 ? soldiers : 0
+		}
 	}
 
 	arms ? soldiers : 0
