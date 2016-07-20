804e1416b2d46d0a618257e30dd1910aab8cdaf2
dojorio <dojo-rio@googlegroups.com>
2016-07-20 20:34:46 -0300

    Marcio Duarte e Otavio Cardoso

--- 2016/20160720 - interseccao de segmento de reta - python/problem.py
+++ 2016/20160720 - interseccao de segmento de reta - python/problem.py
@@ -2,6 +2,17 @@
 # -*- coding: utf-8 -*-
 
 def create_reta(point_a, point_b):
-    if (point_b == (0,2)):
-        return [point_a, (0,1), point_b]
+    result = []
+
+    if point_a[0] == point_b[0]:
+        step = 1
+
+        if point_b[1] < point_a[1]:
+            step = -1
+
+        for i in range(point_a[1], point_b[1] + step, step):
+            result.append((0, i))
+
+        return result
+
     return [point_a, point_b]
