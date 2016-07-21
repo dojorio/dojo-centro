3d3f17444af105fc9d787279b70cd84d72f2a063
dojorio <dojo-rio@googlegroups.com>
2016-07-20 21:01:09 -0300

    Jonatas Emidio e Bruno Barros

--- 2016/20160720 - interseccao de segmento de reta - python/problem.py
+++ 2016/20160720 - interseccao de segmento de reta - python/problem.py
@@ -16,7 +16,10 @@ def create_reta(point_a, point_b):
             for i in range(point_a[0], point_b[0] + step, step)]
 
     #diagonal
-    if point_a[0] == point_a[1]
+    if point_a[0] == point_a[1] and point_b[0] == point_b[1]:
+        step = -1 if point_b[1] < point_a[1] else 1
+        return [(i, i) 
+            for i in range(point_a[1], point_b[1] + step, step)]
 
 
     return [point_a, point_b]
