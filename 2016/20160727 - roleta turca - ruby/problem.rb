c970b02bbb172d5c50bc623caf0faff9693469a4
dojorio <dojo-rio@googlegroups.com>
2016-07-27 20:48:49 -0300

    Toti Cavalcanti e Rafael Miceli

--- 2016/20160727 - roleta turca - ruby/problem.rb
+++ 2016/20160727 - roleta turca - ruby/problem.rb
@@ -5,29 +5,8 @@ def max_profit(roulette, balls)
   if ball > 0
      sum_2_min = roulette_sum - roulette.max
      return sum_2_min * -ball
-  end
-
-  sum_2_max = roulette.reduce(:+) - roulette.min
-
-  if ball > 0
-  return sum_2_max * (-ball)
-end
-  if roulette.count(1) >= 2
-    return balls[0] * -2 
-  end
-
-  if roulette.count(-1) >= 2
-    return balls[0] * 2
-  end
-
-  if balls[0] > 0 &&
-    roulette.all? { |n| n >= 0 } && roulette.count(0) >= 2 
-    return 0
-  end
-
-  if roulette.count(2) >= 2
-    return (roulette[1] + roulette[2]) * -2
-  end
-
-  balls[0] * -1
+  else
+    sum_2_max = roulette.reduce(:+) - roulette.min
+    return sum_2_max * -ball
+  end  
 end
