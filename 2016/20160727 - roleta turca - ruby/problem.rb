2ef540ae5a8b69463753f0b0fa98273af90923d3
dojorio <dojo-rio@googlegroups.com>
2016-07-27 21:13:17 -0300

    Toti Cavalcanti e Raysa Dutra

--- 2016/20160727 - roleta turca - ruby/problem.rb
+++ 2016/20160727 - roleta turca - ruby/problem.rb
@@ -1,9 +1,8 @@
 def max_profit(roulette, balls)
   ball = balls[0]
-  
-  if roulette.size == 3
-    roulette_sum = roulette.reduce(:+)
+  roulette_sum = roulette.reduce(:+)
 
+  if roulette.size == 3
     if ball > 0
        sum_2_min = roulette_sum - roulette.max
        return sum_2_min * -ball
@@ -13,5 +12,16 @@ def max_profit(roulette, balls)
     end
   end
 
-  0
+  min = roulette_sum * ball
+
+  roulette.each_with_index do |e, i|
+    next_e = roulette[(i + 1) % roulette.size]
+
+    if (e + next_e) * ball < min 
+      min = (e + next_e) * ball
+    end    
+  end
+  return min
+
+  # 0
 end
