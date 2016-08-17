7841afb13291e219d4d891dc4ddcdf0fc0ad718c
dojorio <dojo-rio@googlegroups.com>
2016-08-17 20:26:59 -0300

    claudio zatti e Otavio Cardoso

--- 2016/20160817 - troco - javascript/problem.js
+++ 2016/20160817 - troco - javascript/problem.js
@@ -1,10 +1,9 @@
 exports.troco = function (preco, pagamento) {
-
+	var notas = [1, 2, 5]
 	var troco = {}
 	var nota = pagamento - preco
-		
-	if (nota > 0) {
 
+	if (nota > 0) {
 		if (nota == 4) {
 			troco[2] = 2
 		} else {
--- 2016/20160817 - troco - javascript/problem_spec.js
+++ 2016/20160817 - troco - javascript/problem_spec.js
@@ -2,25 +2,28 @@ var assert = require ('assert'),
     troco = require('./problem').troco;
 
 describe('troco', function() {
-    it('pagamento igual preco retorna nenhum troco', function () {
+    it('preco igual pagamento retorna nenhum troco', function () {
         assert.deepEqual(troco(1, 1), {})
     })
 
-    it('pagamento 2 preco 1 retorna uma nota de 1', function () {
+    it('preco 1 pagamento 2 retorna uma nota de 1', function () {
         assert.deepEqual(troco(1, 2), { '1' : 1 })
     })
 
-    it('pagamento 5 preco 3 retorna uma nota de 2', function () {
+    it('preco 3 pagamento 5 retorna uma nota de 2', function () {
         assert.deepEqual(troco(3, 5), { '2' : 1 })
     })
 
-    it('pagamento 5 preco 4 retorna uma nota de 1', function () {
+    it('preco 4 pagamento 5 retorna uma nota de 1', function () {
         assert.deepEqual(troco(4, 5), { '1' : 1 })
     })
 
-
-    it('pagamento 5 preco 1 retorna duas nota de 2', function () {
+    it('preco 1 pagamento 5 retorna duas nota de 2', function () {
         assert.deepEqual(troco(1, 5), { '2' : 2 })
+    })
+
+    it('preco 1 pagamento 7 retorna uma nota de 5 e uma de 1', function () {
+        assert.deepEqual(troco(1, 7), { '5' : 1, '1' : 1 })
     })
 
 })
