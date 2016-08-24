34315a992704b3278761ba3eac0cb1cf89d6293b
dojorio <dojo-rio@googlegroups.com>
2016-08-24 20:10:47 -0300

    Otavio Cardoso e leobeto

--- 2016/20160824 - nomes de autores - ruby/problem.rb
+++ 2016/20160824 - nomes de autores - ruby/problem.rb
@@ -2,6 +2,13 @@ def nome_autor(nome)
   return nome.upcase unless nome.include?(' ')
   nome = nome.split(" ")
   
-  return nome.pop.upcase + ", " + nome.join(' ')
+  return nome.pop.upcase + ", " +
+  	nome.map do |p|
+  		if (p != 'da') 
+  			p[0].upcase + p[1..-1]
+  	    else
+  	    	p
+  		end
+  	end.join(' ')
   
 end
