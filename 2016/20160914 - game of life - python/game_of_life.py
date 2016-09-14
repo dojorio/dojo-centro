1ba77bca370885e6af9702d84bfbb87da3c93c80
dojorio <dojo-rio@googlegroups.com>
2016-09-14 19:57:57 -0300

    leobeto e Raysa Dutra

--- 2016/20160914 - game of life - python/game_of_life.py
+++ 2016/20160914 - game of life - python/game_of_life.py
@@ -4,6 +4,13 @@ def game_of_life(table_of_life):
 	if len(table_of_life[0]) == 2:
 		return [[0, 0]]
 
+	if len(table_of_life[0]) == 3:
+		return [[0, 0, 0]]
+	
+	if len(table_of_life[0]) == 3 and len(table_of_life[0][1] == 1):
+		return [[0, 1, 0]]
+		
+
 	return [
 		[0]
 	]
--- 2016/20160914 - game of life - python/test_game_of_life.py
+++ 2016/20160914 - game of life - python/test_game_of_life.py
@@ -22,4 +22,25 @@ class TestGameOfLife(unittest.TestCase):
 		self.assertEquals(game_of_life(table_of_life_now), 
 			table_of_life_later)
 	
+	def test_0_1(self):
+		table_of_life_now = [
+			[1, 0, 1]
+		]
+		table_of_life_later = [
+			[0, 0, 0]
+		]
+		self.assertEquals(game_of_life(table_of_life_now), 
+			table_of_life_later)
+	
+	def test_0_2(self):
+		table_of_life_now = [
+			[1, 1, 1]
+		]
+		table_of_life_later = [
+			[0, 1, 0]
+		]
+		self.assertEquals(game_of_life(table_of_life_now), 
+			table_of_life_later)
+				
+
 unittest.main()
