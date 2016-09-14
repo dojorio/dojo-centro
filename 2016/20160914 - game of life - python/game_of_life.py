b8c31e094141b59db64f1759cc4b471439fe0211
dojorio <dojo-rio@googlegroups.com>
2016-09-14 20:22:31 -0300

    Jonatas Emidio e Julio Marins

--- 2016/20160914 - game of life - python/game_of_life.py
+++ 2016/20160914 - game of life - python/game_of_life.py
@@ -8,13 +8,12 @@ def game_of_life(table_of_life):
 		result = []
 		size = len(table_of_life[0])
 		for idx in range(size):
-			if table_of_life[0][idx]:
-				previous = table_of_life[0][idx-1] if idx > 0 else 0
-				next = table_of_life[0][idx+1] if idx < size - 1 else 0
-				if previous == 1 and next == 1:
-					result.append(1)
-				else:
-					result.append(0)
+			previous = table_of_life[0][idx-1] if idx > 0 else 0
+			next = table_of_life[0][idx+1] if idx < size - 1 else 0
+			if previous == 1 and next == 1:
+				result.append(1)
+			else:
+				result.append(0)
 
 		return [result]
 
