5a1a5eea475081c2ae68ad703e268637bbc23a88
dojorio <dojo-rio@googlegroups.com>
2016-09-14 20:05:46 -0300

    Otavio Cardoso e leobeto

--- 2016/20160914 - game of life - python/game_of_life.py
+++ 2016/20160914 - game of life - python/game_of_life.py
@@ -5,13 +5,9 @@ def game_of_life(table_of_life):
 		return [[0, 0]]
 
 	if len(table_of_life[0]) == 3:
+		if table_of_life[0][1] == 1:
+			return [[0, 1, 0]]
 		return [[0, 0, 0]]
-	
-	if len(table_of_life[0]) == 3 and len(table_of_life[0][1] == 1):
-		return [[0, 1, 0]]
-		
 
-	return [
-		[0]
-	]
+	return [[0]]
 
--- 2016/20160914 - game of life - python/test_game_of_life.py
+++ 2016/20160914 - game of life - python/test_game_of_life.py
@@ -3,44 +3,33 @@ from game_of_life import game_of_life
 
 class TestGameOfLife(unittest.TestCase):
 	def test_0(self):
-		table_of_life_now = [
-			[0]
-		]
-		table_of_life_later = [
-			[0]
-		]
+		table_of_life_now = [[0]]
+		table_of_life_later = [[0]]
 		self.assertEquals(game_of_life(table_of_life_now), 
 			table_of_life_later)
 
 	def test_0_0(self):
-		table_of_life_now = [
-			[1, 0]
-		]
-		table_of_life_later = [
-			[0, 0]
-		]
+		table_of_life_now = [[1, 0]]
+		table_of_life_later = [[0, 0]]
 		self.assertEquals(game_of_life(table_of_life_now), 
 			table_of_life_later)
 	
 	def test_0_1(self):
-		table_of_life_now = [
-			[1, 0, 1]
-		]
-		table_of_life_later = [
-			[0, 0, 0]
-		]
+		table_of_life_now = [[1, 0, 1]]
+		table_of_life_later = [[0, 0, 0]]
 		self.assertEquals(game_of_life(table_of_life_now), 
 			table_of_life_later)
 	
 	def test_0_2(self):
-		table_of_life_now = [
-			[1, 1, 1]
-		]
-		table_of_life_later = [
-			[0, 1, 0]
-		]
+		table_of_life_now = [[1, 1, 1]]
+		table_of_life_later = [[0, 1, 0]]
 		self.assertEquals(game_of_life(table_of_life_now), 
 			table_of_life_later)
 				
+	def test_0_3(self):
+		table_of_life_now = [[1, 1, 1, 1]]
+		table_of_life_later = [[0, 1, 1, 0]]
+		self.assertEquals(game_of_life(table_of_life_now), 
+			table_of_life_later)
 
 unittest.main()
