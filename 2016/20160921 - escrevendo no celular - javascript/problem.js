a22d30462ea3cd163efd8d67d11b6ab48ea33f18
dojorio <dojo-rio@googlegroups.com>
2016-09-21 19:47:14 -0300

    Leobeto e Rodrigo Antunes

--- 2016/20160921 - escrevendo no celular - javascript/problem.js
+++ 2016/20160921 - escrevendo no celular - javascript/problem.js
@@ -2,9 +2,7 @@ exports.problem = function (str) {
 	
 	if (str.length) {
 		var dict = {}
-		dict['A'] = 2
-		dict['B'] = 22
-		dict['C'] = 222
+		dict['ABC'] = 2
 		dict['D'] = 3
 		return dict[str]
 
