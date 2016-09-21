57b493c1967f083b027fafacea33039f6317da7f
dojorio <dojo-rio@googlegroups.com>
2016-09-21 19:54:02 -0300

    Raysa Dutra e Leobeto

--- 2016/20160921 - escrevendo no celular - javascript/problem.js
+++ 2016/20160921 - escrevendo no celular - javascript/problem.js
@@ -1,10 +1,19 @@
 exports.problem = function (str) {
 	
 	if (str.length) {
-		var dict = {}
-		dict['ABC'] = 2
-		dict['D'] = 3
-		return dict[str]
+		var dict = {
+			'ABC' : '2',
+			'D' : '3'
+		}
+		
+		var keys = Object.keys(dict)
+		
+		for (var i = 0; i < keys.length; i++){
+			var regex = new RegExp('[' + keys[i] + ']')
+			if(regex.test(str)){
+				return dict[keys[i]].repeat(keys[i].indexOf(str) + 1)		
+			}
+		}
 
 	} 
 	
