5a82302d8bedaef537c8b060b7a35deb0c60eac3
dojorio <dojo-rio@googlegroups.com>
2016-09-21 20:24:17 -0300

    Raysa Dutra e Leobeto

--- 2016/20160921 - escrevendo no celular - javascript/problem.js
+++ 2016/20160921 - escrevendo no celular - javascript/problem.js
@@ -1,29 +1,37 @@
+var dict = {
+	'ABC' : '2',
+	'D' : '3'
+}
+
+var keys = Object.keys(dict)
+
+function getCodeFromLetter (letter) {
+	for (var i = 0; i < keys.length; i++){
+		var regex = new RegExp('[' + keys[i] + ']')
+		if(regex.test(letter)){
+			return dict[keys[i]].repeat(keys[i].indexOf(letter) + 1)
+		}
+
+	}
+
+	return '';
+}
+
 exports.problem = function (str) {
 	
 	
 
 	if (str.length) {
-		var dict = {
-			'ABC' : '2',
-			'D' : '3'
-		}
-
-		var keys = Object.keys(dict)
+		
 		var ary = str.split('')
-		var res = []
-		for (var j = 0; j < ary.length;j++){
-			var letter = ary[j]
-			for (var i = 0; i < keys.length; i++){
-				var regex = new RegExp('[' + keys[i] + ']')
-				if(regex.test(letter)){
-					res.push(dict[keys[i]].repeat(keys[i].indexOf(letter) + 1))
-				}
+		var res = ''
 
-			}
+		for (var j = 0; j < ary.length;j++){
+			res += getCodeFromLetter(ary[j])
 
 		}
 
-		return res.join('')
+		return res
 		
 
 	} 
