47a4b3ee19cd28c70224717b4baa87b169bafa1b
dojorio <dojo-rio@googlegroups.com>
2016-09-21 21:07:29 -0300

    Rafael Miceli e Raysa Dutra

--- 2016/20160921 - escrevendo no celular - javascript/problem_spec.js
+++ 2016/20160921 - escrevendo no celular - javascript/problem_spec.js
@@ -47,4 +47,10 @@ describe('problem', function() {
     	var str = ' '
         assert.equal(problem(str), '0')
     })
+
+    it('TESTE DO OTAVIKO!!!!', function () {
+    	var str = 'SEMPRE ACESSO O DOJOPUZZLES'
+        assert.equal(problem(str), '77773367_7773302_222337777_777766606660366656667889999_9999555337777')
+    })
+    
 })
