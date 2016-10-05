cab163139e4af3926aaccaf56b000d525f4099ab
dojorio <dojo-rio@googlegroups.com>
2016-10-05 20:21:32 -0300

    Raysa Dutra e Carlos Figueiredo

--- 2016/20161005 - mictorio - ruby/problem.rb
+++ 2016/20161005 - mictorio - ruby/problem.rb
@@ -1,6 +1,7 @@
 def urinal(pissers)
-	return 2 if pissers == '....'
-	return 1 if pissers.include?('...') && pissers.length 
-	return 1 if pissers.include?('..') && pissers.length < 4
-  pissers == '.' ? 1 : 0 
+	vacancies = 0
+
+	pissers.each do |pisser|
+		vacancies += 1 if pisser == '.'
+	end 
 end
