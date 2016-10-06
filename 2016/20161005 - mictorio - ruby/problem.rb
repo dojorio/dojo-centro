faebc54b2bad4002ec93d504256c8e1c3f2ad8ab
dojorio <dojo-rio@googlegroups.com>
2016-10-05 21:26:43 -0300

    Otavio Cardoso e Raysa Dutra

--- 2016/20161005 - mictorio - ruby/problem.rb
+++ 2016/20161005 - mictorio - ruby/problem.rb
@@ -1,15 +1,24 @@
 def urinal(pissers)
-  return 0 if ['.*.', '*..*.', '.*..*', '.*..*.'].include?(pissers) 
+  # return 0 if ['.*.', '*..*.', '.*..*', '.*..*.'].include?(pissers) 
   
-  if pissers.length > 1 && !pissers.include?('..')
-    return 0
+  # if pissers.length > 1 && !pissers.include?('..')
+  #   return 0
+  # end
+
+  # vacancies = pissers.length.fdiv(2).ceil
+
+  # pissers.split('').each do |pisser|
+  #   vacancies -= 1 if pisser == '*'
+  # end
+
+  # vacancies
+  vacancies = 0
+  (1...(pissers.length)).each do |index| 
+    if pissers[index-1] == '.' && pissers[index] == '.'
+      vacancies += 1
+    elsif pissers[index] == '*'
+      vacancies -= 1
+    end
   end
-
-  vacancies = pissers.length.fdiv(2).ceil
-
-  pissers.split('').each do |pisser|
-    vacancies -= 1 if pisser == '*'
-  end
-
   vacancies
 end
