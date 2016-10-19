bb24e2dfeb4bb1bc0718677cc3781e1ec252f0fa
dojorio <dojo-rio@googlegroups.com>
2016-10-19 20:45:39 -0200

    Elias Tandel e Eduardo Stalinho

--- 2016/20161019 - diamante - python/diamante.py
+++ 2016/20161019 - diamante - python/diamante.py
@@ -1,13 +1,12 @@
 def diamante(letra):
 	result = []
-	n_colunas = (2 * ord(letra) - ord('A')) + 1
-	for l in range(ord('A'), ord(letra)):
-		distancia_ate_a = ord(l) - ord('A')
-		meio = n_colunas / 2 
-		
-		linha = ' ' * n_colunas
+	n_colunas = 2 * (ord(letra) - ord('A')) + 1
+	meio = n_colunas // 2
+	for l in range(ord('A'), ord(letra) + 1):
+		distancia_ate_a = l - ord('A')
+		linha = [' '] * n_colunas
 		linha[meio - distancia_ate_a] = chr(l)
-		linha[-(meio - distancia_ate_a)] = chr(l)	
-		result.append(linha)
+		linha[-(meio - distancia_ate_a) -1] = chr(l)	
+		result.append(''.join(linha))
 
 	return result
