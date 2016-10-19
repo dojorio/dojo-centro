9a32cf34e765d23d9a0f21c44bdd60148a6e3d8c
dojorio <dojo-rio@googlegroups.com>
2016-10-19 20:00:40 -0200

    Toti Cavalcanti e Raysa Dutra

--- 2016/20161019 - diamante - python/diamante.py
+++ 2016/20161019 - diamante - python/diamante.py
@@ -1,8 +1,16 @@
 def diamante(letra):
-	if letra != 'A':
+	if letra == 'B':
 		return [
 			' A ',
 			'B B',
 			' A ',
+		]
+	if letra == 'C':
+		return [ 
+			'  A  ',
+			' B B ',
+			'C   C',
+			' B B ',
+			'  A  ',
 		]
 	return ['A']
