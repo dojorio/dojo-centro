a571724fb81a24732fba26edeb2eb88a73214aec
dojorio <dojo-rio@googlegroups.com>
2016-11-23 19:59:33 -0200

    Raysa Dutra e Leobeto

--- 2016/20161123 - look and say - javascript/problem.js
+++ 2016/20161123 - look and say - javascript/problem.js
@@ -1,13 +1,16 @@
+function len(n) {
+	return n.toString().length
+}
+
 exports.lookNSay = function (n) {
-    if (n == 21) {
+    if (n === 21) {
     	return 1211
     }
 
-    if(n.toString().length == 2){
-    	return 20 + n%10
+    if (n > 9) {
+    	return len(n) * 10 + n % 10
     }
 
     return 10 + n
-    
 
 };
--- 2016/20161123 - look and say - javascript/problem_spec.js
+++ 2016/20161123 - look and say - javascript/problem_spec.js
@@ -1,27 +1,32 @@
 var assert = require ('assert'),
     lookNSay = require('./problem').lookNSay;
 
-describe('when we look 1', function() {
-    it('we say 11', function () {
+describe('when we look 1', () => {
+    it('we say 11', () => {
         assert.strictEqual(lookNSay(1), 11)
     })
 })
 
-describe('when we look 2', function() {
-    it('we say 12', function () {
+describe('when we look 2', () => {
+    it('we say 12', () => {
         assert.strictEqual(lookNSay(2), 12)
     })
 })
 
-describe('when we look 21', function() {
-    it('we say 1211', function () {
+describe('when we look 21', () => {
+    it('we say 1211', () => {
         assert.strictEqual(lookNSay(21), 1211)
     })
 })
 
-describe('when we look 22', function() {
-    it('we say 22', function () {
+describe('when we look 22', () => {
+    it('we say 22', () => {
         assert.strictEqual(lookNSay(22), 22)
     })
 })
 
+describe('when we look 222', () => {
+    it('we say 32', () => {
+        assert.strictEqual(lookNSay(222), 32)
+    })
+})
