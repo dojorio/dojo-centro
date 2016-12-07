4df2d43ad50759acaeacc46475d87515c9f92586
dojorio <dojo-rio@googlegroups.com>
2016-12-07 20:58:46 -0200

    James Peres e Jonatas Emidio

--- 2016/20161207 - battleShip - python/problem.py
+++ 2016/20161207 - battleShip - python/problem.py
@@ -2,11 +2,6 @@
 # -*- coding: utf-8 -*-
 def CriaMatriz(navios,tiros):
 	lista = []
-	for i in navios:
-		for a in i:
-			if tiros[a]:
-				lista.append(a)
-	return lista
 	
 	if tiros[0] in navios[0]:
 		return [(tiros[0],)]
--- 2016/20161207 - battleShip - python/test_problem.py
+++ 2016/20161207 - battleShip - python/test_problem.py
@@ -12,10 +12,13 @@ class TestProblem(unittest.TestCase):
     def test_vazio(self):
     	self.assertEqual([],CriaMatriz([('a1',)],['f1']))
 
-    def test_doisbarcos(self):
+   	def test_doisbarcos(self):
     	self.assertEqual([('b1',)],CriaMatriz([('a1',),('b1',)],['b1']))
+
+    def test_a1c1_c1(self):
+    	self.assertEqual([('c1',)],CriaMatriz([('a1',),('c1',)],['c1']))
 	
-	
+
 if __name__ == "__main__":
     unittest.main()
 
