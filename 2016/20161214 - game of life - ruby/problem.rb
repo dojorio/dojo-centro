4f7156eb1c23ead50f365b6561fddbd9e3dc4a42
dojorio <dojo-rio@googlegroups.com>
2016-12-14 19:35:35 -0200

    Leobeto e Otavio Cardoso

--- 2016/20161214 - game of life - ruby/problem.rb
+++ 2016/20161214 - game of life - ruby/problem.rb
@@ -1,3 +1,11 @@
-def problem(bar)
-  return [[0]]
+def game_of_life(grid)
+  if grid == [[1]]
+    return [[0]]
+  end
+
+  if grid[0].include?(1)
+    return [[0, 0]]
+  end
+
+  grid
 end
--- 2016/20161214 - game of life - ruby/problem_spec.rb
+++ 2016/20161214 - game of life - ruby/problem_spec.rb
@@ -1,15 +1,29 @@
 require_relative 'problem'
 
-describe "problem" do
-  it "sample" do
-    bar = [[]]
-    expect(problem(bar)).to eq([[]])
+describe "game_of_life" do
+  it "entra vazio" do
+    grid = [[]]
+    expect(game_of_life(grid)).to eq([[]])
   end
 
   it "entra valor 1 apenas" do
-    bar = [[1]]
-    expect(problem(bar)).to eq([[0]])
+    grid = [[1]]
+    expect(game_of_life(grid)).to eq([[0]])
   end
 
+  it "entra valor 0 apenas" do
+    grid = [[0]]
+    expect(game_of_life(grid)).to eq([[0]])
+  end
+
+  it "entra 1, 1" do
+    grid = [[1, 1]]
+    expect(game_of_life(grid)).to eq([[0, 0]])
+  end
+
+  it "entra 1, 0" do
+    grid = [[1, 0]]
+    expect(game_of_life(grid)).to eq([[0, 0]])
+  end
 
 end
