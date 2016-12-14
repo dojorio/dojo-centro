817244337f1aabc4f3b15ca959e0c677807e863c
dojorio <dojo-rio@googlegroups.com>
2016-12-14 20:45:45 -0200

    James Peres e Leobeto

--- 2016/20161214 - game of life - ruby/problem.rb
+++ 2016/20161214 - game of life - ruby/problem.rb
@@ -1,12 +1,15 @@
 def game_of_life(grid)
-  grid.map do |line|
-    [
-      line.each_index.map do |i|
-        prev = i == 0 ? 0 : line[i - 1] 
-        nex = line[i + 1] || 0
+  grid.each_with_index.map do |line, idx_line|
+    line.each_index.map do |i|
+      prev = i == 0 ? 0 : line[i - 1] 
+      nex = line[i + 1] || 0
 
-        prev + nex == 2 ? line[i] : 0
-      end
-    ]
+      down = (grid[idx_line + 1]||[])[i] || 0
+      up = (grid[idx_line -1]||[])[i] || 0
+
+      neigh = prev + nex + down + up
+
+      neigh == 2 || neigh == 3 ? line[i] : 0
+    end
   end
 end
--- 2016/20161214 - game of life - ruby/problem_spec.rb
+++ 2016/20161214 - game of life - ruby/problem_spec.rb
@@ -61,4 +61,13 @@ describe "game_of_life" do
     expect(game_of_life(grid)).to eq([[0], [0]])
   end
 
+    it "entra 3 linhas com 1" do
+    grid = [
+      [1],
+      [1],
+      [1]
+    ]
+    expect(game_of_life(grid)).to eq([[0], [1], [0]])
+  end
+
 end
