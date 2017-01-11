04409bff6e587b52f560655c341f1f57e06dcddf
dojorio <dojo-rio@googlegroups.com>
2017-01-11 20:32:32 -0200

    Jonatas Emidio e Lucas R. Martins

--- 2017/20170111 - temer_boolean_case - python/problem.py
+++ 2017/20170111 - temer_boolean_case - python/problem.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 def impeachment(votacao):
+	if len(votacao) == 1:
+		return votacao.lower() == "s"
+	if votacao.count("s") > 1: return True
 	if "S" == votacao[0]: return True
-	return 'n' not in votacao.lower()
+	#return 'n' not in votacao.lower()
 	
