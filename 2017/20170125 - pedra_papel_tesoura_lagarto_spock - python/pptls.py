cd5fc7371ae5287996a745e82d99c5d48067ace0
dojorio <dojo-rio@googlegroups.com>
2017-01-25 19:40:29 -0200

    claudio Berrondo e Toti Cavalcanti

--- 2017/20170125 - pedra_papel_tesoura_lagarto_spock - python/pptls.py
+++ 2017/20170125 - pedra_papel_tesoura_lagarto_spock - python/pptls.py
@@ -1,5 +1,10 @@
 
+tesoura = {
+	'lagarto': 'decapita',
+	'papel': 'corta',
+}
+
 def quem_ganha(str1, str2):
-	if str1== "pedra":
+	if str1 == "pedra":
 		return str2
-	return str1
+	return ' '.join((str1, tesoura[str2], str2))
