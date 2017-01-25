70a174ca2530e9e8599ce8655f3c39a408c7d5d1
dojorio <dojo-rio@googlegroups.com>
2017-01-25 20:03:52 -0200

    Leobeto e Toti Cavalcanti

--- 2017/20170125 - pedra_papel_tesoura_lagarto_spock - python/pptls.py
+++ 2017/20170125 - pedra_papel_tesoura_lagarto_spock - python/pptls.py
@@ -7,12 +7,19 @@ d = {
 	'papel': {
 		'pedra': 'cobre',
 		'spock': 'refuta'
+	},
+	'pedra':{
+		'tesoura': 'quebra',
+		'lagarto': 'esmaga'
+	},
+	'lagarto' :{
+		'spock' : 'envenena'
 	}
 }
 
 
 
 def quem_ganha(str1, str2):
-	if str1 in d:
+	if str1 in d and str2 in d[str1]:
 		return ' '.join((str1, d[str1][str2], str2))
 	return ' '.join((str2, d[str2][str1], str1))
--- 2017/20170125 - pedra_papel_tesoura_lagarto_spock - python/test_pptls.py
+++ 2017/20170125 - pedra_papel_tesoura_lagarto_spock - python/test_pptls.py
@@ -18,6 +18,23 @@ class TestPPTLS(unittest.TestCase):
     def test_papel_refuta_spock(self):
     	self.assertEqual("papel refuta spock", quem_ganha("spock", "papel"))
 
+    def test_tesoura_corta_papel_invertido(self):
+    	self.assertEqual("tesoura corta papel", quem_ganha("papel", "tesoura"))
+
+    def test_pedra_esmaga_lagarto(self):
+    	self.assertEqual("pedra esmaga lagarto", quem_ganha("pedra", "lagarto"))
+
+    def test_pedra_quebra_tesoura(self):
+    	self.assertEqual("pedra quebra tesoura", quem_ganha("pedra", "tesoura"))
+
+    def test_lagarto_envenena_spock(self):
+    	self.assertEqual("lagarto envenena spock", quem_ganha("spock", "lagarto"))
+
+    def test_lagarto_come_papel(self):
+    	self.assertEqual("lagarto come papel", quem_ganha("lagarto", "papel"))
+
+
+
 
 
 if __name__ == "__main__":
