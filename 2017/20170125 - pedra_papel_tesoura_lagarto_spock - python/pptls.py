6024ee5d04e995603a9501463cb65d88cf0efd5e
dojorio <dojo-rio@googlegroups.com>
2017-01-25 19:28:13 -0200

    João Freires e claudio Berrondo

---
+++ 2017/20170125 - pedra_papel_tesoura_lagarto_spock - python/pptls.py
@@ -0,0 +1,3 @@
+
+def quem_ganha(str1, str2):
+	return str1
--- 2017/20170125 - pedra_papel_tesoura_lagarto_spock - python/test_pptls.py
+++ 2017/20170125 - pedra_papel_tesoura_lagarto_spock - python/test_pptls.py
@@ -9,6 +9,9 @@ class TestPPTLS(unittest.TestCase):
     def test_tesoura_corta_papel(self):
         self.assertEqual("tesoura", quem_ganha("tesoura", "papel"))
 
+    def test_papel_embrulha_pedra(self):
+    	self.assertEqual("papel", quem_ganha("pedra", "papel"))
+
 if __name__ == "__main__":
     unittest.main()
 
