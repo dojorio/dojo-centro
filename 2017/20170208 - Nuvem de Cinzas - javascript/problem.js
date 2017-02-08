e8d05c14fe462586b061748f2f9de2666ea5038a
dojorio <dojo-rio@googlegroups.com>
2017-02-08 20:51:45 -0200

    Alessandro Hecht e Larissa Mourullo

--- 2017/20170208 - Nuvem de Cinzas - javascript/problem.js
+++ 2017/20170208 - Nuvem de Cinzas - javascript/problem.js
@@ -2,26 +2,53 @@ exports.problem = function (map) {
     if (!map[0].includes('A')) {
     	return 0
     } 
-    var posFumaca = map[0].indexOf('*')
-    var posAeroporto = map[0].lastIndexOf("A")
-    
-    var direita = posAeroporto+1;
-    var esquerda= posAeroporto-1;
 
-    while(!(direita >= map[0].length && esquerda < 0)){
-    	if(direita <= map[0].length){
-    		if (map[0][direita] === '*'){
-    			return direita - posAeroporto;
+    var primeira_posFumaca = map[0].indexOf('*')
+    var ultima_posAeroporto = map[0].lastIndexOf("A")
+
+    var ultima_posFumaca = map[0].lastIndexOf('*')
+    var primeira_posAeroporto = map[0].indexOf("A")
+    
+    if (primeira_posFumaca < ultima_posAeroporto) {
+
+    	var direita = ultima_posAeroporto+1;
+    	var esquerda= ultima_posAeroporto-1;
+
+    	while(!(direita >= map[0].length && esquerda < 0)){
+    		if(direita <= map[0].length){
+    			if (map[0][direita] === '*'){
+    				return direita - ultima_posAeroporto;
+    			}
+    			direita++;
     		}
-    		direita++;
+    		if (esquerda >= 0){
+    			if (map[0][esquerda] == '*'){
+    				return ultima_posAeroporto - esquerda;
+    			} 
+    			esquerda--;
+    		}
     	}
-    	if (esquerda >= 0){
-    		if (map[0][esquerda] == '*'){
-    			return posAeroporto - esquerda;
-    		} 
-    		esquerda--;
+
+    } else if (primeira_posFumaca < ultima_posAeroporto) {    
+    	var direita = ultima_posAeroporto+1;
+    	var esquerda= ultima_posAeroporto-1;
+
+    	while(!(direita >= map[0].length && esquerda < 0)){
+    		if(direita <= map[0].length){
+    			if (map[0][direita] === '*'){
+    				return direita - ultima_posAeroporto;
+    			}
+    			direita++;
+    		}
+    		if (esquerda >= 0){
+    			if (map[0][esquerda] == '*'){
+    				return ultima_posAeroporto - esquerda;
+    			} 
+    			esquerda--;
+    		}
     	}
-    } 
+
+    }
 
 	return 0;
     
