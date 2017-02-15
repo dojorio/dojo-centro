33980b802cce197b17cd3875d68c4d862d2dc2c2
dojorio <dojo-rio@googlegroups.com>
2017-02-15 20:44:18 -0200

    bruno Araujo e James Peres

--- 2017/20170215 - equilibrio_array - javascript/problem.js
+++ 2017/20170215 - equilibrio_array - javascript/problem.js
@@ -12,15 +12,16 @@ exports.equilibrio = function (vetor) {
 		if (vetor[0] == vetor[2])
 			return 1
 
-
 		var soma1 = vetor[0] + vetor[1]
 		var soma2 = vetor[1] + vetor[2]
 
 		if (soma1 > soma2){
 			return 0
 		}
+		if (vetor[1] == vetor[2])
+			return 1
 
-		return 1
+		return vetor[0] < vetor[2] ? 2 : 1  
 
 		// if (vetor[0] == soma)
 		// 	return 0
