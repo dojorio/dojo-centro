b4034f87559f5f1795736c5e9c9787f442ae3cff
dojorio <dojo-rio@googlegroups.com>
2017-02-15 19:25:41 -0200

    James Peres e Leobeto

---
+++ 2017/20170215 - equilibrio_array - javascript/problem.js
@@ -0,0 +1,3 @@
+exports.equilibrio = function (vetor) {
+    return vetor.length ? vetor.length > 1 ? 1 : 0 : null
+};
---
+++ 2017/20170215 - equilibrio_array - javascript/problem_spec.js
@@ -0,0 +1,19 @@
+var assert = require ('assert'),
+    equilibrio = require('./problem').equilibrio;
+
+describe('equilibrio', function() {
+    it('vetor vazio', function () {
+        var vetor = []
+        assert.equal(equilibrio(vetor), null)
+    })
+
+    it('vetor de 1 elemento', function () {
+        var vetor = [1]
+        assert.equal(equilibrio(vetor), 0)
+    })
+
+    it('vetor de 2 elementos segundo mais pesado', function () {
+        var vetor = [1, 2]
+        assert.equal(equilibrio(vetor), 1)
+    })
+})
