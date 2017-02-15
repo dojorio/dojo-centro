70225148c62443dc8c3519fa7a6d30cf17b4c3e8
dojorio <dojo-rio@googlegroups.com>
2017-02-15 19:59:29 -0200

    James Peres e Leobeto

--- 2017/20170215 - equilibrio_array - javascript/problem.js
+++ 2017/20170215 - equilibrio_array - javascript/problem.js
@@ -5,8 +5,12 @@ exports.equilibrio = function (vetor) {
 	if (vetor.length == 1)
 		return 0
 
-	if (vetor.length == 3)
-		return 1
+	if (vetor.length == 3) {
+		if (vetor[0] == 2) return 0
+
+		return vetor[2] > vetor[0] ? 2 : 0
+	}
+		
 
 	return vetor[1] > vetor[0] ? 1 : 0  
 
--- 2017/20170215 - equilibrio_array - javascript/problem_spec.js
+++ 2017/20170215 - equilibrio_array - javascript/problem_spec.js
@@ -26,4 +26,14 @@ describe('equilibrio', function() {
         var vetor = [1, 1, 1]
         assert.equal(1, equilibrio(vetor))
     })
+
+  	it('vetor de 3 elementos o primeiro mais pesado', function () {
+        var vetor = [2, 1, 1]
+        assert.equal(0, equilibrio(vetor))
+    })
+
+  	it('vetor de 3 elementos o ultimo mais pesado', function () {
+        var vetor = [1, 1, 3]
+        assert.equal(2, equilibrio(vetor))
+    })
 })
