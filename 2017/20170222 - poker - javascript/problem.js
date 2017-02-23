f618e565e846e830267691129ce7cda9264bd08a
dojorio <dojo-rio@googlegroups.com>
2017-02-22 21:15:34 -0300

    Leobeto e Bruno Araujo

--- 2017/20170222 - poker - javascript/problem.js
+++ 2017/20170222 - poker - javascript/problem.js
@@ -1,6 +1,34 @@
+let valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+function pegarValor(carta) {
+	return valores.indexOf(carta.substring(0, carta.length-1))
+}
+
+function maior (arr) {
+	return Math.max.apply(
+		Math,
+		arr.map(pegarValor)
+	)
+}
+
+function contar (arr) {
+	return arr
+		.reduce((acc, x) => {
+			acc[x] = acc[x] ? acc[x]+1 : 1;
+			return acc
+		}, {})
+}
+
 exports.poker = function (primeiroArray, segundoArray) {
-    if (primeiroArray[4] > segundoArray[4]) {
+    let maior1 = maior(primeiroArray)
+    let maior2 = maior(segundoArray)
+
+    if (maior1 > maior2) {
     	return 'Primeiro'
+    }
+
+    if (maior2 > maior1) {
+    	return 'Segundo'
     }
 
     return 'Empate'
--- 2017/20170222 - poker - javascript/problem_spec.js
+++ 2017/20170222 - poker - javascript/problem_spec.js
@@ -11,11 +11,38 @@ describe('poker', function() {
 	    'Empate')
     })
 
-    it('primeiro carta mais alta', function () {
+    it('primeiro carta mais alta ultima posicao', function () {
         assert.equal(
         	poker(
 	        	['2H', '4D', '5S', '6S', '10S'],
 	        	['2D', '4H', '5H', '6C', '7C']
+	        ),
+	    'Primeiro')
+    })
+
+    it('primeiro carta mais alta penultima posicao', function () {
+        assert.equal(
+        	poker(
+	        	['2H', '4D', '5S', '10S', '6S'],
+	        	['2D', '4H', '5H', '6C', '7C']
+	        ),
+	    'Primeiro')
+    })
+
+    it('primeiro carta mais alta penultima posicao', function () {
+        assert.equal(
+        	poker(
+	        	['2D', '4H', '5H', '6C', '7C'],
+	        	['2H', '4D', '5S', '10S', '6S']
+	        ),
+	    'Segundo')
+    })
+
+    it('o par ganha', function () {
+        assert.equal(
+        	poker(
+	        	['2D', '2H', '5H', '6C', '7C'],
+	        	['2H', '4D', '5S', '10S', '6S']
 	        ),
 	    'Primeiro')
     })
