682fce6cbe7365183244eaf02d2ca7448d46b9ed
dojorio <dojo-rio@googlegroups.com>
2017-03-29 21:11:04 -0300

    Larissa Mourullo e rogerio

--- 2017/20170329 - contandoletras - ruby/problem.rb
+++ 2017/20170329 - contandoletras - ruby/problem.rb
@@ -1,45 +1,37 @@
 def contaletras(valor)
-	um = 2
+	soma = 2
 
 	if valor >= 2
-		um += 4
-		 
-	end
+		soma += 4
+	 end
 
 	if valor >= 3
-		um += 4
-		
+		soma += 4
 	end
 
 	if valor >= 4
-		um += 6
-		 
+		soma += 6
 	end
 
 	if valor >= 5
-		um += 5
-		
+		soma += 5
 	end 
 
 	if valor >= 6
-		um += 4
-		
+		soma += 4
 	end 
 
     if valor >= 7
-    	um += 4
-		
+    	soma += 4
 	end 
 
 	if valor >= 8
-		um += 4
-		
+		soma += 4
 	end 
 
 	if valor >= 9
-		um += 4
-		
+		soma += 4
 	end 
 
-  	return um
+  	return soma
 end
