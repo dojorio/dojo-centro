59973dfa4f4f28c9be6b1c49559a46bac3f2533e
dojorio <dojo-rio@googlegroups.com>
2017-03-29 21:05:35 -0300

    rogerio e Vini Natan

--- 2017/20170329 - contandoletras - ruby/problem.rb
+++ 2017/20170329 - contandoletras - ruby/problem.rb
@@ -1,47 +1,45 @@
 def contaletras(valor)
-	um = [] 
+	um = 2
 
-	if valor == 2
+	if valor >= 2
 		um += 4
-		return um 
+		 
 	end
 
-	if valor == 3
-		um += 4 + 4
-		return um
+	if valor >= 3
+		um += 4
+		
 	end
 
-	if valor == 4
-		um += 4 + 4 + 6
-		return um 
+	if valor >= 4
+		um += 6
+		 
 	end
 
-	if valor == 5
-		um += 4 + 4 + 6 + 5
-		return um
+	if valor >= 5
+		um += 5
+		
 	end 
 
-	if valor == 6
-		um += 4 + 4 + 6 + 5 + 4
-		return um
+	if valor >= 6
+		um += 4
+		
 	end 
 
-    if valor == 7
-    	um += 4 + 4 + 6 + 5 + 4 + 4
-		return um
+    if valor >= 7
+    	um += 4
+		
 	end 
 
-	if valor == 8
-		um += 4 + 4 + 6 + 5 + 4 + 4 + 4
-		return um
+	if valor >= 8
+		um += 4
+		
 	end 
 
-	if valor == 9
-		um += 4 + 4 + 6 + 5 + 4 + 4 + 4 + 4
-		return um
+	if valor >= 9
+		um += 4
+		
 	end 
-if valor == 2
-	um += 2
-		return um
+
   	return um
 end
