357c10e188bf5b9eb7cc26bc44492ac6fe7dbce5
dojorio <dojo-rio@googlegroups.com>
2017-04-05 19:30:46 -0300

    Odaraya e Rogerio

--- 2017/20170405 - torre_de_hanoi - python/problem.py
+++ 2017/20170405 - torre_de_hanoi - python/problem.py
@@ -3,15 +3,6 @@
 
 def rodelas(n):
     progressao = 2
-    resultado = 0    
-
-    if n == 1: 
-        return 1
-    elif n == 2:
-        return 3
-    elif n == 3:
-        return 7
-    elif n == 4:
-        return 15
+    resultado += progressao * n
 
     return 0
