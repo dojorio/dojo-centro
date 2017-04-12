d20486f295716f43d5ebb0d7a1cfe78ec4544621
dojorio <dojo-rio@googlegroups.com>
2017-04-12 19:29:15 -0300

    Daniel Drumond e Claudio Berrondo

--- 2017/20170412 - poker - python/problem.py
+++ 2017/20170412 - poker - python/problem.py
@@ -4,4 +4,8 @@
 
 
 def poker(j1, j2):
+	valor1, naipe1 = j1[0], j1[1]
+	valor2, naipe2 = j2[0], j2[1]
+	if valor1 > valor2:
+		return 1
 	return 2
--- 2017/20170412 - poker - python/test_problem.py
+++ 2017/20170412 - poker - python/test_problem.py
@@ -8,12 +8,14 @@ from problem import *
 # Ouro (D), Copa (H), Espadas (S), Paus (C)
 
 class TestProblem(unittest.TestCase):
-    def test_(self):
-        self.assertEqual(2, poker('5C 5P 6E 7E KO', '2P 3E 8E 8O DO'))
+    #def test_(self):
+        #self.assertEqual(2, poker('5C 5P 6E 7E KO', '2P 3E 8E 8O DO'))
 
-    def test_1_carta(self):
-    	self.assertEqual(1, poker('7C', '6C'))
+    def test_uma_carta_mesmo_naipe(self):
+        self.assertEqual(1, poker('7C', '6C'))
 
+    def test_uma_carta_mesmo_naipe_ao_contrario(self):
+        self.assertEqual(2, poker('5C', '6C'))
 
 if __name__ == "__main__":
     unittest.main()
