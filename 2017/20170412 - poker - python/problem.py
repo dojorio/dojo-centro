c8e9faae77537f8f95396c993d12a3b40453c2e8
dojorio <dojo-rio@googlegroups.com>
2017-04-12 20:41:18 -0300

    Claudio Berrondo e Otavio Cardoso

--- 2017/20170412 - poker - python/problem.py
+++ 2017/20170412 - poker - python/problem.py
@@ -2,31 +2,25 @@
 # -*- coding: utf-8 -*-
 
 def eh_uma_dupla(valores):
-	if valores[0] == valores[1]:
-		return True
+	return valores[0] == valores[1]
 
+def mao_maior(valores1, valores2):
+	if max(valores1) > max(valores2):
+		return 1
+	return 2
 
 def poker(jogador1, jogador2):
 	valores1, valores2 = jogador1[::3], jogador2[::3]
 
 	if len(valores1) == 1:
-		if max(valores1) > max(valores2):
-			return 1
-		return 2
+		return mao_maior(valores1, valores2)		
 
-	if eh_uma_dupla(valores1) and eh_uma_dupla(valores2):
-		if max(valores1) > max(valores2):
-			return 1
-		else:
-			return 2
-	else:
-		if eh_uma_dupla(valores1):
-			return 1
-
+	if eh_uma_dupla(valores1):
 		if eh_uma_dupla(valores2):
-			return 2
-			
-	if max(valores1) > max(valores2):
-		return 1
-	else:
+			return mao_maior(valores1, valores2)
+		else:
+			return 1
+	elif eh_uma_dupla(valores2):
 		return 2
+	else:
+		return mao_maior(valores1, valores2)
