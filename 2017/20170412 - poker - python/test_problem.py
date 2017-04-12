7db75666ddc91e133eddea6e4fac422c67c7a306
dojorio <dojo-rio@googlegroups.com>
2017-04-12 19:20:28 -0300

    Claudio Berrondo e Wilson Siqueira

--- 2017/20170412 - poker - python/test_problem.py
+++ 2017/20170412 - poker - python/test_problem.py
@@ -11,6 +11,9 @@ class TestProblem(unittest.TestCase):
     def test_(self):
         self.assertEqual(2, poker('5C 5P 6E 7E KO', '2P 3E 8E 8O DO'))
 
+    def test_1_carta(self):
+    	self.assertEqual(1, poker('7C', '6C'))
+
 
 if __name__ == "__main__":
     unittest.main()
