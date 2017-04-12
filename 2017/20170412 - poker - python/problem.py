3ae11819cca8fa9586a28997f5289dce21f74fc2
dojorio <dojo-rio@googlegroups.com>
2017-04-12 19:14:26 -0300

    Wilson Siqueira e Daniel Drumond

--- 2017/20170412 - poker - python/problem.py
+++ 2017/20170412 - poker - python/problem.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+
+def poker(j1, j2):
+	return 2
--- 2017/20170412 - poker - python/test_problem.py
+++ 2017/20170412 - poker - python/test_problem.py
@@ -9,7 +9,7 @@ from problem import *
 
 class TestProblem(unittest.TestCase):
     def test_(self):
-        self.assertEqual(2, poker('5C 5P 6E 7E KO', '2P 3E 8E 8O TO'))
+        self.assertEqual(2, poker('5C 5P 6E 7E KO', '2P 3E 8E 8O DO'))
 
 
 if __name__ == "__main__":
