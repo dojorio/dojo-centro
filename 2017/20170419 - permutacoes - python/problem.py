ec6056fcf0195f720f3dab6ab80cf53a13fde386
dojorio <dojo-rio@googlegroups.com>
2017-04-19 20:48:41 -0300

    Toti Cavalcanti e james Peres

--- 2017/20170419 - permutacoes - python/problem.py
+++ 2017/20170419 - permutacoes - python/problem.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import math
+import random
+
 def permutacoes(entrada):
-    if len (entrada) > 1:
+    if len (entrada) == 2:
         return sorted([entrada, entrada[::-1]])
+    elif len(entrada) == 3:
+        saida = []
+        lista = list(entrada)
+        while len(saida) != math.factorial(len(entrada)):
+            random.shuffle(lista)
+            print (lista)
+            if lista not in saida:
+                saida.append(lista)
+        return saida
+
 
     return [entrada]
