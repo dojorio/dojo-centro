c7ab63127ee852372355e14fdedc25f37931b3be
dojorio <dojo-rio@googlegroups.com>
2017-04-19 21:02:17 -0300

    Alessandro Hecht e Odaraya

--- 2017/20170419 - permutacoes - python/problem.py
+++ 2017/20170419 - permutacoes - python/problem.py
@@ -5,17 +5,13 @@ import math
 import random
 
 def permutacoes(entrada):
+    
+    saida = entrada 
     if len (entrada) == 2:
-        return sorted([entrada, entrada[::-1]])
+        return [entrada, entrada[::-1]]
     elif len(entrada) == 3:
         saida = []
-        lista = list(entrada)
-        while len(saida) != math.factorial(len(entrada)):
-            random.shuffle(lista)
-            print (lista)
-            if lista not in saida:
-                saida.append(lista)
-        return saida
+        for d in entrada:
+            saida.append(reversed(entrada[1:]))
 
-
-    return [entrada]
+    return [entrada] 
