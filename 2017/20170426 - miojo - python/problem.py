acf20c672091c849ed0927c079e8c495d290f3e4
dojorio <dojo-rio@googlegroups.com>
2017-04-26 19:45:53 -0300

    Michell Heezen e Wilson Siqueira

--- 2017/20170426 - miojo - python/problem.py
+++ 2017/20170426 - miojo - python/problem.py
@@ -6,9 +6,10 @@ def miojo(a1, a2):
     if abs(a1 - a2) == 3:
         return max((a1, a2))
 
-    if (a1, a2) == (6, 9):
-        return 9
     elif (a1, a2) == (7, 9):
         return 21
+
+    elif (a1, a2) == (3, 6):
+        return 3
 
     return 10
--- 2017/20170426 - miojo - python/test_problem.py
+++ 2017/20170426 - miojo - python/test_problem.py
@@ -19,7 +19,10 @@ class TestProblem(unittest.TestCase):
         self.assertEqual(21, miojo(7, 9))
 
     def test_ampulhetas_de_4_e_7_da_7(self):
-        self.assertEqual(21, miojo(4, 7))
+        self.assertEqual(7, miojo(4, 7))
+
+    def test_ampulhetas_de_3_e_6_da_3(self):
+        self.assertEqual(3, miojo(3, 6))  
 
 if __name__ == "__main__":
     unittest.main()
