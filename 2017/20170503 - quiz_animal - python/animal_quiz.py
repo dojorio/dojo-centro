1712753ae45e4cff916eae4562472243d5a54307
dojorio <dojo-rio@googlegroups.com>
2017-05-03 20:17:39 -0300

    Toti Cavalcanti e Odaraya

--- 2017/20170503 - quiz_animal - python/animal_quiz.py
+++ 2017/20170503 - quiz_animal - python/animal_quiz.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#mia, voa, pena, come_banana, nada, couro
+animal= {
+(  1,   0,    0,           0,    0,     0): 'gato',
+(  0,   1,    1,           0,    0,     0): 'pombo',
+(  0,   1,    0,           0,    0,     0): 'morcego', 
+(  0,   0,    1,           0,    1,     0): 'pato',
+(  0,   0,    1,           0,    0,     0): 'galinha',
+}
 def quiz(mia=False, voa=False,
          pena=False, come_banana=False, nada=False, 
          couro=False):
+
+    return animal[(mia, voa, pena, come_banana, nada, couro)]
 
     if mia:
         return "gato"
