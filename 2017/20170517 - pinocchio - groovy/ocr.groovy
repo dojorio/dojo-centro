a6b5c3bf819bfdba978aa07637cc1bafe6f96cdf
dojorio <dojo-rio@googlegroups.com>
2017-05-17 20:58:00 -0300

    wilson Siqueira e Claudio Berrondo

--- 2017/20170517 - pinocchio - groovy/ocr.groovy
+++ 2017/20170517 - pinocchio - groovy/ocr.groovy
@@ -6,19 +6,13 @@ def static problem (pinocchio1, pinocchio2, pista) {
 	def d1 = pista - pinocchio1[1]
 	def d2 = pista - pinocchio2[1]
 
-	def p1 = 5/pinocchio1[0] + pinocchio1[1]
-	def p2 = 5/pinocchio2[0] + pinocchio2[1]
-	
-	if (pinocchio1 == [1,0.1] && pinocchio2 ==[2,2] && pista ==2)
+	def t1 = d1/v1
+	def t2 = d2/v2
+
+	if (t1 > t2)
 		return "pinocchio2"
-
-	if(p1 > p2 || pista == pinocchio1[1]) 
-        return "pinocchio1" 
-
-    else if(p2 > p1 || pista == pinocchio2[1]) 
-        return "pinocchio2"
-
-    else
-        return "empate"
-
+	else if (t1 < t2)
+		return "pinocchio1"
+	else
+		return "empate"
 }
