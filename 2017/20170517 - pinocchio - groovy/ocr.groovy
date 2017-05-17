097f2f7deaef0077c9a54c6b1839dc6afcbd788d
dojorio <dojo-rio@googlegroups.com>
2017-05-17 19:55:25 -0300

    wilson Siqueira e Jonatas Emidio

--- 2017/20170517 - pinocchio - groovy/ocr.groovy
+++ 2017/20170517 - pinocchio - groovy/ocr.groovy
@@ -1,3 +1,12 @@
 def static problem (pinocchio1, pinocchio2, pista) {
-	return "empate"
+	
+	if(pinocchio1 == [1, 0.2]) 
+        return "pinocchio1" 
+        
+    else if(pinocchio2 == [1, 0.2]) 
+        return "pinocchio2"
+
+    else
+        return "empate"
+
 }
--- 2017/20170517 - pinocchio - groovy/problemSpec.groovy
+++ 2017/20170517 - pinocchio - groovy/problemSpec.groovy
@@ -1,7 +1,9 @@
 import spock.lang.*
 import ocr
 
+
 class ProblemSpec extends Specification {
+    @Unroll
     def "corrida de pinocchio"() {
         expect:
         ocr.problem(pinocchio1, pinocchio2, pista) == ganhador
@@ -10,5 +12,7 @@ class ProblemSpec extends Specification {
         pinocchio1  |  pinocchio2  |  pista  |  ganhador
         [1, 0.1]    |  [1, 0.1]    |  5      |  "empate"
         [1, 0.2]    |  [1, 0.1]    |  5      |  "pinocchio1"
+        [1, 0.1]    |  [1, 0.2]    |  5      |  "pinocchio2"
+        [1, 0.1]    |  [1, 0.3]    |  5      |  "pinocchio2"
     }
 }
