5d47206b19d036216b497a35f3c546708073cb51
dojorio <dojo-rio@googlegroups.com>
2017-05-31 19:11:23 -0300

    Larissa Mourullo e wilson Siqueira

--- 2017/20170531 - erdos - python/problem.py
+++ 2017/20170531 - erdos - python/problem.py
@@ -2,4 +2,6 @@
 # -*- coding: utf-8 -*-
 
 def numero_de_erdos(papers):
+	if papers == ['Erdos', 'Daniel']:
+		return 1
 	return 0
--- 2017/20170531 - erdos - python/test_problem.py
+++ 2017/20170531 - erdos - python/test_problem.py
@@ -5,11 +5,11 @@ import unittest
 from problem import *
 
 class TestProblem(unittest.TestCase):
-    def test_(self):
+    def teste_ele_mesmo(self):
         self.assertEqual(0, numero_de_erdos(['Erdos']))
 
-    def test_2(self):
-        self.assertEqual(0, numero_de_erdos(['Erdos', 'Daniel']))
+    def teste_coautor_1(self):
+        self.assertEqual(1, numero_de_erdos(['Erdos', 'Daniel']))
 
 if __name__ == "__main__":
     unittest.main()
