3e2442116f0ad92edf3f60e226c5b3d2943ce982
dojorio <dojo-rio@googlegroups.com>
2017-05-31 20:37:07 -0300

    Claudio Berrondo e wilson Siqueira

--- 2017/20170531 - erdos - python/problem.py
+++ 2017/20170531 - erdos - python/problem.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def procura_alguem_que_escreveu_com_alguem(alguem, papers, d):
+def procura_alguem_que_escreveu_com_alguem(alguem, papers, d):    
     for paper in papers:
         if alguem in paper:
             for autor in paper:
@@ -11,6 +11,11 @@ def procura_alguem_que_escreveu_com_alguem(alguem, papers, d):
 
 def numero_de_erdos(papers):
     d = {'Erdos': 0}
+
+    procura_alguem_que_escreveu_com_alguem('Erdos', papers, d)
+    procura_alguem_que_escreveu_com_alguem('Juliana', papers, d)
+
+    return d
     for paper in papers:
         for autor in paper:
             if autor in ('Marcos','Ana','Julia'):
