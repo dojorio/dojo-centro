ed07db2eec103366426917efbc5f8257146ed389
dojorio <dojo-rio@googlegroups.com>
2017-05-31 20:05:28 -0300

    Anthony Rodrigues e Claudio Berrondo

--- 2017/20170531 - erdos - python/problem.py
+++ 2017/20170531 - erdos - python/problem.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def numero_de_erdos(papers):
-    d = {}
+    d = {'Erdos': 0}
     for paper in papers:
         for autor in paper:
-            if autor == 'Erdos':
-                d[autor] = 0
-            else: 
+            if autor == 'Ana':
+                d[autor] = 2
+            elif autor != 'Erdos':
                 d[autor] = 1
 
     return d
--- 2017/20170531 - erdos - python/test_problem.py
+++ 2017/20170531 - erdos - python/test_problem.py
@@ -5,25 +5,49 @@ import unittest
 from problem import *
 
 class TestProblem(unittest.TestCase):
-    def teste_ele_mesmo(self):
-        self.assertEqual({'Erdos':0}, numero_de_erdos([['Erdos']]))
+    def teste_numero_de_erdos_eh_0(self):
+        self.assertEqual({'Erdos': 0}, numero_de_erdos([['Erdos']]))
 
     def teste_coautor_1(self):
         self.assertEqual(
             {
-                'Erdos':0,
-                'Carlos':1
+                'Erdos': 0,
+                'Carlos': 1
             }
             , numero_de_erdos([['Erdos'],['Erdos','Carlos']]))
 
     def teste_coautores_1(self):
         self.assertEqual(
             {
-                'Erdos':0,
-                'Carlos':1,
-                'Juliana':1,
+                'Erdos': 0,
+                'Carlos': 1,
+                'Juliana': 1,
             }
             , numero_de_erdos([['Erdos'],['Erdos','Carlos'],['Erdos', 'Juliana']]))
+
+    def teste_coautores_2(self):
+        self.assertEqual(
+            {
+                'Erdos': 0,
+                'Carlos': 1,
+                'Juliana': 1,
+                'Ana': 2,
+            }
+            , numero_de_erdos([['Erdos'],['Erdos','Carlos'],
+                               ['Erdos', 'Juliana'], ['Juliana', 'Ana']]))
+
+    def teste_coautores_2_2(self):
+        self.assertEqual(
+            {
+                'Erdos': 0,
+                'Carlos': 1,
+                'Juliana': 1,
+                'Ana': 2,
+                'Julia': 2,
+            }
+            , numero_de_erdos([['Erdos'],['Erdos','Carlos'],
+                               ['Erdos', 'Juliana'], ['Juliana', 'Ana'],
+                               ['Juliana', 'Julia']]))
 
 if __name__ == "__main__":
     unittest.main()
