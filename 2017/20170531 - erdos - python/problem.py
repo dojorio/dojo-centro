f73bb0626c0b305b94a9ba0c3efcb162409c4cab
dojorio <dojo-rio@googlegroups.com>
2017-05-31 19:36:10 -0300

    Anderson Caldeira e Daniel Drumond

--- 2017/20170531 - erdos - python/problem.py
+++ 2017/20170531 - erdos - python/problem.py
@@ -2,4 +2,15 @@
 # -*- coding: utf-8 -*-
 
 def numero_de_erdos(papers):
-	return [0]
+    if len(papers) == 2:
+        return {
+                'Erdos':0,
+                'Carlos':1
+            }
+    elif len(papers) == 3:
+        return {
+                'Erdos':0,
+                'Carlos':1,
+                'Juliana':1,
+            }
+    return {'Erdos':0}
--- 2017/20170531 - erdos - python/test_problem.py
+++ 2017/20170531 - erdos - python/test_problem.py
@@ -6,10 +6,24 @@ from problem import *
 
 class TestProblem(unittest.TestCase):
     def teste_ele_mesmo(self):
-        self.assertEqual([0], numero_de_erdos([['Erdos']]))
+        self.assertEqual({'Erdos':0}, numero_de_erdos([['Erdos']]))
 
     def teste_coautor_1(self):
-        self.assertEqual([1], numero_de_erdos([['Erdos'],['Erdos','Carlos']]))
+        self.assertEqual(
+            {
+                'Erdos':0,
+                'Carlos':1
+            }
+            , numero_de_erdos([['Erdos'],['Erdos','Carlos']]))
+
+    def teste_coautores_1(self):
+        self.assertEqual(
+            {
+                'Erdos':0,
+                'Carlos':1,
+                'Juliana':1,
+            }
+            , numero_de_erdos([['Erdos'],['Erdos','Carlos'],['Erdos', 'Juliana']]))
 
 if __name__ == "__main__":
     unittest.main()
