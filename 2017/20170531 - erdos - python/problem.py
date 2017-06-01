e59c872a52fd4b0fcaac0fe6946576b2bfd434b7
dojorio <dojo-rio@googlegroups.com>
2017-05-31 21:06:53 -0300

    Daniel Drumond e Jorge William

--- 2017/20170531 - erdos - python/problem.py
+++ 2017/20170531 - erdos - python/problem.py
@@ -11,17 +11,16 @@ def panqueca(alguem, papers, d):
 def pacoca(nivel, papers, d):
     l = list(d.items())
     for autor, numero in l:
-        if numero == d:
+        if numero == nivel:
             panqueca(autor, papers, d)
 
 def numero_de_erdos(papers):
     d = {'Erdos': 0}
 
     panqueca('Erdos', papers, d)
+    for x in range(1,10):
+        pacoca(x, papers, d)
 
-    pacoca(1, papers, d)
-    pacoca(2, papers, d)
-    pacoca(3, papers, d)
 
     return d
     
