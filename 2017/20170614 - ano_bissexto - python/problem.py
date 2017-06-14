12be34eb45a9bb09d16fb8e9bca336fbfa1d81d5
dojorio <dojo-rio@googlegroups.com>
2017-06-14 19:31:44 -0300

    Lua Helena e Wilson Siqueira

--- 2017/20170614 - ano_bissexto - python/problem.py
+++ 2017/20170614 - ano_bissexto - python/problem.py
@@ -2,4 +2,5 @@
 # -*- coding: utf-8 -*-
 
 def ano_bissexto(ano):
-	return True
+	return False
+
--- 2017/20170614 - ano_bissexto - python/test_problem.py
+++ 2017/20170614 - ano_bissexto - python/test_problem.py
@@ -9,6 +9,8 @@ class TestAnoBissexto(unittest.TestCase):
     def test_retorno_eh_booleano(self):
         self.assertTrue(ano_bissexto(2000))
 
+    def test_ano_eh_bissexto(self):
+        self.assertFalse(ano_bissexto(2001))
 
 if __name__ == "__main__":
     unittest.main()
