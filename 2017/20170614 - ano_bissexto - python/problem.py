b65786217a693b232aa4704b894af5822335b27d
dojorio <dojo-rio@googlegroups.com>
2017-06-14 20:01:46 -0300

    Thiago Belem e André Novais

--- 2017/20170614 - ano_bissexto - python/problem.py
+++ 2017/20170614 - ano_bissexto - python/problem.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 def ano_bissexto(ano):
-	if ano == 1600:
-		return True
-	if ano % 4 == 0 and not ano % 100 == 0:
-		return True
-	return False
+    if ano == 1600:
+        return True
+    if ano % 4 == 0 and not ano % 100 == 0:
+        return True
+    return False
 
