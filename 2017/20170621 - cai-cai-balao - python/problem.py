6b856b0120cea5ea6fd52293341df8f1729d3721
dojorio <dojo-rio@googlegroups.com>
2017-06-21 20:02:56 -0300

    Otavio Cardoso e Daniel Drumond

--- 2017/20170621 - cai-cai-balao - python/problem.py
+++ 2017/20170621 - cai-cai-balao - python/problem.py
@@ -2,5 +2,10 @@
 # -*- coding: utf-8 -*-
 
 class Balao:
-	def cair(self):
-		return True
+    desejadas = 0
+    def cair(self):
+        self.desejadas += 1
+        return True
+
+    def esta_ok(self):
+        return self.desejadas <= 2
--- 2017/20170621 - cai-cai-balao - python/test_problem.py
+++ 2017/20170621 - cai-cai-balao - python/test_problem.py
@@ -6,10 +6,31 @@ from problem import *
 
 
 class TestProblem(unittest.TestCase):
-    def test_primeira_estrofe(self):
+    def test_primeira_queda(self):
         balao = Balao()
-
         self.assertTrue(balao.cair())
+    
+    def test_cair_2x(self):
+        balao = Balao()
+        balao.cair()
+        balao.cair()
+        self.assertTrue(balao.esta_ok())
+    
+    def test_cair_3x(self):
+        balao = Balao()
+        balao.cair()
+        balao.cair()
+        balao.cair()
+        self.assertFalse(balao.esta_ok())
+
+    def test_cair_3x(self):
+        balao = Balao()
+        balao.cair()
+        balao.cair()
+        balao.ir_para('mao')
+        #self.assertTrue(balao.esta_ok())
+
+
 
 if __name__ == "__main__":
     unittest.main()
