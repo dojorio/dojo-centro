d8cdbc5258326fe8fa1222b1d10108bc98f3af57
dojorio <dojo-rio@googlegroups.com>
2017-06-21 19:20:58 -0300

    Jonatas Emidio e Claudio Berrondo

--- 2017/20170614 - ano_bissexto - python/problem.py
+++ 2017/20170614 - ano_bissexto - python/problem.py
@@ -6,8 +6,4 @@
 #   exceto se ele for também divisível por 400.
 
 def ano_bissexto(ano):
-    if ano % 4 == 0 and ano % 100 != 0:
-        return True
-    elif ano % 400 == 0:
-        return True
-    return False
+    return (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0
---
+++ 2017/20170621 - cai-cai-balao - python/test_problem.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from problem import *
+
+
+class TestProblem(unittest.TestCase):
+    def test_(self):
+        self.assertEqual(1, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
