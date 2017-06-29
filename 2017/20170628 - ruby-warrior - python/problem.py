8988ba2ec919165eaebbf6eea78b6d55336c7d8f
dojorio <dojo-rio@googlegroups.com>
2017-06-28 21:05:29 -0300

    Toti Cavalcanti e wilson Siqueira

--- 2017/20170628 - ruby-warrior - python/problem.py
+++ 2017/20170628 - ruby-warrior - python/problem.py
@@ -10,7 +10,6 @@ def ruby_warrior(mapa, personagens):
 		total_warrior = forca_do_warrior + vida_do_warrior
 		total_monstro = forca_do_monstro + vida_do_monstro
 
-		if  total_warrior >= total_monstro:
-			return False
+		return total_warrior >= total_monstro
 
 	return True
