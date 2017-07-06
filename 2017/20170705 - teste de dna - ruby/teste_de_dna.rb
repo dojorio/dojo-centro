8f781f48362c55c7c6906343126bcfa62d9da100
dojorio <dojo-rio@googlegroups.com>
2017-07-05 21:03:35 -0300

    wilson Siqueira e Rogério Lourenco

--- 2017/20170705 - teste de dna - ruby/teste_de_dna.rb
+++ 2017/20170705 - teste de dna - ruby/teste_de_dna.rb
@@ -4,7 +4,7 @@ def teste_de_dna(dnas)
   if pai1.size == 2
     if filho == pai1
       return "Pai 1"
-      
+
     elsif filho == pai2
       return 'Pai 2'
     end
@@ -14,7 +14,9 @@ def teste_de_dna(dnas)
     elsif pai2 == 'AC'
       return 'Pai 2'
     end
-
+    if pai1 != filho
+      return pai2
+    end
     return 'cadim'
   end
 
