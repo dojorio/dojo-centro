d4708ac9474a32ceecf0d554c0a151e9985bd001
dojorio <dojo-rio@googlegroups.com>
2017-07-12 21:22:45 -0300

    Cristina Silva e Ceres

--- 2017/20170712 - brainfuck - go/main.go
+++ 2017/20170712 - brainfuck - go/main.go
@@ -1,11 +1,19 @@
 package main
+import "strings"
+
+int buffer = 0;
 
 func Interpreta(data string) string {
-if  "+" == data[0] {
-	for index, element := range data {
+	// 43 => '+'
+	if "+" == strings(data[0]) {
+		buffer++;
+		anotherData := make([]string, len(data) - 1)
 
+		for index, element := range data {
+			anotherData[index] = data[index + 1]
+		}
+		Interpreta(anotherData)
 	} 
-	return Interpreta 
 }
 	if data == "." { return "0"}
 	return ""
