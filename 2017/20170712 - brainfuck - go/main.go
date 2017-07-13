f48ed3eb57bba01338cb93255084749e0273580f
dojorio <dojo-rio@googlegroups.com>
2017-07-12 21:11:23 -0300

    Ceres e Rogerio Lourenco

--- 2017/20170712 - brainfuck - go/main.go
+++ 2017/20170712 - brainfuck - go/main.go
@@ -1,6 +1,12 @@
 package main
 
 func Interpreta(data string) string {
+if  "+" == data[0] {
+	for index, element := range data {
+
+	} 
+	return Interpreta 
+}
 	if data == "." { return "0"}
 	return ""
 }
