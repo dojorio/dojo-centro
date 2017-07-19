eeba74fc1f8f8ecf47b2ece8f25b1d9fdb42a3f7
dojorio <dojo-rio@googlegroups.com>
2017-07-19 20:25:07 -0300

    Claudio Berrondo e Rogerio Lourenco

--- 2017/20170719 - army buddies - python/problem.py
+++ 2017/20170719 - army buddies - python/problem.py
@@ -6,5 +6,7 @@ def army_buddies(soldados, baixas):
         if baixas[0][0] == baixas[0][1] and baixas[0][0] == soldados:
             return ['{} *'.format(baixas[0][0]-1)]
         else:
+            if baixas == [(2, 2)]:
+                return ['1 3']
             return ['* 2']
     return ['* *']
