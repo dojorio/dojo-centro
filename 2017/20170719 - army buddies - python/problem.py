f1a06b832035f30a8f48d24ebc5e6f7782edff34
dojorio <dojo-rio@googlegroups.com>
2017-07-19 20:39:40 -0300

    Daniel Drumond e Anderson Caldeira

--- 2017/20170719 - army buddies - python/problem.py
+++ 2017/20170719 - army buddies - python/problem.py
@@ -3,12 +3,12 @@
 
 def army_buddies(soldados, baixas):
     if soldados > 1:
-        if baixas[0][0] == baixas[0][1] and baixas[0][0] == soldados:
-            return ['{} *'.format(baixas[0][0]-1)]
-        else:
-            if baixas == [(2, 2)]:
-                return ['1 3']
-            elif baixas in ([(2, 3)], [(2, 4)]):
-                return ['{} {}'.format(baixas[0][0]-1, soldados)]
-            return ['* 2']
+        primeiro = baixas[0][0] - 1
+        segundo = baixas[0][1] + 1
+        if primeiro == 0:
+            primeiro = '*'
+        if segundo > soldados:
+            segundo = '*'
+        return ['{} {}'.format(primeiro, segundo)]
+        
     return ['* *']
