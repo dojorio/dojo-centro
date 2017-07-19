d62bd2a5502cec0cfc924f784a69bfe0c4809c1a
dojorio <dojo-rio@googlegroups.com>
2017-07-19 19:54:49 -0300

    Anderson Caldeira e Daniel Drumond

--- 2017/20170719 - army buddies - python/problem.py
+++ 2017/20170719 - army buddies - python/problem.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def army_buddies(soldados, baixas):
-	if soldados > 1:
-		if baixas == [(2, 2)]:
-			return ['1 *']
-		return ['* 2']
-	return ['* *']
+    if soldados > 1:
+        if baixas == [(2, 2)]:
+            return ['1 *']
+        elif baixas == [(3, 3)]:
+            return ['2 *']
+        elif baixas == [(4, 4)]:
+            return ['3 *']
+        return ['* 2']
+    return ['* *']
--- 2017/20170719 - army buddies - python/test_problem.py
+++ 2017/20170719 - army buddies - python/test_problem.py
@@ -9,27 +9,37 @@ from problem import *
 class TestProblem(unittest.TestCase):
     def test_1_soldado_1_baixa(self):
         baixas = [(1, 1)]
-        self.assertEqual(army_buddies(1, baixas), ['* *'])
+        self.assertEqual(['* *'], army_buddies(1, baixas))
 
     def test_2_soldados_1_baixa(self):
         baixas = [(1, 1)]
-        self.assertEqual(army_buddies(2, baixas), ['* 2'])
+        self.assertEqual(['* 2'], army_buddies(2, baixas))
 
     def test_2_soldados_a_outra_1_baixa(self):
         baixas = [(2, 2)]
-        self.assertEqual(army_buddies(2, baixas), ['1 *'])
+        self.assertEqual(['1 *'], army_buddies(2, baixas))
 
     def test_3_soldados_1_baixa(self):
         baixas = [(1, 1)]
-        self.assertEqual(army_buddies(3, baixas), ['* 2'])
+        self.assertEqual(['* 2'], army_buddies(3, baixas))
 
     def test_4_soldados_1_baixa(self):
         baixas = [(1, 1)]
-        self.assertEqual(army_buddies(4, baixas), ['* 2'])
+        self.assertEqual(['* 2'], army_buddies(4, baixas))
     
     def test_5_soldados_1_baixa(self):
         baixas = [(1, 1)]
-        self.assertEqual(army_buddies(5, baixas), ['* 2'])
+        self.assertEqual(['* 2'], army_buddies(5, baixas))
+    
+    def test_3_soldados_ultimo_morre(self):
+        soldados = 3
+        baixas = [(3, 3)]
+        self.assertEqual(['2 *'], army_buddies(soldados, baixas)) 
+    
+    def test_4_soldados_ultimo_morre(self):
+        soldados = 4
+        baixas = [(4, 4)]
+        self.assertEqual(['3 *'], army_buddies(soldados, baixas))
 
 if __name__ == "__main__":
     unittest.main()
