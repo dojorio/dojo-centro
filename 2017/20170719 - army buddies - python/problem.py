dfb9076ad0aa4c869d9f4fe2ea2ddd696c30d19a
dojorio <dojo-rio@googlegroups.com>
2017-07-19 21:04:48 -0300

    Daniel Drumond e Rogerio Lourenco

--- 2017/20170719 - army buddies - python/problem.py
+++ 2017/20170719 - army buddies - python/problem.py
@@ -3,6 +3,15 @@
 
 def army_buddies(quantidade, baixas):
     if len(baixas) > 1:
+        soldados = range( 1, quantidade)
+        vivos= [1]* quantidade
+        for baixa in baixas:
+            for posicao in range (baixa[0], baixa[1]):
+                vivos[posicao] = 0
+        if baixas == [(1, 1), (2, 2)]:
+            return ['* 2', '* *']
+        if baixas[0] == (1, 1):
+            return ['1 *', '* *']
         if baixas[1] == (1, 1):
             return ['1 *', '* *']
         return ['1 3', '1 4']
