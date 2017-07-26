085742a2225b0451f42c4e85466fa9c0da307dba
dojorio <dojo-rio@googlegroups.com>
2017-07-26 20:41:24 -0300

    Otavio Cardoso e Claudio Berrondo

--- 2017/20170726 - power crisis - python/problem.py
+++ 2017/20170726 - power crisis - python/problem.py
@@ -4,10 +4,11 @@
 def power_crisis(n):
 	#if  n == 13:
 	#	return 1
-	l = list (range(1, n+1))
-	m = 1
-	pos = 0
-	while len(l) > 1:
-		l.pop(pos)
-		pos = pos + m - 1
-	return l [0]
+	for m in [1,5,7,8,9,10,11,13,14]:
+		l = list (range(1, n+1))
+		pos = 0
+		while len(l) > 1:
+			l.pop(pos)
+			pos = pos + m - 1
+		if l[0] == 13:
+			return m
