b36f5600439394bd0a35b2044c0855b8ff46aa90
dojorio <dojo-rio@googlegroups.com>
2017-07-26 20:57:09 -0300

    wilson Siqueira e Rogerio Lourenco

--- 2017/20170726 - power crisis - python/problem.py
+++ 2017/20170726 - power crisis - python/problem.py
@@ -7,7 +7,7 @@ def power_crisis(n):
 	ms = [1,5,7,8,9,10,11,13,14] + list(range(15, 85))
 
 	for index, m in enumerate(ms):
-		l = list (range(1, n+1))
+		l = list(range(1, n+1))
 		pos = 0
 		while len(l) > 1:
 			l.pop(pos)
--- 2017/20170726 - power crisis - python/test_problem.py
+++ 2017/20170726 - power crisis - python/test_problem.py
@@ -6,14 +6,30 @@ import unittest
 from problem import *
 
 class TestProblem(unittest.TestCase):
+   
     def test_13(self):
         self.assertEqual(1, power_crisis(13))
+   
+    def test_14(self):
+        self.assertEqual(18, power_crisis(14))
+  
+    def test_15(self):
+        self.assertEqual(10, power_crisis(15))
+
+    def test_16(self):
+        self.assertEqual(11, power_crisis(16))
 
     def test_17(self):
         self.assertEqual(7, power_crisis(17))
 
+    def test_18(self):
+        self.assertEqual(29, power_crisis(18))
+
     def test_21(self):
         self.assertEqual(29, power_crisis(21))
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
