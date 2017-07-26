9ad60318aa2d396361720744462ae96b9ac20f24
dojorio <dojo-rio@googlegroups.com>
2017-07-26 20:34:57 -0300

    Claudio Berrondo e wilson Siqueira

--- 2017/20170726 - power crisis - python/problem.py
+++ 2017/20170726 - power crisis - python/problem.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def power_crisis(n):
-	if  n == 13:
-		return 1
-	return 1
+	#if  n == 13:
+	#	return 1
+	l = list (range(1, n+1))
+	m = 1
+	pos = 0
+	while len(l) > 1:
+		l.pop(pos)
+		pos = pos + m - 1
+	return l [0]
