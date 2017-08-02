81392ec282a98d83f14deab20ab8e7b4502d04e6
dojorio <dojo-rio@googlegroups.com>
2017-08-02 19:25:15 -0300

    Toti Cavalcanti e Euclides da Cunha

--- 2017/20170802 - ocr bancario - python/problem.py
+++ 2017/20170802 - ocr bancario - python/problem.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+source = [
+          "   ",
+          "  |",
+          "  |"
+        ]
 
 def ocr(source):
+
 	return 1
+
+def f(x,list):
+	return x in list
