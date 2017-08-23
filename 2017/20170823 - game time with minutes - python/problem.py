e29d6f26a5a7b767723729df7acac9416c5b2f8a
dojorio <dojo-rio@googlegroups.com>
2017-08-23 19:42:41 -0300

    Vinicius Rodrigues e Jonatas Emidio

--- 2017/20170823 - game time with minutes - python/problem.py
+++ 2017/20170823 - game time with minutes - python/problem.py
@@ -2,4 +2,5 @@
 # -*- coding: utf-8 -*-
 
 def time(value):
-	return 'O JOGO DUROU 0 HORA(S) E 1 MINUTO(S)'
+	minuto = '2' if '3' in value else '1' 
+	return 'O JOGO DUROU 0 HORA(S) E ' + minuto +' MINUTO(S)'
