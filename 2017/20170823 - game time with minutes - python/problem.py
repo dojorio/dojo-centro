0d1e9c6e2441ceb99904a8b9fc805d5e8f3d4213
dojorio <dojo-rio@googlegroups.com>
2017-08-23 20:23:19 -0300

    Claudio Berrondo e Cristina Santos

--- 2017/20170823 - game time with minutes - python/problem.py
+++ 2017/20170823 - game time with minutes - python/problem.py
@@ -7,12 +7,12 @@ def time(value):
 
     num_de_horas = cada_item[2] - cada_item[0]
     num_de_minutos = cada_item[3] - cada_item[1]
+    if num_de_minutos <0:
+        num_de_minutos=50
+        num_de_horas=0
 
-    """if value == '1 1 2 1':
-        num_de_horas = 1
-    if value == '1 1 3 1':
-        num_de_horas = 2
+   
 
-    num_de_minutos = int(value.split(' ')[3]) - 1 """
+    """num_de_minutos = int(value.split(' ')[3]) - 1 """
     
     return 'O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)'.format(num_de_horas, num_de_minutos)
