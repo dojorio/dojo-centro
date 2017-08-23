39f73a2ab2d7ac8dfd466d6f00f457e4243c4e47
dojorio <dojo-rio@googlegroups.com>
2017-08-23 20:55:59 -0300

    Jonatas Emidio e Daniel Drumond

--- 2017/20170823 - game time with minutes - python/problem.py
+++ 2017/20170823 - game time with minutes - python/problem.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1047
 
 def time(value):
-    cada_item = list(map(int, value.split(' ')))
-    num_de_horas = cada_item[2] - cada_item[0]
-    num_de_minutos = cada_item[3] - cada_item[1]
+    itens = list(map(int, value.split(' ')))
+    horas = itens[2] - itens[0]
+    minutos = itens[3] - itens[1]
 
-    if num_de_minutos < 0:
-        num_de_minutos += 60
-        num_de_horas -= 1
-    if num_de_horas == 0 == num_de_minutos:
-        num_de_horas = 24
-    if num_de_horas < 0:
-        num_de_horas += 24
+    if minutos < 0:
+        minutos += 60
+        horas -= 1
+    if horas == 0 == minutos:
+        horas = 24
+    if horas < 0:
+        horas += 24
 
-    return 'O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)'.format(num_de_horas, num_de_minutos)
+    plural_horas = ''
+    if horas > 1:
+        plural_horas = 'S'
+    
+    plural_minutos = ''
+    if minutos > 1:
+        plural_minutos = 'S'
+
+    return 'O JOGO DUROU {0} HORA{1} E {2} MINUTO{3}'.format(horas, plural_horas, minutos, plural_minutos)
--- 2017/20170823 - game time with minutes - python/test_problem.py
+++ 2017/20170823 - game time with minutes - python/test_problem.py
@@ -8,61 +8,61 @@ from problem import *
 class TestProblem(unittest.TestCase):
     def test_1_minuto(self):
         value = '1 1 1 2'
-        expected = 'O JOGO DUROU 0 HORA(S) E 1 MINUTO(S)'
+        expected = 'O JOGO DUROU 0 HORA E 1 MINUTO'
         self.assertEqual(expected, time(value))
 
     def test_2_minuto(self):
         value = '1 1 1 3'
-        expected = 'O JOGO DUROU 0 HORA(S) E 2 MINUTO(S)'
+        expected = 'O JOGO DUROU 0 HORA E 2 MINUTOS'
         self.assertEqual(expected, time(value))
 
     def test_3_minuto(self):
         value = '1 1 1 4'
-        expected = 'O JOGO DUROU 0 HORA(S) E 3 MINUTO(S)'
+        expected = 'O JOGO DUROU 0 HORA E 3 MINUTOS'
         self.assertEqual(expected, time(value))
 
     def test_4_minuto(self):
         value = '1 1 1 5'
-        expected = 'O JOGO DUROU 0 HORA(S) E 4 MINUTO(S)'
+        expected = 'O JOGO DUROU 0 HORA E 4 MINUTOS'
         self.assertEqual(expected, time(value))
 
     def test_10_minuto(self):
         value = '1 1 1 11'
-        expected = 'O JOGO DUROU 0 HORA(S) E 10 MINUTO(S)'
+        expected = 'O JOGO DUROU 0 HORA E 10 MINUTOS'
         self.assertEqual(expected, time(value))
 
     def test_1_hora(self):
         value = '1 1 2 1'
-        expected = 'O JOGO DUROU 1 HORA(S) E 0 MINUTO(S)'
+        expected = 'O JOGO DUROU 1 HORA E 0 MINUTO'
         self.assertEqual(expected, time(value))
 
     def test_2_hora(self):
         value = '1 1 3 1'
-        expected = 'O JOGO DUROU 2 HORA(S) E 0 MINUTO(S)'
+        expected = 'O JOGO DUROU 2 HORAS E 0 MINUTO'
         self.assertEqual(expected, time(value))
 
     def test_50_minutos(self):
         value = '1 50 2 40'
-        expected = 'O JOGO DUROU 0 HORA(S) E 50 MINUTO(S)'
+        expected = 'O JOGO DUROU 0 HORA E 50 MINUTOS'
         self.assertEqual(expected, time(value))
 
     def test_10_minutos_2(self):
         value = '2 55 3 5'
-        expected = 'O JOGO DUROU 0 HORA(S) E 10 MINUTO(S)'
+        expected = 'O JOGO DUROU 0 HORA E 10 MINUTOS'
         self.assertEqual(expected, time(value))
 
     def test_24_horas(self):
         value = '1 1 1 1'
-        expected = 'O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)'
+        expected = 'O JOGO DUROU 24 HORAS E 0 MINUTO'
         self.assertEqual(expected, time(value))
 
     def test_viradao_cultural_10_minutos(self):
         value = '23 59 0 9'
-        expected = 'O JOGO DUROU 0 HORA(S) E 10 MINUTO(S)'
+        expected = 'O JOGO DUROU 0 HORA E 10 MINUTOS'
         self.assertEqual(expected, time(value))
     def test_10_minutos_3(self):
         value = '0 9 23 59'
-        expected = 'O JOGO DUROU 23 HORA(S) E 50 MINUTO(S)'
+        expected = 'O JOGO DUROU 23 HORAS E 50 MINUTOS'
         self.assertEqual(expected, time(value))
 
 
