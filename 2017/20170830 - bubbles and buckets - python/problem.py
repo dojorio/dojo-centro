22a9f4b450068cfbb71b4e4ec47018b15181a77c
dojorio <dojo-rio@googlegroups.com>
2017-08-30 20:44:53 -0300

    Otavio Cardoso e Lucas Merquior

--- 2017/20170830 - bubbles and buckets - python/problem.py
+++ 2017/20170830 - bubbles and buckets - python/problem.py
@@ -10,4 +10,14 @@ def bubbles(sequence):
         if sequence[0] == 2 and sequence[1] == 3:
             return 'Carlos'
 
+    if len(sequence) == 4:
+        if sequence[0] == 3 and sequence[1] == 1 and True:
+            return 'Carlos'
+        if sequence[0] == 2 and sequence[1] == 3:
+            return 'Carlos'
+
+
+
+
+
     return 'Marcelo'
