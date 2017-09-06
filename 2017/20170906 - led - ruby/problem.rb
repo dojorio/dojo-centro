4a6a4f11ceb1ed7e281ba61397c52de351ae9bd4
dojorio <dojo-rio@googlegroups.com>
2017-09-06 20:59:07 -0300

    Otavio Cardoso e Júlia Kastrup

--- 2017/20170906 - led - ruby/problem.rb
+++ 2017/20170906 - led - ruby/problem.rb
@@ -1,15 +1,24 @@
 def led(number)
-  if number == 1
-    return 2
-  elsif number == 4 || number == 11 
-    return 4
-  elsif number == 6 || number == 9 || number == 0
-    return 6
-  elsif number == 7
-    return 3
-  elsif number == 8 || number == 12
-    return 7
-  else
-    return 5
+  numbers = number.to_s.split ""
+  leds = 0
+
+  numbers.each do |number|
+    number = number.to_i
+
+    if number == 1
+      leds += 2
+    elsif number == 4
+      leds += 4
+    elsif number == 6 || number == 9 || number == 0
+      leds += 6
+    elsif number == 7
+      leds += 3
+    elsif number == 8
+      leds += 7
+    else
+      leds += 5
+    end
   end
+  
+  leds
 end
