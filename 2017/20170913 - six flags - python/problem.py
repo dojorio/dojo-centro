e881f29f68f8809ed1c5be934fd7a91c4c7184e8
dojorio <dojo-rio@googlegroups.com>
2017-09-13 20:37:45 -0300

    Claudio Berrondo e Toti Cavalcanti

--- 2017/20170913 - six flags - python/problem.py
+++ 2017/20170913 - six flags - python/problem.py
@@ -3,8 +3,15 @@
 
 def sixflags(tempo, atracoes):
     satisfacao = 0
-    for atracao in sorted(atracoes, reverse=True):
+    for atracao in sorted(atracoes):
         while tempo >= atracao[0]:
             tempo -= atracao[0]
             satisfacao += atracao[1]
-    return satisfacao
+
+    satisfacao02 = 0
+    for atracao in sorted(atracoes, reverse=True):
+        while tempo >= atracao[0]:
+            tempo -= atracao[0]
+            satisfacao02 += atracao[1]
+
+    return max(satisfacao, satisfacao02)
