8e825d1be5aa1bb44c80d72b71199c830c9345be
dojorio <dojo-rio@googlegroups.com>
2017-09-13 19:34:16 -0300

    Toti Cavalcanti e Claudio Berrondo

---
+++ 2017/20170913 - six flags - python/test_problem.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1487
+
+import unittest
+from problem import *
+
+# 0 ≤ N ≤ 100 e 0 ≤ T ≤ 600,
+        # 5 60
+        # 10 30
+        # 20 32
+        # 5 4
+        # 50 90
+        # 22 45
+
+class TestProblem(unittest.TestCase):
+    def test_(self):
+
+        self.assertEqual(1, 2)
+
+if __name__ == "__main__":
+    unittest.main()
