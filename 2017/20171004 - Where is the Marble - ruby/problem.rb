15c51f94004d735970198bde38a8c0516c673b99
dojorio <dojo-rio@googlegroups.com>
2017-10-04 20:18:12 -0300

    Toti Cavalcanti e Odaraya Mello

--- 2017/20171004 - Where is the Marble - ruby/problem.rb
+++ 2017/20171004 - Where is the Marble - ruby/problem.rb
@@ -1,12 +1,6 @@
 def where_is_the_marble(marbles, queries)
-  if marbles == [2]
-    return [1]
-  end
-  if marbles == [1,2]
-    return [2]
-  end
-  if marbles == [2,5]
-    return [1]
+  if marbles.length >= 1 
+    return [marbles.index(queries[0]) + 1]
   end
   return [false]
 end
