620bf80809ef0c87abbb7de0c5996c38b15314f6
dojorio <dojo-rio@googlegroups.com>
2017-10-18 19:49:52 -0200

    Otavio Cardoso e Vinicius Carvalho

--- 2017/20171018 - miojo - javascript/problem.js
+++ 2017/20171018 - miojo - javascript/problem.js
@@ -3,6 +3,11 @@ exports.problem = function (amp1, amp2) {
 	if (amp1%2 == 0 && amp2%2 == 0 || amp2 == amp1 && amp1 > 3) {
 		return false
 	}
+
+	if (amp2 == 3) {
+		return 3
+	}
+
 	if ( amp1 > 4 || amp2 > 4){	
 		
 		if ( amp1 - amp2 == 3  ){
@@ -12,10 +17,11 @@ exports.problem = function (amp1, amp2) {
 		if (amp2 - amp1 == 3) {
 			return amp2
 		}
-		
-		if ( Math.abs(amp1 - amp2) == 2) 
+
+		if (Math.abs(amp1 - amp2) == 2) {
 			return 10
-	} 
+		} 
+	}
 
 	return 3  
 };
