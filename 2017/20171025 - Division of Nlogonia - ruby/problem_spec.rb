4f9c69d57a0c345c0f4aff03011e3716a9b948fe
dojorio <dojo-rio@googlegroups.com>
2017-10-25 19:17:43 -0200

    Rogério Lourenco e Otavio Cardoso

---
+++ 2017/20171025 - Division of Nlogonia - ruby/problem_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'problem'
+
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1091
+
+describe "problem" do
+  it "sample" do
+    bar = []
+    expect(problem(bar)).to eq(0)
+  end
+end
