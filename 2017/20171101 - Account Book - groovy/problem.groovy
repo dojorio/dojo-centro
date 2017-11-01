906b3b37f9497f3c427caffff828cb5be0f6e045
dojorio <dojo-rio@googlegroups.com>
2017-11-01 20:56:44 -0200

    Otavio Cardoso e Jonatas Emidio

--- 2017/20171101 - Account Book - groovy/problem.groovy
+++ 2017/20171101 - Account Book - groovy/problem.groovy
@@ -1,10 +1,11 @@
 def static lava_jato (total, values) {
 	if (values.size() == 1) {
-		if (total < 0  && total == -values[0]) {
-		    return '-'
-		} else if(total && total == values[0]) {
-			return '+'
+		if(total.abs() == values[0]) {
+			return total>0 ? "+" : "-"
+		} else {
+			return "*"
 		}
+
 	} else if (values == [1,1] && total > 0) {
 		return '++'
 
