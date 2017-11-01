b1f92b5a78de8454a0ab0ec0d0a9c900b9f7568d
dojorio <dojo-rio@googlegroups.com>
2017-11-01 20:50:01 -0200

    Jonatas Emidio e Lucas Araujo

--- 2017/20171101 - Account Book - groovy/problem.groovy
+++ 2017/20171101 - Account Book - groovy/problem.groovy
@@ -1,5 +1,5 @@
 def static lava_jato (total, values) {
-	if (values.size() == 1 || value != -1) {
+	if (values.size() == 1) {
 		if (total < 0  && total == -values[0]) {
 		    return '-'
 		} else if(total && total == values[0]) {
@@ -11,6 +11,8 @@ def static lava_jato (total, values) {
 	} else if(values == [1,1]) {
 		return '--'
 	}
-
+	  else if(values[0]== 3) {
+	  	return '+-'
+	  }
 	return '*'
 }
