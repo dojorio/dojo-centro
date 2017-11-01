ab97d31e59a1f548446a6ccb50479686d233b92c
dojorio <dojo-rio@googlegroups.com>
2017-11-01 21:02:58 -0200

    Rogério Lourenco e Otavio Cardoso

--- 2017/20171101 - Account Book - groovy/problem.groovy
+++ 2017/20171101 - Account Book - groovy/problem.groovy
@@ -5,15 +5,17 @@ def static lava_jato (total, values) {
 		} else {
 			return "*"
 		}
-
-	} else if (values == [1,1] && total > 0) {
-		return '++'
-
-	} else if(values == [1,1]) {
-		return '--'
 	}
-	  else if(values[0]== 3) {
-	  	return '+-'
-	  }
+
+	if (values.size() == 2) {
+		if (total.abs() == values[0] + values[1]) {
+			return total > 0 ? "++" : "--"
+		} else if (total == values[0] - values[1]) {
+			return '+-'
+		} else if (total == values[1] - values[0]) {
+			return '-+'
+		}
+	}
+
 	return '*'
 }
--- 2017/20171101 - Account Book - groovy/problemSpec.groovy
+++ 2017/20171101 - Account Book - groovy/problemSpec.groovy
@@ -16,6 +16,7 @@ class ProblemSpec extends spock.lang.Specification {
         2     | [1,1]  | '++'
         -2    | [1,1]  | '--'
         2     | [3,1]  | '+-'
+        -2    | [3,1]  | '-+'
 
 
     }
