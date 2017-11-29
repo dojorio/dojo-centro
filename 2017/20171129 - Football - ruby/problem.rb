a25d4fe1c4aa5c5e3853f370b67f14a6e7a053f2
dojorio <dojo-rio@googlegroups.com>
2017-11-29 20:40:23 -0200

    Jonatas Emidio e Toti Cavalcanti

--- 2017/20171129 - Football - ruby/problem.rb
+++ 2017/20171129 - Football - ruby/problem.rb
@@ -1,7 +1,11 @@
 def football(goals, games)
  
+  if games[0][1] > games[0][0]
+ 	return 1
+  end	
+
   if games[0][0] == 1
   	return 3
   end
-  return 1	
+  
 end
--- 2017/20171129 - Football - ruby/problem_spec.rb
+++ 2017/20171129 - Football - ruby/problem_spec.rb
@@ -3,17 +3,23 @@ require_relative 'problem'
 # https://www.urionlinejudge.com.br/judge/en/problems/view/1495
 
 describe "Football" do
-  it "1 goal 1 loss" do
+  it "purchase 1 goal 1 loss" do
     goals = 1
     games = [[0, 1]]
 
     expect(football(goals, games)).to eq(1)
   end
-  it "1 goal 1 draw" do
+  it "purchase 1 goal 1 draw" do
     goals = 1
     games = [[1, 1]]
 
     expect(football(goals, games)).to eq(3)
+  end
+  it "purchase 1 goal " do
+    goals = 1
+    games = [[1, 2]]
+
+    expect(football(goals, games)).to eq(1)
   end
 
 end
