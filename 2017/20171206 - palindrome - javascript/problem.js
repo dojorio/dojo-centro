411e6544f9c80d8d097be4da6fffdfcdcbf344d6
dojorio <dojo-rio@googlegroups.com>
2017-12-06 19:59:16 -0200

    Rudá Almeida e Rogerio Lourenco

--- 2017/20171206 - palindrome - javascript/problem.js
+++ 2017/20171206 - palindrome - javascript/problem.js
@@ -1,5 +1,5 @@
 exports.problem = function (bar) {
-	if(bar[0] == "a") {
+	if((bar[0].length) == 1) {
 		return 1
 	}
     
--- 2017/20171206 - palindrome - javascript/problem_spec.js
+++ 2017/20171206 - palindrome - javascript/problem_spec.js
@@ -12,8 +12,12 @@ describe('panlidrome', function() {
         assert.equal(problem(bar), 0)
     })
 
-    it('when "b" returns 0', function () {
-        var bar = ["ab"]
+    it('when "b" returns 1', function () {
+        var bar = ["b"]
+        assert.equal(problem(bar), 1)
+    })
+    it('when "7" returns 0', function () {
+        var bar = ["7"]
         assert.equal(problem(bar), 0)
     })
 })
