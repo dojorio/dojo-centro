c5868199362687e8e5000cc125b3f1cd3d8f227a
dojorio <dojo-rio@googlegroups.com>
2017-12-13 20:32:42 -0200

    Rogério Lourenco e Odaraya Mello

--- 2017/20171213 - letrasdosnumeros - javascript/problem.js
+++ 2017/20171213 - letrasdosnumeros - javascript/problem.js
@@ -1,5 +1,9 @@
 exports.problem = function (numeros) {
+var soma = 0
 
+numeros.forEach(function(item, index, lista){
+	soma += item.length
+}) 
 
-    	return numeros[].length
+    	return soma
 };
