b1ace1f723919a462efc763134ba52818dde5484
dojorio <dojo-rio@googlegroups.com>
2017-12-20 20:59:40 -0200

    Rogerio Lourenco e Toti Cavalcanti

--- 2017/20171220 - efeito magnetico - groovy/problem.groovy
+++ 2017/20171220 - efeito magnetico - groovy/problem.groovy
@@ -1,7 +1,10 @@
 def static efeitoMagnetico (magnetos, cursor) {
 	def magneto = magnetos[0]
 
-	if (magneto == [] || (magneto[0] != cursor[0] && magneto [1] != cursor [1])) {
+	if (magneto == [] ||  	 
+	   	(magneto[0] != cursor[0] && 
+	   	magneto [1] != cursor [1]) && 
+	   	magneto[2] == 1) {
 		return cursor
 	}
 
