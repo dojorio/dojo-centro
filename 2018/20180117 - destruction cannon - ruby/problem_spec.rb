c0dfe1c3b83699616d38f9ba367638f8134949e1
dojorio <dojo-rio@googlegroups.com>
2018-01-17 19:59:03 -0200

    Venancio mota e Rodolfo Carvalho

--- 2018/20180117 - destruction cannon - ruby/problem_spec.rb
+++ 2018/20180117 - destruction cannon - ruby/problem_spec.rb
@@ -3,29 +3,34 @@ require_relative 'problem'
 # https://www.urionlinejudge.com.br/judge/en/problems/view/1288
 
 describe "Destruction Cannon" do
-  it "fails if no lead" do
-  	mission = Mission.new
-    expect(mission).not_to be_completable
-  end  
 
-  it "succeeds if has mission" do
-  	mission = Mission.new(leads: [700, 5], castle_resistance: 680 , cannon: 10 )
-    expect(mission).to be_completable
-  end
+	context "success when" do
+		it "has mission" do
+			mission = Mission.new(leads: [700, 5], castle_resistance: 680 , cannon: 10 )
+			expect(mission).to be_completable
+		end
+	end
 
-  it "fails if has mission without leads" do
-  	mission = Mission.new(leads: [], castle_resistance: 680 , cannon: 10 )
-    expect(mission).not_to be_completable
-  end
+	context "fails when" do
+		it "no lead" do
+			mission = Mission.new
+			expect(mission).not_to be_completable
+		end  
 
-  it "fails if cannon capacity is to low" do
-  	mission = Mission.new(leads: [10, 2], castle_resistance: 5 , cannon: 1)
-    expect(mission).not_to be_completable
-  end
+		it "has mission without leads" do
+			mission = Mission.new(leads: [], castle_resistance: 680 , cannon: 10 )
+			expect(mission).not_to be_completable
+		end
 
- it "fails if castle ressistence is to high" do
-  	mission = Mission.new(leads: [10, 2], castle_resistance: 12 , cannon: 5)
-    expect(mission).not_to be_completable
-  end
+		it "cannon capacity is to low" do
+			mission = Mission.new(leads: [10, 2], castle_resistance: 5 , cannon: 1)
+			expect(mission).not_to be_completable
+		end
+
+		it "castle ressistence is to high" do
+			mission = Mission.new(leads: [10, 2], castle_resistance: 12 , cannon: 5)
+			expect(mission).not_to be_completable
+		end
+	end
 
 end
