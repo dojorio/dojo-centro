6d745da83359f07e07b29dafea7f98ff01d7ff11
dojorio <dojo-rio@googlegroups.com>
2018-01-17 20:49:56 -0200

    Rodolfo Carvalho e Claudio Berrondo

--- 2018/20180117 - destruction cannon - ruby/problem.rb
+++ 2018/20180117 - destruction cannon - ruby/problem.rb
@@ -1,25 +1,29 @@
 class Mission
 	def completable?
-		if ! @mission.empty? 
-
-			power = @mission[:leads][0]
-			if @mission[:castle_resistance] == 16 # 10 < 16
-				return false
-			end
-
-		   if @mission[:castle_resistance] == 14 # 10 < 14
-				return false
-			end
-
-			if @mission[:castle_resistance] == 12 # 10 < 12
-				return false
-			end
-
-			if ! @mission.empty? && ! @mission[:leads].empty?
-				@mission[:leads][1] <= @mission[:cannon]
-			end
-		else 
+		if @mission.empty?
 			return false
+		end
+
+		if @mission[:leads].empty?
+			return false
+		end
+
+		power = @mission[:leads][0]
+
+		if @mission[:castle_resistance] == 16 # 10 < 16
+			return false
+		end
+
+	    if @mission[:castle_resistance] == 14 # 10 < 14
+			return false
+		end
+
+		if @mission[:castle_resistance] == 12 # 10 < 12
+			return false
+		end
+
+		if ! @mission.empty? && ! @mission[:leads].empty?
+			@mission[:leads][1] <= @mission[:cannon]
 		end
 
 		
