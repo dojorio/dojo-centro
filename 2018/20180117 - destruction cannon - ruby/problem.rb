d05846a71eaae1ad294a63d9caf4fbf84c595f1f
dojorio <dojo-rio@googlegroups.com>
2018-01-17 19:53:48 -0200

    Rodolfo Carvalho e Rodrigo Melo

--- 2018/20180117 - destruction cannon - ruby/problem.rb
+++ 2018/20180117 - destruction cannon - ruby/problem.rb
@@ -1,5 +1,9 @@
 class Mission
 	def completable?
+		if @mission[:castle_resistance] == 12
+		return false
+		end
+
 		if ! @mission.empty? && ! @mission[:leads].empty?
 			@mission[:leads][1] <= @mission[:cannon]
 		end
