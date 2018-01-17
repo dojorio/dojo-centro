cefea00566de007d13227f48600ae2e6ec6f20e9
dojorio <dojo-rio@googlegroups.com>
2018-01-17 19:18:44 -0200

    venancio mota e Fabio Abreu

--- 2018/20180117 - destruction cannon - ruby/problem_spec.rb
+++ 2018/20180117 - destruction cannon - ruby/problem_spec.rb
@@ -7,4 +7,8 @@ describe "Destruction Cannon" do
   	mission = Mission.new
     expect(mission).not_to be_completable
   end
+  it "" do
+  	mission = Mission.new(leads: [500, 5], castle_resistance: 680 , cannon: 10 )
+    expect(mission).to be_completable
+  end
 end
