04d3d4056e5459af9c115569c819c98d8aadef8a
dojorio <dojo-rio@googlegroups.com>
2018-01-24 20:51:25 -0200

    Vinicius Mattos e Felipe Lyra

--- 2018/20180124 - world cup - ruby/problem_spec.rb
+++ 2018/20180124 - world cup - ruby/problem_spec.rb
@@ -135,4 +135,15 @@ describe "World Cup" do
     expect(world_cup.draws).to eq(5)
   end
 
+  it "has 1 draws when 2 matches was played and one team has 4 points v2" do
+    matches = 2
+    teams = {
+    	Croacia: 4,
+    	Australia: 1
+    }
+    world_cup = WorldCup.new(matches, teams)
+
+    expect(world_cup.draws).to eq(1)
+  end
+
 end
