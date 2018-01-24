fa7da4c156b94f8e80bbf6f19e127f92c9ae9644
dojorio <dojo-rio@googlegroups.com>
2018-01-24 20:29:44 -0200

    Otavio Cardoso e Claudio Berrondo

--- 2018/20180124 - world cup - ruby/problem.rb
+++ 2018/20180124 - world cup - ruby/problem.rb
@@ -3,19 +3,12 @@ class WorldCup
     @matches = matches
     @teams = teams
   end
-  def draws 
-    if @matches == 3 
+  def draws
+    if @matches == 3 && @matches == @teams[:Brasil]
       return 3
-    end
-    if @teams.values.include?(2)
-      return 2
-    end
-    if @teams.values.include?(3)
-      return 0
-    end
-    if @teams.values.include?(0)
-      return 0
-    end
-    @matches > 0 ? 1 : 0
+    end 
+
+    @teams[:Brasil] % 3
+
   end
 end
