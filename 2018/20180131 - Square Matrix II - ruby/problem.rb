907c967f57b7d75560360b0fa7dad115f53c5e8c
dojorio <dojo-rio@googlegroups.com>
2018-01-31 20:26:47 -0200

    Fábio Abreu e Jonatas Emidio

--- 2018/20180131 - Square Matrix II - ruby/problem.rb
+++ 2018/20180131 - Square Matrix II - ruby/problem.rb
@@ -1,19 +1,8 @@
 def problem(ord)
 	output = [[]]
-
-	if ord == 2
-		return [[1, 2], [2, 1]]
+	value = 1
+	for i in 0..ord-1
+		output[i][i] = value
 	end
-	if ord == 3
-		for i in 1..ord
-			for j in 1..ord
-			    output[i - 1][j - 1] = i - 1
-			end
-		end
-
-		return [[1, 2, 3], [2, 1, 2], [3, 2, 1]]
-	end
-	if ord == 4
-		return [[1, 2, 3, 4], [2, 1, 2, 3], [3, 2, 1, 2], [4, 3, 2, 1]] end
-  return [[1]]
+    return output
 end
