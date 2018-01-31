4a6d8b856204c332258a881548b1882cfd16f33b
dojorio <dojo-rio@googlegroups.com>
2018-01-31 21:06:26 -0200

    Fabio Abreu e Jonatas Emidio

--- 2018/20180131 - Square Matrix II - ruby/problem.rb
+++ 2018/20180131 - Square Matrix II - ruby/problem.rb
@@ -9,10 +9,14 @@ def problem(ord)
 	for i in 0..ord-1
 		# preenche apos 1
 		for j in i..ord-1
-			output[i][j+1] = j + 1 
+			if j != 1 
+				output[i][j+1] = j
+			end 
 		end
-		for j in ord-1..0
-			output[i][j-1] = j - 1
+		for j in ord-1..i
+			if j != 1 
+				output[i][j-1] = j
+			end
 		end
 	end
 
