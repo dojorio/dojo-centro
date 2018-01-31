0f2507f16e7b71bb0951a6d20ae774099308f8bc
dojorio <dojo-rio@googlegroups.com>
2018-01-31 20:55:07 -0200

    Toti Cavalcanti e Felipe Lyra

--- 2018/20180131 - Square Matrix II - ruby/problem.rb
+++ 2018/20180131 - Square Matrix II - ruby/problem.rb
@@ -5,5 +5,18 @@ def problem(ord)
 	for i in 0..ord-1
 		output[i][i] = value
 	end
+
+	for i in 0..ord-1
+		# preenche apos 1
+		for j in i..ord-1
+			output[i][j] = i+1 
+		end
+
+		# preenche antes do 1
+		for 
+			
+		end
+	end
+
     return output
 end
