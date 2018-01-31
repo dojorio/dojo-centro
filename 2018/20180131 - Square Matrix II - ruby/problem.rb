56694a7a4c4deec1a8cacd4ba8e4dac457b2b7ed
dojorio <dojo-rio@googlegroups.com>
2018-01-31 19:46:23 -0200

    Fabio Abreu e Toti Cavalcanti

---
+++ 2018/20180131 - Square Matrix II - ruby/problem.rb
@@ -0,0 +1,6 @@
+def problem(ord)
+	if ord == 2
+		return [[1, 2], [2, 1]]
+	end
+  return [[1]]
+end
---
+++ 2018/20180131 - Square Matrix II - ruby/problem_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'problem'
+
+describe "problem" do
+  it "matriz 1" do
+    order = 1
+    expec = [[1]]
+    expect(problem(order)).to eq(expec)
+  end
+
+  it "matriz 2" do
+    order = 2
+    expec = [[1, 2], 
+    		 [2, 1]]
+    expect(problem(order)).to eq(expec)
+  end
+end
