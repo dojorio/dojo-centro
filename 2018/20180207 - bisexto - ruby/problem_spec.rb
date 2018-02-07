0f99cd71294e99658da1e50a954c4fdf799cdbb6
dojorio <dojo-rio@googlegroups.com>
2018-02-07 20:27:12 -0200

    Felipe Lyra e Fabio Abreu

--- 2018/20180207 - bisexto - ruby/problem_spec.rb
+++ 2018/20180207 - bisexto - ruby/problem_spec.rb
@@ -5,7 +5,7 @@ describe "bisexto" do
     expect(bisexto(2000)).to eq(true)
   end
     it "2020 é divisivel por 4 e por 400" do
-    expect(bisexto(2020)).to eq(false)
+    expect(bisexto(2020)).to eq(true)
   end
 end
 
