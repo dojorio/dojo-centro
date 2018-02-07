9a3235515d88e430054fc0af2a6d90bd86a365b9
dojorio <dojo-rio@googlegroups.com>
2018-02-07 20:14:49 -0200

    Rogerio Lourenco e David Lima

--- 2018/20180207 - bisexto - ruby/problem.rb
+++ 2018/20180207 - bisexto - ruby/problem.rb
@@ -1,11 +1,6 @@
 def bisexto(ano)
 	if 
-		ano % 4 == 0
-		return true
-	end
-
-	if 
-		ano % 100 == 0  && ano % 4 == 0
+		ano % 400 == 0  && ano % 4 == 0
 		return true
 	else 
 		return false
--- 2018/20180207 - bisexto - ruby/problem_spec.rb
+++ 2018/20180207 - bisexto - ruby/problem_spec.rb
@@ -1,13 +1,11 @@
 require_relative 'problem'
 
 describe "bisexto" do
-  it "2021 não é divisivel por 4" do
-    expect(bisexto(2021)).to eq(false)
-  end
-
-  it "2000 é divisivel por 4 e por 100" do
+  it "2000 é divisivel por 4 e por 400" do
     expect(bisexto(2000)).to eq(true)
   end
+
+
 
   
 end
