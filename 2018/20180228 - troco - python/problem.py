f0212943d4756d1c67dfdd8462730665d538fa44
dojorio <dojo-rio@googlegroups.com>
2018-02-28 20:22:11 -0300

    Rogerio Lourenco e Felipe Lyra

--- 2018/20180228 - troco - python/problem.py
+++ 2018/20180228 - troco - python/problem.py
@@ -7,19 +7,15 @@ def change_for(value, paid):
 
 	troco_total = paid - value
 
-
-	if troco_total == 4:
-		return { 2: 2}
+	if value == paid:
+		return { }
 	elif troco_total in bills:
 		return { troco_total: 1 } 
-	elif value == paid:
-		return { }
-	elif value == 5:
-		return { 5: 1 }
-	elif value == 15:
-		return { 5: 1 }
-
-	#elif value == 20:
-	#   return { 20: 1, 10: 1 }
+	elif troco_total == 4:
+		for nota in bills:
+			quantidade = troco_total / nota
+		return { troco: quantidade}
+	elif value == 20:
+	   return { 20: 1, 10: 1 }
 	else:
 		return { 2: 1 }
