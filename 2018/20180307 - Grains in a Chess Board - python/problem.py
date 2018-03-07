bf431273ecb26dab64a82f1031898191e4e06b48
dojorio <dojo-rio@googlegroups.com>
2018-03-07 19:43:16 -0300

    Rodrigo Melo e Daniel Drumond

--- 2018/20180307 - Grains in a Chess Board - python/problem.py
+++ 2018/20180307 - Grains in a Chess Board - python/problem.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def board_to_kg(cells):
+    return board_to_g(cells)//1000
+
+def board_to_g(cells):
+    return board_to_grain(cells)//12
+
 def board_to_grain(cells):
-	return (2 ** cells) - 1
+    return (2 ** cells) - 1
--- 2018/20180307 - Grains in a Chess Board - python/test_problem.py
+++ 2018/20180307 - Grains in a Chess Board - python/test_problem.py
@@ -16,6 +16,20 @@ class TestProblem(unittest.TestCase):
     def test_4(self):
         self.assertEqual(board_to_grain(12), 4095)
     def test_5(self):
-        self.assertEqual(board_to_g(2), 3)
+        self.assertEqual(board_to_g(4), 1)
+    def test_6(self):
+        self.assertEqual(board_to_g(3), 0)
+    def test_7(self):
+        self.assertEqual(board_to_g(5), 2)
+    def test_8(self):
+        self.assertEqual(board_to_kg(12), 0)
+    def test_9(self):
+        self.assertEqual(board_to_kg(14), 1)
+    def test_10(self):
+        self.assertEqual(board_to_kg(19), 43)
+    def test_11(self):
+        self.assertEqual(board_to_kg(0), 0)
+    def test_12(self):
+        self.assertEqual(board_to_kg(7), 0)
 if __name__ == "__main__":
     unittest.main()
