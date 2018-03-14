ae17e9e51b2eea12cdaec63921db8c2d227189aa
dojorio <dojo-rio@googlegroups.com>
2018-03-14 19:57:59 -0300

    Toti Cavalcanti e Rodrigo Melo

--- 2018/20180314 - Letter Range - python/problem.py
+++ 2018/20180314 - Letter Range - python/problem.py
@@ -4,11 +4,13 @@
 def letter_range(str):
 	if str == '':
 		return []
-
+	if len(str) == 1:
+		return [str+":"+str]
 	str = sorted(str)
-	if len(str) == 2 and (ord(str[1]) - ord(str[0])):		
+	if len(str) == 2 and (ord(str[1]) - ord(str[0]) == 1):		
 		return [str[0]+":"+str[1]]
-	elif (ord(str[1]) - ord(str[0]) >= 1):
+
+	elif (ord(str[1]) - ord(str[0]) > 1):
 		return [str[0]+":"+str[0], str[1]+":"+str[1]]
 
-	return [str[0]+":"+str[0]]
+	
