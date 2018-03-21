a8f5c9de544613dabb7f8fd1429fd1001ac1b6d5
dojorio <dojo-rio@googlegroups.com>
2018-03-21 20:39:22 -0300

    Toti Cavalcanti e Bruno Braat

--- 2018/20180321 - escudo antibruxa - python/problem.py
+++ 2018/20180321 - escudo antibruxa - python/problem.py
@@ -7,13 +7,13 @@ KIDS_WIN   = 'KIDS WIN'
 def escudo_antibruxa(criancas, sal):
 	kids_distance_x = 0
 	number_of_kids = len(criancas) 
-
-	if number_of_kids > 1:
-		kids_distance_x = abs(criancas[1][0] - criancas[0][0])
+		
 	if number_of_kids == 1:	
 		if sal >= 8:
 			return KIDS_WIN
-	elif number_of_kids == 2: 
+	elif number_of_kids == 2:
+		kids_distance_x = abs(criancas[1][0] - criancas[0][0])
+		kids_distance_y = abs(criancas[0][1] - criancas[0][0])
 		needed = 16 if kids_distance_x >= 4 else (8 + kids_distance_x*2)
 		if (sal >= needed):
 			return KIDS_WIN
