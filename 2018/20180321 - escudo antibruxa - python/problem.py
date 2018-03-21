b60f9a41071293adb143131fe766983de0cefef4
dojorio <dojo-rio@googlegroups.com>
2018-03-21 19:31:24 -0300

    Julio Marins e Felipe Lyra

--- 2018/20180321 - escudo antibruxa - python/problem.py
+++ 2018/20180321 - escudo antibruxa - python/problem.py
@@ -4,8 +4,8 @@
 def escudo_antibruxa(criancas, sal):
 	BRUXA_WINS = 'BRUXA WINS'
 	KIDS_WIN   = 'KIDS WIN'
-	if sal >= 8 and len(criancas) == 1:		
+	if sal >= 8 and len(criancas) == 1:	
 		return KIDS_WIN
-	elif sal >= 10 and len(criancas) == 2 and :		
+	elif sal >= 10 and len(criancas) == 2 and (criancas[1][0] - criancas[0][0] == 1):
 		return KIDS_WIN
 	return BRUXA_WINS
