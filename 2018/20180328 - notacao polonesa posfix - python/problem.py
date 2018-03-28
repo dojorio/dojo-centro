4e6373da2058f30bc11337990f494d718ad4247d
dojorio <dojo-rio@googlegroups.com>
2018-03-28 19:16:58 -0300

    Julio Marins e Gustavo Fontinha

--- 2018/20180328 - notacao polonesa posfix - python/problem.py
+++ 2018/20180328 - notacao polonesa posfix - python/problem.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def transliterate(expression):
-	if expression[0:1] == "a+":
-		return "a" + expression[-1] + "+"
+	if expression[0] + expression[1] == "a+":
+		return expression[0] + expression[1] + "+" 
 
 	return expression
