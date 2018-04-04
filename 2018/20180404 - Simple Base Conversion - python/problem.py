fdcb3f0774be9b55471fd2e60fc6ce003c4a4883
dojorio <dojo-rio@googlegroups.com>
2018-04-04 20:47:21 -0300

    Felipe Lyra e Rafael Nunes

--- 2018/20180404 - Simple Base Conversion - python/problem.py
+++ 2018/20180404 - Simple Base Conversion - python/problem.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 def converter(inputy):
-    dic = {'10':'A', '11':'B', '12':'C', '13':'D', '14':'E', '15':'F'}
+    dic = {'10':'A', '11':'B', '12':'C',
+           '13':'D', '14':'E', '15':'F'}
 
     if int(inputy) > 15:
-        return '0x10'
+        return '0x1' + dic.get(str(int(inputy)-16), str(int(inputy)-16))
 
     return '0x' + dic.get(inputy, inputy)
