bb1452390a46ae02f86e51a3fd1ff5b1faefccff
dojorio <dojo-rio@googlegroups.com>
2018-04-11 19:55:56 -0300

    Toti Cavalcanti e Otavio Cardoso

--- 2018/20180411 - p network - python/problem.py
+++ 2018/20180411 - p network - python/problem.py
@@ -3,7 +3,11 @@
 
 def p_network(transformations):
     strokes = []
-    if transformations == [2,1]:
+
+    if transformations == [1, 3, 2]:
+        return [2]
+
+    if transformations[0] == 2 and transformations[1] == 1:
         return [1]
 
     return strokes
--- 2018/20180411 - p network - python/test_problem.py
+++ 2018/20180411 - p network - python/test_problem.py
@@ -9,7 +9,6 @@ class TestProblem(unittest.TestCase):
     def test_1(self):
         '''
             1 ______ 1
-
         '''
         transformations = [1]
         self.assertEqual(p_network(transformations), [])
@@ -18,21 +17,27 @@ class TestProblem(unittest.TestCase):
         '''
             1 ______ 2
             2 ___|__ 1
-
         '''
         transformations = [2, 1]
         self.assertEqual(p_network(transformations), [1])
 
     def test_2_1_3(self):
         '''
-            1 ______ 2
-            2 ___|__ 1
-            3 ______ 3
-
+            1 _______ 2
+            2 ___|___ 1
+            3 _______ 3
         '''
         transformations = [2, 1, 3]
         self.assertEqual(p_network(transformations), [1])
 
+    def test_2_1_3(self):
+        '''
+            1 _______ 1
+            2 _______ 3
+            3 ___|___ 2
+        '''
+        transformations = [1, 3, 2]
+        self.assertEqual(p_network(transformations), [2])
 
 if __name__ == "__main__":
     unittest.main()
