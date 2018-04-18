e94de0b098e8247495d40ab89c40f5db29160a77
dojorio <dojo-rio@googlegroups.com>
2018-04-18 18:57:37 -0300

    Lucas Rosalino e Julio Marins

--- 2018/20180418 - pascal triangle - python/problem.py
+++ 2018/20180418 - pascal triangle - python/problem.py
@@ -9,12 +9,17 @@ def pascal_triangle(n):
 			   [1, 2, 1]]
 	"""
 	triangle = []
+	last_row = None
 	for i in range(n):
-		print(i)
 		if i == 0:
 			triangle.append([1])
 		if i == 1:
-			triangle.append([1, 1])
+			last_row = [1, 1]
+			triangle.append(last_row)
 		if i == 2:
-			triangle.append([1, 2, 1])
+			last_row = [1, sum(last_row), 1]
+			triangle.append(last_row)
+		if i == 3:
+			new_number = last_row[0] + last_row[1]
+			triangle.append([1, new_number, new_number, 1])
 	return triangle
--- 2018/20180418 - pascal triangle - python/test_problem.py
+++ 2018/20180418 - pascal triangle - python/test_problem.py
@@ -10,10 +10,22 @@ class TestProblem(unittest.TestCase):
         self.assertEqual(pascal_triangle(1), [[1]])
 
     def test_2_row(self):
-        self.assertEqual(pascal_triangle(2), [[1], [1, 1]])
+        self.assertEqual(
+        	pascal_triangle(2), 
+        	[[1], [1, 1]]
+        )
 
     def test_3_row(self):
-        self.assertEqual(pascal_triangle(3), [[1], [1, 1], [1, 2, 1]])
+        self.assertEqual(
+        	pascal_triangle(3), 
+        	[[1], [1, 1], [1, 2, 1]]
+        )
+
+    def test_4_row(self):
+        self.assertEqual(
+        	pascal_triangle(4), 
+        	[[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
+        )
 
 
 if __name__ == "__main__":
