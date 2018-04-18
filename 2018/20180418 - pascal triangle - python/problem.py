76ce45fabf66d10c0ccd7184a00d6831e07bd5b3
dojorio <dojo-rio@googlegroups.com>
2018-04-18 19:19:35 -0300

    Lucas Rosalino e Julio Marins

--- 2018/20180418 - pascal triangle - python/problem.py
+++ 2018/20180418 - pascal triangle - python/problem.py
@@ -3,7 +3,7 @@
 
 def pascal_triangle(n):
 	"""
-		input = n
+		input = n # 3
 		output = [[1], 
 				[1, 1], 
 			   [1, 2, 1]]
@@ -17,15 +17,16 @@ def pascal_triangle(n):
 			last_row = [1, 1]
 			triangle.append(last_row)
 		if i == 2:
-			last_row = [1, sum(last_row), 1]
+			new_number = sum(last_row[0:2])
+			last_row = [1, new_number, 1]
 			triangle.append(last_row)
 		if i == 3:
-			new_number = last_row[0] + last_row[1]
+			new_number = sum(last_row[0:2])
 			last_row = [1, new_number, new_number, 1]
 			triangle.append(last_row)
 		if i == 4:
-			new_number = last_row[1] + last_row[2]
-			last_row = [1, new_number-2,new_number, new_number -2, 1]
+			new_number = sum(last_row[1:3])
+			last_row = [1, new_number- 2,new_number, new_number -2, 1]
 			triangle.append(last_row)
 
 	return triangle
