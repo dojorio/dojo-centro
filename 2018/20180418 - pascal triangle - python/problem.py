b0a2f964591c94e7000a47d2d035feb9e886a54a
dojorio <dojo-rio@googlegroups.com>
2018-04-18 18:51:57 -0300

    Julio Marins e Lucas Rosalino

--- 2018/20180418 - pascal triangle - python/problem.py
+++ 2018/20180418 - pascal triangle - python/problem.py
@@ -4,13 +4,17 @@
 def pascal_triangle(n):
 	"""
 		input = n
-		output = [[1], [1, 1], [1, 2, 1]]
+		output = [[1], 
+				[1, 1], 
+			   [1, 2, 1]]
 	"""
 	triangle = []
-	for n in range(n):
-		print(n)
-		if n == 1:
+	for i in range(n):
+		print(i)
+		if i == 0:
 			triangle.append([1])
-		if n == 2:
+		if i == 1:
 			triangle.append([1, 1])
+		if i == 2:
+			triangle.append([1, 2, 1])
 	return triangle
--- 2018/20180418 - pascal triangle - python/test_problem.py
+++ 2018/20180418 - pascal triangle - python/test_problem.py
@@ -12,6 +12,9 @@ class TestProblem(unittest.TestCase):
     def test_2_row(self):
         self.assertEqual(pascal_triangle(2), [[1], [1, 1]])
 
+    def test_3_row(self):
+        self.assertEqual(pascal_triangle(3), [[1], [1, 1], [1, 2, 1]])
+
 
 if __name__ == "__main__":
     unittest.main()
