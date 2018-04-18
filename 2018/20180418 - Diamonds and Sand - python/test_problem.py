66fac7d8812d60c443a3c9152bbd69d1ddacbec6
dojorio <dojo-rio@googlegroups.com>
2018-04-18 20:35:30 -0300

    Luan Marques e Felipe Lyra

--- 2018/20180418 - Diamonds and Sand - python/test_problem.py
+++ 2018/20180418 - Diamonds and Sand - python/test_problem.py
@@ -23,7 +23,7 @@ class TestProblem(unittest.TestCase):
         self.assertEqual(count_diamonds('><.<.>'), 1)
 
     def test_with_sand_and_diamond_and_piece_and_diamonds_and_diamonds(self):
-        self.assertEqual(count_diamonds('><.<.>>'), 2)
+        self.assertEqual(count_diamonds('<<.>>'), 2)
 
 if __name__ == "__main__":
     unittest.main()
