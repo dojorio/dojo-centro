16e4e5eb9297f21844718b5177df88a74aac6a95
dojorio <dojo-rio@googlegroups.com>
2018-04-18 20:11:59 -0300

    Felipe Lyra e Heyder Dias

--- 2018/20180418 - Diamonds and Sand - python/problem.py
+++ 2018/20180418 - Diamonds and Sand - python/problem.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+pattern = /<.*>/g
+
+def count_diamonds(mine):
+	print(pattern.exec(mine))
+	return 0
--- 2018/20180418 - Diamonds and Sand - python/test_problem.py
+++ 2018/20180418 - Diamonds and Sand - python/test_problem.py
@@ -8,7 +8,10 @@ from problem import *
 
 class TestProblem(unittest.TestCase):
     def test_without_diamond(self):
-        self.assertEqual(count_diamond(''), 0)
+        self.assertEqual(count_diamonds(''), 0)
+
+    def test_with_diamond(self):
+        self.assertEqual(count_diamonds('<>'), 1)
 
 
 if __name__ == "__main__":
