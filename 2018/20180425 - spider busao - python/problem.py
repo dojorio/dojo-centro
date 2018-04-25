078eb1cd7f34d1486074bcef5816584be92f0e28
dojorio <dojo-rio@googlegroups.com>
2018-04-25 20:15:09 -0300

    Otavio Cardoso e Julio Marins

--- 2018/20180425 - spider busao - python/problem.py
+++ 2018/20180425 - spider busao - python/problem.py
@@ -8,6 +8,21 @@ def spider_walk(riocard, bus_graph):
 	edge_price_1 = bus_graph[0][2]
 	edge_price_2 = bus_graph[-1][2]
 
+	# construct graph
+	graph = {}
+
+	for edge in bus_graph:
+		start = edge[0]
+		finish = edge[1]
+		cost = edge[2]
+		if graph.get(start, None) is None: 
+			graph[start] = []
+		graph[start].append((finish, cost))
+
+	print(graph)
+
+	accumulated_value = 0
+
 	if (
 		edge_price_1 > riocard or
 		edge_price_2 > riocard
