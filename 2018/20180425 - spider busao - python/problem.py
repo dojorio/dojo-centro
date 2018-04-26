9fd8997ba9ba17497f3f9c6dc3995abea4d0e528
dojorio <dojo-rio@googlegroups.com>
2018-04-25 21:17:45 -0300

    Julio Marins e Otavio Cardoso

--- 2018/20180425 - spider busao - python/problem.py
+++ 2018/20180425 - spider busao - python/problem.py
@@ -16,31 +16,18 @@ def spider_walk(minascard, bus_graph):
 			graph[finish] = []
 		graph[start].append((finish, cost))
 
-	stack = [
-		(
-			1, # current_node 
-			0, # accumulated_value
-			minascard # balance
-		),
-	]
+	stack = [1]
 	destination = max(graph.keys())
 
 	while len(stack) > 0:
-		current_node, accumulated_value, balance = stack.pop()
+		current_node = stack.pop()
 
 		for edge in graph[current_node]:
-			current_edge_price = edge[1] - accumulated_value
-
-			if current_edge_price <= balance:
-				if edge[0] == destination:
+			next_node, cost = edge
+			if cost <= minascard:
+				if next_node == destination:
 					return ARRIVED
 
-				stack.append(
-					(
-						edge[0],
-						accumulated_value + current_edge_price,
-						balance - current_edge_price,
-					)
-				)
+				stack.append(next_node)
 
 	return NOT_ARRIVED
