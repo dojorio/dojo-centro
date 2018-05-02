5704cdc44b0b44c4517fe913a5cc2ff948b27db8
dojorio <dojo-rio@googlegroups.com>
2018-05-02 20:21:44 -0300

    Lucas Nable e Felipe Lyra

--- 2018/20180502 - skyline - python/problem.py
+++ 2018/20180502 - skyline - python/problem.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 
 def skyline(buildings):
-	if len(buildings) >= 2:
-		if (buildings[1] == [9, 16, 10]):
-			return [
-            	[2, 10], [16, 0],
-        	]
+    if len(buildings) >= 2:
+        if (buildings[0][1] == buildings[1][0]):
+            return [
+                [buildings[0][0], buildings[0][2]], 
+                [buildings[1][1], 0]          ]
+            ]
 
-	result = []
-	for building in buildings:
-		result += [
-			[building[0], building[2]], 
-			[building[1], 0]
-		] 
-	return result
+    result = []
+    for building in buildings:
+        result += [
+            [building[0], building[2]], 
+            [building[1], 0]
+        ] 
+    return result
