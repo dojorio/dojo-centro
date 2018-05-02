594655caf449a43184fc2f57453fac5ec7ef4fca
dojorio <dojo-rio@googlegroups.com>
2018-05-02 20:59:53 -0300

    Rudá Almeida e Felipe Lyra

--- 2018/20180502 - skyline - python/problem.py
+++ 2018/20180502 - skyline - python/problem.py
@@ -6,7 +6,7 @@ def skyline(buildings):
 
     building_count = len(buildings)
     if building_count > 1:
-        if (buildings[0][1] == buildings[1][0]):
+        if (buildings[0][1] == buildings[1][0] and buildings[1][1] == buildings[2][0]):
             return [
                     [buildings[0][0], buildings[0][2]], 
                     [buildings[building_count - 1][1], 0]
