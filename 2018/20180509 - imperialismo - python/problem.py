ac7eeccd379aeb52cd6a49a88264e3a474c5847c
dojorio <dojo-rio@googlegroups.com>
2018-05-09 19:34:43 -0300

    Julio Marins e Felipe Lyra

--- 2018/20180509 - imperialismo - python/problem.py
+++ 2018/20180509 - imperialismo - python/problem.py
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 
 def get_domination_years(connections):
-    if len(set(connections)) <= 2:
+    if len(set(connections)) <= 2 and len(connections) == 3:
         return 1
 
-    if len(connections) == 3 and len(set(connections)) >= 3:
+    if (
+        len(connections) == 3 and len(set(connections)) >= 3
+        or len(connections) == 4 and len(set(connections)) == 1
+    ):
         return 2
 
     if len(connections) == 4:
         return 2
+
+    return 1
