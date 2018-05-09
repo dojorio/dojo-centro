29bb28678725666c1dd1e763f85a451f4ce434b1
dojorio <dojo-rio@googlegroups.com>
2018-05-09 19:16:25 -0300

    Vitor Rodrigo Rosan e Otavio Cardoso

--- 2018/20180509 - imperialismo - python/problem.py
+++ 2018/20180509 - imperialismo - python/problem.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 def get_domination_years(connections):
-	if len(connections) == 3:
-		return 2
-	return 1
+    if len(connections) == 3 and len(set(connections)) == 3:
+        return 2
+
+    if len(connections) == 4:
+        return 2
+
+    return 1
--- 2018/20180509 - imperialismo - python/test_problem.py
+++ 2018/20180509 - imperialismo - python/test_problem.py
@@ -23,6 +23,24 @@ class TestProblem(unittest.TestCase):
             2
         )
 
+    def test_four_different_empires(self):
+        # 2 - 1 - 3
+        #     |
+        #     4
+        connections = [1, 1, 1]
+        self.assertEqual(
+            get_domination_years(connections), 
+            1
+        )
+
+    def test_five_empires(self):
+        # 1 - 2 - 3 - 4 - 5
+        connections = [1, 2, 3, 4]
+        self.assertEqual(
+            get_domination_years(connections), 
+            2
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
