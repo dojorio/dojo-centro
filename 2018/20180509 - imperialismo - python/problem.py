1266935a8a290d833708215145124ce788abfc84
dojorio <dojo-rio@googlegroups.com>
2018-05-09 19:08:42 -0300

    Otavio Cardoso e Julio Marins

---
+++ 2018/20180509 - imperialismo - python/problem.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def get_domination_years(connections):
+	if len(connections) == 3:
+		return 2
+	return 1
---
+++ 2018/20180509 - imperialismo - python/test_problem.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1994
+
+import unittest
+from problem import *
+
+
+class TestProblem(unittest.TestCase):
+    def test_two_empires(self):
+        # 1 - 2
+        connections = [1]
+        self.assertEqual(
+            get_domination_years(connections), 
+            1
+        )
+
+    def test_four_empires(self):
+        # 1 - 2 - 3 - 4
+        connections = [1, 2, 3]
+        self.assertEqual(
+            get_domination_years(connections), 
+            2
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
+
