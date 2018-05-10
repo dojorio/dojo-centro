761472b1c7328b9976364f480f84deddbdb5c388
dojorio <dojo-rio@googlegroups.com>
2018-05-09 21:01:52 -0300

    Otavio Cardoso e Vitor Rodrigo Rosan

--- 2018/20180509 - imperialismo - python/problem.py
+++ 2018/20180509 - imperialismo - python/problem.py
@@ -3,26 +3,11 @@
 
 def get_domination_years(connections):
     number_of_edges = len(connections)
-    unique_edges = len(set(connections))
+    unique_edges = len(set(connections[1:]))
 
     max_years = (number_of_edges + 1) // 2 
 
-    if number_of_edges == unique_edges:
+    if number_of_edges == (unique_edges + 1):
         return max_years
 
-    if unique_edges == 1:
-        return unique_edges
-
-    if number_of_edges < 3:
-        return unique_edges
-
-    if number_of_edges == 3 and unique_edges == 2:
-        return unique_edges - 1
-
-    if number_of_edges < 6 and unique_edges < 5:        
-        return unique_edges
-
-    if number_of_edges <= 6 and unique_edges <= 3:        
-        return 2
-
-    return 3
+    return 1 + unique_edges // 2
