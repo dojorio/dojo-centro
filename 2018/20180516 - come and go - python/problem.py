4bdc4436ba260e78b67542ea49a9f56f80821d75
dojorio <dojo-rio@googlegroups.com>
2018-05-16 20:23:37 -0300

    Julio Marins e Toti Cavalcanti

--- 2018/20180516 - come and go - python/problem.py
+++ 2018/20180516 - come and go - python/problem.py
@@ -8,5 +8,7 @@ def is_connected(streets):
     if len(streets) == 2:
         return False
 
+    if len(streets) == len(set([n[1] for n in streets])):
+        return True
     return ((streets[0][2] == 2 and streets[1][2] == 2) or
             (len(streets) == 3 and streets[1][2] == 2))
