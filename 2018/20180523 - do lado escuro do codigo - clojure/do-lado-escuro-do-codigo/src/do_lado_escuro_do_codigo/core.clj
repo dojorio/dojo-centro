afa2670bb93ca806a2bcfa0560c0f50b0a15c497
dojorio <dojo-rio@googlegroups.com>
2018-05-23 20:24:26 -0300

    Julio Marins e Otavio Cardoso

--- 2018/20180523 - do lado escuro do codigo - clojure/do-lado-escuro-do-codigo/src/do_lado_escuro_do_codigo/core.clj
+++ 2018/20180523 - do lado escuro do codigo - clojure/do-lado-escuro-do-codigo/src/do_lado_escuro_do_codigo/core.clj
@@ -7,19 +7,14 @@
 
 (defn get-max-pow-of-2
 	[n]
-	(Math/pow 2 (Math/floor (log2 n))))
+	(Integer. (Math/pow 2 (Math/floor (log2 n)))))
 
 (defn get-time-distributed
 	"this is my function"
 	[time tasks_qty]
 	(if (= tasks_qty 1)
 		[time]
-		(if (< time 6)
-			[(- time 1) 1]
-			(if (= time 9)
-				[8 1]
-				(if (= time 12)
-					[8 4]
-					(if (= time 13)
-						[9 4]
-						[(- time 2) 2]))))))
+		(let [max-time (get-max-pow-of-2 (- time 1))
+			  min-time (get-max-pow-of-2 (- time (+ max-time 1)))
+			  remaining-time (- time (+ max-time min-time))]
+			  [(+ max-time remaining-time) min-time])))
