50bffaa64f59342d9a257ac34c05decfdbd7beef
dojorio <dojo-rio@googlegroups.com>
2018-06-06 20:36:12 -0300

    Otavio Cardoso e Julio Marins

--- 2018/20180606 - tic tac toe - clojure/tic-tac-toe/src/tic_tac_toe/core.clj
+++ 2018/20180606 - tic tac toe - clojure/tic-tac-toe/src/tic_tac_toe/core.clj
@@ -1,12 +1,22 @@
 (ns tic-tac-toe.core
   (:gen-class))
 
-(defn tic-tac-toe
+(defn transpose [m]
+  (apply mapv vector m))
+
+(defn verify-winner
 	[board]
 	(if (= (count board) 0)
 		nil
 		(let [uniq (distinct (first board))]
 			(if (or (> (count uniq) 1) (= (first uniq) "")) 
 				(tic-tac-toe (rest board))
-				(first uniq)
-			))))
+				(first uniq)))))
+
+(defn tic-tac-toe
+	[board]
+	(let [result (verify-winner board)
+		  tresult (verify-winner (transpose board))]
+		  (or result tresult)))
+
+;(tic-tac-toe (transpose board)) ;nil
