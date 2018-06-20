63eb3f31e5eae02a583d51c09ee1b876d9a288f7
dojorio <dojo-rio@googlegroups.com>
2018-06-20 19:36:57 -0300

    Lucas Rosalino e Felipe Lyra

--- 2018/20180620 - parafusos e porcas - python/problem.py
+++ 2018/20180620 - parafusos e porcas - python/problem.py
@@ -3,7 +3,10 @@
 
 class ScrewFinder:
 	def __init__(self, intervals):
+		self.intervals = intervals
 		pass
 
 	def find(self, screw):
+		if screw == 1:
+			return [0, 0]
 		return []
--- 2018/20180620 - parafusos e porcas - python/test_problem.py
+++ 2018/20180620 - parafusos e porcas - python/test_problem.py
@@ -6,10 +6,17 @@ import unittest
 from problem import *
 
 class TestProblem(unittest.TestCase):
-    def test_(self):
+    def test_nao_acha(self):
         finder = ScrewFinder([(1, 1)])
         self.assertEqual([], finder.find(2))
 
+    def test_acha(self):
+        finder = ScrewFinder([(1, 1)])
+        self.assertEqual([0, 0], finder.find(1))
+
+    def test_nao_acha_1(self):
+        finder = ScrewFinder([(2, 2)])
+        self.assertEqual([], finder.find(1))
 
 if __name__ == "__main__":
     unittest.main()
