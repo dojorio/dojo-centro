a6135e7448ecbcbf990b7941b5cd13e62d9fac98
dojorio <dojo-rio@googlegroups.com>
2018-06-20 19:59:50 -0300

    Lucas Rosalino e Felipe Lyra

--- 2018/20180620 - parafusos e porcas - python/problem.py
+++ 2018/20180620 - parafusos e porcas - python/problem.py
@@ -3,15 +3,19 @@
 
 class ScrewFinder:
 	def __init__(self, intervals):
-		self.intervals = intervals		
+		self.intervals = intervals
+		self.intervals[0][1] = intervals[0][1]+1
+		self.shelf = list(range(*intervals[0]))
 
 	def find(self, screw):
-		if screw in self.intervals[0]:
-			if screw == self.intervals[0][0]:
-				return [0, 0]
-			else:
-				return [1, 1]
+		# if screw in self.intervals[0]:
+		# 	if screw == self.intervals[0][0]:
+		# 		return [0, 0]
+		# 	else:
+		# 		return [1, 1]
 
+		if screw in self.shelf:
+			return [self.shelf.index(screw), self.shelf.index(screw)]
 		
 
 		return []
