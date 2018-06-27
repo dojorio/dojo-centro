2d3fde0357c06aa3eccae2b2fee78b3822a5421e
dojorio <dojo-rio@googlegroups.com>
2018-06-27 20:01:09 -0300

    Otavio Cardoso e Israel Teixeira

--- 2018/20180627 - Imposto Real - python/problem.py
+++ 2018/20180627 - Imposto Real - python/problem.py
@@ -2,9 +2,4 @@
 # -*- coding: utf-8 -*-
 
 def traveled_distance(capacity, debits, roads):
-	if debits[1] != 1:
-		return roads[0][2] * debits[1] * 2 
-	if debits[1]:
-		return roads[0][2] * 2
-	else:
-		return 0
+	return roads[0][2] * debits[1]/capacity * 2 
