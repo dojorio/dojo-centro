30bd6c7c353f80e3debac7def8839525f107c9a7
dojorio <dojo-rio@googlegroups.com>
2018-06-27 19:55:38 -0300

    Israel Teixeira e Otavio Cardoso

--- 2018/20180627 - Imposto Real - python/problem.py
+++ 2018/20180627 - Imposto Real - python/problem.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 def traveled_distance(capacity, debits, roads):
+	if debits[1] != 1:
+		return roads[0][2] * debits[1] * 2 
 	if debits[1]:
 		return roads[0][2] * 2
 	else:
--- 2018/20180627 - Imposto Real - python/test_problem.py
+++ 2018/20180627 - Imposto Real - python/test_problem.py
@@ -14,6 +14,18 @@ def test_2():
     assert traveled_distance(1, [0, 1], [(1, 2, 2)]) == 4
 
 def test_3():
-    "capacity 1, debits 0 1, road 2"
+    "capacity 1, debits 0 2, road 1"
+    assert traveled_distance(1, [0, 2], [(1, 2, 1)]) == 4
+
+def test_4():
+    "capacity 1, debits 0 3, road 1"
+    assert traveled_distance(1, [0, 3], [(1, 2, 1)]) == 6
+
+def test_5():
+    "capacity 2, debits 0 2, road 1"
+    assert traveled_distance(2, [0, 2], [(1, 2, 1)]) == 2
+
+def test_n():
+    "capacity 1, debits 0 0, road 2"
     roads = [(1, 2, 2)]
     assert traveled_distance(1, [0, 0], roads) == 0
