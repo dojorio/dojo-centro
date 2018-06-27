9b2b8632de13525df855c0faca1154054d11a320
dojorio <dojo-rio@googlegroups.com>
2018-06-27 20:06:43 -0300

    Israel Teixeira e Otavio Cardoso

--- 2018/20180627 - Imposto Real - python/problem.py
+++ 2018/20180627 - Imposto Real - python/problem.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from math import ceil
+
 def traveled_distance(capacity, debits, roads):
-	return roads[0][2] * debits[1]/capacity * 2 
+	return roads[0][2] * ceil(debits[1]/capacity) * 2 
--- 2018/20180627 - Imposto Real - python/test_problem.py
+++ 2018/20180627 - Imposto Real - python/test_problem.py
@@ -5,27 +5,37 @@
 
 from problem import *
 
-def test_1():
-    "capacity 1, debits 0 1, road 1"
-    assert traveled_distance(1, [0, 1], [(1, 2, 1)]) == 2
+class OneTrip:
+    def test_1():
+        "capacity 1, debits 0 1, road 1"
+        assert traveled_distance(1, [0, 1], [(1, 2, 1)]) == 2
 
-def test_2():
-    "capacity 1, debits 0 1, road 2"
-    assert traveled_distance(1, [0, 1], [(1, 2, 2)]) == 4
+    def test_2():
+        "capacity 1, debits 0 1, road 2"
+        assert traveled_distance(1, [0, 1], [(1, 2, 2)]) == 4
 
-def test_3():
-    "capacity 1, debits 0 2, road 1"
-    assert traveled_distance(1, [0, 2], [(1, 2, 1)]) == 4
+class TwoTrips:
+    def test_3():
+        "capacity 1, debits 0 2, road 1"
+        assert traveled_distance(1, [0, 2], [(1, 2, 1)]) == 4
 
-def test_4():
-    "capacity 1, debits 0 3, road 1"
-    assert traveled_distance(1, [0, 3], [(1, 2, 1)]) == 6
+    def test_4():
+        "capacity 1, debits 0 3, road 1"
+        assert traveled_distance(1, [0, 3], [(1, 2, 1)]) == 6
 
-def test_5():
-    "capacity 2, debits 0 2, road 1"
-    assert traveled_distance(2, [0, 2], [(1, 2, 1)]) == 2
+    def test_5():
+        "capacity 2, debits 0 2, road 1"
+        assert traveled_distance(2, [0, 2], [(1, 2, 1)]) == 2
 
-def test_n():
-    "capacity 1, debits 0 0, road 2"
-    roads = [(1, 2, 2)]
-    assert traveled_distance(1, [0, 0], roads) == 0
+    def test_6():
+        "capacity 2, debits 0 5, road 1"
+        assert traveled_distance(2, [0, 5], [(1, 2, 1)]) == 6
+
+    def test_n():
+        "capacity 1, debits 0 0, road 2"
+        roads = [(1, 2, 2)]
+        assert traveled_distance(1, [0, 0], roads) == 0
+
+def test_7():
+    "capacity 2, debits 0 5 , road 1"
+    assert traveled_distance(2, [0, 5], [(1, 2, 1)]) == 6
