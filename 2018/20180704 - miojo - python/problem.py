eea2642f2a949d5c0d8e396836c81b1cbe2513f4
dojorio <dojo-rio@googlegroups.com>
2018-07-04 19:47:53 -0300

    Otavio Cardoso e Julio Marins

--- 2018/20180704 - miojo - python/problem.py
+++ 2018/20180704 - miojo - python/problem.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 
 def miojo(amp1, amp2):
-	if 8 in (amp1, amp2):
-		return 8
-	if min(amp1, amp2) == 5:
-		return 10
 	if 3 in (amp1, amp2):
 		return 3
+		
+	min_amp, max_amp = sorted([amp1, amp2])
+	if max_amp - min_amp == 3:
+		return max_amp
+
+	if min(amp1, amp2) == 5:
+		return 10
+	return max(amp1, amp2)
