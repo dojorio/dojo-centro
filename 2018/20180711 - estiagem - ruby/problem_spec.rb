45597c5ca47a21fc6b8d68ee24ba3d0ada077c81
dojorio <dojo-rio@googlegroups.com>
2018-07-11 19:27:42 -0300

    Thiago Alves e Otavio Cardoso

---
+++ 2018/20180711 - estiagem - ruby/problem_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'problem'
+
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1023
+
+describe "problem" do
+  it "sample" do
+    bar = []
+    expect(problem(bar)).to eq(0)
+  end
+end
