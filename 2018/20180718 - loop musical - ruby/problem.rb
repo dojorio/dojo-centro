ef1c2198581501db52cef3dbb1c9b333ea46b7c7
dojorio <dojo-rio@googlegroups.com>
2018-07-18 20:01:00 -0300

    Toti Cavalcanti e Otavio Cardoso

--- 2018/20180718 - loop musical - ruby/problem.rb
+++ 2018/20180718 - loop musical - ruby/problem.rb
@@ -1,14 +1,11 @@
 def musical_loop(magnitudes)
-	if magnitudes.length > 3
-		if magnitudes[2] == -1
-			return 2
-		else
-			return 4
-		end
-	end
-	
-
+  if magnitudes.length > 3
+    if (magnitudes[0] <=> magnitudes[1]) == (magnitudes[1] <=> magnitudes[2])
+      return 2
+    else
+      return 4
+    end
+  end
 
   2
-
 end
--- 2018/20180718 - loop musical - ruby/problem_spec.rb
+++ 2018/20180718 - loop musical - ruby/problem_spec.rb
@@ -8,25 +8,28 @@ describe "Loop Musical" do
     expect(musical_loop(magnitudes)).to eq(2)
   end
 
-
   it "4 magnitudes, 4 peaks" do
     magnitudes = [1, 0, 1, 0]
     expect(musical_loop(magnitudes)).to eq(4)
   end
 
   it "3 magnitudes, 2 peaks" do
-	magnitudes = [1, 0, -1]
+  magnitudes = [1, 0, -1]
     expect(musical_loop(magnitudes)).to eq(2)
   end
 
   it "4 magnitudes, 2 peaks" do 
-  	magnitudes = [1, 0, -1, 0]
+    magnitudes = [1, 0, -1, 0]
     expect(musical_loop(magnitudes)).to eq(2)
   end
 
   it "4 magnitudes, 2 peaks again" do 
-  	magnitudes = [0, 1, 2, 1]
+    magnitudes = [0, 1, 2, 1]
     expect(musical_loop(magnitudes)).to eq(2)
   end
 
+  it "4 magnitudes, 2 peaks again yet" do 
+    magnitudes = [1, 0, 1, 2]
+    expect(musical_loop(magnitudes)).to eq(2)
+  end
 end
