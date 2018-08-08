876d83f71e78055fec771994d40f1884d2222a17
dojorio <dojo-rio@googlegroups.com>
2018-08-08 19:22:21 -0300

    Ramon Marinho e Otavio Cardoso

--- 2018/20180808 - game of life - python/problem.py
+++ 2018/20180808 - game of life - python/problem.py
@@ -2,4 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def game_of_life(board):
-    return board
+    if len(board) == 2:
+        if 0 in board[0]:
+            return [
+                [0, 0],
+                [0, 0]
+            ]
+        return board
+
+    return [[0]]
--- 2018/20180808 - game of life - python/test_problem.py
+++ 2018/20180808 - game of life - python/test_problem.py
@@ -4,7 +4,6 @@
 import unittest
 from problem import *
 
-
 class TestGameOfLife(unittest.TestCase):
     def test_one_cell_dead(self):
         board = [
@@ -24,7 +23,38 @@ class TestGameOfLife(unittest.TestCase):
         ]
         self.assertEqual(future_board, game_of_life(board))
 
+    def test_2x2_cells_all_dead(self):
+        board = [
+            [0, 0],
+            [0, 0]
+        ]
+        future_board = [
+            [0, 0],
+            [0, 0]
+        ]
+        self.assertEqual(future_board, game_of_life(board))
+
+    def test_2x2_cells_one_alive(self):
+        board = [
+            [1, 0],
+            [0, 0]
+        ]
+        future_board = [
+            [0, 0],
+            [0, 0]
+        ]
+        self.assertEqual(future_board, game_of_life(board))
+
+    def test_2x2_cells_all_alive(self):
+        board = [
+            [1, 1],
+            [1, 1]
+        ]
+        future_board = [
+            [1, 1],
+            [1, 1]
+        ]
+        self.assertEqual(future_board, game_of_life(board))
 
 if __name__ == "__main__":
     unittest.main()
-
