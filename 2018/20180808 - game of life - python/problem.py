1aa2f9cb99aa0b042448d28f06a9a4abae7cdcbf
dojorio <dojo-rio@googlegroups.com>
2018-08-08 20:19:22 -0300

    Julio Marins e Cleiton Marques

--- 2018/20180808 - game of life - python/problem.py
+++ 2018/20180808 - game of life - python/problem.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 
 def forward_cell(board, x, y):
-    if len(board) == 2:
-        if 1 in board[0]:
-            return 1
+    if 1 in board[0]:
+        return 1
     return 0
 
 def game_of_life(board):
