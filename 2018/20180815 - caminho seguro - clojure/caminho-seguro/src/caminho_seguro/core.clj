6cf6a8da3c21fe8ec02a5ffe5d122ada88885601
dojorio <dojo-rio@googlegroups.com>
2018-08-15 20:41:33 -0300

    Ramon Marinho e Toti Cavalcanti

--- 2018/20180815 - caminho seguro - clojure/caminho-seguro/src/caminho_seguro/core.clj
+++ 2018/20180815 - caminho seguro - clojure/caminho-seguro/src/caminho_seguro/core.clj
@@ -1,8 +1,15 @@
 (ns caminho-seguro.core
   (:gen-class))
 
+
+[[1 2 10] [2 3 10] [3 4 20] [4 3 20] [3 1 10]]
+
+
 (defn caminho-seguro
 	[caminhos]
-	(if (> (count caminhos) 2)
+	(if (distinct (map (rest caminhos)))
+		()
+		(if (> (count caminhos) 2)
 		(reduce + (map (fn [caminho] (last caminho)) caminhos))
-		"Pernoite"))
+		"Pernoite")))
+	
