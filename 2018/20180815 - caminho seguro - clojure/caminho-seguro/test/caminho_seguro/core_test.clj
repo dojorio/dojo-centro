eee170ded5fdfe57e03defd2f0a2ffe75b944536
dojorio <dojo-rio@googlegroups.com>
2018-08-15 19:58:38 -0300

    Ramon Marinho e Toti Cavalcanti

--- 2018/20180815 - caminho seguro - clojure/caminho-seguro/test/caminho_seguro/core_test.clj
+++ 2018/20180815 - caminho seguro - clojure/caminho-seguro/test/caminho_seguro/core_test.clj
@@ -4,4 +4,6 @@
 
 (deftest a-test
   (testing "1 2 999"
-    (is (= (caminho-seguro [[1 2 999]]) "Pernoite"))))
+    (is (= (caminho-seguro [[1 2 999]]) "Pernoite")))
+  (testing "1 2 999"
+    (is (= (caminho-seguro [[1 2 10] [2 3 10] [3 1 10]]) 30))))
