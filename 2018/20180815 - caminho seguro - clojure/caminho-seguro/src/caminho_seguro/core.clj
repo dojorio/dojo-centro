7e194a3ce70af641b7ebba52bb07d5b8b2006d36
dojorio <dojo-rio@googlegroups.com>
2018-08-15 21:02:36 -0300

    Raphael Sathler e Felipe Lyra

--- 2018/20180815 - caminho seguro - clojure/caminho-seguro/src/caminho_seguro/core.clj
+++ 2018/20180815 - caminho seguro - clojure/caminho-seguro/src/caminho_seguro/core.clj
@@ -3,24 +3,24 @@
 
 
 
-(defn counter
-	[all-elements]
-	(let [counter {}]
-		(map (fn [[k v]] {k (inc v) }) all-elements)))
+; (defn counter
+; 	[all-elements]
+; 	(let [counter {}]
+; 		(reduce {} (fn [v] {k (inc v) }) all-elements)))
 
 
 (defn caminho-seguro
 	[caminhos]
 	(let [
 		 all-elements (concat (map first caminhos) (map second caminhos ) )
-		 non-dupes (distinct all-elements)
+		 dupe-counts (frequencies all-elements)
 	    ]
-	(if (distinct (map (rest caminhos)))
+	(if (filter (fn [k v] (= v 1)) dupe-counts))
 		(if (> (count caminhos) 2)
 		(reduce + (map (fn [caminho] (last caminho)) caminhos))
 		"Pernoite"))))
 	
 		
-		
+
 
 	
