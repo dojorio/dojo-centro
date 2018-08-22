18423cd6f8585879e3b0490e44ad21c2fb9532bd
dojorio <dojo-rio@googlegroups.com>
2018-08-22 19:25:44 -0300

    Ramon Marinho e Toti Cavalcanti

--- 2018/20180822 - estradas escuras - python/test_problem.py
+++ 2018/20180822 - estradas escuras - python/test_problem.py
@@ -52,6 +52,22 @@ class TestProblem(unittest.TestCase):
         ]
         self.assertEqual(estradas_escuras(graph_hash), 2)
 
+
+    def test_two_nodes_com_economy_3(self):
+        """
+        0 - 1
+        | \ |
+        2 - 3 
+        """
+        graph_hash = [
+            (0, 1, 2),
+            (0, 2, 3),
+            (1, 3, 4),
+            (2, 3, 2),
+            (0, 3, 2)
+        ]
+        self.assertEqual(estradas_escuras(graph_hash), 7)
+
 if __name__ == "__main__":
     unittest.main()
 
