f368c92e40e90991e68fae240a1191aba26a3cd5
dojorio <dojo-rio@googlegroups.com>
2018-08-22 20:55:55 -0300

    Julio Marins e Toti Cavalcanti

--- 2018/20180822 - estradas escuras - python/problem.py
+++ 2018/20180822 - estradas escuras - python/problem.py
@@ -17,30 +17,22 @@ def estradas_escuras(graph_hash):
 	tried_elements = []
 	total_economy = 0
 
-	while tries <= len(graph_hash_2):
+	while tries != 0:
 		graph_hash_2 = graph_hash
 		for n in graph_hash_2:
 			if n not in tried_elements:
 				tried_elements.append(n)
 			max_element = n[2]
 
-		for i in range(len(graph_hash_2)-1):
+		for i in range(len(graph_hash_2) - 1):
 			if graph_hash_2[i][2] == max_element:
 				graph_hash_2.pop(max_element)
 
-			if is_connected(graph_hash, graph_hash_2):
-				total_economy += max_element
-				tries -= 1
-			else:
-				graph_hash_2 = graph_hash
-				tried_elements.append(max_element)
-				
+		if is_connected(graph_hash, graph_hash_2):
+			total_economy += max_element
+		else:
+			graph_hash_2 = graph_hash
+		tries -= 1
 
-	
-	max_n = max(n[2] for n in graph_hash)
-	if len(graph_hash) == 5:
-		return sum(sorted([n[2] for n in graph_hash], reverse=True)[0:2])
-
-	return max_n
-
+	return total_economy
 	
