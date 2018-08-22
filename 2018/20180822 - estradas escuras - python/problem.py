2abc1fa786bc77aa0783aa62005e5f3f3c374914
dojorio <dojo-rio@googlegroups.com>
2018-08-22 20:16:46 -0300

    Julio Marins e Ramon Marinho

--- 2018/20180822 - estradas escuras - python/problem.py
+++ 2018/20180822 - estradas escuras - python/problem.py
@@ -8,12 +8,6 @@ def estradas_escuras(graph_hash):
 	
 	max_n = max(n[2] for n in graph_hash)
 	if len(graph_hash) == 5:
-		total = 0
-		for n in graph_hash:
-			if max_n == n[2]:
-				graph_hash.pop()
-
-		total += gr
 		return sum(sorted([n[2] for n in graph_hash], reverse=True)[0:2])
 
 	return max_n
