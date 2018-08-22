b9469251107a6ee16eb6bf5b3b3197054a070e51
dojorio <dojo-rio@googlegroups.com>
2018-08-22 19:14:51 -0300

    Felipe Lyra e Julio Marins

---
+++ 2018/20180822 - estradas escuras - python/problem.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def estradas_escuras(graph_hash):
+	if len(graph_hash) == 3:
+		return 2
+	return 0
---
+++ 2018/20180822 - estradas escuras - python/test_problem.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from problem import *
+
+
+class TestProblem(unittest.TestCase):
+    def test_two_nodes_no_economy(self):
+        graph_hash = [
+            (0, 1, 3)
+        ]
+        self.assertEqual(estradas_escuras(graph_hash), 0)
+
+    def test_two_nodes_com_economy(self):
+        """
+        0 - 1
+        |  /
+        2
+        """
+        graph_hash = [
+            (0, 1, 2),
+            (0, 2, 2),
+            (1, 2, 2),
+        ]
+        self.assertEqual(estradas_escuras(graph_hash), 2)
+
+    def test_two_nodes_com_economy_2(self):
+        """
+        0 - 1
+        |  /
+        2
+        """
+        graph_hash = [
+            (0, 1, 2),
+            (0, 2, 2),
+            (1, 2, 3),
+        ]
+        self.assertEqual(estradas_escuras(graph_hash), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
