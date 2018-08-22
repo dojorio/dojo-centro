8b588a086ad71233190e55e86a548220a5d79b45
dojorio <dojo-rio@googlegroups.com>
2018-08-22 20:24:09 -0300

    Felipe Lyra e Julio Marins

--- 2018/20180822 - estradas escuras - python/problem.py
+++ 2018/20180822 - estradas escuras - python/problem.py
@@ -2,9 +2,21 @@
 # -*- coding: utf-8 -*-
 from collections import Counter
 
+def is_connected(graph_hash, graph_hash_2):
+	elements_gh = [n[0] for n in graph_hash] + [n[1] for n in graph_hash]
+	elements_gh2 = [n[0] for n in graph_hash_2] + [n[1] for n in graph_hash_2]
+	return set(elements) == set(elements_gh2)
+
 def estradas_escuras(graph_hash):
 	if len(graph_hash) < 3:
 		return 0
+
+	while True:
+		max_element = max(n[2] for n in graph_hash)
+		for n in graph_hash:
+			if n[2] == max_element:
+				max_element_t = n
+		graph_hash_2 = graph_hash.pop(max_element_t)
 	
 	max_n = max(n[2] for n in graph_hash)
 	if len(graph_hash) == 5:
