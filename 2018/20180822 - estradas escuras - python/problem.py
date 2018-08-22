65468ae57c04fdf0517adf1eebda157166cc09aa
dojorio <dojo-rio@googlegroups.com>
2018-08-22 20:50:43 -0300

    Toti Cavalcanti e Felipe Lyra

--- 2018/20180822 - estradas escuras - python/problem.py
+++ 2018/20180822 - estradas escuras - python/problem.py
@@ -5,7 +5,7 @@ from collections import Counter
 def is_connected(graph_hash, graph_hash_2):
 	elements_gh = [n[0] for n in graph_hash] + [n[1] for n in graph_hash]
 	elements_gh2 = [n[0] for n in graph_hash_2] + [n[1] for n in graph_hash_2]
-	return set(elements) == set(elements_gh2)
+	return set(elements_gh) == set(elements_gh2)
 
 def estradas_escuras(graph_hash):
 	if len(graph_hash) < 3:
@@ -17,15 +17,16 @@ def estradas_escuras(graph_hash):
 	tried_elements = []
 	total_economy = 0
 
-	while tries != 0:
+	while tries <= len(graph_hash_2):
 		graph_hash_2 = graph_hash
 		for n in graph_hash_2:
 			if n not in tried_elements:
 				tried_elements.append(n)
 			max_element = n[2]
-		for i in range(len(graph_hash_2)):
+
+		for i in range(len(graph_hash_2)-1):
 			if graph_hash_2[i][2] == max_element:
-				graph_hash_2.pop(n)
+				graph_hash_2.pop(max_element)
 
 			if is_connected(graph_hash, graph_hash_2):
 				total_economy += max_element
@@ -33,7 +34,7 @@ def estradas_escuras(graph_hash):
 			else:
 				graph_hash_2 = graph_hash
 				tried_elements.append(max_element)
-
+				
 
 	
 	max_n = max(n[2] for n in graph_hash)
