804a891d8c346105a1e3eda14a3e2c6a92503bba
dojorio <dojo-rio@googlegroups.com>
2018-09-12 20:43:11 -0300

    Rodrigo Melo e Felipe Lyra

--- 2018/20180912 - crise de energia - python/problem.py
+++ 2018/20180912 - crise de energia - python/problem.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 def menor_salto(regioes, ultima_estacao, step):
-    if step == 2:
+    if step == 2 or step == 1 and regioes == ultima_estacao:
         return True
     return False
--- 2018/20180912 - crise de energia - python/test_problem.py
+++ 2018/20180912 - crise de energia - python/test_problem.py
@@ -6,15 +6,24 @@ from problem import *
 
 
 class TestProblem(unittest.TestCase):
-    def test_n_3_m_1(self):
+    def test_n_3_u_2_m_1(self):
         # 1 = 1 - X
         # 2 = 1 - 3 - 2
         self.assertFalse(menor_salto(regioes=3, ultima_estacao=2, step=1))
 
-    def test_n_3_m_2(self):
+    def test_n_3_u_2_m_2(self):
         # 1 = 1 - X
         # 2 = 1 - 3 - 2
         self.assertTrue(menor_salto(regioes=3, ultima_estacao=2, step=2))
+
+    def test_n_3_u_3_m_2(self):
+        # 1 = 1 - 2 - 3
+        self.assertTrue(menor_salto(regioes=3, ultima_estacao=3, step=1))
+
+    def test_n_4_u_3_m_1(self):
+        # 1 = 1 - 2 - X
+        self.assertFalse(menor_salto(regioes=4, ultima_estacao=3, step=1))
+
 
 
 if __name__ == "__main__":
