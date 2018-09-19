58ac4a28bbc0e14e5bd226b2f33e3a65386179b1
dojorio <dojo-rio@googlegroups.com>
2018-09-19 20:46:00 -0300

    Joao Ortigao e Felipe Lyra

--- 2018/20180919 - jogando cartas fora - ruby/problem.rb
+++ 2018/20180919 - jogando cartas fora - ruby/problem.rb
@@ -7,16 +7,16 @@ def cartasfora(cartas)
 	  count += 1
 	end
 
-	if(cartas == 3)
-		return [[1, 3], 2]
-  elsif( cartas == 5)
-  	return [[1, 3, 5, 4], 2]
-  elsif(cartas == 7)
-		return [[1, 3, 5, 7, 4, 2], 6]
+	descartes = []
 
-	elsif(cartas == 4)
-		return [[1, 3, 2], 4]
-	elsif(cartas == 6)
-		return [[1, 3, 5, 2, 6], 4]
+	while pilha.size > 1
+		topo = pilha.shift
+		descartes << topo
+		topo = pilha.shift
+		pilha << topo
 	end
+
+	return [descartes, pilha[0]]
+
+
 end
--- 2018/20180919 - jogando cartas fora - ruby/problem_spec.rb
+++ 2018/20180919 - jogando cartas fora - ruby/problem_spec.rb
@@ -22,4 +22,8 @@ describe "problem" do
    it "sete cartas" do
     expect(cartasfora(7)).to eq([[1, 3, 5, 7, 4, 2], 6])
   end
+
+  it "dezenove cartas" do
+  	expect(cartasfora(19)).to eq([[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 4, 8, 12, 16, 2, 10, 18, 14], 6])
+  end		
 end
