6f051b8c41af75fc32b6d6e166d871f3eb343bf5
dojorio <dojo-rio@googlegroups.com>
2018-09-19 20:18:12 -0300

    Ramon Marinho e Joao Ortigao

--- 2018/20180919 - jogando cartas fora - ruby/problem.rb
+++ 2018/20180919 - jogando cartas fora - ruby/problem.rb
@@ -1,7 +1,10 @@
 def cartasfora(cartas)
 	if(cartas == 3)
 		return [[1, 3], 2]
+	end
+	if(cartas == 4)
+		return [[1, 3, 2], 4]
 	else
-  	return [[1, 3, 2], 4]
+  	return [[1, 3, 5, 4], 2]
 	end
 end
