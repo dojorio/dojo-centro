6b929d466bd689fa321c9b52123d20c7aa3bbeb7
dojorio <dojo-rio@googlegroups.com>
2018-10-03 19:55:08 -0300

    marcell Monteiro e marcell Monteiro

--- 2018/20181003 - livro caixa - javascript/problem.js
+++ 2018/20181003 - livro caixa - javascript/problem.js
@@ -1,11 +1,14 @@
 exports.box_book = function (total, list) {
+	function add ( a, b){
+		return a + b
+	}
 	if (total < 0) {
-		return '-'
+		return '-' 
 	}
  	if (list[0] < total){
  		return '*'
  	}
- 	 	if ( list.reduce(add, 0) 
+ 	if ( list.reduce(add, 0) === total) { 
  		return '++'	
  	}
 
