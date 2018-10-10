69b85884d94b11b2de6b1a68bdc6c104f154035d
dojorio <dojo-rio@googlegroups.com>
2018-10-10 20:52:28 -0300

    marcell Monteiro e Otavio Cardoso

--- 2018/20181010 - Fuga do Cavalo - javascript/problem.js
+++ 2018/20181010 - Fuga do Cavalo - javascript/problem.js
@@ -1,17 +1,27 @@
 exports.knightRun = function (knight, pawns) {
-    if (pawns[0] == "6a" && knight == '3c') {
-    	return 7
-    }
+	if (knight[0] == '3') {
+		for (pawn of pawns) {
+			if (pawn[0] == '6' && pawn[1].charCodeAt(0) == knight[1].charCodeAt(0) - 2) {
+				return 7
+			}
+		}
+	}
 
-    if (pawns[1] == "6b" && knight == '3d') {
-    	return 7
-    }
-    if (pawns[2] == "6c" && knight == '3e') {
-    	return 7
-    }
-    if (pawns[3] == "6d" && knight == '3f') {
-    	return 7
-    }
+    // if (pawns[0] == "6a" && knight == '3c') {
+    // 	return 7
+    // }
+
+    // if (pawns[1] == "6b" && knight == '3d') {
+    // 	return 7
+    // }
+
+    // if (pawns[2] == "6c" && knight == '3e') {
+    // 	return 7
+    // }
+
+    // if (pawns[3] == "6d" && knight == '3f') {
+    // 	return 7
+    // }
 
     return 8
 };
