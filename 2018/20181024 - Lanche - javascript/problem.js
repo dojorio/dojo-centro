38699972ddf1421f042c3baa8b076cb555cfc2da
dojorio <dojo-rio@googlegroups.com>
2018-10-24 19:42:22 -0200

    Luiza Marques e Felipe Lyra

--- 2018/20181024 - Lanche - javascript/problem.js
+++ 2018/20181024 - Lanche - javascript/problem.js
@@ -1,3 +1,7 @@
-exports.problem = function (bar) {
-    return 5
+exports.problem = function (item, quantidade) {
+	if(item == 3) {
+    	return quantidade * 5
+    } else {
+    	return 2
+    }
 };
--- 2018/20181024 - Lanche - javascript/problem_spec.js
+++ 2018/20181024 - Lanche - javascript/problem_spec.js
@@ -3,10 +3,14 @@ var assert = require ('assert'),
 
 describe('Lanche', function() {
     it('quando a entrada é 3 1 o resultado sera 5', function () {
-        assert.equal(problem([3, 1]), 5)
+        assert.equal(problem(3, 1), 5)
     })
 
     it('quando a entrada é 4 1 o resultado sera 2', function () {
-        assert.equal(problem([4, 1]), 2)
+        assert.equal(problem(4, 1), 2)
+    })
+
+    it('quando a entrada é 3 2 o resultado sera 10', function () {
+        assert.equal(problem(3, 2), 10)
     })
 })
