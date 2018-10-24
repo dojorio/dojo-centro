c77c742eb7ef6b6013b2f2c5ce5c66f548ad9c8d
dojorio <dojo-rio@googlegroups.com>
2018-10-24 19:48:26 -0200

    marcell Monteiro e Luiza Marques

--- 2018/20181024 - Lanche - javascript/problem.js
+++ 2018/20181024 - Lanche - javascript/problem.js
@@ -1,7 +1,9 @@
 exports.problem = function (item, quantidade) {
 	if(item == 3) {
     	return quantidade * 5
-    } else {
-    	return 2
+    } else if(item == 4) {
+
+    	return quantidade * 2
     }
+
 };
--- 2018/20181024 - Lanche - javascript/problem_spec.js
+++ 2018/20181024 - Lanche - javascript/problem_spec.js
@@ -13,4 +13,12 @@ describe('Lanche', function() {
     it('quando a entrada é 3 2 o resultado sera 10', function () {
         assert.equal(problem(3, 2), 10)
     })
+
+    it('quando a entrada é 4 2 o resultado sera 2', function () {
+        assert.equal(problem(4, 2), 4)
+    })
+
+    it('quando a entrada é 5 1 o resultado sera 1.5', function () {
+        assert.equal(problem(5, 1), 1.5)
+    })
 })
