8dc85db80dfe0ede120548ba42189482c2082b31
dojorio <dojo-rio@googlegroups.com>
2018-10-24 20:55:41 -0200

    Felipe Lyra e marcell Monteiro

--- 2018/20181024 - Área do Círculo - python/problem.py
+++ 2018/20181024 - Área do Círculo - python/problem.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def area(raio):
-	if(raio == 2.00):
-		return 'A=12.5664'
-	elif (raio == 150.00):
-		return 'A=70685.7750'
+PI = 3.14159 
 
-	return 'A=31819.3103'
+def area(raio):
+	return 'A=' + str((raio**2) * PI)
 	
 
