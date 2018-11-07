8f82f5bbd343a96f845ba6bf3013906ef5cb1a6a
dojorio <dojo-rio@googlegroups.com>
2018-11-07 20:53:23 -0200

    Marcell Monteiro e Daniel Drumond

--- 2018/20181107 - fizzbuzz - javascript/problem.js
+++ 2018/20181107 - fizzbuzz - javascript/problem.js
@@ -1,4 +1,8 @@
 exports.fizzbuzz = function (bar) {
+	if (bar % 15 == 0) return 'FizzBuzz';
+	//var saida = ''
 	if (bar % 3 == 0) return 'Fizz';
+	if (bar % 5 == 0) return 'Buzz';
+	//if (saida == '') saida = bar
     return bar
 };
--- 2018/20181107 - fizzbuzz - javascript/problem_spec.js
+++ 2018/20181107 - fizzbuzz - javascript/problem_spec.js
@@ -2,19 +2,43 @@ var assert = require ('assert'),
     fizzbuzz = require('./problem').fizzbuzz;
 
 describe('fizzbuzz', function() {
-    it('quando a entrada for 1 a saida sera 1', function () {
+    it('quando a entrada for 1 a saida será 1', function () {
         assert.equal(fizzbuzz(1), 1)
     })
 
-    it('quando a entrada for 2 a saida sera 2', function () {
+    it('quando a entrada for 2 a saida será 2', function () {
         assert.equal(fizzbuzz(2), 2)
     })
 
-    it('quando a entrada for 3 a saida sera Fizz', function () {
+    it('quando a entrada for 3 a saida será Fizz', function () {
         assert.equal(fizzbuzz(3), 'Fizz')
     })
 
-    it('quando a entrada for 6 a saida sera Fizz', function () {
+    it('quando a entrada for 6 a saida será Fizz', function () {
         assert.equal(fizzbuzz(6), 'Fizz')
+    })
+
+    it('quando a entrada for 9 a saida será Fizz', function () {
+        assert.equal(fizzbuzz(9), 'Fizz')
+    })
+
+    it('quando a entrada for 5 a saida será Buzz', function () {
+        assert.equal(fizzbuzz(5), 'Buzz')
+    })
+
+    it('quando a entrada for 10 a saida será Buzz', function () {
+        assert.equal(fizzbuzz(10), 'Buzz')
+    })
+
+    it('quando a entrada for 15 a saida será FizzBuzz', function () {
+        assert.equal(fizzbuzz(15), 'FizzBuzz')
+    })
+
+    it('quando a entrada for 30 a saida será FizzBuzz', function () {
+        assert.equal(fizzbuzz(30), 'FizzBuzz')
+    })
+
+    it('quando a entrada for 45 a saida será FizzBuzz', function () {
+        assert.equal(fizzbuzz(45), 'FizzBuzz')
     })
 })
