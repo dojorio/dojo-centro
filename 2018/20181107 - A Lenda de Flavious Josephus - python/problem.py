230ba0017bee0dae7d545d4ab7513db7e3bc8716
dojorio <dojo-rio@googlegroups.com>
2018-11-07 20:18:07 -0200

    Daniel Drumond e Marcell Monteiro

--- 2018/20181107 - A Lenda de Flavious Josephus - python/problem.py
+++ 2018/20181107 - A Lenda de Flavious Josephus - python/problem.py
@@ -3,6 +3,14 @@
 
 def josephus(homens, salto):
     os_homi = list(range(1, homens+1))
+    pos = 0
+    while len(os_homi) > 1:
+        pos += salto - 1
+        if pos >= len(os_homi):
+            pos -= len(os_homi)
+        os_homi.pop(pos)
+
+    return os_homi[0]
     if salto == 1:
         return homens
     if homens == 6:
