a89edfe6bf979002ab836d75c6802b183514f328
dojorio <dojo-rio@googlegroups.com>
2018-12-05 20:26:14 -0200

    Kleiton Albuquerque e Gustavo Rodrigues

--- 2018/20181205 - cards - javascript/problem.js
+++ 2018/20181205 - cards - javascript/problem.js
@@ -1,16 +1,20 @@
 exports.maxPoints = function (cards) {
 	let resultado = 0
+	let jogador1 = 0
+
 	for (var idx = 0; idx < cards.length / 2; idx ++) {
-	    if (cards[0] > cards[cards.length - 1]) {
+		const ultima = cards.length - 1
+	    if (cards[0] > cards[ultima]) {
 	    	resultado = resultado + cards[0]
 	    	cards.splice(0, 1)
 	    } else {
-	    	const ultima = cards.length - 1
 	    	resultado = resultado + cards[ultima]
 	    	cards.splice(ultima, 1)	
 	    }
+
+	    //if(idx)
 	}
 
     return resultado;
 };
-//1 1 1
+//2 1 1 2
