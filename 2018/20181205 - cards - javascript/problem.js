6c98911f82c76c15c2454aa12a70ca2ed7b75b22
dojorio <dojo-rio@googlegroups.com>
2018-12-05 20:55:11 -0200

    Leandro Thimóteo e Marcio Henrique

--- 2018/20181205 - cards - javascript/problem.js
+++ 2018/20181205 - cards - javascript/problem.js
@@ -1,18 +1,20 @@
 exports.maxPoints = function (cards) {
 	let resultado = 0
-	let jogador1 = 0
 	let ultima = null
-	for (var idx = 0; idx < cards.length / 2; idx ++) {
+	while (cards.length > 0) 
+	    jogador1 = cards.length % 2 === 0
 		ultima = cards[cards.length - 1]
 	    if (cards[0] > ultima) {
-	    	resultado = resultado + cards[0]
+	    	carta = cards[0]
 	    	cards.splice(0, 1)
 	    } else {
-	    	resultado = resultado + ultima
+	    	carta = ultima
 	    	cards.pop()	
 	    }
 
-	    //if(idx)
+	    if (jogador1) {
+	    	resultado += carta
+	    }
 	}
 
     return resultado;
