3a684cdc06840d42ba97863d167495884a468de7
dojorio <dojo-rio@googlegroups.com>
2019-01-30 19:17:24 -0200

    Vito e Otavio Cardoso

--- 2019/20190130 - buracos nas letras - python/problem.py
+++ 2019/20190130 - buracos nas letras - python/problem.py
@@ -4,4 +4,6 @@
 def quantidade_buracos(texto):
 	if texto == 'B':
 		return 2
+	if texto == 'C' or texto == 'E' or texto == 'F' or texto == 'G':
+		return 0
 	return 1
--- 2019/20190130 - buracos nas letras - python/test_problem.py
+++ 2019/20190130 - buracos nas letras - python/test_problem.py
@@ -1,9 +1,8 @@
-	#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import unittest
 from problem import *
-
 
 class TestBuracos(unittest.TestCase):
     def test_A_tem_1_buraco(self):
@@ -12,6 +11,17 @@ class TestBuracos(unittest.TestCase):
     def test_B_tem_2_buracos(self):
     	self.assertEqual(quantidade_buracos('B'), 2)
 
+    def test_C_tem_0_buraco(self):
+    	self.assertEqual(quantidade_buracos('C'), 0)
+
+    def test_E_tem_0_buraco(self):
+    	self.assertEqual(quantidade_buracos('E'), 0)
+
+    def test_F_tem_0_buraco(self):
+    	self.assertEqual(quantidade_buracos('F'), 0)
+
+    def test_G_tem_0_buraco(self):
+    	self.assertEqual(quantidade_buracos('G'), 0)
 
 if __name__ == "__main__":
     unittest.main()
