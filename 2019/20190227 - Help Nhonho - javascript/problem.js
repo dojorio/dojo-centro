069cf9abc1e234f675733b91e1fd3eb3502dded8
dojorio <dojo-rio@googlegroups.com>
2019-02-27 20:35:50 -0300

    Felipe Lyra e Otavio Cardoso

--- 2019/20190227 - Help Nhonho - javascript/problem.js
+++ 2019/20190227 - Help Nhonho - javascript/problem.js
@@ -1,6 +1,14 @@
 exports.helpNhonho = function (k, digits) {
     result = []
 
+    divisor = 0
+
+    for (var i = 0; i < digits; i++) {
+        divisor = (digits - 1) + divisor * 10
+    }
+
+    return divisor
+
     for(var i = 0; i < 10; i++) {
         for(var j = i + 1; j < 10; j++) {
             if ((10*i + j) + (10*j + i) == k) {
@@ -8,6 +16,14 @@ exports.helpNhonho = function (k, digits) {
             }
         }
     }
+
+    for(var i = 0; i < 10; i++) {
+        for(var j = i + 1; j < 10; j++) {
+            for(var l = j + 1; j < 10; j++) {
+            }
+        }
+    }
+
 
     return result
 }
