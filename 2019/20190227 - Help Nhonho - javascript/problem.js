3e48eb5a299e3abf6b1aab311706fabe48b20a6e
dojorio <dojo-rio@googlegroups.com>
2019-02-27 19:56:46 -0300

    Otavio Cardoso e Felipe Lyra

--- 2019/20190227 - Help Nhonho - javascript/problem.js
+++ 2019/20190227 - Help Nhonho - javascript/problem.js
@@ -1,4 +1,13 @@
 exports.helpNhonho = function (k, digits) {
+    var algs = []
+
+    while (k > 0) {
+        if (algs.indexOf(k % 10) == -1) {
+            algs.push(k % 10)
+        }
+        k = Math.floor(k / 10)
+    }
+
     if (k == 11) {
         return [[0, 1]]
     }
