f6e82ec153517c0eda7dee18a7c5b72869d1caa2
dojorio <dojo-rio@googlegroups.com>
2019-02-27 21:02:20 -0300

    Felipe Lyra e Otavio Cardoso

--- 2019/20190227 - Help Nhonho - javascript/problem.js
+++ 2019/20190227 - Help Nhonho - javascript/problem.js
@@ -15,20 +15,25 @@ exports.helpNhonho = function (k, digits) {
 
     return testSoma(0, soma, digits);
 
-
-
     function testSoma(lastNumber, soma, digits) {
         var results = [];
 
-        if(digits == 1) {
-            return [soma - lastNumber]
+        if (digits == 1) {
+            if (soma > lastNumber && soma - lastNumber < 10){
+                return [soma - lastNumber]
+            } else {
+                return []
+            }
         }
 
         for(var x = lastNumber + 1; x < 10; x++) {
             list = testSoma(x, soma - x, digits - 1);
-            list.unshift(x);
 
-            results.push(list);
+            if (list.length == digits - 1) {
+                list.unshift(x);
+
+                results.push(list);
+            }
         }
 
         return results;
