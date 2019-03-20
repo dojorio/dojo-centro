efe3929344b2755c0f55fa3a20e5451ed36e2555
dojorio <dojo-rio@googlegroups.com>
2019-03-20 19:16:53 -0300

    Otavio Cardoso e thiago cruz

--- 2019/20190320 - Troca ou Não Troca - ruby/problem_spec.rb
+++ 2019/20190320 - Troca ou Não Troca - ruby/problem_spec.rb
@@ -11,8 +11,8 @@ describe "Troca ou Não Troca" do
     expect(troca_ou_nao(rodadas)).to eq(1)
   end
 
-  it "3 prêmios, 3 sim, 2 não" do
-    rodadas = [[3, 1], [2, 0]]
-    expect(troca_ou_nao(rodadas)).to eq(0)
+  it "3 prêmios, 2 sim, 3 não" do
+    rodadas = [[2, 1], [3, 0]]
+    expect(troca_ou_nao(rodadas)).to eq(1)
   end
 end
