d9e326e69496c00cb15c692ce79083d039fc65bd
dojorio <dojo-rio@googlegroups.com>
2019-03-20 20:06:34 -0300

    thiago cruz e Otavio Cardoso

--- 2019/20190320 - Troca ou Não Troca - ruby/problem.rb
+++ 2019/20190320 - Troca ou Não Troca - ruby/problem.rb
@@ -2,20 +2,18 @@ def troca_ou_nao(rodadas)
   maior  = rodadas.size + 1
   trocas = 0
 
-  if rodadas[0][1] == 0
-    if rodadas.size == 1
-      return 1
-    else
-      trocas = 1
+  if rodadas[0][1] == 0 && rodadas[0][0] == maior
+    trocas += 1
+  end
+
+  if rodadas.size == 2
+    if rodadas[1][1] == 1 && rodadas[1][0] != maior 
+      trocas += 1
     end
-  end
 
-  if rodadas.size > 1 && (rodadas[1][1] == 1 && rodadas[1][0] != maior) 
-    trocas += 1
-  end
-
-  if rodadas.size > 1 && (rodadas[1][1] == 0 && rodadas[1][0] == maior) 
-    trocas += 1
+    if rodadas[1][1] == 0 && rodadas[1][0] == maior
+      trocas += 1
+    end
   end
 
   trocas
