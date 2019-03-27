600e3d9f0d6b5a3f59e31455c6114f5d53d3764f
dojorio <dojo-rio@googlegroups.com>
2019-03-27 20:07:20 -0300

    Otavio Cardoso e Valéria Parajara

--- 2019/20190327 - Dynamic Frog - javascript/problem.js
+++ 2019/20190327 - Dynamic Frog - javascript/problem.js
@@ -1,11 +1,20 @@
 exports.dynamicFrog = function (riverWidth, stones) {
-    if (stones.length >= 1) {
+    if (stones.length == 1) {
+        var stoneDistance = stones[0].split('-')[1]
+        return riverWidth - stoneDistance
+    }
+
+    if (stones.length > 1) {
         var stoneDistance = stones[0].split('-')[1]
         var stoneDistance2 = stones[1].split('-')[1]
-        if (stoneDistance >= stoneDistance2)
+    
+        if (stoneDistance >= stoneDistance2){
             return riverWidth - stoneDistance
-        else
+        }else{
             return riverWidth - stoneDistance2
-    } 
+        }
+    }
+
+
     return riverWidth
 };
