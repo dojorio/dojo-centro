b7d81c5e942843b0d28104c2a132db652b615719
dojorio <dojo-rio@googlegroups.com>
2019-03-27 20:51:20 -0300

    Carlos Figueiredo e Jonatas Emidio

--- 2019/20190327 - Dynamic Frog - javascript/problem.js
+++ 2019/20190327 - Dynamic Frog - javascript/problem.js
@@ -3,45 +3,11 @@ exports.dynamicFrog = function (riverWidth, stones) {
     var lastDistance = 0
 
     stones.forEach(function (stone) {
-        distances.push(stone[0].split('-')[1] - lastDistance)
-        lastDistance = stone[0].split('-')[1]
+        distances.push(stone.split('-')[1] - lastDistance)
+        lastDistance = stone.split('-')[1]
     })
 
     distances.push(riverWidth - lastDistance)
 
-    return distances.reduce(function (acc, distance) {
-        return Math.max(acc, distance)
-    }, 0)
-
-
-    if (stones.length == 1) {
-        var stoneDistance = stones[0].split('-')[1]
-        return riverWidth - stoneDistance
-    }
-
-    if (stones.length == 2) {
-        var stoneDistance  = stones[0].split('-')[1]
-        var stoneDistance2 = stones[1].split('-')[1]
-
-        return Math.max(
-            stoneDistance,
-            riverWidth - stoneDistance2,
-            stoneDistance2 - stoneDistance
-        )
-    }
-    else if (stones.length === 3) {
-        var stoneDistance  = stones[0].split('-')[1]
-        var stoneDistance2 = stones[1].split('-')[1]
-        var stoneDistance3 = stones[2].split('-')[1]
-
-        return Math.max(
-            stoneDistance,
-            riverWidth - stoneDistance3,
-            stoneDistance3 - stoneDistance2,
-            stoneDistance2 - stoneDistance
-        )
-    }
-
-
-    return riverWidth
+    return Math.max.apply(null, distances)
 };
