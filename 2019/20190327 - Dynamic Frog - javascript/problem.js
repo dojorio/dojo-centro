faa3b71fada2b2adc750b9bff714e600b1c8915a
dojorio <dojo-rio@googlegroups.com>
2019-03-27 19:57:49 -0300

    Valéria Parajara e João Ortigão

--- 2019/20190327 - Dynamic Frog - javascript/problem.js
+++ 2019/20190327 - Dynamic Frog - javascript/problem.js
@@ -1,8 +1,11 @@
 exports.dynamicFrog = function (riverWidth, stones) {
     if (stones.length >= 1) {
         var stoneDistance = stones[0].split('-')[1]
-
-        return riverWidth - stoneDistance
+        var stoneDistance2 = stones[1].split('-')[1]
+        if (stoneDistance >= stoneDistance2)
+            return riverWidth - stoneDistance
+        else
+            return riverWidth - stoneDistance2
     } 
     return riverWidth
 };
