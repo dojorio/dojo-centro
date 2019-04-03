c31f38f71cfe3b2371ac702872da7ab1122e403f
dojorio <dojo-rio@googlegroups.com>
2019-04-03 20:47:09 -0300

    BElmira e bALDEZ

--- 2019/20190403 - Pula Sapo - javascript/problem.js
+++ 2019/20190403 - Pula Sapo - javascript/problem.js
@@ -1,6 +1,7 @@
 exports.pulaSapo = function (alturaPulo, alturasCanos) {
-	if (alturasCanos[1] > alturasCanos[0] + 1 ||
-		alturasCanos[1] > alturasCanos[0] + 2 ) {
+	if (alturasCanos[1] > alturasCanos[0] + 1 &&
+		alturasCanos[1] > alturasCanos[0] + 2 ||
+		alturasCanos[0] > alturasCanos[1] + 2) {
 
 		return 'GAME OVER'
 	}
