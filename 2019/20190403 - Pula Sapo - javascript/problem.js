8e01b2e4b4e77e607ec4214f0793eb681191fdac
dojorio <dojo-rio@googlegroups.com>
2019-04-03 20:53:00 -0300

    Otavio Cardoso e BElmira

--- 2019/20190403 - Pula Sapo - javascript/problem.js
+++ 2019/20190403 - Pula Sapo - javascript/problem.js
@@ -1,7 +1,6 @@
 exports.pulaSapo = function (alturaPulo, alturasCanos) {
-	if (alturasCanos[1] > alturasCanos[0] + 1 &&
-		alturasCanos[1] > alturasCanos[0] + 2 ||
-		alturasCanos[0] > alturasCanos[1] + 2) {
+	if (alturasCanos[1] > alturasCanos[0] + 1 ||
+		alturasCanos[1] > alturasCanos[0] + 2 ) {
 
 		return 'GAME OVER'
 	}
