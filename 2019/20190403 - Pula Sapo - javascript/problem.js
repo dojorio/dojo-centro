a3144bcfdc10b6fa58b11d2d09c6794b858edb35
dojorio <dojo-rio@googlegroups.com>
2019-04-03 20:41:32 -0300

    bALDEZ e Rafaela

--- 2019/20190403 - Pula Sapo - javascript/problem.js
+++ 2019/20190403 - Pula Sapo - javascript/problem.js
@@ -1,5 +1,6 @@
 exports.pulaSapo = function (alturaPulo, alturasCanos) {
-	if (!(alturasCanos[1] > alturasCanos[0] + 1 )) {
+	if (alturasCanos[1] > alturasCanos[0] + 1 ||
+		alturasCanos[1] > alturasCanos[0] + 2 ) {
 
 		return 'GAME OVER'
 	}
