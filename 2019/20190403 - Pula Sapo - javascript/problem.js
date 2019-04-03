4e4f71755c8033977f5846aa9460c261e53fb679
dojorio <dojo-rio@googlegroups.com>
2019-04-03 20:12:11 -0300

    Rafaela e Belmira

--- 2019/20190403 - Pula Sapo - javascript/problem.js
+++ 2019/20190403 - Pula Sapo - javascript/problem.js
@@ -1,6 +1,7 @@
 exports.pulaSapo = function (alturaPulo, alturasCanos) {
 	if (alturasCanos[0] == 1 && alturasCanos[1] == 3 ||
-		alturasCanos[0] == 2 && alturasCanos[1] == 4) {
+		alturasCanos[0] == 2 && alturasCanos[1] == 4 ||
+		alturasCanos[0] == 1 && alturasCanos[1] == 4) {
 		return 'GAME OVER'
 	}
 
