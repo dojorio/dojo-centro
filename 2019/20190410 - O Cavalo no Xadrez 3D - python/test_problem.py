f342d918693f4576270b02c3e57576099c13beca
dojorio <dojo-rio@googlegroups.com>
2019-04-10 19:47:19 -0300

    Odilton Junior e João Ortigão

--- 2019/20190410 - O Cavalo no Xadrez 3D - python/test_problem.py
+++ 2019/20190410 - O Cavalo no Xadrez 3D - python/test_problem.py
@@ -8,6 +8,9 @@ from problem import *
 class TestProblem(unittest.TestCase):
     def test_(self):
         self.assertEqual(1, 1)
+    
+    def test_(self):
+        self.assertEqual(tabuleiro[0][0], 8)
 
 
 if __name__ == "__main__":
