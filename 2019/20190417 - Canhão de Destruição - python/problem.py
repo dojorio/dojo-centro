adc556af789b2ddd691263c800d4698cdb5486e1
dojorio <dojo-rio@googlegroups.com>
2019-04-17 20:04:36 -0300

    Otavio Cardoso e Daniel Drumond

--- 2019/20190417 - Canhão de Destruição - python/problem.py
+++ 2019/20190417 - Canhão de Destruição - python/problem.py
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 
 def mission_accomplished(bombs, cannon, castle):
-	return True
+	return bombs[0][0] >= castle and bombs[0][1] <= cannon
--- 2019/20190417 - Canhão de Destruição - python/test_problem.py
+++ 2019/20190417 - Canhão de Destruição - python/test_problem.py
@@ -8,16 +8,40 @@ from problem import *
 
 class TestProblem(unittest.TestCase):
     def test_1(self):
-        bombs  = ((1, 1),)
+        bombs  = ((100, 1),)
         cannon = 1
-        castle = 1
+        castle = 100
         self.assertTrue(mission_accomplished(bombs, cannon, castle))
 
-    def test_2(self):
-        bombs  = ((1, 1),)
+    def test_castle_2(self):
+        bombs  = ((100, 1),)
         cannon = 1
-        castle = 2
+        castle = 200
         self.assertFalse(mission_accomplished(bombs, cannon, castle))
+
+    def test_castle_and_bomb_2(self):
+        bombs  = ((200, 1),)
+        cannon = 1
+        castle = 200
+        self.assertTrue(mission_accomplished(bombs, cannon, castle))
+
+    def test_castle_300_and_bomb_200(self):
+        bombs  = ((200, 1),)
+        cannon = 1
+        castle = 300
+        self.assertFalse(mission_accomplished(bombs, cannon, castle))
+
+    def test_castle_overweight(self):
+        bombs  = ((300, 2),)
+        cannon = 1
+        castle = 300
+        self.assertFalse(mission_accomplished(bombs, cannon, castle))
+
+    def test_castle_300_bombs_200_100(self):
+        bombs  = ((200, 1), (100, 1), )
+        cannon = 2
+        castle = 300
+        self.assertTrue(mission_accomplished(bombs, cannon, castle))
 
 if __name__ == "__main__":
     unittest.main()
