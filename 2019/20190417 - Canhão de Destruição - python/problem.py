4922e838304a00dedc351327a35e5c839777f59a
dojorio <dojo-rio@googlegroups.com>
2019-04-17 20:47:18 -0300

    Otavio Cardoso e Daniel Drumond

--- 2019/20190417 - Canhão de Destruição - python/problem.py
+++ 2019/20190417 - Canhão de Destruição - python/problem.py
@@ -2,6 +2,22 @@
 # -*- coding: utf-8 -*-
 
 def mission_accomplished(bombs, cannon, castle):
+    bag = [None]*(cannon+1)
+    bag[0] = 0
+
+    for bomb in bombs:
+        power, weight = bomb
+        ind = cannon
+        while ind >= 0:
+            if bag[ind] is not None:
+                if ind + weight <= cannon:
+                    if bag[ind + weight] < bag[ind] + power:
+                        bag[ind + weight] = bag[ind] + power
+            ind -= 1
+
+    return bag[-1] >= castle
+
+
     bombs = list(bombs)
     total_weight = sum([bomb[1] for bomb in bombs])
 
