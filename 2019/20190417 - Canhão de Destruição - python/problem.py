b5195aae2f36dbeb380afde5713b37b1fc452680
dojorio <dojo-rio@googlegroups.com>
2019-04-17 20:28:20 -0300

    Otavio Cardoso e Daniel Drumond

--- 2019/20190417 - Canhão de Destruição - python/problem.py
+++ 2019/20190417 - Canhão de Destruição - python/problem.py
@@ -2,24 +2,19 @@
 # -*- coding: utf-8 -*-
 
 def mission_accomplished(bombs, cannon, castle):
+    total_power  = 0
+    total_weight = 0
+
     for bomb in bombs:
-        if bomb[0] >= castle and bomb[1] <= cannon:
+        total_power  += bomb[0]
+        total_weight += bomb[1]
+
+        if total_power >= castle and total_weight <= cannon:
             return True
 
-    if len(bombs) > 1:
-        total_power  = 0
-        total_weight = 0
-
-        for bomb in bombs:
-            total_power  += bomb[0]
-            total_weight += bomb[1]
-
-            if total_power >= castle and total_weight <= cannon:
-                return True
-
-            if total_weight == cannon:
-                total_power  -= bomb[0]
-                total_weight -= bomb[1]
+        if total_weight == cannon:
+            total_power  -= bomb[0]
+            total_weight -= bomb[1]
 
 
     return False
