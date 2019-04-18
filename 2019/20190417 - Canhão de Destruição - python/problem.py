26bb49c0f6549ef4e3cbc823db2a90cbb87524b3
dojorio <dojo-rio@googlegroups.com>
2019-04-17 21:05:13 -0300

    Daniel Drumond e Otavio Cardoso

--- 2019/20190417 - Canhão de Destruição - python/problem.py
+++ 2019/20190417 - Canhão de Destruição - python/problem.py
@@ -7,14 +7,13 @@ def mission_accomplished(bombs, cannon, castle):
 
     for bomb in bombs:
         power, weight = bomb
-        ind = cannon
-        while ind >= 0:
-            if bag[ind] is not None:
-                check = ind + weight
-                if check <= cannon:
-                    if bag[check] is None or bag[check] < bag[ind] + power:
-                        bag[check] = bag[ind] + power
-            ind -= 1
+
+        for ind in range(cannon, -1, -1):
+            check = ind + weight
+
+            if bag[ind] is not None and check <= cannon:
+                if bag[check] is None or bag[check] < bag[ind] + power:
+                    bag[check] = bag[ind] + power
     
     new_bag = [item for item in bag if item is not None]
     return max(new_bag) >= castle
