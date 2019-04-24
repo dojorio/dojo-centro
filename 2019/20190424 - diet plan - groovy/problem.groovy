fd546dc8bcc2bff35d224c5023f10954bc8da5b8
dojorio <dojo-rio@googlegroups.com>
2019-04-24 20:18:36 -0300

    Rogerio Lourenco e Jonatas Emidio

---
+++ 2019/20190424 - diet plan - groovy/problem.groovy
@@ -0,0 +1,3 @@
+def static problem (input) {
+	return ""
+}
---
+++ 2019/20190424 - diet plan - groovy/problemSpec.groovy
@@ -0,0 +1,14 @@
+import spock.lang.*
+import problem
+
+class ProblemSpec extends spock.lang.Specification {
+    def "test"() {
+        expect:
+        problem.problem(input) == output
+
+        where:
+        input        | output
+        ["", "", ""] | ""
+        ["A", "", ""] | "A"
+    }
+}
