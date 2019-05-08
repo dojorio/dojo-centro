16fb9ea809fc69432e0666890457d8993b7b18ae
dojorio <dojo-rio@googlegroups.com>
2019-05-08 20:49:36 -0300

    Fernanda França e tito

--- 2019/20190508 - campo minado - python/problem.py
+++ 2019/20190508 - campo minado - python/problem.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 def gera_campo(campo):
+
+	lista = campo[0].replace('.','0')
+
 	while '*' in campo[0]:
 		lista  = list(campo[0])
 		indice = lista.index('*')
 
 		if indice > 0:
-			lista[indice - 1] = '1'
-
+			lista[indice - 1] = int(lista[indice - 1]) + 1
 		if indice < len(lista) - 1:
-			lista[indice + 1] = '1'
+			lista[indice + 1] = int(lista[indice + 1]) + 1
 
 		lista[indice] = '9'
 
