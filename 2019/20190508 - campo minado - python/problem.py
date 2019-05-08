2ca831f8f87027708a2608ddcecc24f05cc9c710
dojorio <dojo-rio@googlegroups.com>
2019-05-08 20:27:11 -0300

    Otavio Cardoso e tito

--- 2019/20190508 - campo minado - python/problem.py
+++ 2019/20190508 - campo minado - python/problem.py
@@ -2,14 +2,18 @@
 # -*- coding: utf-8 -*-
 
 def gera_campo(campo):
-	if campo == ['*']:
-		return ['*']
 
-	if campo == ['*.']:
-		return ['*1']
 
-	if campo == ['.*']:
-		return ['1*']
+	if '*' in campo[0]:
+		lista = campo[0].split()
+		print(lista)
+		indice = lista.index('*')
+		if indice > 0:
+			lista[indice-1] = '1'
+		if indice < len(lista)-1:
+			lista[indice+1] = '1'
+		campo[0] = join(lista)
 
+		
 	return [campo[0].replace('.', '0')]
 	
