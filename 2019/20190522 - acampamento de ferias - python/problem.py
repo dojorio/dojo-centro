cba6d4566950e7b17d9e3e28199ccf6fb260e5dc
dojorio <dojo-rio@googlegroups.com>
2019-05-22 20:19:26 -0300

    Thiago Bonfim e Toti Cavalcanti

--- 2019/20190522 - acampamento de ferias - python/problem.py
+++ 2019/20190522 - acampamento de ferias - python/problem.py
@@ -2,11 +2,17 @@
 # -*- coding: utf-8 -*-
 
 def gira(criancas):
-	pos = 0
-    crianca, numero = criancas.items()
-	while len(criancas) > 1:
-        list(criancas.keys())[pos]	   
+    pos = 0
+    # 2_1_1
+    while len(criancas) > 1:
+        crianca, numero = list(criancas.items())[pos]
+        if not numero % 2:
+            pos = pos + numero
+        else:
+            pos = pos - numero
+        
+        return list(criancas.keys())[pos]     
 
-	if list(criancas.values())[0] % 2 == 0:
-		return list(criancas)[1]
-	return list(criancas)[0]
+    if list(criancas.values())[0] % 2 == 0:
+        return list(criancas)[1]
+    return list(criancas)[0]
