14fe8d09636bc0e2feaf123ef9dd9c815e4869a8
dojorio <dojo-rio@googlegroups.com>
2019-05-22 19:54:18 -0300

    Filipe Ferreira e Daniel Drumond

--- 2019/20190522 - acampamento de ferias - python/test_problem.py
+++ 2019/20190522 - acampamento de ferias - python/test_problem.py
@@ -35,6 +35,23 @@ class TestProblem(unittest.TestCase):
         }
         self.assertEqual('Jose', gira(criancas))
 
+    def test_criancas_1_1_1(self):
+        criancas = {
+            'Joao': 1,
+            'Jose': 1,
+            'Maria': 1,
+        }
+        self.assertEqual('Joao', gira(criancas))
+
+    def test_criancas_2_1_1(self):
+        criancas = {
+            'Joao': 2,
+            'Jose': 1,
+            'Maria': 1,
+        }
+        self.assertEqual('Joao', gira(criancas))
+
+
 
 if __name__ == "__main__":
     unittest.main()
