71aeed1ae96e4f1f2c5debbb48e7f69627c1946f
dojorio <dojo-rio@googlegroups.com>
2019-05-22 20:01:13 -0300

    Claudio Berrondo e Filipe Ferreira

--- 2019/20190522 - acampamento de ferias - python/problem.py
+++ 2019/20190522 - acampamento de ferias - python/problem.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 
 def gira(criancas):
+	pos = 0
+    crianca, numero = criancas.items()
+	while len(criancas) > 1:
+        list(criancas.keys())[pos]	   
+
 	if list(criancas.values())[0] % 2 == 0:
 		return list(criancas)[1]
 	return list(criancas)[0]
