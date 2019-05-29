3aee0042c2050517878c872498e5da877542a94b
dojorio <dojo-rio@googlegroups.com>
2019-05-29 19:56:13 -0300

    Marina Ribeiro e Otavio Cardoso

---
+++ 2019/20190529 - Máquina de dobra - python/problem.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def dobra_ok(fita_entrada, fita_saida):
+    return fita_saida != [2]
---
+++ 2019/20190529 - Máquina de dobra - python/test_problem.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1470
+
+import unittest
+from problem import *
+
+
+class TestProblem(unittest.TestCase):
+    def test_1_1(self):
+        fita_entrada = [1]
+        fita_saida   = [1]
+        self.assertTrue(dobra_ok(fita_entrada, fita_saida))
+
+    def test_1_2(self):
+        fita_entrada = [1]
+        fita_saida   = [2]
+        self.assertFalse(dobra_ok(fita_entrada, fita_saida))
+
+if __name__ == "__main__":
+    unittest.main()
+
