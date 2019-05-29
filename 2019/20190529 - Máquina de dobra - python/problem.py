50e3f7c528846286b375b9c47df8145958b6481f
dojorio <dojo-rio@googlegroups.com>
2019-05-29 20:15:02 -0300

    Rudá Almeida e Claudio Berrondo

--- 2019/20190529 - Máquina de dobra - python/problem.py
+++ 2019/20190529 - Máquina de dobra - python/problem.py
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 
 def dobra_ok(fita_entrada, fita_saida):
-    return fita_saida == fita_entrada
+    return fita_saida in [fita_entrada, fita_entrada[::-1]] 
--- 2019/20190529 - Máquina de dobra - python/test_problem.py
+++ 2019/20190529 - Máquina de dobra - python/test_problem.py
@@ -23,11 +23,20 @@ class TestProblem(unittest.TestCase):
         fita_saida   = [1]
         self.assertFalse(dobra_ok(fita_entrada, fita_saida))    
 
-    def test_1_1_2(self):
+    def test_1_1_1_1(self):
+        fita_entrada = [1, 1]
+        fita_saida   = [1, 1]
+        self.assertTrue(dobra_ok(fita_entrada, fita_saida))    
+
+    def test_1_2_2_1(self):
+        fita_entrada = [1, 2]
+        fita_saida   = [2, 1]
+        self.assertTrue(dobra_ok(fita_entrada, fita_saida))    
+
+    def test_1_1__2(self):
         fita_entrada = [1, 1]
         fita_saida   = [2]
-        self.assertFalse(dobra_ok(fita_entrada, fita_saida))    
-
+        self.assertTrue(dobra_ok(fita_entrada, fita_saida))    
 
 if __name__ == "__main__":
     unittest.main()
