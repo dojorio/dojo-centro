5436320d8752169fff788c0f5671546b92258b8c
dojorio <dojo-rio@googlegroups.com>
2019-05-29 20:26:06 -0300

    Daniel Drumond e Otavio Cardoso

--- 2019/20190529 - Máquina de dobra - python/problem.py
+++ 2019/20190529 - Máquina de dobra - python/problem.py
@@ -2,6 +2,4 @@
 # -*- coding: utf-8 -*-
 
 def dobra_ok(fita_entrada, fita_saida):
-    if len(fita_entrada) > len(fita_saida) and fita_saida[0] == sum(fita_entrada):
-        return True
-    return fita_saida in [fita_entrada, fita_entrada[::-1]] 
+    return sum(fita_entrada) == sum(fita_saida)
--- 2019/20190529 - Máquina de dobra - python/test_problem.py
+++ 2019/20190529 - Máquina de dobra - python/test_problem.py
@@ -8,27 +8,27 @@ from problem import *
 
 
 class TestProblem(unittest.TestCase):
-    def test_1_1(self):
+    def test_1__1(self):
         fita_entrada = [1]
         fita_saida   = [1]
         self.assertTrue(dobra_ok(fita_entrada, fita_saida))
 
-    def test_1_2(self):
+    def test_1__2(self):
         fita_entrada = [1]
         fita_saida   = [2]
         self.assertFalse(dobra_ok(fita_entrada, fita_saida))
 
-    def test_2_1(self):
+    def test_2__1(self):
         fita_entrada = [2]
         fita_saida   = [1]
         self.assertFalse(dobra_ok(fita_entrada, fita_saida))    
 
-    def test_1_1_1_1(self):
+    def test_1_1__1_1(self):
         fita_entrada = [1, 1]
         fita_saida   = [1, 1]
         self.assertTrue(dobra_ok(fita_entrada, fita_saida))    
 
-    def test_1_2_2_1(self):
+    def test_1_2__2_1(self):
         fita_entrada = [1, 2]
         fita_saida   = [2, 1]
         self.assertTrue(dobra_ok(fita_entrada, fita_saida))    
@@ -38,10 +38,15 @@ class TestProblem(unittest.TestCase):
         fita_saida   = [2]
         self.assertTrue(dobra_ok(fita_entrada, fita_saida))    
 
-    def test_1_1__2(self):
+    def test_10_20__15(self):
         fita_entrada = [10, 20]
         fita_saida   = [15]
         self.assertFalse(dobra_ok(fita_entrada, fita_saida))    
+
+    def test_1_2_3__1_5(self):
+        fita_entrada = [1, 2, 3]
+        fita_saida   = [1, 5]
+        self.assertTrue(dobra_ok(fita_entrada, fita_saida))  
 
 if __name__ == "__main__":
     unittest.main()
