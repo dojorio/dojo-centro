95ec79b7214e87ddbdecea543ec1f854ab5cf2d5
dojorio <dojo-rio@googlegroups.com>
2019-07-03 20:21:15 -0300

    Vito e Otavio Cardoso

---
+++ 2019/20190703 - figurinhas - python/problem.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def maior_pilha(pilha_1, pilha_2):
+	return min(pilha_1, pilha_2)
---
+++ 2019/20190703 - figurinhas - python/test_problem.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from problem import *
+
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1028
+
+class TestProblem(unittest.TestCase):
+    def test_1_1(self):
+        self.assertEqual(maior_pilha(1, 1), 1)
+
+    def test_2_2(self):
+        self.assertEqual(maior_pilha(2, 2), 2)
+
+    def test_3_3(self):
+        self.assertEqual(maior_pilha(3, 3), 3)
+
+    def test_2_1(self):
+        self.assertEqual(maior_pilha(2, 1), 1)
+
+    def test_1_2(self):
+        self.assertEqual(maior_pilha(1, 2), 1)
+
+if __name__ == "__main__":
+    unittest.main()
+
