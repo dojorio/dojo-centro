aefba7b0f75dd663552a2b7a654f109d8d9f0af7
dojorio <dojo-rio@googlegroups.com>
2019-07-10 19:19:38 -0300

    Otavio Cardoso e Vito

---
+++ 2019/20190710 - miojo - python/problem.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def miojo(amp1, amp2):
+	if amp1 == 2:
+		return 5
+
+	return 3
---
+++ 2019/20190710 - miojo - python/test_problem.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from problem import *
+
+class TestProblem(unittest.TestCase):
+    def test_1_1(self):
+        self.assertEqual(miojo(1, 1), 3)
+
+    def test_2_5(self):
+        self.assertEqual(miojo(2, 5), 5)
+
+    def test_5_2(self):
+        self.assertEqual(miojo(5, 2), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
