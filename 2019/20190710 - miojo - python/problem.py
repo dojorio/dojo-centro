24ce654362f168b91ad286d95991df646e76c3f9
dojorio <dojo-rio@googlegroups.com>
2019-07-10 20:00:53 -0300

    Vinicius Mattos e Charley Oliveira

--- 2019/20190710 - miojo - python/problem.py
+++ 2019/20190710 - miojo - python/problem.py
@@ -4,6 +4,14 @@
 def eh_par(amp):
     return not amp % 2
 
+def verifica_em_lista(amp1, amp2):
+    l_amp1 = [amp1*i for i in range(0, 10)]
+    l_amp2 = [amp2*i for i in range(0, 10)]
+
+    for tempo1 in l_amp1:
+        for tempo2 in l_amp2:
+            if abs(tempo1 - tempo2) == 3:
+                return max(tempo1,tempo2)
 
 def miojo(amp1, amp2):
     if 3 in (amp1, amp2):
@@ -15,17 +23,6 @@ def miojo(amp1, amp2):
     if abs(amp1 - amp2) == 3:
         return max((amp1, amp2))
 
-    # if amp1 == 2 or amp2 == 2:
-    #     return 5
+    verifica_em_lista(amp1, amp2)
 
-    l_amp1 = [amp1*i for i in range(0, 10)]
-    l_amp2 = [amp2*i for i in range(0, 10)]
-
-    for tempo1 in l_amp1:
-        for tempo2 in l_amp2:
-            if abs(tempo1 - tempo2) == 3:
-                return max(tempo1,tempo2)
-
-
-
-    return 3
+    return None
