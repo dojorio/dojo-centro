6c8fef40205834d6ac33dd753be4d1e872308b0e
dojorio <dojo-rio@googlegroups.com>
2019-07-10 19:40:24 -0300

    Daniel Drumond e Claudio Berrondo

--- 2019/20190710 - miojo - python/problem.py
+++ 2019/20190710 - miojo - python/problem.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def eh_par(amp):
+    return not amp % 2
+
+
 def miojo(amp1, amp2):
-	if 3 in (amp1, amp2):
-		return 3
-		
-	if amp1 == 2 and amp2 == 2:
-		return None
+    if eh_par(amp1) and eh_par(amp2):
+        return None
 
-	if amp1 == 2 or amp2 == 2:
-		return 5
+    if 3 in (amp1, amp2):
+        return 3
 
+    if amp1 == 2 or amp2 == 2:
+        return 5
 
-
-	return 3
+    return 3
--- 2019/20190710 - miojo - python/test_problem.py
+++ 2019/20190710 - miojo - python/test_problem.py
@@ -23,5 +23,8 @@ class TestProblem(unittest.TestCase):
     def test_4_2(self):
         self.assertEqual(miojo(4, 2), None)
 
+    def test_8_5(self):
+        self.assertEqual(miojo(8, 5), 8)
+
 if __name__ == "__main__":
     unittest.main()
