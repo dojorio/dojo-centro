211f9b9d2a61ef4059779b85cb590384e71d67ad
dojorio <dojo-rio@googlegroups.com>
2019-07-10 20:32:32 -0300

    Rodrigo Tenorio e Daniel Drumond

--- 2019/20190710 - miojo - python/problem.py
+++ 2019/20190710 - miojo - python/problem.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 
 def miojo(amp1, amp2):
+    if not amp1 % 2 and not amp2 % 2:
+        return None
+    if amp1 == amp2 and amp1 not in (1, 3) and amp2 not in (1, 3):
+        return None
 
-    l_amp1 = [amp1*i for i in range(0, 1000000)]
-    l_amp2 = [amp2*i for i in range(0, 1000000)]
+    l_amp1 = (amp1*i for i in range(0, 1000000))
+    l_amp2 = [amp2*i for i in range(0, 999999)]
 
     for tempo1 in l_amp1:
         for tempo2 in l_amp2:
             diff = abs(tempo1 - tempo2)
-
             if diff == 3:
                 return max(tempo1, tempo2)
             if diff > 3 and tempo2 > tempo1:
-                break 
+                break
