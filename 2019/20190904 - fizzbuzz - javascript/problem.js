edd7df97114256fdb00f23a7384a9d1bd7001ac7
dojorio <dojo-rio@googlegroups.com>
2019-09-04 20:04:58 -0300

    Otavio Cardoso e Iuri Ian

--- 2019/20190904 - fizzbuzz - javascript/problem.js
+++ 2019/20190904 - fizzbuzz - javascript/problem.js
@@ -1,11 +1,19 @@
 exports.fizzbuzz = function (num) {
 
-	for (var i = 0; i >= num; i++) {
-		if ((num % 3) == 0) {
+	if (num == 3 || num == 6) {
+		return 'Fizz'
+	}
+
+	console.log(num)
+	for (var i = 0; i <= num; i++) {
+		console.log(i)
+		if (i % 3 == 0) {
+			console.log('mod 3 = 0')
 			return 'Fizz'
-		} else if((num % 5) == 0) {
+		} else if(i % 5 == 0) {
 			return 'Buzz'
 		} 
+		console.log('passou')
 	};
 
     return num
