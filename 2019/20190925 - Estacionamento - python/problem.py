f1e7f32179d4e3c934a1b0b368ff9ed552801ea2
dojorio <dojo-rio@googlegroups.com>
2019-09-25 20:20:25 -0300

    Otavio Cardoso e Larissa Loureiro

--- 2019/20190925 - Estacionamento - python/problem.py
+++ 2019/20190925 - Estacionamento - python/problem.py
@@ -3,12 +3,16 @@
 
 def valor_total(tamanho, eventos):
     total = 0
+    tamanhoUltimoCarro = 0
 
     for evento in eventos:
         if evento[0] == 'C':
             if evento[2] <= tamanho:
+                tamanhoUltimoCarro = evento[2]
                 total = total + 10
                 tamanho = tamanho - evento[2]
+        if evento[0] == 'S':
+            tamanho = tamanho + tamanhoUltimoCarro
 
 
     return total
