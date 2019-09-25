7b3e9e8f57d4a7e743c77d133c55be4c1b0e3007
dojorio <dojo-rio@googlegroups.com>
2019-09-25 20:46:59 -0300

    Larissa Loureiro e Bernardo Muricy

--- 2019/20190925 - Estacionamento - python/problem.py
+++ 2019/20190925 - Estacionamento - python/problem.py
@@ -4,22 +4,37 @@
 def valor_total(tamanho, eventos):
     total = 0
     carros = {}
+    vagas = [None] * tamanho
 
     for evento in eventos:
         tipo  = evento[0]
         placa = evento[1]
 
         if tipo == 'C':
+            inicioVaga = None
+            tamanhoVaga = 0 
             tamanhoCarro = evento[2]
 
-            if tamanhoCarro <= tamanho:
-                carros[placa] = tamanhoCarro
+            for posicao in range(tamanho):
+                if vagas[posicao] == None:
+                    tamanhoVaga = tamanhoVaga + 1
+                else:
+                    tamanhoVaga = 0
+                if tamanhoVaga == tamanhoCarro:
+                    inicioVaga = posicao
+                    break
+
+            if inicioVaga != None:
                 total = total + 10
-                tamanho = tamanho - tamanhoCarro
+                for metro in range(tamanhoCarro):
+                    vagas[inicioVaga + metro] = placa
+
 
         if tipo == 'S':
-            tamanhoCarroSaindo = carros[placa]
-            tamanho = tamanho + tamanhoCarroSaindo
+            for vaga in range(tamanho):
+                if vagas[vaga] == placa:
+                    vagas[vaga] = None
+
 
 
     return total
