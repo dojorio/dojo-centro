f91b391fb1ba653c20118942c495aa688249802c
dojorio <dojo-rio@googlegroups.com>
2019-12-04 20:34:36 -0200

    Rafael Figueiredo e Otavio Cardoso

--- 2019/20191204 - Tobias Contra o Relógio - javascript/problem.js
+++ 2019/20191204 - Tobias Contra o Relógio - javascript/problem.js
@@ -3,38 +3,30 @@ exports.agenda = function (atividades) {
 		return a[0] - b[0]
 	})
 
-	let maximo = atividades.length
+	let maximo = 1 //atividades.length
 
-	if (atividades.length == 2) {
+	if (atividades.length > 1) {
+		// 1 com 2
 		final1 = atividades[0][0] + atividades[0][1]
 
-		if (final1 > atividades[1][0]) {
-			return 1
+		if (final1 <= atividades[1][0]) {
+			maximo += 1
 		}
 	}
 
-	if (atividades.length == 3) {
-		// 1 com 2
-		let final1 = atividades[0][0] + atividades[0][1]
-
-		if (final1 > atividades[1][0]) {
-			maximo -= 1
-		}
-
+	if (atividades.length > 2) {
 		// 2 com 3
 		let final2 = atividades[1][0] + atividades[1][1]
 
-		if (final2 > atividades[2][0]){
-			maximo -= 1
+		if (final2 <= atividades[2][0]){
+			maximo += 1
 		}
 
-		if (final1 <= atividades[2][0]){
+		// 1 com 3
+		if (maximo == 1 && final1 <= atividades[2][0]){
 			maximo +=1
 		}
-
-		
-		return maximo
 	}
 
-	return atividades.length
+	return maximo
 };
