47e92c2f37d894828e5d2f6ac070ab38e4fa7df2
dojorio <dojo-rio@googlegroups.com>
2019-12-11 19:35:05 -0200

    Rafael Figueiredo e Pedro Barros

--- 2019/20191211 - Comparação de Substring - javascript/problem.js
+++ 2019/20191211 - Comparação de Substring - javascript/problem.js
@@ -1,3 +1,10 @@
-exports.tamanhoSubstring = function (bar) {
-    return 1
+exports.tamanhoSubstring = function (string1, string2) {
+
+	if (string2==string1){
+
+		return 1
+
+	}
+
+    return 0
 };
