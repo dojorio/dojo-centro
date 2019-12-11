9660732e6e927035b424b17d2f640ac3beb65500
dojorio <dojo-rio@googlegroups.com>
2019-12-11 20:32:48 -0200

    Carolina de Sá e Rafael Figueiredo

--- 2019/20191211 - Comparação de Substring - javascript/problem.js
+++ 2019/20191211 - Comparação de Substring - javascript/problem.js
@@ -3,17 +3,13 @@ exports.tamanhoSubstring = function (string1, string2) {
 	let count = 0
 	//let amostra2 = string1[1]
 	//let amostra = string1[0] 
-	for(let comprimento_substring = 1; 
-		comprimento_substring < string1.length; 
-		comprimento_substring++){
-	
-		for(let i = 0; i < ((string1.length) - 1); i++){
+	for(let comprimento_substring = 1; comprimento_substring<2; comprimento_substring++)
+	for(let i=0;i<2;i++){
 
-			let amostra = string1[i]
+		let amostra = string1[i]
 
-			if (string2.includes(amostra)){
-				count ++			
-			}
+		if (string2.includes(amostra)){
+			count ++			
 		}
 	}
 
