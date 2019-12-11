9961429a92790a3fb5d2ef9d116a38df79d6f284
dojorio <dojo-rio@googlegroups.com>
2019-12-11 19:52:10 -0200

    Pedro Barros e Carol Albuquerque

--- 2019/20191211 - Comparação de Substring - javascript/problem.js
+++ 2019/20191211 - Comparação de Substring - javascript/problem.js
@@ -2,9 +2,13 @@ exports.tamanhoSubstring = function (string1, string2) {
 
 	let amostra2 = string1[1]
 	let amostra = string1[0] 
-	if (string2.includes(amostra) || string2.includes(amostra2)){
-		return 1
-	}
 
+	/*if (string2.includes(amostra) || string2.includes(amostra2)){
+		return 1
+	}*/
+
+	if (string2.equals(string1)){
+		return 2
+	}
     return 0
 };
