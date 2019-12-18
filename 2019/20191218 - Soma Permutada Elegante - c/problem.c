5038323dd49e9fc4b3f1a9687fec5771d5da41c6
dojorio <dojo-rio@googlegroups.com>
2019-12-18 19:38:36 -0200

    Otavio Cardoso e Pedro Barros

--- 2019/20191218 - Soma Permutada Elegante - c/problem.c
+++ 2019/20191218 - Soma Permutada Elegante - c/problem.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
 int main() {
-	int count;
+	int cases;
+	int list;
+	int n;
 
-    scanf("%d", &count);
+    scanf("%d", &cases);
 
-	printf("0");
+    for (; cases > 0; cases--) {
+    	scanf("%d", &list);
+
+    	for (; list > 0; list--) {
+    		scanf("%d", &n);
+    	}
+
+    	if (n == 1) {
+    		printf("0\n");
+    	} else {
+    		printf("1\n");
+    	}
+    }
+
 	return 0;
 
 }
