6a07c362c23545df9e0eebfb032b226ab02916f1
dojorio <dojo-rio@googlegroups.com>
2020-01-15 20:00:28 -0200

    Otavio Cardoso e Pedro Barros

--- 2020/20200115 - blocos de gelo - javascript/problem.js
+++ 2020/20200115 - blocos de gelo - javascript/problem.js
@@ -1,14 +1,9 @@
 exports.iceBlock = function (blocks, required) {
+    
     if (blocks.length == 2) {
-        if (required > 2 * blocks[1]) {
-            return 3
-        }
 
-        if (required > blocks[1]) {
-            return 2
-        }
-
-        return 1
+        return Math.floor(required / blocks[1] )+ required % blocks[1]
+        
     }
 
     return required 
