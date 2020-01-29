39e30ef061d12c80d89a93f52d9bda2b623bdc18
dojorio <dojo-rio@googlegroups.com>
2020-01-29 19:51:33 -0200

    Pedro Barros e Joao Guilherme

--- 2020/20200129 - Caixas muito especiais - ruby/problem.rb
+++ 2020/20200129 - Caixas muito especiais - ruby/problem.rb
@@ -1,8 +1,8 @@
 def volume_extra(pedido, estoque)
-  if pedido[0][0]==2 || pedido[0][1]==2 || pedido[0][2]==2
-    if estoque[0][0] != 2 && estoque[0][1] != 2 && estoque[0][2] != 2
-      return "error"
-    end
+  if pedido[0][0] == 2 || pedido[0][1] == 2 || pedido[0][2] == 2
+      if estoque[0][0] != 2 && estoque[0][1] != 2 && estoque[0][2] != 2
+        return "error"
+      end
   end
 
   return 0
