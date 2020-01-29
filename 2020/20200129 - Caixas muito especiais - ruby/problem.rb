66f8f287848e902e541bb26e9db5f4cde5c4640d
dojorio <dojo-rio@googlegroups.com>
2020-01-29 19:40:49 -0200

    Otavio Cardoso e Pedro Barros

--- 2020/20200129 - Caixas muito especiais - ruby/problem.rb
+++ 2020/20200129 - Caixas muito especiais - ruby/problem.rb
@@ -1,3 +1,8 @@
 def volume_extra(pedido, estoque)
+
+  if pedido[0][0]==2 || pedido[0][1]==2 || pedido[0][2]==2
+    return "error"
+  end
+
   return 0
 end
--- 2020/20200129 - Caixas muito especiais - ruby/problem_spec.rb
+++ 2020/20200129 - Caixas muito especiais - ruby/problem_spec.rb
@@ -9,4 +9,25 @@ describe 'Caixas muito especiais' do
 
     expect(volume_extra(pedido, estoque)).to eq(0)
   end
+
+  it 'return error when order is greater than 1' do
+    pedido  = [[2, 1, 1]]
+    estoque = [[1, 1, 1]]
+
+     expect(volume_extra(pedido, estoque)).to eq("error")
+  end
+
+  it 'return error when order is greater than 2' do
+    pedido  = [[1, 2, 1]]
+    estoque = [[1, 1, 1]]
+
+     expect(volume_extra(pedido, estoque)).to eq("error")
+  end
+
+   it 'return error when order is greater than 3' do
+    pedido  = [[1, 1, 2]]
+    estoque = [[1, 1, 1]]
+
+     expect(volume_extra(pedido, estoque)).to eq("error")
+  end
 end
