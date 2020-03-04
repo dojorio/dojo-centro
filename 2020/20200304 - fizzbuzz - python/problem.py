9692a52e5a72dae2297cc68ed3346da5cc921b5c
dojorio <dojo-rio@googlegroups.com>
2020-03-04 19:45:04 -0300

    venancio mota e Otavio Cardoso

--- 2020/20200304 - fizzbuzz - python/problem.py
+++ 2020/20200304 - fizzbuzz - python/problem.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 def fizzbuzz(num):
+	if num % 3 == 0 and num % 5 == 0:
+		return 'fizzbuzz'
 	if num % 3 == 0:
 		return 'fizz'
 	if num % 5 == 0:
 		return 'buzz'
-	
 
-	
 	return num
--- 2020/20200304 - fizzbuzz - python/test_problem.py
+++ 2020/20200304 - fizzbuzz - python/test_problem.py
@@ -37,9 +37,12 @@ class TestProblem(unittest.TestCase):
 
     def test_11(self):
         self.assertEqual(fizzbuzz(11), 11)
-    
+
     def test_15(self):
         self.assertEqual(fizzbuzz(15), 'fizzbuzz')
+
+    def test_30(self):
+        self.assertEqual(fizzbuzz(30), 'fizzbuzz')
 
 if __name__ == "__main__":
     unittest.main()
