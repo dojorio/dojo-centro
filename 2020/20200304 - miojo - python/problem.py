d2c8d8c6363a7cfe346778f4ad2ef78d102b0c05
dojorio <dojo-rio@googlegroups.com>
2020-03-04 20:12:07 -0300

    Otavio Cardoso e Vinicius Garcia

--- 2020/20200304 - miojo - python/problem.py
+++ 2020/20200304 - miojo - python/problem.py
@@ -2,4 +2,8 @@
 # -*- coding: utf-8 -*-
 
 def miojo(temp_1, temp_2):
+	if temp_1 == 2:
+		return 5
+
+
 	return 3
--- 2020/20200304 - miojo - python/test_problem.py
+++ 2020/20200304 - miojo - python/test_problem.py
@@ -9,8 +9,11 @@ class TestProblem(unittest.TestCase):
     def test_1_1(self):
         self.assertEqual(miojo(1,1), 3)
 
-    def test_1_2(self):
-        self.assertEqual(miojo(2,5), 5)    
+    def test_2_5(self):
+        self.assertEqual(miojo(2,5), 5) 
+
+    def test_2_7(self):
+        self.assertEqual(miojo(2,7), 7)   
 
 
 if __name__ == "__main__":
