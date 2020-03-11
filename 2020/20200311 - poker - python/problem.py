69ebba813f799e0d0dbd7081e784799efd9245b8
dojorio <dojo-rio@googlegroups.com>
2020-03-11 19:23:44 -0300

    Vinicius Garcia e Otavio Cardoso

--- 2020/20200311 - poker - python/problem.py
+++ 2020/20200311 - poker - python/problem.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def value_of_hand(hand):
+def hand_value(hand):
 	return 'carta-alta'
+
+def sort_hand(hand):
+	for card in enumerate(hand):
+		print card
+	return hand[::-1]
--- 2020/20200311 - poker - python/test_problem.py
+++ 2020/20200311 - poker - python/test_problem.py
@@ -16,9 +16,13 @@ class TestProblem(unittest.TestCase):
         hand = ['2D','3C', '4H', '5S', '6D']
         self.assertEqual(hand_value(hand), 'carta-alta')
 
-    def test_hand_value_2(self):
+    def test_higher_card_1(self):
         hand = ['2D','3C', '4H', '5S', '6D']
-        self.assertEqual(hand_value(hand), 'carta-alta')
+        self.assertEqual(sort_hand(hand), ['6D', '5S', '4H', '3C', '2D'])
+
+    def test_higher_card_2(self):
+        hand = ['2D','3C', 'TH', '5S', '6D']
+        self.assertEqual(sort_hand(hand), ['TH', '6D', '5S', '3C', '2D'])
 
 if __name__ == "__main__":
     unittest.main()
