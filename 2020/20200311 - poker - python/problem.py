5f0706d6ee97042a2874592d7615a92afcfbc0d7
dojorio <dojo-rio@googlegroups.com>
2020-03-11 19:45:47 -0300

    Vinicius Garcia e Otavio Cardoso

--- 2020/20200311 - poker - python/problem.py
+++ 2020/20200311 - poker - python/problem.py
@@ -9,13 +9,26 @@ def sort_hand(hand):
 
     for card in hand:
         splited.append(list(card))
+
     for card in splited:
         value = card[0]
-        if value = 'T':
+        if value == 'T':
             card[0] = 10
         else:
             card[0] = int(value)    
 
+    splited.sort()
 
+    for card in splited:
+        value = card[0]
+        if value == 10:
+            card[0] = 'T'
+        else:
+            card[0] = str(value)
 
-    return hand[::-1]
+    result = []
+
+    for card in splited:
+        result.append(card[0]+card[1])
+
+    return result[::-1]
